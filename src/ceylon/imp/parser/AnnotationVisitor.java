e7f5c8da2af7a1a63aba46a4c64124ba43b1661c
Gavin King <gavin@hibernate.org>
2011-08-05 23:42:56 -0400

    better impl of annotation highlighting

--- src/ceylon/imp/parser/AnnotationVisitor.java
+++ src/ceylon/imp/parser/AnnotationVisitor.java
@@ -3,24 +3,29 @@ package ceylon.imp.parser;
 import java.util.Set;
 
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
-import com.redhat.ceylon.compiler.typechecker.tree.Tree.Identifier;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
 
 class AnnotationVisitor extends Visitor {
     
 	private final Set<Integer> annotatations;
+	private boolean inAnnotation = false;
 	
 	AnnotationVisitor(Set<Integer> annotatations) {
 		this.annotatations = annotatations;
-
 	}
 
 	@Override
     public void visit(Tree.Annotation that) {
+		inAnnotation = true;
     	super.visit(that);
-    	Identifier identifier = ((Tree.BaseMemberExpression) that.getPrimary() ).getIdentifier();
-		annotatations.add(identifier.getAntlrTreeNode().getToken().getTokenIndex());
+		inAnnotation = false;
     }
     
-
+	@Override
+    public void visit(Tree.Identifier that) {
+		if (inAnnotation) {
+			annotatations.add(that.getAntlrTreeNode().getToken().getTokenIndex());
+		}
+	}
+	
 }
