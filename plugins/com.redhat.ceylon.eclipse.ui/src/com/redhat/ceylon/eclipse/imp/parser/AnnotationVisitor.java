3d2f1a32fd9a682e30d0ef4e9d5d0fcdf1735516
Max Rydahl Andersen <max@jboss.org>
2011-08-07 17:35:10 +0200

    more cleanup

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/parser/AnnotationVisitor.java
@@ -0,0 +1,45 @@
+package com.redhat.ceylon.eclipse.imp.parser;
+
+import java.util.Set;
+
+import com.redhat.ceylon.compiler.typechecker.tree.Tree;
+import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
+
+class AnnotationVisitor extends Visitor {
+    
+	private final Set<Integer> annotatations;
+	private boolean inAnnotation = false;
+	
+	AnnotationVisitor(Set<Integer> annotatations) {
+		this.annotatations = annotatations;
+	}
+
+	@Override
+    public void visit(Tree.Annotation that) {
+		inAnnotation = true;
+    	super.visit(that);
+		inAnnotation = false;
+    }
+    
+	@Override
+    public void visit(Tree.CompilerAnnotation that) {
+		inAnnotation = true;
+    	super.visit(that);
+		inAnnotation = false;
+    }
+    
+	@Override
+    public void visit(Tree.Identifier that) {
+		if (inAnnotation) {
+			annotatations.add(that.getAntlrTreeNode().getToken().getTokenIndex());
+		}
+	}
+	
+	@Override
+    public void visit(Tree.StringLiteral that) {
+		if (inAnnotation) {
+			annotatations.add(that.getAntlrTreeNode().getToken().getTokenIndex());
+		}
+	}
+	
+}
