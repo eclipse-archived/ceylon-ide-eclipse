bcce21f5ad1cc4a6160139e545bcbcfacdd4a924
Gavin King <gavin@hibernate.org>
2011-08-27 00:13:40 -0500

    minor

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/refactoring/RenameInputPage.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/refactoring/RenameInputPage.java
@@ -10,6 +10,8 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
+import com.redhat.ceylon.compiler.typechecker.model.Declaration;
+
 public class RenameInputPage extends UserInputWizardPage {
 	public RenameInputPage(String name) {
 		super(name);
@@ -19,15 +21,21 @@ public class RenameInputPage extends UserInputWizardPage {
 	 * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
 	 */
 	public void createControl(Composite parent) {
+		Declaration dec = getRenameRefactoring().getDeclaration();
 		Composite result = new Composite(parent, SWT.NONE);
 		setControl(result);
 		GridLayout layout = new GridLayout();
 		layout.numColumns = 2;
 		result.setLayout(layout);
+		/*Label title = new Label(result, SWT.RIGHT);  
+		title.setText("Rename declaration '" + dec.getQualifiedNameString() + "'");
+		GridData gd = new GridData();
+		gd.horizontalSpan=2;
+		title.setLayoutData(gd);*/
 		Label label = new Label(result, SWT.RIGHT);  
 		label.setText("Rename to: ");
 		final Text text = new Text(result, SWT.SINGLE|SWT.BORDER);
-		text.setText(getRenameRefactoring().getDeclaration().getName());
+		text.setText(dec.getName());
 		text.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
 		text.addModifyListener(new ModifyListener() {
 			@Override
