357480b202bc2f27d3d640457ddeb9ff85df7007
Gavin King <gavin@hibernate.org>
2011-08-29 00:44:39 -0500

    keyboard shortcuts!

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/refactoring/RefactoringContributor.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/refactoring/RefactoringContributor.java
@@ -3,6 +3,7 @@ package com.redhat.ceylon.eclipse.imp.refactoring;
 import org.eclipse.imp.editor.UniversalEditor;
 import org.eclipse.imp.services.IRefactoringContributor;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.swt.SWT;
 
 public class RefactoringContributor implements IRefactoringContributor {
 	
@@ -12,10 +13,26 @@ public class RefactoringContributor implements IRefactoringContributor {
 
 	public static IAction[] getActions(UniversalEditor editor) {
 		return new IAction[] { 
-				new RenameRefactoringAction(editor),
-				new InlineRefactoringAction(editor),
-				new ExtractValueRefactoringAction(editor),
-				new ExtractFunctionRefactoringAction(editor)
+				new RenameRefactoringAction(editor) {
+					{
+						setAccelerator(SWT.CONTROL | SWT.ALT | 'R');
+					}
+				},
+				new InlineRefactoringAction(editor) {
+					{
+						setAccelerator(SWT.CONTROL | SWT.ALT | 'I');
+					}
+				},
+				new ExtractValueRefactoringAction(editor) {
+					{
+						setAccelerator(SWT.CONTROL | SWT.ALT | 'V');
+					}
+				},
+				new ExtractFunctionRefactoringAction(editor) {
+					{
+						setAccelerator(SWT.CONTROL | SWT.ALT | 'F');
+					}
+				}
 			};
 	}
 	
