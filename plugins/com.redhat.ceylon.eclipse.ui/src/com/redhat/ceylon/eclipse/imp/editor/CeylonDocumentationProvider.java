f72be7d752752c813804c0ed64989f4c181dad30
Gavin King <gavin@hibernate.org>
2011-09-13 02:03:19 -0500

    more info in doc hover

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/editor/CeylonDocumentationProvider.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/editor/CeylonDocumentationProvider.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.eclipse.imp.parser.IParseController;
 import org.eclipse.imp.services.IDocumentationProvider;
 
+import com.redhat.ceylon.compiler.typechecker.model.ClassOrInterface;
 import com.redhat.ceylon.compiler.typechecker.model.Declaration;
+import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
 import com.redhat.ceylon.compiler.typechecker.model.TypeDeclaration;
+import com.redhat.ceylon.compiler.typechecker.model.Class;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.redhat.ceylon.eclipse.imp.contentProposer.CeylonContentProposer;
 import com.redhat.ceylon.eclipse.imp.treeModelBuilder.CeylonLabelProvider;
@@ -21,27 +24,50 @@ public class CeylonDocumentationProvider implements IDocumentationProvider {
       return null;
     }
   }
+  
+  private static String sanitize(String s) {
+      return s.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;");
+  }
 
   public static String getDocumentation(Tree.Declaration decl) {
-	  String documentation = "";
-	  if (decl!=null) {
-	  String pkg = decl.getUnit().getPackage().getQualifiedNameString();
-	  if (pkg.isEmpty()) pkg="default package";
-	  documentation += "<p>[" + pkg + "]</p>";
-      documentation += "<p><b>" + CeylonLabelProvider.getLabelFor(decl)
-    		  .replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;") + "</b></p>";
+    String documentation = "";
+    if (decl!=null) {
+	  documentation += "<p>[" + getPackageLabel(decl) + "]</p>";
+      documentation += "<p><b>" + sanitize(CeylonLabelProvider.getLabelFor(decl))
+    		   + "</b></p>";
       
-      if (decl.getDeclarationModel()!=null) {
-          if (decl.getDeclarationModel().isClassOrInterfaceMember()) {
-              TypeDeclaration declaring = (TypeDeclaration) decl.getDeclarationModel().getContainer();
-              documentation += "<ul><li>declared by " + declaring.getName() + " [" + pkg + "]</li>";
-              if (decl.getDeclarationModel().isActual()) {
-                  Declaration refined = decl.getDeclarationModel().getRefinedDeclaration();
+      Declaration model = decl.getDeclarationModel();
+      if (model!=null) {
+          if (model instanceof Class) {
+            ProducedType extended = ((Class) model).getExtendedType();
+            if (extended!=null) {
+              documentation += "<ul><li>extends " + sanitize(extended.getProducedTypeName()) + 
+                      " [" + getPackageLabel(extended.getDeclaration()) + "]</li></ul>";
+            }
+          }
+          if (model instanceof TypeDeclaration) {
+            List<ProducedType> types = ((TypeDeclaration) model).getSatisfiedTypes();
+            if (!types.isEmpty()) {
+                documentation += "<ul>";
+                for (ProducedType satisfied : types) {
+                    documentation += "<li>satisfies "+ sanitize(satisfied.getProducedTypeName()) 
+                            + " [" + getPackageLabel(satisfied.getDeclaration()) + "]</li>";
+                }
+                documentation += "</ul>";
+            }
+          }
+          if (model.isClassOrInterfaceMember()) {
+              TypeDeclaration declaring = (TypeDeclaration) model.getContainer();
+              documentation += "<ul><li>declared by " + declaring.getName() + 
+                      " [" + getPackageLabel(declaring) + "]</li>";
+              if (model.isActual()) {
+                  Declaration refined = model.getRefinedDeclaration();
                   TypeDeclaration supertype = (TypeDeclaration) refined.getContainer();
                   String spkg = supertype.getUnit().getPackage().getQualifiedNameString();
                   if (spkg.isEmpty()) spkg="default package";
                   documentation += "<li>refines '" + CeylonContentProposer.getDescriptionFor(refined) + 
-                          "' declared by " + supertype.getName() + " [" + pkg + "]</li>";
+                          "' declared by " + supertype.getName() + 
+                          " [" + getPackageLabel(refined) + "]</li>";
               }
               documentation +="</ul>";
           }
@@ -73,7 +99,20 @@ public class CeylonDocumentationProvider implements IDocumentationProvider {
           }
         }
       }
-	  }
-      return documentation;
+    }
+    return documentation;
   }
+
+  public static String getPackageLabel(Tree.Declaration decl) {
+    String pkg = decl.getUnit().getPackage().getQualifiedNameString();
+	if (pkg.isEmpty()) pkg="default package";
+    return pkg;
+  }
+  
+  public static String getPackageLabel(Declaration decl) {
+    String pkg = decl.getUnit().getPackage().getQualifiedNameString();
+    if (pkg.isEmpty()) pkg="default package";
+    return pkg;
+  }
+    
 }
