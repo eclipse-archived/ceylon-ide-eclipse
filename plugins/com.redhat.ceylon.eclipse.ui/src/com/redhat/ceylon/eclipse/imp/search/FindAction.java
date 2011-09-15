99069ac10560c0a34ef48a77b9583caf7613078d
Gavin King <gavin@hibernate.org>
2011-09-14 23:12:12 -0500

    handle case where no appropriate node is selected

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/search/FindAction.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/search/FindAction.java
@@ -5,12 +5,15 @@ import static com.redhat.ceylon.eclipse.imp.core.CeylonReferenceResolver.getRefe
 import org.eclipse.core.resources.IProject;
 import org.eclipse.imp.editor.UniversalEditor;
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.search.ui.NewSearchUI;
+import org.eclipse.swt.widgets.Shell;
 
 import com.redhat.ceylon.compiler.typechecker.model.Declaration;
 import com.redhat.ceylon.compiler.typechecker.tree.Node;
 import com.redhat.ceylon.eclipse.imp.parser.CeylonParseController;
 import com.redhat.ceylon.eclipse.imp.parser.CeylonSourcePositionLocator;
+import com.redhat.ceylon.eclipse.ui.CeylonPlugin;
 import com.redhat.ceylon.eclipse.util.Util;
 
 abstract class FindAction extends Action {
@@ -27,9 +30,18 @@ abstract class FindAction extends Action {
         CeylonParseController cpc = (CeylonParseController) editor.getParseController();
         Node node = CeylonSourcePositionLocator.findNode(cpc.getRootNode(), 
                 editor.getSelection().x, editor.getSelection().x+editor.getSelection().y);
-        NewSearchUI.runQueryInBackground(createSearchQuery(
-                getReferencedDeclaration(node), 
-                Util.getProject(editor.getEditorInput())));
+        if (node!=null) {
+            Declaration dec = getReferencedDeclaration(node);
+            if (dec!=null) {
+                NewSearchUI.runQueryInBackground(createSearchQuery(dec, 
+                        Util.getProject(editor.getEditorInput())));
+                return;
+            }
+        }
+        Shell shell= CeylonPlugin.getInstance().getWorkbench()
+                .getActiveWorkbenchWindow().getShell();
+        MessageDialog.openWarning(shell, "Ceylon Find Error", 
+                "No declaration name selected");
     }
 
     public abstract FindSearchQuery createSearchQuery(Declaration declaration, IProject project);
