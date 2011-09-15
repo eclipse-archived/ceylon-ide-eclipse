7f670f3b42f78c3215bbf26ae330de0815ac61c5
Gavin King <gavin@hibernate.org>
2011-09-14 22:53:49 -0500

    minor

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/search/FindAction.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/search/FindAction.java
@@ -1,5 +1,7 @@
 package com.redhat.ceylon.eclipse.imp.search;
 
+import static com.redhat.ceylon.eclipse.imp.core.CeylonReferenceResolver.getReferencedDeclaration;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.imp.editor.UniversalEditor;
 import org.eclipse.jface.action.Action;
@@ -7,7 +9,6 @@ import org.eclipse.search.ui.NewSearchUI;
 
 import com.redhat.ceylon.compiler.typechecker.model.Declaration;
 import com.redhat.ceylon.compiler.typechecker.tree.Node;
-import com.redhat.ceylon.eclipse.imp.core.CeylonReferenceResolver;
 import com.redhat.ceylon.eclipse.imp.parser.CeylonParseController;
 import com.redhat.ceylon.eclipse.imp.parser.CeylonSourcePositionLocator;
 import com.redhat.ceylon.eclipse.util.Util;
@@ -27,7 +28,7 @@ abstract class FindAction extends Action {
         Node node = CeylonSourcePositionLocator.findNode(cpc.getRootNode(), 
                 editor.getSelection().x, editor.getSelection().x+editor.getSelection().y);
         NewSearchUI.runQueryInBackground(createSearchQuery(
-                CeylonReferenceResolver.getReferencedDeclaration(node), 
+                getReferencedDeclaration(node), 
                 Util.getProject(editor.getEditorInput())));
     }
 
