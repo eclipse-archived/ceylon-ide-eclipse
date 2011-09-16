72468e6168de1222479c400c857ba54ae0c24900
Gavin King <gavin@hibernate.org>
2011-09-16 00:12:22 -0500

    dynamic enablement of toplevel menu items caused big performance bug :(

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/util/ActionWrapper.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/util/ActionWrapper.java
@@ -3,38 +3,47 @@ package com.redhat.ceylon.eclipse.util;
 import org.eclipse.imp.editor.UniversalEditor;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IEditorActionDelegate;
 import org.eclipse.ui.IEditorPart;
 
 public abstract class ActionWrapper implements IEditorActionDelegate {
-    private Action delegate;
+    //private Action delegate;
     private UniversalEditor editor;
     
     @Override
     public void run(IAction action) {
-        if (delegate!=null) delegate.run();
+        //if (delegate!=null) delegate.run();
+        if (editor!=null) createAction(editor).run();
     }
     
     @Override
     public void selectionChanged(IAction action, ISelection selection) {
-        if (editor!=null) {
-            delegate = createAction(editor);
-            action.setEnabled(delegate.isEnabled());
+        if (editor==null || !(selection instanceof ITextSelection)) {
+            action.setEnabled(false);
+        }
+        else {
+            //delegate = createAction(editor);
+            //action.setEnabled(delegate.isEnabled());
+            action.setEnabled(true);
         }
     }
     
     @Override
     public void setActiveEditor(IAction action, IEditorPart targetEditor) {
-        if (targetEditor instanceof UniversalEditor) {
-            editor = (UniversalEditor) targetEditor;
-            delegate = createAction(editor);
-            action.setEnabled(delegate.isEnabled());
-        }
-        else {
-            delegate = null;
-            editor = null;
-            action.setEnabled(false);
+        if (targetEditor!=editor) {
+            if (targetEditor instanceof UniversalEditor) {
+                editor = (UniversalEditor) targetEditor;
+                //delegate = createAction(editor);
+                //action.setEnabled(delegate.isEnabled());
+                action.setEnabled(true);
+            }
+            else {
+                //delegate = null;
+                editor = null;
+                action.setEnabled(false);
+            }
         }
     }
 
