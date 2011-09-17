eed3791ae864c24df801a7eb484bd404e74f9941
Gavin King <gavin@hibernate.org>
2011-09-17 00:55:31 -0500

    add range indicator to editor

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/editor/CeylonAnnotationCreator.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/editor/CeylonAnnotationCreator.java
@@ -1,16 +1,24 @@
 package com.redhat.ceylon.eclipse.imp.editor;
 
+import static com.redhat.ceylon.eclipse.imp.parser.CeylonSourcePositionLocator.findScope;
+
 import java.util.Iterator;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.imp.editor.UniversalEditor;
 import org.eclipse.imp.parser.IParseController;
 import org.eclipse.imp.services.base.EditorServiceBase;
+import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.text.Position;
 import org.eclipse.jface.text.source.Annotation;
 import org.eclipse.jface.text.source.IAnnotationModel;
+import org.eclipse.jface.viewers.IPostSelectionProvider;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 
 import com.redhat.ceylon.compiler.typechecker.model.Declaration;
 import com.redhat.ceylon.compiler.typechecker.model.TypeDeclaration;
+import com.redhat.ceylon.compiler.typechecker.tree.Node;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
 import com.redhat.ceylon.eclipse.imp.parser.CeylonParseController;
@@ -72,4 +80,31 @@ public class CeylonAnnotationCreator extends EditorServiceBase {
                         spl.getLength(that)+1));
     }
     
+    @Override
+    public void setEditor(UniversalEditor editor) {
+        super.setEditor(editor);
+        System.out.println("Adding SelectionListener to editor " + editor);
+        ((IPostSelectionProvider) editor.getSelectionProvider())
+            .addPostSelectionChangedListener(new SelectionListener(editor));
+    }
+    
+    static class SelectionListener implements ISelectionChangedListener {
+        UniversalEditor editor;
+        SelectionListener(UniversalEditor editor) {
+            this.editor = editor;
+        }
+        @Override
+        public void selectionChanged(SelectionChangedEvent event) {
+            final CeylonParseController cpc = (CeylonParseController) editor.getParseController();
+            if (cpc.getRootNode()==null) return;
+            Node node = findScope(cpc.getRootNode(), (ITextSelection) event.getSelection());
+            if (node!=null) {
+                editor.setHighlightRange(node.getStartIndex(), 
+                        node.getStopIndex()-node.getStartIndex(), false);
+            }
+            else {
+                editor.resetHighlightRange();
+            }
+        }
+    }
 }
