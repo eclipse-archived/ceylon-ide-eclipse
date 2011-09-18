f337f7a0ffb792f0af11cecba348c2df1b97d172
Gavin King <gavin@hibernate.org>
2011-09-18 17:38:38 -0500

    reasonable impl of clean imports

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/imports/CleanImportsHandler.java
@@ -0,0 +1,96 @@
+package com.redhat.ceylon.eclipse.imp.imports;
+
+import static com.redhat.ceylon.eclipse.imp.editor.Util.getCurrentEditor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.imp.editor.UniversalEditor;
+import org.eclipse.ltk.core.refactoring.PerformChangeOperation;
+import org.eclipse.ltk.core.refactoring.TextFileChange;
+import org.eclipse.text.edits.MultiTextEdit;
+import org.eclipse.text.edits.ReplaceEdit;
+import org.eclipse.ui.IFileEditorInput;
+
+import com.redhat.ceylon.compiler.typechecker.model.Import;
+import com.redhat.ceylon.compiler.typechecker.tree.Tree;
+import com.redhat.ceylon.compiler.typechecker.tree.Tree.ImportList;
+
+public class CleanImportsHandler extends AbstractHandler {
+    
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        UniversalEditor editor = (UniversalEditor) getCurrentEditor();
+        Tree.CompilationUnit cu = (Tree.CompilationUnit) editor
+                .getParseController().getCurrentAst();
+        DetectUnusedImportsVisitor duiv = new DetectUnusedImportsVisitor();
+        cu.visit(duiv);
+        TextFileChange tfc = new TextFileChange("Clean Imports", 
+                ((IFileEditorInput) editor.getEditorInput()).getFile());
+        tfc.setEdit(new MultiTextEdit());
+        List<Tree.Import> importList = new ArrayList<Tree.Import>();
+        ImportList til = cu.getImportList();
+        importList.addAll(til.getImports());
+        Collections.sort(importList, new Comparator<Tree.Import>() {
+            @Override
+            public int compare(Tree.Import i1, Tree.Import i2) {
+                return packageName(i1).compareTo(packageName(i2));
+            }
+        });
+        StringBuilder builder = new StringBuilder();
+        for (Tree.Import ti: importList) {
+            List<Import> list = new ArrayList<Import>();
+            for (Import i: ti.getImportList().getImports()) {
+                if (!duiv.getResult().contains(i.getDeclaration())) {
+                    list.add(i);
+                }
+            }
+            if (!list.isEmpty()) {
+                builder.append("import ")
+                        .append(packageName(ti))
+                        .append(" { ");
+                for (Import i: list) {
+                    if ( !i.getAlias().equals(i.getDeclaration().getName()) ) {
+                        builder.append(i.getAlias()).append("=");
+                    }
+                    builder.append(i.getDeclaration().getName())
+                            .append(", ");
+                }
+                /*if (ti.getImportMemberOrTypeList().getImportWildcard()!=null) {
+                    builder.append(" ... ");
+                }*/
+                builder.setLength(builder.length()-2);
+                builder.append(" }\n");
+            }
+        }
+        tfc.addEdit(new ReplaceEdit(til.getStartIndex(), 
+                til.getStopIndex()-til.getStartIndex()+1, 
+                builder.toString()));
+        /*for (ImportMemberOrType imt: duiv.getResult()) {
+            tfc.addEdit( new DeleteEdit(imt.getStartIndex(), 
+                    imt.getStopIndex()-imt.getStartIndex()+1) );
+        }*/
+        tfc.initializeValidationData(null);
+        try {
+            ResourcesPlugin.getWorkspace().run(new PerformChangeOperation(tfc), 
+                    new NullProgressMonitor());
+        }
+        catch (CoreException ce) {
+            throw new ExecutionException("Error cleaning imports", ce);
+        }
+        return null;
+    }
+    
+    private static String packageName(Tree.Import i) {
+        return i.getImportList().getImportedPackage()
+                .getQualifiedNameString();
+    }
+}
---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/imports/DetectUnusedImportsVisitor.java
@@ -0,0 +1,56 @@
+package com.redhat.ceylon.eclipse.imp.imports;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.redhat.ceylon.compiler.typechecker.model.Declaration;
+import com.redhat.ceylon.compiler.typechecker.model.Import;
+import com.redhat.ceylon.compiler.typechecker.tree.Tree;
+import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
+
+public class DetectUnusedImportsVisitor extends Visitor {
+    //private List<ImportMemberOrType> result = new ArrayList<ImportMemberOrType>();
+    private List<Declaration> result = new ArrayList<Declaration>();
+    
+    /*@Override
+    public void visit(ImportMemberOrType that) {
+        result.add(that.getDeclarationModel());
+    }*/
+    
+    @Override
+    public void visit(Tree.Import that) {
+        super.visit(that);
+        for (Import i: that.getImportList().getImports()) {
+            result.add(i.getDeclaration());
+        }
+    }
+    
+    
+    @Override
+    public void visit(Tree.BaseMemberOrTypeExpression that) {
+        super.visit(that);
+        for (Iterator<Declaration> it = result.iterator();
+                it.hasNext();) {
+            if ( it.next().equals(that.getDeclaration()) ) {
+                it.remove();
+            }
+        }
+    }
+    
+    @Override
+    public void visit(Tree.BaseType that) {
+        super.visit(that);
+        for (Iterator<Declaration> it = result.iterator();
+                it.hasNext();) {
+            if ( it.next().equals(that.getDeclarationModel()) ) {
+                it.remove();
+            }
+        }
+    }    
+    
+    public List<Declaration> getResult() {
+        return result;
+    }
+    
+}
