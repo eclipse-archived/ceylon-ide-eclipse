6023aed954f7a61daba431489851d06955cdf400
Gavin King <gavin@hibernate.org>
2011-09-19 13:59:51 -0500

    ws

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/editor/CeylonOccurrenceMarker.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/editor/CeylonOccurrenceMarker.java
@@ -18,40 +18,39 @@ import com.redhat.ceylon.eclipse.util.FindReferenceVisitor;
 
 public class CeylonOccurrenceMarker implements ILanguageService, IOccurrenceMarker {
     
-	private List<Object> fOccurrences = Collections.emptyList();
-
-	public String getKindName() {
-		return "Ceylon Occurence Marker";
-	}
-
-	public List<Object> getOccurrencesOf(IParseController parseController,
-			Object node) {
-	    
-		if (node instanceof Node) {
-
-    		// Check whether we even have an AST in which to find occurrences
-    		Tree.CompilationUnit root = (Tree.CompilationUnit) parseController.getCurrentAst();
-    		if (root == null) {
-    			return Collections.emptyList();
-    		}
+    private List<Object> fOccurrences = Collections.emptyList();
     
-    		fOccurrences = new ArrayList<Object>();
-    		Declaration declaration = getReferencedDeclaration((Node) node);
-    		if (declaration!=null) {
-    			FindReferenceVisitor frv = new FindReferenceVisitor(declaration);
-    			root.visit(frv);
-    			fOccurrences.addAll(frv.getNodes());
-    			FindDeclarationVisitor fdv = new FindDeclarationVisitor(declaration);
-    			root.visit(fdv);
-    			fOccurrences.add(fdv.getDeclarationNode());
-    		}
-    		return fOccurrences;
-    		
-		}
-		else {
-	          return Collections.emptyList();
-		}
-
-	}
-
+    public String getKindName() {
+        return "Ceylon Occurence Marker";
+    }
+    
+    public List<Object> getOccurrencesOf(IParseController parseController, Object node) {
+        
+        if (node instanceof Node) {
+            
+            // Check whether we even have an AST in which to find occurrences
+            Tree.CompilationUnit root = (Tree.CompilationUnit) parseController.getCurrentAst();
+            if (root == null) {
+                return Collections.emptyList();
+            }
+            
+            fOccurrences = new ArrayList<Object>();
+            Declaration declaration = getReferencedDeclaration((Node) node);
+            if (declaration != null) {
+                FindReferenceVisitor frv = new FindReferenceVisitor(declaration);
+                root.visit(frv);
+                fOccurrences.addAll(frv.getNodes());
+                FindDeclarationVisitor fdv = new FindDeclarationVisitor(declaration);
+                root.visit(fdv);
+                fOccurrences.add(fdv.getDeclarationNode());
+            }
+            return fOccurrences;
+            
+        }
+        else {
+            return Collections.emptyList();
+        }
+        
+    }
+    
 }
