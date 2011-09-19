77c41210bb3be439c81a378c4995fd14f5ae2197
Gavin King <gavin@hibernate.org>
2011-09-18 23:43:36 -0500

    Better approach to images for refinement annotations

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/editor/RefinementAnnotationImageProvider.java
@@ -0,0 +1,33 @@
+package com.redhat.ceylon.eclipse.imp.editor;
+
+import static com.redhat.ceylon.eclipse.ui.ICeylonResources.CEYLON_DEFAULT_REFINEMENT;
+import static com.redhat.ceylon.eclipse.ui.ICeylonResources.CEYLON_FORMAL_REFINEMENT;
+import static com.redhat.ceylon.eclipse.imp.outline.CeylonLabelProvider.imageRegistry;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.text.source.Annotation;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.texteditor.IAnnotationImageProvider;
+
+public class RefinementAnnotationImageProvider implements IAnnotationImageProvider {
+    
+    private static Image DEFAULT = imageRegistry.get(CEYLON_DEFAULT_REFINEMENT);
+    private static Image FORMAL = imageRegistry.get(CEYLON_FORMAL_REFINEMENT);
+    
+    @Override
+    public Image getManagedImage(Annotation annotation) {
+        return ((RefinementAnnotation) annotation).getDeclaration().isFormal() ? 
+                FORMAL : DEFAULT;
+    }
+    
+    @Override
+    public String getImageDescriptorId(Annotation annotation) {
+        return null;
+    }
+    
+    @Override
+    public ImageDescriptor getImageDescriptor(String imageDescritporId) {
+        return null;
+    }
+    
+}
