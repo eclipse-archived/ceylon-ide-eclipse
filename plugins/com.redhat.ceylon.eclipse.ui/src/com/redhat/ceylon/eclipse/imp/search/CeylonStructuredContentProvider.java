6bdb88664b75905bb8887bee1e80df52e092b8d1
Gavin King <gavin@hibernate.org>
2011-09-19 13:04:43 -0500

    allow search page to be cleared

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/search/CeylonStructuredContentProvider.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/search/CeylonStructuredContentProvider.java
@@ -3,5 +3,6 @@ package com.redhat.ceylon.eclipse.imp.search;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 
 public interface CeylonStructuredContentProvider extends IStructuredContentProvider {
-    abstract public void elementsChanged(Object[] updatedElements);
+    public void elementsChanged(Object[] updatedElements);
+    public void clear();
 }
