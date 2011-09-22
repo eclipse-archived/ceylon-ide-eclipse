09ef1f62cf4dabb11840a272dbf620b326f214e2
Gavin King <gavin@hibernate.org>
2011-09-22 17:17:55 -0500

    reorg menus

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/editor/CeylonEditor.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/editor/CeylonEditor.java
@@ -2,8 +2,10 @@ package com.redhat.ceylon.eclipse.imp.editor;
 
 import java.lang.reflect.Field;
 
+import org.eclipse.imp.editor.GenerateActionGroup;
 import org.eclipse.imp.editor.UniversalEditor;
 import org.eclipse.imp.ui.DefaultPartListener;
+import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.swt.widgets.Composite;
 
 public class CeylonEditor extends UniversalEditor {
@@ -13,12 +15,24 @@ public class CeylonEditor extends UniversalEditor {
         getSite().getPage().hideActionSet(IMP_CODING_ACTION_SET);
         //getSite().getPage().hideActionSet(IMP_OPEN_ACTION_SET);
         try {
-            Field field = UniversalEditor.class.getDeclaredField("fRefreshContributions");
-            field.setAccessible(true);
-            getSite().getPage().removePartListener((DefaultPartListener) field.get(this));
+            Field frc = UniversalEditor.class.getDeclaredField("fRefreshContributions");
+            frc.setAccessible(true);
+            Field fgag = UniversalEditor.class.getDeclaredField("fGenerateActionGroup");
+            fgag.setAccessible(true);
+            getSite().getPage().removePartListener((DefaultPartListener) frc.get(this));
+            fgag.set(this, new CeylonGenerateActionGroup(this));
         }
         catch (Exception e) {
             e.printStackTrace();
         }
     }
+    
+    class CeylonGenerateActionGroup extends GenerateActionGroup {
+        public CeylonGenerateActionGroup(UniversalEditor editor) {
+            super(editor, "");
+        }
+        @Override
+        public void fillContextMenu(IMenuManager menu) {}
+    }
+
 }
