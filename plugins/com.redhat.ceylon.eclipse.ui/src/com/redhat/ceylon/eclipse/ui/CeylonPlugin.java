0470459a6c69f205b95a45a5be1a82b6cf972b31
Gavin King <gavin@hibernate.org>
2011-09-22 02:59:15 -0500

    work around some issues affecting IMP's startup with open editors

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/ui/CeylonPlugin.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/ui/CeylonPlugin.java
@@ -22,6 +22,7 @@ import static com.redhat.ceylon.eclipse.ui.ICeylonResources.CEYLON_SEARCH_RESULT
 import static org.eclipse.core.resources.IncrementalProjectBuilder.FULL_BUILD;
 
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URL;
 
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -30,14 +31,30 @@ import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.imp.editor.UniversalEditor;
+import org.eclipse.imp.preferences.PreferenceConstants;
 import org.eclipse.imp.runtime.PluginBase;
+import org.eclipse.imp.runtime.RuntimePlugin;
 import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.PreferenceConverter;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.WorkbenchPart;
+import org.eclipse.ui.texteditor.AbstractDecoratedTextEditorPreferenceConstants;
 import org.osgi.framework.BundleContext;
+
+import com.redhat.ceylon.eclipse.imp.outline.CeylonLabelProvider;
 
 public class CeylonPlugin extends PluginBase {
 
@@ -61,7 +78,17 @@ public class CeylonPlugin extends PluginBase {
 	@Override
 	public void start(BundleContext context) throws Exception {
 	    super.start(context);
-        runInitialBuild();
+        final IWorkbench workbench = PlatformUI.getWorkbench();
+        workbench.getDisplay().asyncExec(new Runnable() {
+           public void run() {
+             IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
+             if (window != null) {
+                 setIconForOpenWindows(window);
+                 setDefaults(RuntimePlugin.getInstance().getPreferenceStore());
+                 runInitialBuild();
+             }
+           }
+        });
 	}
 
 	@Override
@@ -86,38 +113,7 @@ public class CeylonPlugin extends PluginBase {
         	return null;
         }
 	}
-	
-    private void runInitialBuild() {
-        final IWorkbench workbench = PlatformUI.getWorkbench();
-        workbench.getDisplay().asyncExec(new Runnable() {
-           public void run() {
-             IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
-             if (window != null) {
-                 try {
-                    PlatformUI.getWorkbench().getProgressService()
-                     .busyCursorWhile(new IRunnableWithProgress() {
-                        @Override
-                        public void run(IProgressMonitor monitor) 
-                                throws InvocationTargetException, InterruptedException {
-                           try {
-                               ResourcesPlugin.getWorkspace().build(FULL_BUILD, monitor);
-                           }
-                           catch (CoreException ce) {
-                               throw new InvocationTargetException(ce);
-                           }
-                        }
-                     });
-                }
-                catch (InvocationTargetException e) {
-                    e.printStackTrace();
-                }
-                catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-             }
-           }
-        });
-    }
+	    
 	@Override
 	protected void initializeImageRegistry(ImageRegistry reg) {
 		reg.put(CEYLON_PACKAGE, image("package_obj.gif"));
@@ -140,5 +136,89 @@ public class CeylonPlugin extends PluginBase {
         reg.put(CEYLON_SEARCH_RESULTS, image("search_ref_obj.gif"));
         reg.put(CEYLON_CORRECTION, image("correction_change.gif"));
 	}
+	
+	/**
+	 * Kick off an initial build at startup time in order
+	 * to build the model.
+	 */
+    private void runInitialBuild() {
+        try {
+            PlatformUI.getWorkbench().getProgressService()
+             .busyCursorWhile(new IRunnableWithProgress() {
+                @Override
+                public void run(IProgressMonitor monitor) 
+                        throws InvocationTargetException, InterruptedException {
+                   try {
+                       ResourcesPlugin.getWorkspace().build(FULL_BUILD, monitor);
+                   }
+                   catch (CoreException ce) {
+                       throw new InvocationTargetException(ce);
+                   }
+                }
+             });
+        }
+        catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+	/**
+	 * Awful little hack to get a nonugly icon on already-open 
+	 * editors at startup time, since IMP doesn't init the
+	 * editor to the language-specific icon until the editor
+	 * becomes active.
+	 */
+    private void setIconForOpenWindows(IWorkbenchWindow window) {
+        for (IWorkbenchPage page: window.getPages()) {
+             for (IEditorReference ref: page.findEditors(null, UniversalEditor.EDITOR_ID, IWorkbenchPage.MATCH_ID)) {
+                 try {
+                    Method method = WorkbenchPart.class.getDeclaredMethod("setTitleImage", Image.class);
+                    method.setAccessible(true);
+                    method.invoke(ref.getEditor(true), CeylonLabelProvider.FILE_IMAGE);
+                 }
+                 catch (Exception e) {
+                     e.printStackTrace();
+                 }
+             }
+         }
+    }
+    
+    private static final String FLAG = "preferencesInitialized";
+
+    /**
+     * Really awful hack to initialize some preferences the 
+     * first time we run the IDE, since IMP sometimes fails
+     * to set the defaults in time for already-open editors
+     * to detect them.
+     */
+    private static void setDefaults(IPreferenceStore store) {
+        if (!store.getBoolean(FLAG)) {
+            System.out.println("INITIALIZING PREFERENCES");
+            store.setValue(FLAG, true);
+            int size = Platform.getOS().equals("macosx") ? 11 : 10;
+            PreferenceConverter.setValue(store, PreferenceConstants.P_SOURCE_FONT, 
+                    new FontData[] { new FontData("Monaco", size, SWT.NORMAL),
+                                     new FontData("Courier New", size, SWT.NORMAL), 
+                                     new FontData("Monospace", size, SWT.NORMAL)});
+    
+            store.setValue(PreferenceConstants.P_EMIT_MESSAGES, false);
+            store.setValue(PreferenceConstants.P_EMIT_BUILDER_DIAGNOSTICS, false);
+            store.setValue(PreferenceConstants.P_TAB_WIDTH, 4);
+            store.setValue(PreferenceConstants.P_SPACES_FOR_TABS, true);
+            store.setValue(PreferenceConstants.P_DUMP_TOKENS, false);
+            store.setValue(PreferenceConstants.EDITOR_MATCHING_BRACKETS, true);
+            store.setValue(PreferenceConstants.EDITOR_CORRECTION_INDICATION, true);
+            store.setValue(PreferenceConstants.EDITOR_CLOSE_FENCES, true);
+            
+            store.setValue(AbstractDecoratedTextEditorPreferenceConstants.EDITOR_TAB_WIDTH, 4);
+            store.setValue(AbstractDecoratedTextEditorPreferenceConstants.EDITOR_SPACES_FOR_TABS, true);
+        
+            PreferenceConverter.setValue(store, PreferenceConstants.EDITOR_MATCHING_BRACKETS_COLOR,
+                    new RGB(0,120,255));
+        }
+    }
 
 }
