78a83a3bf7e0cff21457775ca5bf5720e863a8a9
Gavin King <gavin@hibernate.org>
2011-09-21 20:04:48 -0500

    take advantage of Unit replacement to give the editor its own working copy

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/vfs/ResourceVirtualFile.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/vfs/ResourceVirtualFile.java
@@ -61,4 +61,21 @@ public abstract class ResourceVirtualFile implements VirtualFile {
         return resource;
     }
 
+    @Override
+    public int hashCode() {
+        return getPath().hashCode();
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof ResourceVirtualFile) {
+            return ((ResourceVirtualFile) obj).getPath().equals(getPath());
+        }
+        else if (obj instanceof SourceCodeVirtualFile) {
+            return ((SourceCodeVirtualFile) obj).getPath().equals(getPath());
+        }
+        else {
+            return super.equals(obj);
+        }
+    }
 }
