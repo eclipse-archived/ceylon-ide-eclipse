feff32f05cbb05348a9f20d0c2f96c5ebcaf3006
Gavin King <gavin@hibernate.org>
2011-09-24 03:52:04 -0500

    rework code for doing contextual proposals

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/parser/FindNodeVisitor.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/parser/FindNodeVisitor.java
@@ -1,6 +1,13 @@
 package com.redhat.ceylon.eclipse.imp.parser;
 
-import static com.redhat.ceylon.eclipse.imp.parser.CeylonSourcePositionLocator.OccurrenceLocation;
+import static com.redhat.ceylon.eclipse.imp.parser.CeylonSourcePositionLocator.OccurrenceLocation.EXPRESSION;
+import static com.redhat.ceylon.eclipse.imp.parser.CeylonSourcePositionLocator.OccurrenceLocation.EXTENDS;
+import static com.redhat.ceylon.eclipse.imp.parser.CeylonSourcePositionLocator.OccurrenceLocation.IMPORT;
+import static com.redhat.ceylon.eclipse.imp.parser.CeylonSourcePositionLocator.OccurrenceLocation.PARAMETER_LIST;
+import static com.redhat.ceylon.eclipse.imp.parser.CeylonSourcePositionLocator.OccurrenceLocation.SATISFIES;
+import static com.redhat.ceylon.eclipse.imp.parser.CeylonSourcePositionLocator.OccurrenceLocation.TYPE_ARGUMENT_LIST;
+import static com.redhat.ceylon.eclipse.imp.parser.CeylonSourcePositionLocator.OccurrenceLocation.TYPE_PARAMETER_LIST;
+import static com.redhat.ceylon.eclipse.imp.parser.CeylonSourcePositionLocator.OccurrenceLocation.UPPER_BOUND;
 
 import com.redhat.ceylon.compiler.typechecker.tree.NaturalVisitor;
 import com.redhat.ceylon.compiler.typechecker.tree.Node;
@@ -8,6 +15,7 @@ import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.QualifiedMemberOrTypeExpression;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.Term;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
+import com.redhat.ceylon.eclipse.imp.parser.CeylonSourcePositionLocator.OccurrenceLocation;
 
 class FindNodeVisitor extends Visitor
         implements NaturalVisitor {
@@ -20,7 +28,12 @@ class FindNodeVisitor extends Visitor
     private Node node;
     private int startOffset;
     private int endOffset;
+    
+    //TODO: write a specialized visitor that handles OccurrenceLocation
+    //      determination, just for CeylonContentProposer. Other clients
+    //      don't care about this.
     private OccurrenceLocation occurrence;
+    private boolean inTypeConstraint = false;
     
     public Node getNode() {
         return node;
@@ -30,26 +43,40 @@ class FindNodeVisitor extends Visitor
         return occurrence;
     }
     
+    public void visit(Tree.TypeConstraint that) {
+        inTypeConstraint=true;
+        super.visit(that);
+        inTypeConstraint=false;
+    }
+    
+    public void visit(Tree.ImportMemberOrTypeList that) {
+        if (inBounds(that)) {
+            occurrence = IMPORT;
+        }
+        super.visit(that);
+    }
+    
     public void visit(Tree.ExtendedType that) {
         if (inBounds(that)) {
-            occurrence = OccurrenceLocation.EXTENDS;
+            occurrence = EXTENDS;
         }
         super.visit(that);
     }
     
     public void visit(Tree.SatisfiedTypes that) {
         if (inBounds(that)) {
-            occurrence = OccurrenceLocation.SATISFIES;
+            occurrence = inTypeConstraint? 
+                    UPPER_BOUND : SATISFIES;
         }
         super.visit(that);
     }
     
     @Override
     public void visitAny(Node that) {
-        super.visitAny(that);
-        if (node==null && inBounds(that)) {
+        if (node!=that && inBounds(that)) {
             node=that;
         }
+        super.visitAny(that);
     }
     
     @Override
@@ -64,28 +91,74 @@ class FindNodeVisitor extends Visitor
     
     @Override
     public void visit(Tree.BinaryOperatorExpression that) {
-        super.visit(that);
         Term right = that.getRightTerm();
         if (right==null) {
             right = that;
         }
-        if (node==null && inBounds(that.getLeftTerm(), right)) {
+        Term left = that.getLeftTerm();
+        if (left==null) {
+            left = that;
+        }
+        if (inBounds(left, right)) {
+            occurrence = EXPRESSION;
             node=that;
         }
+        super.visit(that);
     }
     
     @Override
     public void visit(Tree.UnaryOperatorExpression that) {
-        super.visit(that);
-        if (node==null && (inBounds(that, that.getTerm())
-                ||inBounds(that.getTerm(),that))) {
+        Term term = that.getTerm();
+        if (term==null) {
+            term = that;
+        }
+        if (inBounds(that, term) || inBounds(term, that)) {
+            occurrence = EXPRESSION;
             node=that;
         }
+        super.visit(that);
+    }
+    
+    @Override
+    public void visit(Tree.ParameterList that) {
+        if (inBounds(that)) {
+            occurrence = PARAMETER_LIST;
+            node=that;
+        }
+        super.visit(that);
+    }
+    
+    @Override
+    public void visit(Tree.TypeParameterList that) {
+        if (inBounds(that)) {
+            occurrence = TYPE_PARAMETER_LIST;
+            node=that;
+        }
+        super.visit(that);
+    }
+    
+    @Override
+    public void visit(Tree.ArgumentList that) {
+        if (inBounds(that)) {
+            occurrence = EXPRESSION;
+            node=that;
+        }
+        super.visit(that);
+    }
+    
+    @Override
+    public void visit(Tree.TypeArgumentList that) {
+        if (inBounds(that)) {
+            occurrence = TYPE_ARGUMENT_LIST;
+            node=that;
+        }
+        super.visit(that);
     }
     
     @Override
     public void visit(QualifiedMemberOrTypeExpression that) {
         if (inBounds(that.getMemberOperator(), that.getIdentifier())) {
+            occurrence = EXPRESSION;
             node=that;
         }
         else {
@@ -97,6 +170,8 @@ class FindNodeVisitor extends Visitor
     public void visit(Tree.StaticMemberOrTypeExpression that) {
         if (inBounds(that.getIdentifier())) {
             node = that;
+            //Note: we can't be sure that this is "really"
+            //      an EXPRESSION!
         }
         else {
             super.visit(that);
