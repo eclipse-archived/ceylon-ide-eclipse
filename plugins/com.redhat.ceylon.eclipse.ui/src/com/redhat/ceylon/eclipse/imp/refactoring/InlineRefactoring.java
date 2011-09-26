f7b65f7f49347ba358c228c9411af28f45628e07
Gavin King <gavin@hibernate.org>
2011-09-26 08:03:03 -0500

    refactor

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/refactoring/InlineRefactoring.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/refactoring/InlineRefactoring.java
@@ -82,208 +82,238 @@ public class InlineRefactoring extends AbstractRefactoring {
 
 	public Change createChange(IProgressMonitor pm) throws CoreException,
 			OperationCanceledException {
-        CompositeChange cc = new CompositeChange("Inline");
+
         Tree.Declaration declarationNode=null;
         PhasedUnit declarationUnit=null;
-        Tree.Term t=null;
-        
+        Tree.Term term = null;
         if (declaration!=null) {
             for (final PhasedUnit pu: CeylonBuilder.getUnits(project)) {
                 if (pu.getUnit().equals(declaration.getUnit())) {
+                    declarationUnit = pu;
         			FindDeclarationVisitor fdv = new FindDeclarationVisitor(declaration);
         			pu.getCompilationUnit().visit(fdv);
         			declarationNode = fdv.getDeclarationNode();
-        			if (declarationNode!=null) {
-                        declarationUnit = pu;
-            			if (declarationNode instanceof Tree.AttributeDeclaration) {
-            				Tree.AttributeDeclaration att = (Tree.AttributeDeclaration) declarationNode;
-            				t = att.getSpecifierOrInitializerExpression().getExpression().getTerm();
-            			}
-            			else if (declarationNode instanceof Tree.MethodDefinition) {
-            				Tree.MethodDefinition meth = (Tree.MethodDefinition) declarationNode;
-            				if (meth.getBlock().getStatements().size()!=1) {
-            					throw new RuntimeException("method has multiple statements");
-            				}
-            				if (meth.getType() instanceof Tree.VoidModifier) {
-            					Tree.ExpressionStatement e = (Tree.ExpressionStatement) meth.getBlock()
-            							.getStatements().get(0);
-            					t = e.getExpression().getTerm();
-            					
-            				}
-            				else {
-            					Tree.Return r = (Tree.Return) meth.getBlock().getStatements().get(0);
-            					t = r.getExpression().getTerm();
-            				}
-            			}
-            			else if (declarationNode instanceof Tree.AttributeGetterDefinition) {
-            				Tree.AttributeGetterDefinition att = (Tree.AttributeGetterDefinition) declarationNode;
-            				if (att.getBlock().getStatements().size()!=1) {
-            					throw new RuntimeException("getter has multiple statements");
-            				}
-            				Tree.Return r = (Tree.Return) att.getBlock().getStatements().get(0);
-            				t = r.getExpression().getTerm();
-            			}
-            			else {
-            				throw new RuntimeException("not a value or function");
-            			}
-                        break;
-        			}
+        			term = getInlinedTerm(declarationNode);
+                    break;
                 }
     		}
         }
-        final Tree.Term term = t;
 		
+        CompositeChange cc = new CompositeChange("Inline");
         if (declarationNode!=null) {
-            for (final PhasedUnit pu: CeylonBuilder.getUnits(project)) {
-                final TextFileChange tfc = new TextFileChange("Inline", CeylonBuilder.getFile(pu));
+            for (PhasedUnit pu: CeylonBuilder.getUnits(project)) {
+                TextFileChange tfc = new TextFileChange("Inline", CeylonBuilder.getFile(pu));
                 tfc.setEdit(new MultiTextEdit());
-    			final String template = toString(term, declarationUnit.getTokenStream());
-    			final int templateStart = term.getStartIndex();
-    			if (declarationNode instanceof Tree.AnyAttribute) {
-    				new Visitor() {
-    					@Override
-    					public void visit(Tree.BaseMemberExpression that) {
-    						super.visit(that);
-    						if (that.getDeclaration().equals(declaration)) {
-    							tfc.addEdit(new ReplaceEdit(that.getStartIndex(), 
-    									that.getStopIndex()-that.getStartIndex()+1, 
-    									template));	
-    						}
-    					}
-    				}.visit(pu.getCompilationUnit());
-    			}
-    			else {
-    				new Visitor() {
-    					@Override
-    					public void visit(final Tree.InvocationExpression that) {
-    						super.visit(that);
-    						if (that.getPrimary().getDeclaration().equals(declaration)) {
-    							//TODO: breaks for invocations like f(f(x, y),z)
-    							final StringBuilder result = new StringBuilder();
-    							class InterpolateVisitor extends Visitor {
-    								int start = 0;
-    								@Override
-    								public void visit(Tree.BaseMemberExpression it) {
-    									super.visit(it);
-    									if (it.getDeclaration() instanceof Parameter) {
-    										Parameter param = (Parameter) it.getDeclaration();
-    										if ( param.getDeclaration().equals(declaration) ) {
-    											result.append(template.substring(start,it.getStartIndex()-templateStart));
-    											start = it.getStopIndex()-templateStart+1;
-    											boolean sequenced = param.isSequenced();
-    											if (that.getPositionalArgumentList()!=null) {
-    												interpolatePositionalArguments(result, that, it, sequenced);
-    											}
-    											if (that.getNamedArgumentList()!=null) {
-    												interpolateNamedArguments(result, that, it, sequenced);
-    											}
-    										}
-    									}
-    								}
-    								private void interpolatePositionalArguments(StringBuilder result, 
-    										Tree.InvocationExpression that, Tree.BaseMemberExpression it,
-    										boolean sequenced) {
-    									boolean first = true;
-    									boolean found = false;
-    									if (sequenced) {
-    										result.append("{");
-    									}
-    									for (Tree.PositionalArgument arg: that.getPositionalArgumentList()
-    											.getPositionalArguments()) {
-    										if (it.getDeclaration().equals(arg.getParameter())) {
-    											if (arg.getParameter().isSequenced() && 
-    													that.getPositionalArgumentList().getEllipsis()==null) {
-    												if (first) result.append(" ");
-    												if (!first) result.append(", ");
-    												first = false;
-    											}
-    											result.append(AbstractRefactoring.toString(arg.getExpression().getTerm(), 
-    											        pu.getTokenStream()));
-    											found = true;
-    										}
-    									}
-    									if (sequenced) {
-    										if (!first) result.append(" ");
-    										result.append("}");
-    									}
-    									if (!found) {} //TODO: use default value!
-    								}
-    								private void interpolateNamedArguments(StringBuilder result,
-    										Tree.InvocationExpression that, Tree.BaseMemberExpression it,
-    										boolean sequenced) {
-    									boolean found = false;
-    									for (Tree.NamedArgument arg: that.getNamedArgumentList().getNamedArguments()) {
-    										if (it.getDeclaration().equals(arg.getParameter())) {
-    											Term argTerm = ((Tree.SpecifiedArgument) arg).getSpecifierExpression()
-                                                				.getExpression().getTerm();
-                                                result//.append(template.substring(start,it.getStartIndex()-templateStart))
-    												.append(AbstractRefactoring.toString( argTerm, pu.getTokenStream()) );
-    											//start = it.getStopIndex()-templateStart+1;
-    											found=true;
-    										}
-    									}
-    									SequencedArgument seqArg = that.getNamedArgumentList().getSequencedArgument();
-    									if (seqArg!=null && it.getDeclaration().equals(seqArg.getParameter())) {
-    										result//.append(template.substring(start,it.getStartIndex()-templateStart))
-    										    .append("{");
-    										//start = it.getStopIndex()-templateStart+1;;
-    										boolean first=true;
-    										for (Tree.Expression e: seqArg.getExpressionList().getExpressions()) {
-    											if (first) result.append(" ");
-    											if (!first) result.append(", ");
-    											first=false;
-    											result.append(AbstractRefactoring.toString(e.getTerm(), pu.getTokenStream()));
-    										}
-    										if (!first) result.append(" ");
-    										result.append("}");
-    										found=true;
-    									}
-    									if (!found) {
-    										if (sequenced) {
-    											result.append("{}");
-    										}
-    										else {} //TODO: use default value!
-    									}
-    								}
-    								void finish() {
-    									result.append(template.substring(start, template.length()));
-    								}
-    							}
-    							InterpolateVisitor iv = new InterpolateVisitor();
-    							iv.visit(term);
-    							iv.finish();
-    							tfc.addEdit(new ReplaceEdit(that.getStartIndex(), 
-    									that.getStopIndex()-that.getStartIndex()+1, 
-    									result.toString()));
-    						}
-    					}
-    				}.visit(pu.getCompilationUnit());
-    			}
-                if (delete && pu==declarationUnit) {
-                    CommonToken from = (CommonToken) declarationNode.getToken();
-                    Tree.AnnotationList anns = declarationNode.getAnnotationList();
-                    if (!anns.getAnnotations().isEmpty()) {
-                        from = (CommonToken) anns.getAnnotations().get(0).getToken();
-                    }
-                    int prevIndex = from.getTokenIndex()-1;
-                    if (prevIndex>=0) {
-                        CommonToken tok = (CommonToken) pu.getTokenStream()
-                                .get(prevIndex);
-                        if (tok.getChannel()==Token.HIDDEN_CHANNEL) {
-                            from=tok;
-                        }
-                    }
-                    tfc.addEdit(new DeleteEdit(from.getStartIndex(), 
-                            declarationNode.getStopIndex()-from.getStartIndex()+1));
-                }
+                inlineReferences(declarationNode, declarationUnit, term, pu, tfc);
+                deleteDeclaration(declarationNode, declarationUnit, pu, tfc);
     			if (tfc.getEdit().hasChildren()) {
     			    cc.add(tfc);
     			}
     		}
         }
         return cc;
+        
 	}
 
+    private void deleteDeclaration(Tree.Declaration declarationNode, PhasedUnit declarationUnit,
+            PhasedUnit pu, TextFileChange tfc) {
+        if (delete && pu==declarationUnit) {
+            CommonToken from = (CommonToken) declarationNode.getToken();
+            Tree.AnnotationList anns = declarationNode.getAnnotationList();
+            if (!anns.getAnnotations().isEmpty()) {
+                from = (CommonToken) anns.getAnnotations().get(0).getToken();
+            }
+            int prevIndex = from.getTokenIndex()-1;
+            if (prevIndex>=0) {
+                CommonToken tok = (CommonToken) pu.getTokenStream()
+                        .get(prevIndex);
+                if (tok.getChannel()==Token.HIDDEN_CHANNEL) {
+                    from=tok;
+                }
+            }
+            tfc.addEdit(new DeleteEdit(from.getStartIndex(), 
+                    declarationNode.getStopIndex()-from.getStartIndex()+1));
+        }
+    }
+
+    private static Tree.Term getInlinedTerm(Tree.Declaration declarationNode) {
+        if (declarationNode!=null) {
+        	if (declarationNode instanceof Tree.AttributeDeclaration) {
+        		Tree.AttributeDeclaration att = (Tree.AttributeDeclaration) declarationNode;
+        		return att.getSpecifierOrInitializerExpression().getExpression().getTerm();
+        	}
+        	else if (declarationNode instanceof Tree.MethodDefinition) {
+        		Tree.MethodDefinition meth = (Tree.MethodDefinition) declarationNode;
+        		if (meth.getBlock().getStatements().size()!=1) {
+        			throw new RuntimeException("method has multiple statements");
+        		}
+        		if (meth.getType() instanceof Tree.VoidModifier) {
+        			Tree.ExpressionStatement e = (Tree.ExpressionStatement) meth.getBlock()
+        					.getStatements().get(0);
+        			return e.getExpression().getTerm();
+        			
+        		}
+        		else {
+        			Tree.Return r = (Tree.Return) meth.getBlock().getStatements().get(0);
+        			return r.getExpression().getTerm();
+        		}
+        	}
+        	else if (declarationNode instanceof Tree.AttributeGetterDefinition) {
+        		Tree.AttributeGetterDefinition att = (Tree.AttributeGetterDefinition) declarationNode;
+        		if (att.getBlock().getStatements().size()!=1) {
+        			throw new RuntimeException("getter has multiple statements");
+        		}
+        		Tree.Return r = (Tree.Return) att.getBlock().getStatements().get(0);
+        		return r.getExpression().getTerm();
+        	}
+        	else {
+        		throw new RuntimeException("not a value or function");
+        	}
+        }
+        else {
+            return null;
+        }
+    }
+
+    private void inlineReferences(Tree.Declaration declarationNode,
+            PhasedUnit declarationUnit, final Tree.Term term, final PhasedUnit pu,
+            final TextFileChange tfc) {
+        String template = toString(term, declarationUnit.getTokenStream());
+        int templateStart = term.getStartIndex();
+        if (declarationNode instanceof Tree.AnyAttribute) {
+        	inlineAttributeReferences(pu, template, tfc);
+        }
+        else {
+        	inlineFunctionReferences(pu, term, template, templateStart, tfc);
+        }
+    }
+
+    private void inlineFunctionReferences(final PhasedUnit pu, final Tree.Term term,
+            final String template, final int templateStart, final TextFileChange tfc) {
+        new Visitor() {
+        	@Override
+        	public void visit(final Tree.InvocationExpression that) {
+        		super.visit(that);
+        		if (that.getPrimary().getDeclaration().equals(declaration)) {
+        			//TODO: breaks for invocations like f(f(x, y),z)
+        			final StringBuilder result = new StringBuilder();
+        			class InterpolateArgumentsVisitor extends Visitor {
+        				int start = 0;
+        				@Override
+        				public void visit(Tree.BaseMemberExpression it) {
+        					super.visit(it);
+        					if (it.getDeclaration() instanceof Parameter) {
+        						Parameter param = (Parameter) it.getDeclaration();
+        						if ( param.getDeclaration().equals(declaration) ) {
+        							result.append(template.substring(start,it.getStartIndex()-templateStart));
+        							start = it.getStopIndex()-templateStart+1;
+        							boolean sequenced = param.isSequenced();
+        							if (that.getPositionalArgumentList()!=null) {
+        								interpolatePositionalArguments(result, that, it, sequenced, pu);
+        							}
+        							if (that.getNamedArgumentList()!=null) {
+        								interpolateNamedArguments(result, that, it, sequenced, pu);
+        							}
+        						}
+        					}
+        				}
+        				void finish() {
+        					result.append(template.substring(start, template.length()));
+        				}
+        			}
+        			InterpolateArgumentsVisitor iv = new InterpolateArgumentsVisitor();
+        			iv.visit(term);
+        			iv.finish();
+        			tfc.addEdit(new ReplaceEdit(that.getStartIndex(), 
+        					that.getStopIndex()-that.getStartIndex()+1, 
+        					result.toString()));
+        		}
+        	}
+        }.visit(pu.getCompilationUnit());
+    }
+
+    private void inlineAttributeReferences(final PhasedUnit pu, final String template,
+            final TextFileChange tfc) {
+        new Visitor() {
+        	@Override
+        	public void visit(Tree.BaseMemberExpression that) {
+        		super.visit(that);
+        		if (that.getDeclaration().equals(declaration)) {
+        			tfc.addEdit(new ReplaceEdit(that.getStartIndex(), 
+        					that.getStopIndex()-that.getStartIndex()+1, 
+        					template));	
+        		}
+        	}
+        }.visit(pu.getCompilationUnit());
+    }
+
+    private static void interpolatePositionalArguments(StringBuilder result, 
+            Tree.InvocationExpression that, Tree.BaseMemberExpression it,
+            boolean sequenced, PhasedUnit pu) {
+        boolean first = true;
+        boolean found = false;
+        if (sequenced) {
+            result.append("{");
+        }
+        for (Tree.PositionalArgument arg: that.getPositionalArgumentList()
+                .getPositionalArguments()) {
+            if (it.getDeclaration().equals(arg.getParameter())) {
+                if (arg.getParameter().isSequenced() && 
+                        that.getPositionalArgumentList().getEllipsis()==null) {
+                    if (first) result.append(" ");
+                    if (!first) result.append(", ");
+                    first = false;
+                }
+                result.append(AbstractRefactoring.toString(arg.getExpression().getTerm(), 
+                        pu.getTokenStream()));
+                found = true;
+            }
+        }
+        if (sequenced) {
+            if (!first) result.append(" ");
+            result.append("}");
+        }
+        if (!found) {} //TODO: use default value!
+    }
+
+    private static void interpolateNamedArguments(StringBuilder result,
+            Tree.InvocationExpression that, Tree.BaseMemberExpression it,
+            boolean sequenced, PhasedUnit pu) {
+        boolean found = false;
+        for (Tree.NamedArgument arg: that.getNamedArgumentList().getNamedArguments()) {
+            if (it.getDeclaration().equals(arg.getParameter())) {
+                Term argTerm = ((Tree.SpecifiedArgument) arg).getSpecifierExpression()
+                                .getExpression().getTerm();
+                result//.append(template.substring(start,it.getStartIndex()-templateStart))
+                    .append(AbstractRefactoring.toString( argTerm, pu.getTokenStream()) );
+                //start = it.getStopIndex()-templateStart+1;
+                found=true;
+            }
+        }
+        SequencedArgument seqArg = that.getNamedArgumentList().getSequencedArgument();
+        if (seqArg!=null && it.getDeclaration().equals(seqArg.getParameter())) {
+            result//.append(template.substring(start,it.getStartIndex()-templateStart))
+                .append("{");
+            //start = it.getStopIndex()-templateStart+1;;
+            boolean first=true;
+            for (Tree.Expression e: seqArg.getExpressionList().getExpressions()) {
+                if (first) result.append(" ");
+                if (!first) result.append(", ");
+                first=false;
+                result.append(AbstractRefactoring.toString(e.getTerm(), pu.getTokenStream()));
+            }
+            if (!first) result.append(" ");
+            result.append("}");
+            found=true;
+        }
+        if (!found) {
+            if (sequenced) {
+                result.append("{}");
+            }
+            else {} //TODO: use default value!
+        }
+    }
+    
 	public Declaration getDeclaration() {
 		return declaration;
 	}
