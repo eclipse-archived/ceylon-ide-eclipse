7d951e83d357461cd628cb5cbcc28c89f3bc31ad
Gavin King <gavin@hibernate.org>
2011-09-27 10:33:22 -0500

    fix indentation

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/vfs/SourceCodeVirtualFile.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/vfs/SourceCodeVirtualFile.java
@@ -12,66 +12,66 @@ import com.redhat.ceylon.compiler.typechecker.io.VirtualFile;
 
 
 public class SourceCodeVirtualFile implements VirtualFile {
-  public static final List<VirtualFile> EMPTY_CHILDREN = Collections.unmodifiableList( new ArrayList<VirtualFile>(0) );
-  
-  InputStream stream;
-  String path = "unknown.ceylon";
-  String name = "unknown.ceylon";
-  
-  public SourceCodeVirtualFile(String fileContent)
-  {
-    stream = new ByteArrayInputStream(fileContent.getBytes());
-  }
-  
-  public SourceCodeVirtualFile(String fileContent, IPath path)
-  {
-    stream = new ByteArrayInputStream(fileContent.getBytes());
-    this.path = path.toString();
-    this.name = path.toFile().getName();
-  }
-  
-  public boolean isFolder() {
-    return false;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getPath() {
-    return path;
-  }
-
-  public InputStream getInputStream() {
-    return stream;
-  }
-
-  public List<VirtualFile> getChildren() {
-    return EMPTY_CHILDREN;
-  }
-
-  @Override
-  public String toString() {
-      final StringBuilder sb = new StringBuilder();
-      sb.append("SourceCodeVirtualFile");
-      return sb.toString();
-  }  
-
-  @Override
-  public int hashCode() {
-      return getPath().hashCode();
-  }
-  
-  @Override
-  public boolean equals(Object obj) {
-      if (obj instanceof ResourceVirtualFile) {
-          return ((ResourceVirtualFile) obj).getPath().equals(getPath());
-      }
-      else if (obj instanceof SourceCodeVirtualFile) {
-          return ((SourceCodeVirtualFile) obj).getPath().equals(getPath());
-      }
-      else {
-          return super.equals(obj);
-      }
-  }
+    public static final List<VirtualFile> EMPTY_CHILDREN = Collections.unmodifiableList( new ArrayList<VirtualFile>(0) );
+    
+    InputStream stream;
+    String path = "unknown.ceylon";
+    String name = "unknown.ceylon";
+    
+    public SourceCodeVirtualFile(String fileContent)
+    {
+        stream = new ByteArrayInputStream(fileContent.getBytes());
+    }
+    
+    public SourceCodeVirtualFile(String fileContent, IPath path)
+    {
+        stream = new ByteArrayInputStream(fileContent.getBytes());
+        this.path = path.toString();
+        this.name = path.toFile().getName();
+    }
+    
+    public boolean isFolder() {
+        return false;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getPath() {
+        return path;
+    }
+    
+    public InputStream getInputStream() {
+        return stream;
+    }
+    
+    public List<VirtualFile> getChildren() {
+        return EMPTY_CHILDREN;
+    }
+    
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("SourceCodeVirtualFile");
+        return sb.toString();
+    }  
+    
+    @Override
+    public int hashCode() {
+        return getPath().hashCode();
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof ResourceVirtualFile) {
+            return ((ResourceVirtualFile) obj).getPath().equals(getPath());
+        }
+        else if (obj instanceof SourceCodeVirtualFile) {
+            return ((SourceCodeVirtualFile) obj).getPath().equals(getPath());
+        }
+        else {
+            return super.equals(obj);
+        }
+    }
 }
