9b7ed829495c06cb8f5ac18f77a3916458663de3
Gavin King <gavin@hibernate.org>
2011-10-02 13:21:59 -0500

    implement source folder / package selection using JDT APIs

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/wizard/NewUnitWizardPage.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/wizard/NewUnitWizardPage.java
@@ -2,6 +2,15 @@ package com.redhat.ceylon.eclipse.imp.wizard;
 
 import static com.redhat.ceylon.eclipse.ui.ICeylonResources.CEYLON_NEW_FILE;
 
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.internal.ui.refactoring.nls.SourceContainerDialog;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.IWizardPage;
@@ -18,20 +27,32 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
-import com.redhat.ceylon.compiler.typechecker.model.Package;
 import com.redhat.ceylon.eclipse.ui.CeylonPlugin;
 
 public class NewUnitWizardPage extends WizardPage implements IWizardPage {
 
-    private String packageName="";
     private String unitName="";
+    private IPackageFragmentRoot sourceDir;
+    private IPackageFragment packageFragment;
     
-    protected NewUnitWizardPage() {
+    IStructuredSelection selection;
+    
+    NewUnitWizardPage() {
         super("New Ceylon Unit", "New Ceylon Unit", CeylonPlugin.getInstance()
                 .getImageRegistry().getDescriptor(CEYLON_NEW_FILE));
         setDescription("Create a new Ceylon compilation unit that will contain Ceylon source.");
     }
 
+    private IJavaElement getSelectedElement() {
+        if (selection!=null && selection.size()==1) {
+            return (IJavaElement) ((IAdaptable) selection.getFirstElement())
+                    .getAdapter(IJavaElement.class);
+        }
+        else {
+            return null;
+        }
+    }
+    
     @Override
     public void createControl(Composite parent) {
         initializeDialogUnits(parent);
@@ -42,6 +63,111 @@ public class NewUnitWizardPage extends WizardPage implements IWizardPage {
         GridLayout layout = new GridLayout();
         layout.numColumns = 4;
         composite.setLayout(layout);
+        
+        Label folderLabel = new Label(composite, SWT.LEFT | SWT.WRAP);
+        folderLabel.setText("Source folder: ");
+        GridData flgd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+        flgd.horizontalSpan = 1;
+        folderLabel.setLayoutData(flgd);
+
+        final Text folder = new Text(composite, SWT.SINGLE | SWT.BORDER);
+        GridData fgd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+        fgd.horizontalSpan = 2;
+        fgd.grabExcessHorizontalSpace = true;
+        folder.setLayoutData(fgd);
+        folder.addModifyListener(new ModifyListener() {
+            @Override
+            public void modifyText(ModifyEvent e) {
+                String folderName = folder.getText();
+                try {
+                    for (IJavaProject jp: JavaCore.create(ResourcesPlugin.getWorkspace().getRoot())
+                            .getJavaProjects()) {
+                        for (IPackageFragmentRoot pfr: jp.getPackageFragmentRoots()) {
+                            if (pfr.getPath().toPortableString().equals(folderName)) {
+                                sourceDir = pfr;
+                            }
+                        }
+                    }
+                }
+                catch (JavaModelException jme) {
+                    jme.printStackTrace();
+                }
+                setPageComplete(isComplete());
+            }
+        });
+        
+        IJavaElement je = getSelectedElement();
+        if (je instanceof IPackageFragmentRoot) {
+            sourceDir = (IPackageFragmentRoot) je;
+            String folderName = sourceDir.getPath().toPortableString();
+            folder.setText(folderName);
+        }
+        
+        Button selectFolder = new Button(composite, SWT.PUSH);
+        selectFolder.setText("Browse...");
+        GridData sfgd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+        sfgd.horizontalSpan = 1;
+        selectFolder.setLayoutData(sfgd);
+        selectFolder.addSelectionListener(new SelectionListener() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                sourceDir = SourceContainerDialog.getSourceContainer(getShell(), 
+                        ResourcesPlugin.getWorkspace().getRoot(), sourceDir);
+                if (sourceDir!=null) {
+                    String folderName = sourceDir.getPath().toPortableString();
+                    folder.setText(folderName);
+                    setPageComplete(isComplete());
+                }
+            }
+            @Override
+            public void widgetDefaultSelected(SelectionEvent e) {}
+        });
+        
+        Label packageLabel = new Label(composite, SWT.LEFT | SWT.WRAP);
+        packageLabel.setText("Package: ");
+        GridData plgd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+        plgd.horizontalSpan = 1;
+        packageLabel.setLayoutData(plgd);
+
+        final Text pkg = new Text(composite, SWT.SINGLE | SWT.BORDER);
+        GridData pgd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+        pgd.horizontalSpan = 2;
+        pgd.grabExcessHorizontalSpace = true;
+        pkg.setLayoutData(pgd);
+        pkg.addModifyListener(new ModifyListener() {
+            @Override
+            public void modifyText(ModifyEvent e) {
+                String packageName = pkg.getText();
+                packageFragment = sourceDir.getPackageFragment(packageName);
+                setPageComplete(isComplete());
+            }
+        });
+        
+        Button selectPackage = new Button(composite, SWT.PUSH);
+        selectPackage.setText("Browse...");
+        GridData spgd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+        spgd.horizontalSpan = 1;
+        selectPackage.setLayoutData(spgd);
+        selectPackage.addSelectionListener(new SelectionListener() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                if (sourceDir!=null) {
+                    PackageSelectionDialog dialog = new PackageSelectionDialog(getShell(), sourceDir);
+                    dialog.setMultipleSelection(false);
+                    dialog.setTitle("Package Selection");
+                    dialog.setMessage("Select a package:");
+                    dialog.open();
+                    Object result = dialog.getFirstResult();
+                    if (result!=null) {
+                        String packageName = ((IPackageFragment) result).getElementName();
+                        pkg.setText(packageName);
+                        setPageComplete(isComplete());
+                    }
+                }
+            }
+            @Override
+            public void widgetDefaultSelected(SelectionEvent e) {}
+        });
         
         Label nameLabel = new Label(composite, SWT.LEFT | SWT.WRAP);
         nameLabel.setText("Compilation unit name: ");
@@ -57,51 +183,9 @@ public class NewUnitWizardPage extends WizardPage implements IWizardPage {
         name.addModifyListener(new ModifyListener() {
             @Override
             public void modifyText(ModifyEvent e) {
-                NewUnitWizardPage.this.unitName = name.getText();
+                unitName = name.getText();
                 setPageComplete(isComplete());
             }
-        });
-        
-        Label packageLabel = new Label(composite, SWT.LEFT | SWT.WRAP);
-        packageLabel.setText("Package: ");
-        GridData plgd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
-        plgd.horizontalSpan = 1;
-        nameLabel.setLayoutData(plgd);
-
-        final Text pkg = new Text(composite, SWT.SINGLE | SWT.BORDER);
-        GridData pgd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
-        pgd.horizontalSpan = 2;
-        pgd.grabExcessHorizontalSpace = true;
-        pkg.setLayoutData(pgd);
-        pkg.addModifyListener(new ModifyListener() {
-            @Override
-            public void modifyText(ModifyEvent e) {
-                NewUnitWizardPage.this.packageName = pkg.getText();
-                setPageComplete(isComplete());
-            }
-        });
-        
-        Button selectPackage = new Button(composite, SWT.PUSH);
-        selectPackage.setText("Browse...");
-        GridData spgd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
-        spgd.horizontalSpan = 1;
-        selectPackage.setLayoutData(spgd);
-        selectPackage.addSelectionListener(new SelectionListener() {
-            @Override
-            public void widgetSelected(SelectionEvent e) {
-                PackageSelectionDialog dialog = new PackageSelectionDialog(getShell());
-                dialog.setMultipleSelection(false);
-                dialog.setTitle("Package Selection");
-                dialog.setMessage("Select a package:");
-                dialog.open();
-                Object result = dialog.getFirstResult();
-                String pn = ((Package) result).getQualifiedNameString();
-                pkg.setText(pn);
-                NewUnitWizardPage.this.packageName = pn;
-                setPageComplete(isComplete());
-            }
-            @Override
-            public void widgetDefaultSelected(SelectionEvent e) {}
         });
         
         setControl(composite);
@@ -109,16 +193,8 @@ public class NewUnitWizardPage extends WizardPage implements IWizardPage {
         Dialog.applyDialogFont(composite);
     }
     
-    public String getPackageName() {
-        return packageName;
-    }
-    
-    public String getUnitName() {
-        return unitName;
-    }
-
     public void init(IStructuredSelection selection) {
-        // TODO
+        this.selection = selection;
     }
     
     @Override
@@ -128,8 +204,9 @@ public class NewUnitWizardPage extends WizardPage implements IWizardPage {
     }
     
     private boolean isComplete() {
-        return !packageName.equals("")
-                && !unitName.equals("");
+        return packageFragment!=null &&
+                sourceDir!=null &&
+                !unitName.equals("");
     }
     
 }
