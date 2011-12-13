a76fc097787c7d982dc1d2f288dda12c442857a9
Gavin King <gavin@hibernate.org>
2011-12-13 15:34:02 -0600

    Export Module Wizard

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/wizard/ExportModuleWizard.java
@@ -0,0 +1,123 @@
+package com.redhat.ceylon.eclipse.imp.wizard;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IExportWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class ExportModuleWizard extends Wizard implements IExportWizard {
+
+    IStructuredSelection selection;
+    IWorkbench workbench;
+    ExportModuleWizardPage page;
+    
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+        this.selection = selection;
+        this.workbench = workbench;
+	}
+
+    @Override
+    public void addPages() {
+        super.addPages();
+        if (page == null) {
+            page = new ExportModuleWizardPage();
+            page.init(selection);
+        }
+        addPage(page);
+    }
+
+    private IJavaElement getSelectedElement() {
+        if (selection!=null && selection.size()==1) {
+            IJavaElement je = (IJavaElement) ((IAdaptable) selection.getFirstElement())
+                    .getAdapter(IJavaElement.class);
+            //TODO: handle the case of an IFile
+            return je;
+        }
+        else {
+            return null;
+        }
+    }
+    
+	@Override
+	public boolean performFinish() {
+		IJavaElement selectedElem = getSelectedElement();
+		if (selectedElem==null) {
+			return false;
+		}
+		else {
+			IJavaProject javaProject = selectedElem.getJavaProject();
+			/*IProject project = javaProject.getProject();
+			List<PhasedUnit> list = CeylonBuilder.getProjectTypeChecker(project)
+				.getPhasedUnits().getPhasedUnits();
+			Set<String> moduleNames = new HashSet<String>();
+			for (PhasedUnit phasedUnit: list) {
+				Module module = phasedUnit.getUnit().getPackage().getModule();
+				moduleNames.add(module.getNameAsString());
+			}*/
+			try {
+				//URL platformLoc = Platform.getInstanceLocation().getURL();
+				IPath projectLoc = javaProject.getProject().getLocation().makeAbsolute();
+				projectLoc = projectLoc.uptoSegment(projectLoc.segmentCount()-1);
+				IPath outputDir = javaProject.getOutputLocation();
+				File source = projectLoc.append(outputDir).toFile();
+				/*File source = new File(platformLoc.getFile(), 
+						javaProject.getOutputLocation().toFile().getPath());*/
+				File dest = new File(page.getRepositoryPath());
+				copyFolder(source, dest);
+			} 
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+			return true;
+		}
+	}
+	
+	public static void copyFolder(File src, File dest)
+			throws IOException{
+    	if (src.isDirectory()) {
+    		if ( !dest.exists() ) dest.mkdir();
+    		for (String file: src.list()) {
+    		   File srcFile = new File(src, file);
+    		   File destFile = new File(dest, file);
+    		   copyFolder(srcFile, destFile);
+    		}
+    	}
+    	else if (src.getName().endsWith(".car")) {
+    	    FileChannel source = null;
+    	    FileChannel destination = null;
+    	    try {
+    	        source = new FileInputStream(src).getChannel();
+    	        destination = new FileOutputStream(dest).getChannel();
+    	        destination.transferFrom(source, 0, source.size());
+    	    }
+    	    finally {
+    	        if (source != null) {
+    	            source.close();
+    	        }
+    	        if (destination != null) {
+    	            destination.close();
+    	        }
+    	    }
+	    	System.out.println("Archive exported from " + src + " to " + dest);
+    	}
+    }
+    
+	@Override
+	public boolean canFinish() {
+		return super.canFinish() &&
+				page.isPageComplete() && 
+				new File(page.getRepositoryPath()).exists();
+	}
+
+}
---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/wizard/ExportModuleWizardPage.java
@@ -0,0 +1,112 @@
+package com.redhat.ceylon.eclipse.imp.wizard;
+
+import static com.redhat.ceylon.eclipse.ui.ICeylonResources.CEYLON_NEW_FILE;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.IWizardPage;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+import com.redhat.ceylon.eclipse.ui.CeylonPlugin;
+
+public class ExportModuleWizardPage extends WizardPage implements IWizardPage {
+
+    IStructuredSelection selection;
+    String repositoryPath = System.getProperty("user.home") + "/.ceylon/repo";
+    
+    ExportModuleWizardPage() {
+        super("New Ceylon Unit", "New Ceylon Unit", CeylonPlugin.getInstance()
+                .getImageRegistry().getDescriptor(CEYLON_NEW_FILE));
+        setDescription("Export a Ceylon module to a module repository.");
+    }
+
+    public void init(IStructuredSelection selection) {
+        this.selection = selection;
+    }
+    
+    @Override
+    public void createControl(Composite parent) {
+        initializeDialogUnits(parent);
+        
+        Composite composite= new Composite(parent, SWT.NONE);
+        composite.setFont(parent.getFont());
+
+        GridLayout layout = new GridLayout();
+        layout.numColumns = 4;
+        composite.setLayout(layout);
+        
+        //TODO: let you select a module descriptor to
+        //      export just that module!
+        
+		addSelectRepo(composite);
+        
+        setControl(composite);
+
+        Dialog.applyDialogFont(composite);
+    }
+
+	void addSelectRepo(Composite composite) {
+		Label folderLabel = new Label(composite, SWT.LEFT | SWT.WRAP);
+        folderLabel.setText("Source module repository: ");
+        GridData flgd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+        flgd.horizontalSpan = 1;
+        folderLabel.setLayoutData(flgd);
+
+        final Text folder = new Text(composite, SWT.SINGLE | SWT.BORDER);
+        GridData fgd = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+        fgd.horizontalSpan = 2;
+        fgd.grabExcessHorizontalSpace = true;
+        folder.setLayoutData(fgd);
+        folder.addModifyListener(new ModifyListener() {
+            @Override
+            public void modifyText(ModifyEvent e) {
+                repositoryPath = folder.getText();
+            }
+        });
+        
+        folder.setText(repositoryPath);
+        
+        Button selectFolder = new Button(composite, SWT.PUSH);
+        selectFolder.setText("Browse...");
+        GridData sfgd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+        sfgd.horizontalSpan = 1;
+        selectFolder.setLayoutData(sfgd);
+        selectFolder.addSelectionListener(new SelectionListener() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+            	String dir = new DirectoryDialog(getShell()).open();
+            	if (dir!=null) {
+            		repositoryPath = dir;
+            		folder.setText(repositoryPath);
+            	}
+            }
+            @Override
+            public void widgetDefaultSelected(SelectionEvent e) {}
+        });
+	
+	}
+	
+	@Override
+	public boolean isPageComplete() {
+		return super.isPageComplete() &&
+				repositoryPath!=null &&
+				!repositoryPath.isEmpty();
+	}
+	
+	public String getRepositoryPath() {
+		return repositoryPath;
+	}
+    
+}
