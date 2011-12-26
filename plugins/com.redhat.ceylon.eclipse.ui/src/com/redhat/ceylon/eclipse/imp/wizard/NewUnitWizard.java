6f88c165dd3d0d4df2564eb3ea90c17782f93fbc
Gavin King <gavin@hibernate.org>
2011-12-26 12:20:59 -0600

    Create new toplevel declaration quick fix Fix bug where src folder and package info was not populated from editor

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/wizard/NewUnitWizard.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/wizard/NewUnitWizard.java
@@ -14,6 +14,8 @@ public class NewUnitWizard extends Wizard implements INewWizard {
     
     private IStructuredSelection selection;
     private NewUnitWizardPage page;
+    private String defaultUnitName="";
+    private String contents="";
     
     @Override
     public void init(IWorkbench workbench, IStructuredSelection selection) {
@@ -24,7 +26,7 @@ public class NewUnitWizard extends Wizard implements INewWizard {
     public boolean performFinish() {
         FileCreationOp op = new FileCreationOp(page.getSourceDir(), 
                 page.getPackageFragment(), page.getUnitName(), 
-                page.isIncludePreamble(), "");
+                page.isIncludePreamble(), contents);
         try {
             getContainer().run(true, true, op);
         } 
@@ -45,9 +47,22 @@ public class NewUnitWizard extends Wizard implements INewWizard {
         if (page == null) {
             page= new NewUnitWizardPage("New Ceylon Unit",
                     "Create a new Ceylon compilation unit that will contain Ceylon source.",
-                    "", CEYLON_NEW_FILE);
+                    defaultUnitName, CEYLON_NEW_FILE);
             page.init(selection);
         }
         addPage(page);
     }
+    
+    public void setDefaultUnitName(String defaultUnitName) {
+		this.defaultUnitName = defaultUnitName;
+	}
+    
+    public void setSelection(IStructuredSelection selection) {
+		this.selection = selection;
+	}
+    
+    public void setContents(String contents) {
+		this.contents = contents;
+	}
+    
 }
