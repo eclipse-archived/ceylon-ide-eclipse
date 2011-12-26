3bc4c4aa4441d9bbc4197ac0c3290dc439bfe1fa
Gavin King <gavin@hibernate.org>
2011-12-26 09:50:37 -0600

    let you select the project from inside export module wizard

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/wizard/ProjectSelectionDialog.java
@@ -0,0 +1,70 @@
+package com.redhat.ceylon.eclipse.imp.wizard;
+
+import static com.redhat.ceylon.eclipse.imp.outline.CeylonLabelProvider.PROJECT;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.imp.services.ILabelProvider;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.dialogs.ElementListSelectionDialog;
+
+public class ProjectSelectionDialog extends ElementListSelectionDialog {
+
+    public ProjectSelectionDialog(Shell parent) {
+        super(parent, new ILabelProvider() {
+            @Override
+            public void removeListener(ILabelProviderListener listener) {}
+            @Override
+            public boolean isLabelProperty(Object element, String property) {
+                return false;
+            }
+            @Override
+            public void dispose() {}
+            @Override
+            public void addListener(ILabelProviderListener listener) {}
+            @Override
+            public String getText(Object element) {
+                return ((IJavaProject) element).getElementName();
+            }
+            @Override
+            public Image getImage(Object element) {
+                return PROJECT;
+            }
+        });
+    }
+    
+    @Override
+    public int open() {
+        List<IJavaProject> elements = new ArrayList<IJavaProject>();
+        try {
+            addChildren(elements, JavaCore.create(ResourcesPlugin.getWorkspace().getRoot())
+                    .getJavaProjects());
+        }
+        catch (JavaModelException jme) {
+            jme.printStackTrace();
+        }
+        /*Collections.sort(elements, new Comparator<IPackageFragment>() {
+            @Override
+            public int compare(IPackageFragment pf1, IPackageFragment pf2) {
+                return pf1.getElementName().compareTo(pf2.getElementName());
+            }
+        });*/
+        setElements(elements.toArray());
+        return super.open();
+    }
+
+    public void addChildren(List<IJavaProject> elements, IJavaProject[] children)
+            throws JavaModelException {
+        for (IJavaProject jp: children) {
+            elements.add(jp);
+        }
+    }
+    
+}
