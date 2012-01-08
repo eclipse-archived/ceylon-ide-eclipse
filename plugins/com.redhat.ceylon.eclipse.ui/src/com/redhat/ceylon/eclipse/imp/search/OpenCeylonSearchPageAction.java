c4d16f517e21014af5b376b0362653b068895eb8
Gavin King <gavin@hibernate.org>
2012-01-08 09:36:01 -0600

    Don't use actionsets anymore (finally!)

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/search/OpenCeylonSearchPageAction.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/search/OpenCeylonSearchPageAction.java
@@ -1,40 +1,30 @@
 package com.redhat.ceylon.eclipse.imp.search;
 
-import org.eclipse.jface.action.IAction;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.search.ui.NewSearchUI;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PlatformUI;
 
 import com.redhat.ceylon.eclipse.ui.CeylonPlugin;
 
-public class OpenCeylonSearchPageAction implements IWorkbenchWindowActionDelegate {
+public class OpenCeylonSearchPageAction extends AbstractHandler {
 
-    private IWorkbenchWindow window;
-
-    public void init(IWorkbenchWindow window) {
-        this.window = window;
-    }
-
-    public void run(IAction action) {
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
         if (window == null || window.getActivePage() == null) {
-            Shell shell= CeylonPlugin.getInstance().getWorkbench()
+            Shell shell = CeylonPlugin.getInstance().getWorkbench()
                     .getActiveWorkbenchWindow().getShell();
             MessageDialog.openError(shell, "Ceylon Search Error", "No active window");
         }
         else {
             NewSearchUI.openSearchDialog(window, "com.redhat.ceylon.eclipse.ui.searchPage");
         }
+        return null;
     }
 
-    public void selectionChanged(IAction action, ISelection selection) {
-        // do nothing since the action isn't selection dependent.
-    }
-
-    public void dispose() {
-        window = null;
-    }
-        
 }
