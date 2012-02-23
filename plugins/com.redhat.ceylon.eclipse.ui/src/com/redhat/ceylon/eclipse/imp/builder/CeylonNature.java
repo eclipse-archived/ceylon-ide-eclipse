bf5ba33a93157c8b8dfe4bd8695550fd71d9852c
David Festal <david.festal@serli.com>
2012-02-23 18:29:00 +0100

    Run the Java builder after the Ceylon Builder

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/builder/CeylonNature.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/builder/CeylonNature.java
@@ -4,6 +4,8 @@ import org.eclipse.core.resources.IProject;
 
 import org.eclipse.imp.builder.ProjectNatureBase;
 import org.eclipse.imp.runtime.IPluginLog;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.internal.core.builder.JavaBuilder;
 
 import com.redhat.ceylon.eclipse.ui.CeylonPlugin;
 
@@ -31,8 +33,12 @@ public class CeylonNature extends ProjectNatureBase {
         return CeylonPlugin.getInstance();
     }
     
+    /**
+     * Returns the ID of the builder that processes the artifacts that this
+     * nature's builder produces. If there is no such dependency, returns null.
+     */
+    // TODO this should be a property of the builder itself...
     protected String getDownstreamBuilderID() {
-        // TODO If needed, specify the builder that will consume artifacts created by this nature's builder, or null if none
-        return null;
+        return JavaCore.BUILDER_ID;
     }
 }
