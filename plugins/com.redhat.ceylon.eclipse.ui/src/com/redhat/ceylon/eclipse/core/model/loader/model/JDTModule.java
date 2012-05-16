41c51e11c083e8ee913aba0a6c7cfd5e7f07e441
David Festal <david.festal@serli.com>
2012-05-16 02:08:29 +0200

    Redefine getAllPackages to include binary packages in import proposals

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/model/loader/model/JDTModule.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/model/loader/model/JDTModule.java
@@ -20,12 +20,28 @@
 
 package com.redhat.ceylon.eclipse.core.model.loader.model;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.List;
+import java.util.TreeSet;
 
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceVisitor;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.IParent;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.internal.core.JarPackageFragmentRoot;
+import org.eclipse.jdt.internal.core.PackageFragment;
 
+import com.redhat.ceylon.cmr.api.ArtifactResult;
 import com.redhat.ceylon.compiler.loader.AbstractModelLoader;
 import com.redhat.ceylon.compiler.loader.model.LazyModule;
+import com.redhat.ceylon.compiler.typechecker.model.ModuleImport;
+import com.redhat.ceylon.compiler.typechecker.model.Package;
 
 public class JDTModule extends LazyModule {
 
@@ -46,4 +62,68 @@ public class JDTModule extends LazyModule {
         return moduleManager.getModelLoader();
     }
 
+
+    @Override
+    public List<Package> getAllPackages() {
+        // force-load every package from the module if we can
+        loadAllPackages();
+        // now force-load other modules
+        for (ModuleImport mi: getImports()) {
+            if(mi.getModule() instanceof JDTModule){
+                ((JDTModule)mi.getModule()).loadAllPackages();
+            }
+        }
+        // now delegate
+        return super.getAllPackages();
+    }
+
+    private void loadAllPackages() {
+        Set<String> packageList = new TreeSet<String>();
+        if(isJava()){
+            for(IPackageFragmentRoot fragmentRoot : packageFragmentRoots){
+                IParent parent = fragmentRoot;
+                listPackages(packageList, parent);
+            }
+            for (String packageName : packageList) {
+                getPackage(packageName);
+            }
+        }
+    }
+
+    private void listPackages(Set<String> packageList, IParent parent) {
+        try {
+            for (IJavaElement child : parent.getChildren()) {
+                if (child instanceof PackageFragment) {
+                    packageList.add(child.getElementName());
+                    listPackages(packageList, (IPackageFragment) child);
+                }
+            }
+        } catch (JavaModelException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void loadPackageList(ArtifactResult artifact) {
+        super.loadPackageList(artifact);
+        String jarPath = artifact.artifact().getAbsolutePath();
+        try {
+            for (IPackageFragmentRoot root : moduleManager.getJavaProject().getPackageFragmentRoots()) {
+                if (root instanceof JarPackageFragmentRoot) {
+                    JarPackageFragmentRoot jarRoot = (JarPackageFragmentRoot) root;
+                    try {
+                        if (jarRoot.getJar().getName().equals(jarPath)) {
+                            packageFragmentRoots.add(root);
+                        }
+                    } catch (CoreException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (JavaModelException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }    
 }
