2b83e82b063a93ed01363f1399829f79573076e2
David Festal <david.festal@serli.com>
2012-05-22 23:22:42 +0200

    Refactor CeylonDeclaration and SourceClass

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/model/CeylonDeclaration.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/model/CeylonDeclaration.java
@@ -1,6 +1,7 @@
 package com.redhat.ceylon.eclipse.core.model;
 
 import com.redhat.ceylon.compiler.typechecker.context.PhasedUnit;
+import com.redhat.ceylon.compiler.typechecker.model.Declaration;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 
 public class CeylonDeclaration {
@@ -8,6 +9,7 @@ public class CeylonDeclaration {
     
     private PhasedUnit phasedUnit;
     private Tree.Declaration astDeclaration;
+    private Declaration modelDeclaration = null;
 
     public CeylonDeclaration(PhasedUnit phasedUnit, Tree.Declaration astDeclaration) {
         this.phasedUnit = phasedUnit;
@@ -20,5 +22,25 @@ public class CeylonDeclaration {
 
     public Tree.Declaration getAstDeclaration() {
         return astDeclaration;
+    }
+
+    public Declaration getModelDeclaration() {
+        if (modelDeclaration != null) {
+            return modelDeclaration;
+        }
+        if (! phasedUnit.isDeclarationsScanned()) {
+            phasedUnit.scanDeclarations();
+        }
+        if (! phasedUnit.isTypeDeclarationsScanned()) {
+            phasedUnit.scanTypeDeclarations();
+        }
+        if (! phasedUnit.isRefinementValidated()) {
+            phasedUnit.validateRefinement();
+        }
+        if (! phasedUnit.isFullyTyped()) {
+            phasedUnit.analyseTypes();
+        }
+        modelDeclaration = astDeclaration.getDeclarationModel();
+        return modelDeclaration;
     }
 }
