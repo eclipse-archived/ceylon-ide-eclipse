8ab75f667f83cb11e4b1ebc95fb06850ac2eb8d9
Gavin King <gavin@hibernate.org>
2012-05-25 18:04:00 +0200

    correctly handle sequenced arguments in RequiredTypeVisitor

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/proposals/RequiredTypeVisitor.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/proposals/RequiredTypeVisitor.java
@@ -1,35 +1,70 @@
 package com.redhat.ceylon.eclipse.imp.proposals;
 
+import java.util.List;
+
 import com.redhat.ceylon.compiler.typechecker.model.Functional;
 import com.redhat.ceylon.compiler.typechecker.model.Parameter;
 import com.redhat.ceylon.compiler.typechecker.model.ProducedReference;
 import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
 import com.redhat.ceylon.compiler.typechecker.tree.Node;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
+import com.redhat.ceylon.compiler.typechecker.tree.Tree.NamedArgumentList;
+import com.redhat.ceylon.compiler.typechecker.tree.Tree.PositionalArgumentList;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
 
 class RequiredTypeVisitor extends Visitor {
     private Node node;
     ProducedType requiredType = null;
+    
     RequiredTypeVisitor(Node node) {
         this.node = node;
     }
+    
     @Override
     public void visit(Tree.InvocationExpression that) {
         super.visit(that);
-        if (that.getPositionalArgumentList()==node) {
-            int pos = that.getPositionalArgumentList().getPositionalArguments().size();
-            Tree.Primary p = that.getPrimary();
-            if (p instanceof Tree.MemberOrTypeExpression) {
-                ProducedReference pr = ((Tree.MemberOrTypeExpression) p).getTarget();
-                if (pr!=null) {
-                    Parameter param = ((Functional) pr.getDeclaration()).getParameterLists()
-                            .get(0).getParameters().get(pos);
+        PositionalArgumentList pal = that.getPositionalArgumentList();
+        if (pal==node) {
+            int pos = pal.getPositionalArguments().size();
+            ProducedReference pr = getTarget(that);
+            if (pr!=null) {
+                Parameter param = getParameters(pr).get(pos);
+                requiredType = pr.getTypedParameter(param).getType();
+                if (param.isSequenced()) {
+                    requiredType = that.getUnit().getElementType(requiredType);
+                }
+            }
+        }
+        NamedArgumentList nal = that.getNamedArgumentList();
+        if (nal==node || (nal!=null &&
+            nal.getSequencedArgument().getExpressionList()==node)) {
+            ProducedReference pr = getTarget(that);
+            if (pr!=null) {
+                List<Parameter> params = getParameters(pr);
+                Parameter param = params.get(params.size()-1);
+                if (param.isSequenced()) {
                     requiredType = pr.getTypedParameter(param).getType();
+                    requiredType = that.getUnit().getElementType(requiredType);
                 }
             }
         }
     }
+
+    private static ProducedReference getTarget(Tree.InvocationExpression that) {
+        Tree.Primary p = that.getPrimary();
+        if (p instanceof Tree.MemberOrTypeExpression) {
+            return ((Tree.MemberOrTypeExpression) p).getTarget();
+        }
+        else {
+            return null;
+        }
+    }
+    
+    private static List<Parameter> getParameters(ProducedReference pr) {
+        return ((Functional) pr.getDeclaration()).getParameterLists()
+                .get(0).getParameters();
+    }
+    
     @Override
     public void visit(Tree.SpecifiedArgument that) {
         super.visit(that);
@@ -38,6 +73,7 @@ class RequiredTypeVisitor extends Visitor {
             requiredType = that.getParameter().getType();
         }
     }
+    
     @Override
     public void visit(Tree.SpecifierStatement that) {
         super.visit(that);
@@ -45,6 +81,7 @@ class RequiredTypeVisitor extends Visitor {
             requiredType = that.getBaseMemberExpression().getTypeModel();
         }
     }
+    
     @Override
     public void visit(Tree.AttributeDeclaration that) {
         super.visit(that);
@@ -52,6 +89,7 @@ class RequiredTypeVisitor extends Visitor {
             requiredType = that.getType().getTypeModel();
         }
     }
+    
     @Override
     public void visit(Tree.AssignOp that) {
         super.visit(that);
@@ -59,6 +97,7 @@ class RequiredTypeVisitor extends Visitor {
             requiredType = that.getLeftTerm().getTypeModel();
         }
     }
+    
     @Override
     public void visit(Tree.Return that) {
         super.visit(that);
@@ -66,4 +105,5 @@ class RequiredTypeVisitor extends Visitor {
             requiredType = CeylonContentProposer.type(that.getDeclaration());
         }
     }
+    
 }
