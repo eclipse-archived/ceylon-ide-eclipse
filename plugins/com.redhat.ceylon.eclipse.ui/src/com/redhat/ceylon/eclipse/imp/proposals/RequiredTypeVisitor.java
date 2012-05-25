8d287660f03cd64239f8bf9cead8de34c6d023a9
Gavin King <gavin@hibernate.org>
2012-05-25 20:15:47 +0200

    make autocomplete much more sensitive to the needed type

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/proposals/RequiredTypeVisitor.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/proposals/RequiredTypeVisitor.java
@@ -6,48 +6,71 @@ import com.redhat.ceylon.compiler.typechecker.model.Functional;
 import com.redhat.ceylon.compiler.typechecker.model.Parameter;
 import com.redhat.ceylon.compiler.typechecker.model.ProducedReference;
 import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
+import com.redhat.ceylon.compiler.typechecker.tree.NaturalVisitor;
 import com.redhat.ceylon.compiler.typechecker.tree.Node;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.NamedArgumentList;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.PositionalArgumentList;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
 
-class RequiredTypeVisitor extends Visitor {
+class RequiredTypeVisitor extends Visitor 
+        implements NaturalVisitor {
+    
     private Node node;
-    ProducedType requiredType = null;
+    private ProducedType requiredType = null;
+    private ProducedType finalResult = null;
+    private ProducedReference namedArgTarget = null;
+    
+    ProducedType getType() {
+        return finalResult;
+    }
     
     RequiredTypeVisitor(Node node) {
         this.node = node;
     }
     
     @Override
+    public void visitAny(Node that) {
+        if (node==that) {
+            finalResult=requiredType;  
+        }
+        super.visitAny(that);
+    }
+    
+    @Override
     public void visit(Tree.InvocationExpression that) {
-        super.visit(that);
+        ProducedType ort = requiredType;
+        ProducedReference onat = namedArgTarget;
         PositionalArgumentList pal = that.getPositionalArgumentList();
-        if (pal==node) {
+        if (pal!=null) {
             int pos = pal.getPositionalArguments().size();
             ProducedReference pr = getTarget(that);
             if (pr!=null) {
-                Parameter param = getParameters(pr).get(pos);
-                requiredType = pr.getTypedParameter(param).getType();
-                if (param.isSequenced()) {
-                    requiredType = that.getUnit().getElementType(requiredType);
+                List<Parameter> params = getParameters(pr);
+                if (params.size()>pos) {
+                    Parameter param = params.get(pos);
+                    requiredType = pr.getTypedParameter(param).getType();
+                    if (param.isSequenced()) {
+                        requiredType = that.getUnit().getElementType(requiredType);
+                    }
                 }
             }
         }
         NamedArgumentList nal = that.getNamedArgumentList();
-        if (nal==node || (nal!=null && nal.getSequencedArgument()!=null &&
-                nal.getSequencedArgument().getExpressionList()==node)) {
-            ProducedReference pr = getTarget(that);
-            if (pr!=null) {
-                List<Parameter> params = getParameters(pr);
+        if (nal!=null) {
+            namedArgTarget = getTarget(that);
+            if (namedArgTarget!=null) {
+                List<Parameter> params = getParameters(namedArgTarget);
                 Parameter param = params.get(params.size()-1);
                 if (param.isSequenced()) {
-                    requiredType = pr.getTypedParameter(param).getType();
+                    requiredType = namedArgTarget.getTypedParameter(param).getType();
                     requiredType = that.getUnit().getElementType(requiredType);
                 }
             }
         }
+        super.visit(that);
+        requiredType = ort;
+        namedArgTarget = onat;
     }
 
     private static ProducedReference getTarget(Tree.InvocationExpression that) {
@@ -67,43 +90,50 @@ class RequiredTypeVisitor extends Visitor {
     
     @Override
     public void visit(Tree.SpecifiedArgument that) {
-        super.visit(that);
-        if (that.getSpecifierExpression()==node) {
-            //TODO: does not substitute type args!
-            requiredType = that.getParameter().getType();
+        ProducedType ort = requiredType;
+        Parameter p = that.getParameter();
+        if (p!=null) {
+            if (namedArgTarget!=null) {
+                requiredType = namedArgTarget.getTypedParameter(p).getType();
+            }
+            else {
+                requiredType = p.getType();            
+            }
         }
+        super.visit(that);
+        requiredType = ort;
     }
     
     @Override
     public void visit(Tree.SpecifierStatement that) {
+        ProducedType ort = requiredType;
+        requiredType = that.getBaseMemberExpression().getTypeModel();
         super.visit(that);
-        if (that.getSpecifierExpression()==node) {
-            requiredType = that.getBaseMemberExpression().getTypeModel();
-        }
+        requiredType = ort;
     }
     
     @Override
     public void visit(Tree.AttributeDeclaration that) {
+        ProducedType ort = requiredType;
+        requiredType = that.getType().getTypeModel();
         super.visit(that);
-        if (that.getSpecifierOrInitializerExpression()==node) {
-            requiredType = that.getType().getTypeModel();
-        }
+        requiredType = ort;
     }
     
     @Override
     public void visit(Tree.AssignOp that) {
+        ProducedType ort = requiredType;
+        requiredType = that.getLeftTerm().getTypeModel();
         super.visit(that);
-        if (that==node) {
-            requiredType = that.getLeftTerm().getTypeModel();
-        }
+        requiredType = ort;
     }
     
     @Override
     public void visit(Tree.Return that) {
+        ProducedType ort = requiredType;
+        requiredType = CeylonContentProposer.type(that.getDeclaration());
         super.visit(that);
-        if (that==node) {
-            requiredType = CeylonContentProposer.type(that.getDeclaration());
-        }
+        requiredType = ort;
     }
     
 }
