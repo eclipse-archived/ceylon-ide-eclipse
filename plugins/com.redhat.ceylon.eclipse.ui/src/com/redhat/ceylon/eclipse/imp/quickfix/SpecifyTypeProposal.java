dc8314f3f5b4bed9081f4a26f77ef4262cedf24a
Gavin King <gavin@hibernate.org>
2012-05-28 01:51:11 +0200

    cool HM-style type inference algorithm ;-)

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/quickfix/SpecifyTypeProposal.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/quickfix/SpecifyTypeProposal.java
@@ -1,5 +1,7 @@
 package com.redhat.ceylon.eclipse.imp.quickfix;
 
+import static com.redhat.ceylon.compiler.typechecker.model.Util.intersectionType;
+import static com.redhat.ceylon.compiler.typechecker.model.Util.unionType;
 import static com.redhat.ceylon.eclipse.imp.outline.CeylonLabelProvider.CORRECTION;
 
 import java.util.Collection;
@@ -12,10 +14,14 @@ import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.ltk.core.refactoring.TextFileChange;
 import org.eclipse.text.edits.ReplaceEdit;
 
+import com.redhat.ceylon.compiler.typechecker.model.BottomType;
 import com.redhat.ceylon.compiler.typechecker.model.Declaration;
 import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
+import com.redhat.ceylon.compiler.typechecker.model.TypedDeclaration;
+import com.redhat.ceylon.compiler.typechecker.model.UnknownType;
 import com.redhat.ceylon.compiler.typechecker.tree.Node;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
+import com.redhat.ceylon.compiler.typechecker.tree.Tree.Term;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
 import com.redhat.ceylon.eclipse.imp.editor.Util;
 
@@ -45,36 +51,113 @@ class SpecifyTypeProposal extends ChangeCorrectionProposal {
         class InferTypeVisitor extends Visitor {
             Declaration dec;
             ProducedType inferredType;
+            void intersect(ProducedType pt) {
+                if (pt!=null && !(pt.getDeclaration() instanceof UnknownType)) {
+                    if (inferredType==null) {
+                        inferredType = pt;
+                    }
+                    else {
+                        ProducedType it = intersectionType(inferredType, pt, type.getUnit());
+                        if (!(it.getDeclaration() instanceof BottomType)) {
+                            inferredType = it;
+                        }
+                    }
+                }
+            }
+            void union(ProducedType pt) {
+                if (pt!=null && !(pt.getDeclaration() instanceof UnknownType)) {
+                    if (inferredType==null) {
+                        inferredType = pt;
+                    }
+                    else {
+                        inferredType = unionType(inferredType, pt, type.getUnit());
+                    }
+                }
+            }
             @Override public void visit(Tree.TypedDeclaration that) {
-                super.visit(that);
                 if (that.getType()==type) {
                     dec = that.getDeclarationModel();
-                    inferredType = type.getTypeModel();
+                    union(that.getType().getTypeModel());
+                }
+                super.visit(that);
+            }
+            @Override public void visit(Tree.AttributeDeclaration that) {
+                super.visit(that);
+                Term term = that.getSpecifierOrInitializerExpression()==null ? 
+                        null : that.getSpecifierOrInitializerExpression().getExpression().getTerm();
+                if (term instanceof Tree.BaseMemberExpression) {
+                    if (((Tree.BaseMemberExpression) term).getDeclaration().equals(dec)) {
+                        intersect(that.getType().getTypeModel());
+                    }
                 }
             }
             @Override public void visit(Tree.SpecifierStatement that) {
                 super.visit(that);
                 Tree.Term bme = that.getBaseMemberExpression();
+                Term term = that.getSpecifierExpression()==null ? 
+                        null : that.getSpecifierExpression().getExpression().getTerm();
                 if (bme instanceof Tree.BaseMemberExpression) {
                 	if (((Tree.BaseMemberExpression) bme).getDeclaration().equals(dec)) {
-                		inferredType = that.getSpecifierExpression().getExpression().getTypeModel();
+                		if (term!=null) union(term.getTypeModel());
                 	}
+                } 
+                if (term instanceof Tree.BaseMemberExpression) {
+                    if (((Tree.BaseMemberExpression) term).getDeclaration().equals(dec)) {
+                        if (bme!=null) intersect(bme.getTypeModel());
+                    }
                 }
             }
             @Override public void visit(Tree.AssignmentOp that) {
                 super.visit(that);
-                if (that.getLeftTerm() instanceof Tree.BaseMemberExpression) {
-                    Tree.BaseMemberExpression bme = (Tree.BaseMemberExpression) that.getLeftTerm();
-                    if (bme.getDeclaration().equals(dec)) {
-                        //TODO: take a union if there are multiple assignments
-                        inferredType = that.getRightTerm().getTypeModel();
+                Term rt = that.getRightTerm();
+                Term lt = that.getLeftTerm();
+                if (lt instanceof Tree.BaseMemberExpression) {
+                    if (((Tree.BaseMemberExpression) lt).getDeclaration().equals(dec)) {
+                        if (rt!=null) union(rt.getTypeModel());
+                    }
+                }
+                if (rt instanceof Tree.BaseMemberExpression) {
+                    if (((Tree.BaseMemberExpression) rt).getDeclaration().equals(dec)) {
+                        if (lt!=null) intersect(lt.getTypeModel());
                     }
                 }
             }
+            @Override public void visit(Tree.PositionalArgument that) {
+                super.visit(that);
+                Tree.Term bme = that.getExpression().getTerm();
+                if (bme instanceof Tree.BaseMemberExpression) {
+                    if (((Tree.BaseMemberExpression) bme).getDeclaration().equals(dec)) {
+                        intersect(that.getParameter().getType());
+                    }
+                }
+            }
+            @Override public void visit(Tree.SpecifiedArgument that) {
+                super.visit(that);
+                Tree.Term bme = that.getSpecifierExpression().getExpression().getTerm();
+                if (bme instanceof Tree.BaseMemberExpression) {
+                    if (((Tree.BaseMemberExpression) bme).getDeclaration().equals(dec)) {
+                        intersect(that.getParameter().getType());
+                    }
+                }
+            }
+            @Override public void visit(Tree.Return that) {
+                super.visit(that);
+                Tree.Term bme = that.getExpression().getTerm();
+                if (bme instanceof Tree.BaseMemberExpression) {
+                    if (((Tree.BaseMemberExpression) bme).getDeclaration().equals(dec)) {
+                        Declaration d = that.getDeclaration();
+                        if (d instanceof TypedDeclaration) {
+                            intersect(((TypedDeclaration) d).getType());
+                        }
+                    }
+                }
+            }
+            //TODO: MethodDeclarations
         }
         InferTypeVisitor itv = new InferTypeVisitor();
         itv.visit(cu);
-        String explicitType = itv.inferredType.getProducedTypeName();
+        ProducedType it = itv.inferredType;
+        String explicitType = it==null ? "Object" : it.getProducedTypeName();
         TextFileChange change =  new TextFileChange("Specify Type", file);
         change.setEdit(new ReplaceEdit(problem.getOffset(), type.getText().length(), 
                 explicitType)); //Note: don't use problem.getLength() because it's wrong from the problem list
