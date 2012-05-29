b44ca501985c23401487b71eb5dd86eee227cc6b
Gavin King <gavin@hibernate.org>
2012-05-29 20:27:36 +0200

    more tests for autoeditstrategy

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/editor/CeylonAutoEditStrategy.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/editor/CeylonAutoEditStrategy.java
@@ -8,15 +8,12 @@ import static org.eclipse.ui.texteditor.AbstractDecoratedTextEditorPreferenceCon
 import org.antlr.runtime.CommonToken;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.preferences.IPreferencesService;
-import org.eclipse.imp.runtime.RuntimePlugin;
 import org.eclipse.imp.services.IAutoEditStrategy;
 import org.eclipse.jface.text.BadLocationException;
-import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.DocumentCommand;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.TextUtilities;
-import org.eclipse.swt.graphics.Point;
 import org.eclipse.ui.IEditorPart;
 
 import com.redhat.ceylon.compiler.typechecker.parser.CeylonLexer;
@@ -590,120 +587,6 @@ public class CeylonAutoEditStrategy implements IAutoEditStrategy {
         String[] delimiters = doc.getLegalLineDelimiters();
         if (delimiters != null) {
             return TextUtilities.endsWith(delimiters, text)!=-1;
-        }
-        return false;
-    }
-    
-    public static void main(String[] args) {
-        CeylonAutoEditStrategy instance = new CeylonAutoEditStrategy();
-        
-        Document doc = new Document("class Test()\n\t\textends Super(){\n\nvoid method(){\n\nfor (x in xs){}\n\n}\n\n}");
-        instance.doCorrectIndentation(doc);
-        assertResult(doc, "class Test()\n\t\textends Super(){\n\t\n\tvoid method(){\n\t\t\n\t\tfor (x in xs){}\n\t\t\n\t}\n\t\n}");
-        
-        doc = new Document("class Test()\n\t\textends Super(){\n\nvoid method(){}\n\n}");
-        instance.doCorrectIndentation(doc);
-        assertResult(doc, "class Test()\n\t\textends Super(){\n\t\n\tvoid method(){}\n\t\n}");
-        
-        doc = new Document("class Test()\n\t\textends Super(){\n\n\t\tvoid method(){}\n\t\n}");
-        instance.doCorrectIndentation(doc);
-        assertResult(doc, "class Test()\n\t\textends Super(){\n\t\n\tvoid method(){}\n\t\n}");
-        
-        doc = new Document("class Test()\n\t\textends Super(){\nvoid method(){}\n}");
-        instance.doCorrectIndentation(doc);
-        assertResult(doc, "class Test()\n\t\textends Super(){\n\tvoid method(){}\n}");
-        
-        doc = new Document("class Test()\n\t\textends Super(){\nvoid method(){\nfor (x in xs){}\n}\n}");
-        instance.doCorrectIndentation(doc);
-        assertResult(doc, "class Test()\n\t\textends Super(){\n\tvoid method(){\n\t\tfor (x in xs){}\n\t}\n}");
-        
-        doc = new Document("class Test()\n\t\textends Super(){\n\t\tvoid method(){}\n}");
-        instance.doCorrectIndentation(doc);
-        assertResult(doc, "class Test()\n\t\textends Super(){\n\tvoid method(){}\n}");
-        
-        doc = new Document("class Test()\nextends Super(){\n\nvoid method(){}\n\n}");
-        instance.doCorrectIndentation(doc);
-        assertResult(doc, "class Test()\nextends Super(){\n\t\n\tvoid method(){}\n\t\n}");
-        
-        doc = new Document("class Test() extends Super(){\n\nvoid method(){}\n\t\n}");
-        instance.doCorrectIndentation(doc);
-        assertResult(doc, "class Test() extends Super(){\n\t\n\tvoid method(){}\n\t\n}");
-        
-        doc = new Document("class Test()\n\t\textends Super()\n{\n\nvoid method(){}\n\t\n}");
-        instance.doCorrectIndentation(doc);
-        assertResult(doc, "class Test()\n\t\textends Super()\n{\n\t\n\tvoid method(){}\n\t\n}");
-        
-        //failing:
-        doc = new Document("\tclass Test()\n\t\textends Super(){\n\nvoid method(){\n}\n\t\n}");
-        instance.doCorrectIndentation(doc);
-        assertResult(doc, "\tclass Test()\n\t\t\textends Super(){\n\t\t\n\t\tvoid method(){\n\t\t}\n\t\t\n\t}");
-
-        doc = new Document("\tclass Test()\n\t\textends Super(){\nvoid method(){\n}\n}\n");
-        instance.doCorrectIndentation(doc);
-        assertResult(doc, "\tclass Test()\n\t\t\textends Super(){\n\t\tvoid method(){\n\t\t}\n\t}\n");
-        
-        doc = new Document("\tclass Test()\n\t\textends Super(){//foo\nvoid method(){//bar\n}//baz\n}\n");
-        instance.doCorrectIndentation(doc);
-        assertResult(doc, "\tclass Test()\n\t\t\textends Super(){//foo\n\t\tvoid method(){//bar\n\t\t}//baz\n\t}\n");
-        
-        /*doc = new Document("doc \"Hello\n     World\n     !\"\n void hello(){}");
-        instance.doCorrectIndentation(doc);
-        assertResult(doc, "doc \"Hello\n     World\n     !\"\n void hello(){}");*/
-    }
-
-    private static void assertResult(Document doc, String result) {
-        if (!doc.get().equals(result)) {
-            System.out.println(doc.get());
-        }
-    }
-    
-    private void doCorrectIndentation(IDocument doc) {
-        Point p = new Point(0, doc.getLength());
-        try {
-            final int selStart= p.x;
-            final int selLen= p.y;
-            final int selEnd= selStart + selLen;
-            final int startLine= doc.getLineOfOffset(selStart);
-            int endLine= doc.getLineOfOffset(selEnd);
-
-            // If the selection extends just to the beginning of the next line, don't indent that one too
-            if (selLen > 0 && lookingAtLineEnd(doc, selEnd)) {
-                endLine--;
-            }
-
-            // Indent each line using the AutoEditStrategy
-            for(int line= startLine; line <= endLine; line++) {
-                int lineStartOffset= doc.getLineOffset(line);
-
-                // Replace the existing indentation with the desired indentation.
-                // Use the language-specific AutoEditStrategy, which requires a DocumentCommand.
-                DocumentCommand cmd= new DocumentCommand() { };
-                cmd.offset= lineStartOffset;
-                cmd.length= 0;
-                cmd.text= Character.toString('\t');
-                cmd.doit= true;
-                cmd.shiftsCaret= false;
-//              boolean saveMode= fAutoEditStrategy.setFixMode(true);
-                customizeDocumentCommand(doc, cmd);
-//              fAutoEditStrategy.setFixMode(saveMode);
-                doc.replace(cmd.offset, cmd.length, cmd.text);
-            }
-        } catch (BadLocationException e) {
-            RuntimePlugin.getInstance().logException("Correct Indentation command failed", e);
-        }
-    }
-    
-    private boolean lookingAtLineEnd(IDocument doc, int pos) {
-        String[] legalLineTerms= doc.getLegalLineDelimiters();
-        try {
-            for(String lineTerm: legalLineTerms) {
-                int len= lineTerm.length();
-                if (pos > len && doc.get(pos - len, len).equals(lineTerm)) {
-                    return true;
-                }
-            }
-        } catch (BadLocationException e) {
-            //RuntimePlugin.getInstance().logException("Error examining document for line termination", e);
         }
         return false;
     }
