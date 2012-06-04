f19e1a171b288e0145d9bd826d25b8f43baab478
Gavin King <gavin@hibernate.org>
2012-06-04 23:53:46 +0200

    fox #265 Convert To Class refactoring

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/refactoring/ConvertToClassRefactoringAction.java
@@ -0,0 +1,27 @@
+package com.redhat.ceylon.eclipse.imp.refactoring;
+
+import org.eclipse.ltk.ui.refactoring.RefactoringWizard;
+import org.eclipse.ui.IEditorPart;
+
+public class ConvertToClassRefactoringAction extends AbstractRefactoringAction {
+	public ConvertToClassRefactoringAction(IEditorPart editor) {
+		super("ConvertToClass.", editor);
+		setActionDefinitionId("com.redhat.ceylon.eclipse.ui.action.convertToClass");
+	}
+	
+	@Override
+	public AbstractRefactoring createRefactoring() {
+	    return new ConvertToClassRefactoring(getTextEditor());
+	}
+	
+	@Override
+	public RefactoringWizard createWizard(AbstractRefactoring refactoring) {
+	    return new ConvertToClassWizard((ConvertToClassRefactoring) refactoring);
+	}
+	
+	@Override
+	String message() {
+	    return "No declaration name selected";
+	}
+	
+}
