6433c58094644f84400b09c1cd38bdc034df5807
Gavin King <gavin@hibernate.org>
2012-06-11 01:21:44 -0500

    simple workflow for convert to named arguments + hide from menus

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/refactoring/RefactorMenuItems.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/refactoring/RefactorMenuItems.java
@@ -11,7 +11,6 @@ import com.redhat.ceylon.eclipse.imp.editor.DynamicMenuItem;
 import com.redhat.ceylon.eclipse.imp.imports.CleanImportsHandler;
 import com.redhat.ceylon.eclipse.imp.refine.CreateSubtypeHandler;
 import com.redhat.ceylon.eclipse.imp.refine.MoveDeclarationHandler;
-import com.redhat.ceylon.eclipse.imp.refine.RefineFormalMembersHandler;
 
 public class RefactorMenuItems extends CompoundContributionItem {
     
@@ -47,13 +46,13 @@ public class RefactorMenuItems extends CompoundContributionItem {
                                     "Convert to Class...",
                                     editor!=null && new ConvertToClassRefactoringAction(editor).isEnabled()),
                     new Separator(),
-                    new DynamicMenuItem("com.redhat.ceylon.eclipse.ui.action.convertToNamedArguments", 
+                    /*new DynamicMenuItem("com.redhat.ceylon.eclipse.ui.action.convertToNamedArguments", 
                             "Convert to Named Arguments...",
-                            editor!=null && new ConvertToNamedArgumentsRefactoringAction(editor).isEnabled()),
-                    new Separator(),
-                    new DynamicMenuItem("com.redhat.ceylon.eclipse.ui.action.refineFormals", 
+                            editor!=null && new ConvertToNamedArgumentsHandler().isEnabled()),
+                    new Separator(),*/
+                    /*new DynamicMenuItem("com.redhat.ceylon.eclipse.ui.action.refineFormals", 
                             "Refine Formal Members",
-                            new RefineFormalMembersHandler().isEnabled()),
+                            new RefineFormalMembersHandler().isEnabled()),*/
                     new DynamicMenuItem("com.redhat.ceylon.eclipse.ui.action.createSubtype", 
                             "Create Subtype...",
                             new CreateSubtypeHandler().isEnabled()),
