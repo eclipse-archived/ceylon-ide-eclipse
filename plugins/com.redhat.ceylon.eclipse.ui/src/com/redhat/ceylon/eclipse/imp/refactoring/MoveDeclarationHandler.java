7b9c5c4dee518e2a0946e6a7e9e1170623c44dab
Gavin King <gavin@hibernate.org>
2012-06-16 13:47:15 -0400

    propagate imports when moving dec

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/refactoring/MoveDeclarationHandler.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/refactoring/MoveDeclarationHandler.java
@@ -20,6 +20,7 @@ import com.redhat.ceylon.compiler.typechecker.tree.Node;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.redhat.ceylon.eclipse.imp.editor.CeylonEditor;
 import com.redhat.ceylon.eclipse.imp.editor.Util;
+import com.redhat.ceylon.eclipse.imp.imports.CleanImportsHandler;
 import com.redhat.ceylon.eclipse.imp.wizard.NewUnitWizard;
 
 public class MoveDeclarationHandler extends AbstractHandler {
@@ -35,14 +36,15 @@ public class MoveDeclarationHandler extends AbstractHandler {
         if (cu==null) return;
         Node node = getSelectedNode(editor);
         if (node instanceof Tree.Declaration) {
-            String contents;
             try {
                 IDocument document = editor.getDocumentProvider()
                         .getDocument(editor.getEditorInput());
                 int start = node.getStartIndex();
                 int length = node.getStopIndex()-start+1;
-                contents = document.get(start, length);
-                boolean success = NewUnitWizard.open(contents, Util.getFile(editor.getEditorInput()), 
+                String contents = document.get(start, length);
+                String imports = CleanImportsHandler.imports(node, cu.getImportList());
+                boolean success = NewUnitWizard.open(imports + "\n" + contents, 
+                        Util.getFile(editor.getEditorInput()), 
                         ((Tree.Declaration) node).getIdentifier().getText(), "Move to New Unit", 
                         "Create a new Ceylon compilation unit containing the selected declaration.");
                 if (success) {
