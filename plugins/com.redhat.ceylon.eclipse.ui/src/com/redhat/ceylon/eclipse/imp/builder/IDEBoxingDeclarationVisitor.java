7eda196db44dd438b95b07ee80db9029ac0eb452
Stephane Epardaud <stef@epardaud.fr>
2012-06-21 01:30:00 +0200

    Added boxing declaration visitors to be able to set boxing like the compiler
    This should be used on the language module after typechecking, when the
    experimental builder is enabled back, to get a complete language module
    model

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/builder/IDEBoxingDeclarationVisitor.java
@@ -0,0 +1,34 @@
+package com.redhat.ceylon.eclipse.imp.builder;
+
+import com.redhat.ceylon.compiler.java.codegen.BoxingDeclarationVisitor;
+import com.redhat.ceylon.compiler.typechecker.model.BottomType;
+import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
+
+public class IDEBoxingDeclarationVisitor extends BoxingDeclarationVisitor {
+
+	@Override
+	protected boolean isCeylonBasicType(ProducedType type) {
+		return (isCeylonString(type) || isCeylonBoolean(type) || isCeylonInteger(type) || isCeylonFloat(type) || isCeylonCharacter(type));
+	}
+
+	private boolean isCeylonBoolean(ProducedType type) {
+		return type.isSubtypeOf(type.getDeclaration().getUnit().getBooleanDeclaration().getType())
+				&& !(type.getDeclaration() instanceof BottomType);
+	}
+
+	private boolean isCeylonString(ProducedType type) {
+		return type.getDeclaration().getUnit().getStringDeclaration() == type.getDeclaration();
+	}
+
+	private boolean isCeylonInteger(ProducedType type) {
+		return type.getDeclaration().getUnit().getIntegerDeclaration() == type.getDeclaration();
+	}
+
+	private boolean isCeylonFloat(ProducedType type) {
+		return type.getDeclaration().getUnit().getFloatDeclaration() == type.getDeclaration();
+	}
+
+	private boolean isCeylonCharacter(ProducedType type) {
+		return type.getDeclaration().getUnit().getCharacterDeclaration() == type.getDeclaration();
+	}
+}
