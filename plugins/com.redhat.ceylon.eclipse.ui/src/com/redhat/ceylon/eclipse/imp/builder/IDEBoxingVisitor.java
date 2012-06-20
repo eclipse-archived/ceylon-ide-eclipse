af9128e94e1a9466f3163ac7e4469dcc7c473a71
Stephane Epardaud <stef@epardaud.fr>
2012-06-21 01:30:00 +0200

    Added boxing declaration visitors to be able to set boxing like the compiler
    This should be used on the language module after typechecking, when the
    experimental builder is enabled back, to get a complete language module
    model

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/builder/IDEBoxingVisitor.java
@@ -0,0 +1,18 @@
+package com.redhat.ceylon.eclipse.imp.builder;
+
+import com.redhat.ceylon.compiler.java.codegen.BoxingVisitor;
+import com.redhat.ceylon.compiler.typechecker.model.Declaration;
+
+public class IDEBoxingVisitor extends BoxingVisitor {
+
+	@Override
+	protected boolean isBooleanTrue(Declaration decl) {
+		return decl.getUnit().getLanguageModuleDeclaration("true") == decl;
+	}
+
+	@Override
+	protected boolean isBooleanFalse(Declaration decl) {
+		return decl.getUnit().getLanguageModuleDeclaration("false") == decl;
+	}
+
+}
