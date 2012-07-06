7e7a62bfcdad4caab0de3bf2e0771b13fe1bbd86
Gavin King <gavin@hibernate.org>
2012-07-06 03:01:36 -0500

    initial experimentation with UI for setting output paths

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/wizard/NewProjectWizard.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/imp/wizard/NewProjectWizard.java
@@ -3,12 +3,6 @@ package com.redhat.ceylon.eclipse.imp.wizard;
 import static com.redhat.ceylon.compiler.typechecker.TypeChecker.LANGUAGE_MODULE_VERSION;
 import static com.redhat.ceylon.eclipse.ui.ICeylonResources.CEYLON_NEW_FILE;
 import static org.eclipse.jdt.launching.JavaRuntime.JRE_CONTAINER;
-import static org.eclipse.jface.dialogs.IDialogConstants.HORIZONTAL_MARGIN;
-import static org.eclipse.jface.dialogs.IDialogConstants.HORIZONTAL_SPACING;
-import static org.eclipse.jface.dialogs.IDialogConstants.VERTICAL_MARGIN;
-import static org.eclipse.jface.dialogs.IDialogConstants.VERTICAL_SPACING;
-import static org.eclipse.swt.layout.GridData.FILL_HORIZONTAL;
-import static org.eclipse.swt.layout.GridData.HORIZONTAL_ALIGN_FILL;
 
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
@@ -23,7 +17,6 @@ import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.jdt.internal.ui.util.ExceptionHandler;
 import org.eclipse.jdt.internal.ui.wizards.NewElementWizard;
@@ -33,8 +26,6 @@ import org.eclipse.jdt.launching.IVMInstall2;
 import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.jdt.ui.IPackagesViewPart;
 import org.eclipse.jdt.ui.actions.ShowInPackageViewAction;
-import org.eclipse.jdt.ui.wizards.NewJavaProjectWizardPageOne;
-import org.eclipse.jdt.ui.wizards.NewJavaProjectWizardPageTwo;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
@@ -44,7 +35,6 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
@@ -64,8 +54,8 @@ import com.redhat.ceylon.eclipse.ui.CeylonPlugin;
 
 public class NewProjectWizard extends NewElementWizard implements IExecutableExtension {
 
-    private NewJavaProjectWizardPageOne fFirstPage;
-    private NewJavaProjectWizardPageTwo fSecondPage;
+    private NewCeylonProjectWizardPageOne fFirstPage;
+    private NewCeylonProjectWizardPageTwo fSecondPage;
 
     private IConfigurationElement fConfigElement;
 
@@ -78,7 +68,7 @@ public class NewProjectWizard extends NewElementWizard implements IExecutableExt
         this(null, null);
     }
 
-    public NewProjectWizard(NewJavaProjectWizardPageOne pageOne, NewJavaProjectWizardPageTwo pageTwo) {
+    public NewProjectWizard(NewCeylonProjectWizardPageOne pageOne, NewCeylonProjectWizardPageTwo pageTwo) {
         setDefaultPageImageDescriptor(CeylonPlugin.getInstance()
                 .getImageRegistry().getDescriptor(CEYLON_NEW_FILE));
         setDialogSettings(JavaPlugin.getDefault().getDialogSettings());
@@ -88,72 +78,21 @@ public class NewProjectWizard extends NewElementWizard implements IExecutableExt
     }
 
     public void addPages() {
-        if (fFirstPage == null)
-            fFirstPage= new NewJavaProjectWizardPageOne() {
-            private GridLayout initGridLayout(GridLayout layout, boolean margins) {
-                layout.horizontalSpacing= convertHorizontalDLUsToPixels(HORIZONTAL_SPACING);
-                layout.verticalSpacing= convertVerticalDLUsToPixels(VERTICAL_SPACING);
-                if (margins) {
-                    layout.marginWidth= convertHorizontalDLUsToPixels(HORIZONTAL_MARGIN);
-                    layout.marginHeight= convertVerticalDLUsToPixels(VERTICAL_MARGIN);
-                } else {
-                    layout.marginWidth= 0;
-                    layout.marginHeight= 0;
-                }
-                return layout;
-            }
-            public void createControl(Composite parent) {
-                initializeDialogUnits(parent);
-
-                final Composite composite= new Composite(parent, SWT.NULL);
-                composite.setFont(parent.getFont());
-                composite.setLayout(initGridLayout(new GridLayout(1, false), true));
-                composite.setLayoutData(new GridData(HORIZONTAL_ALIGN_FILL));
-
-                // create UI elements
-                Control nameControl= createNameControl(composite);
-                nameControl.setLayoutData(new GridData(FILL_HORIZONTAL));
-
-                Control locationControl= createLocationControl(composite);
-                locationControl.setLayoutData(new GridData(FILL_HORIZONTAL));
-
-                addSelectRepo(composite);
-
-                Control jreControl= createJRESelectionControl(composite);
-                jreControl.setLayoutData(new GridData(FILL_HORIZONTAL));
-
-                /*Control layoutControl= createProjectLayoutControl(composite);
-                layoutControl.setLayoutData(new GridData(FILL_HORIZONTAL));*/
-
-                Control workingSetControl= createWorkingSetControl(composite);
-                workingSetControl.setLayoutData(new GridData(FILL_HORIZONTAL));
-
-                Control infoControl= createInfoControl(composite);
-                infoControl.setLayoutData(new GridData(FILL_HORIZONTAL));
-                
-                setControl(composite);
-            }
-            @Override
-            public IClasspathEntry[] getSourceClasspathEntries() {
-                IClasspathEntry[] entries = super.getSourceClasspathEntries();
-                entries[0] = JavaCore.newSourceEntry(entries[0].getPath()
-                        .removeLastSegments(1).append("source"));
-                return entries;
-            }
-        };
+        if (fFirstPage == null) {
+            fFirstPage= new NewCeylonProjectWizardPageOne() { 
+	        	@Override
+	            void addSelectRepo(Composite parent) {
+	            	NewProjectWizard.this.addSelectRepo(parent);
+	            }
+        	};
+        }
         fFirstPage.setTitle("New Ceylon Project");
         fFirstPage.setDescription("Create a Ceylon project in the workspace or in an external location.");
         addPage(fFirstPage);
 
-        if (fSecondPage == null)
-            fSecondPage= new NewJavaProjectWizardPageTwo(fFirstPage) {
-            @Override
-            public void init(IJavaProject jproject, IPath outputLocation,
-                    IClasspathEntry[] entries, boolean overrideExisting) {
-                super.init(jproject, outputLocation.removeLastSegments(1).append("modules"), 
-                        entries, overrideExisting);
-            }
-        };
+        if (fSecondPage == null) {
+            fSecondPage= new NewCeylonProjectWizardPageTwo(fFirstPage);
+        }
         fSecondPage.setTitle("Ceylon Project Settings");
         fSecondPage.setDescription("Define the Ceylon build settings.");
         addPage(fSecondPage);
@@ -317,11 +256,10 @@ public class NewProjectWizard extends NewElementWizard implements IExecutableExt
         
         boolean res= super.performFinish();
         if (res) {
-            final IJavaElement newElement= getCreatedElement();
-
             IWorkingSet[] workingSets= fFirstPage.getWorkingSets();
             if (workingSets.length > 0) {
-                PlatformUI.getWorkbench().getWorkingSetManager().addToWorkingSets(newElement, workingSets);
+                PlatformUI.getWorkbench().getWorkingSetManager()
+                        .addToWorkingSets(getCreatedElement(), workingSets);
             }
 
             BasicNewProjectResourceWizard.updatePerspective(fConfigElement);
@@ -357,7 +295,7 @@ public class NewProjectWizard extends NewElementWizard implements IExecutableExt
                 public void run() {
                     IWorkbenchPart activePart= getActivePart();
                     if (activePart instanceof IPackagesViewPart) {
-                        (new ShowInPackageViewAction(activePart.getSite())).run(newElement);
+                        (new ShowInPackageViewAction(activePart.getSite())).run(getCreatedElement());
                     }
                 }
             });
