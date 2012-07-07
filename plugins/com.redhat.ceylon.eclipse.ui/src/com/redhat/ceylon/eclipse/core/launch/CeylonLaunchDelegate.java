c1431d3d36ea796dd79a1782899191d269bfad94
Gavin King <gavin@hibernate.org>
2012-07-07 18:34:41 -0500

    use classpath container when launching (only add project-local cars)

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/launch/CeylonLaunchDelegate.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/launch/CeylonLaunchDelegate.java
@@ -1,98 +1,89 @@
 package com.redhat.ceylon.eclipse.core.launch;
 
-import static com.redhat.ceylon.eclipse.core.builder.CeylonBuilder.getCeylonModulesOutputDirectory;
+import static com.redhat.ceylon.eclipse.core.builder.CeylonBuilder.PROBLEM_MARKER_ID;
+import static com.redhat.ceylon.eclipse.core.builder.CeylonBuilder.getCeylonModulesOutputFolder;
+import static com.redhat.ceylon.eclipse.core.builder.CeylonBuilder.getProjectTypeChecker;
+import static java.util.Arrays.asList;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.launching.JavaLaunchDelegate;
 
 import com.redhat.ceylon.cmr.api.ArtifactContext;
 import com.redhat.ceylon.cmr.api.RepositoryManager;
-import com.redhat.ceylon.cmr.impl.FileContentStore;
-import com.redhat.ceylon.cmr.impl.SimpleRepositoryManager;
-import com.redhat.ceylon.compiler.typechecker.TypeChecker;
 import com.redhat.ceylon.compiler.typechecker.context.Context;
 import com.redhat.ceylon.compiler.typechecker.model.Module;
 import com.redhat.ceylon.compiler.typechecker.model.Modules;
-import com.redhat.ceylon.eclipse.core.builder.CeylonBuilder;
-import com.redhat.ceylon.eclipse.util.EclipseLogger;
 
 public class CeylonLaunchDelegate extends JavaLaunchDelegate {
 
     @Override
     public String[] getClasspath(ILaunchConfiguration configuration)
             throws CoreException {
-        String[] javaClasspath = super.getClasspath(configuration);
+
+    	String[] javaClasspath = super.getClasspath(configuration);
+        final List<String> classpathList = new ArrayList<String>(asList(javaClasspath));
         
-        // Also add the car files of the output directory
+        //add the car files of the output directory
+        
         IJavaProject javaProject = getJavaProject(configuration);
-        final List<String> classpathList = new ArrayList<String>(Arrays.asList(javaClasspath));
+        Context context = getProjectTypeChecker(javaProject.getProject()).getContext();
 
-        TypeChecker typeChecker = CeylonBuilder.getProjectTypeChecker(javaProject.getProject());
+        IPath modulesFolder = getCeylonModulesOutputFolder(javaProject).getLocation();
+        classpathList.add(modulesFolder.append("default").append("default.car").toOSString());
 
-        Context context = typeChecker.getContext();
         Modules projectModules = context.getModules();
-
         Set<Module> modulesToAdd = new HashSet<Module>();
         modulesToAdd.add(projectModules.getLanguageModule());
-        for (Module module : projectModules.getListOfModules()) {
-            if (!module.equals(projectModules.getDefaultModule())) {
+        for (Module module: projectModules.getListOfModules()) {
+            if (!module.equals(projectModules.getDefaultModule()) && 
+            		!module.getNameAsString().equals("java")) {
                 modulesToAdd.add(module); 
             }
         }
         
-        List<RepositoryManager> repositoryManagers = new ArrayList<RepositoryManager>();
-        
-        File outputDirectory = getCeylonModulesOutputDirectory(javaProject);
-        if (outputDirectory != null) {
-        	RepositoryManager outputRepo = new SimpleRepositoryManager(new FileContentStore(outputDirectory), 
-        	        new EclipseLogger());
-            repositoryManagers.add(outputRepo);
-        }
-        
-        classpathList.add(new File(new File(outputDirectory, "default"), "default.car").getAbsolutePath());
-
-        repositoryManagers.add(context.getRepositoryManager());
-        
-        for (Module module : modulesToAdd) {
+    	IPath rootLocation = javaProject.getProject().getWorkspace().getRoot().getLocation();
+        RepositoryManager provider = context.getRepositoryManager();
+    	for (Module module: modulesToAdd) {
             boolean artifactFound = false;
             ArtifactContext ctx = new ArtifactContext(module.getNameAsString(), module.getVersion());
-            for (RepositoryManager provider : repositoryManagers) {
-            	// try first with car
-            	ctx.setSuffix(ArtifactContext.CAR);
-                File moduleArtifact = null;
-				moduleArtifact = provider.getArtifact(ctx);
-                if (moduleArtifact == null){
-                	// try with .jar
-                	ctx.setSuffix(ArtifactContext.JAR);
-					moduleArtifact = provider.getArtifact(ctx);
-                }
-                if (moduleArtifact != null) {
-                    String modulePath = moduleArtifact.getPath();
-                    File moduleFile = new File(modulePath);
-                    if (moduleFile.exists()) {
-                        classpathList.add(moduleFile.getAbsolutePath());
-                        artifactFound = true;
-                        break;
-                    } 
-                    else {
-                        System.out.println("Ignoring non-existing module artifact (" + modulePath + 
-                                ") for launching classpath");
-                    }
-                }
+            // try first with car
+            ctx.setSuffix(ArtifactContext.CAR);
+            File moduleArtifact = null;
+            moduleArtifact = provider.getArtifact(ctx);
+            if (moduleArtifact == null){
+            	// try with .jar
+            	ctx.setSuffix(ArtifactContext.JAR);
+            	moduleArtifact = provider.getArtifact(ctx);
+            }
+            if (moduleArtifact != null) {
+            	String modulePath = moduleArtifact.getPath();
+            	File moduleFile = new File(modulePath);
+            	if (moduleFile.exists()) {
+            		artifactFound = true;
+            		IPath moduleWorkspacePath = new Path(modulePath).makeRelativeTo(rootLocation);
+					if (javaProject.getPath().isPrefixOf(moduleWorkspacePath)) {
+            			classpathList.add(moduleFile.getAbsolutePath());
+            		}
+            	} 
+            	else {
+            		System.err.println("ignoring nonexistent module artifact for launch classpath: " + 
+            				modulePath);
+            	}
             }
             if (!artifactFound) {
-                System.out.println("Artifact not found for module '" + module.getNameAsString() + 
-                        "/" + module.getVersion() + "' for launching classpath");
+                System.err.println("no module archive found for launch classpath: " + 
+                        module.getNameAsString() + "/" + module.getVersion());
             }
         }
 
@@ -105,14 +96,13 @@ public class CeylonLaunchDelegate extends JavaLaunchDelegate {
         if (super.isLaunchProblem(problemMarker)) {
             return true; 
         }
-        if (! problemMarker.getType().equals(CeylonBuilder.PROBLEM_MARKER_ID)) {
+        if (!problemMarker.getType().equals(PROBLEM_MARKER_ID)) {
             return false;
         }
-        Integer severity = (Integer)problemMarker.getAttribute(IMarker.SEVERITY);
-        if (severity != null) {
-            return severity.intValue() >= IMarker.SEVERITY_ERROR;
-        } 
-        
+        Integer severity = (Integer) problemMarker.getAttribute(IMarker.SEVERITY);
+        if (severity!=null) {
+            return severity.intValue()>=IMarker.SEVERITY_ERROR;
+        }
         return false;
     }
 }
