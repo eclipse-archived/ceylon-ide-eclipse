d8e022e38957e485c82def3cb4d990ce9680e4d9
Gavin King <gavin@hibernate.org>
2012-07-21 09:22:18 -0500

    settable color for outline+hierarchy popups

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/outline/Popup.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/outline/Popup.java
@@ -67,6 +67,7 @@ import com.redhat.ceylon.compiler.typechecker.tree.Tree.Identifier;
 import com.redhat.ceylon.eclipse.code.editor.CeylonEditor;
 import com.redhat.ceylon.eclipse.code.editor.Util;
 import com.redhat.ceylon.eclipse.code.parse.CeylonParseController;
+import com.redhat.ceylon.eclipse.code.parse.CeylonTokenColorer;
 import com.redhat.ceylon.eclipse.ui.CeylonPlugin;
 
 /**
@@ -196,6 +197,10 @@ public abstract class Popup extends PopupDialog
 
 		// Create all controls early to preserve the life cycle of the original implementation.
 		create();
+		
+		Color popup = CeylonTokenColorer.getCurrentThemeColor("popup");
+		getShell().setBackground(popup);
+		setBackgroundColor(popup);
 
 		// Status field text can only be computed after widgets are created.
 		setInfoText(getStatusFieldText());
