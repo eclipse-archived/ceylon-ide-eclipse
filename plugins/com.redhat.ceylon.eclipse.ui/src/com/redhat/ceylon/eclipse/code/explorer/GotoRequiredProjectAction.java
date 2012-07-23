09f7b386432bf2e2618c734f4ead66c30306d3c2
Gavin King <gavin@hibernate.org>
2012-07-22 20:57:27 -0500

    Add Ceylon Explorer by huge copy/paste from JDT

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/explorer/GotoRequiredProjectAction.java
@@ -0,0 +1,31 @@
+package com.redhat.ceylon.eclipse.code.explorer;
+
+import org.eclipse.jdt.internal.ui.packageview.ClassPathContainer;
+import org.eclipse.jdt.internal.ui.packageview.PackagesMessages;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.viewers.IStructuredSelection;
+
+/**
+ * Goto to the referenced required project
+ */
+class GotoRequiredProjectAction extends Action {
+
+	private PackageExplorerPart fPackageExplorer;
+
+	GotoRequiredProjectAction(PackageExplorerPart part) {
+		super(PackagesMessages.GotoRequiredProjectAction_label);
+		setDescription(PackagesMessages.GotoRequiredProjectAction_description);
+		setToolTipText(PackagesMessages.GotoRequiredProjectAction_tooltip);
+		fPackageExplorer= part;
+	}
+
+	@Override
+	public void run() {
+		IStructuredSelection selection= (IStructuredSelection)fPackageExplorer.getSite().getSelectionProvider().getSelection();
+		Object element= selection.getFirstElement();
+		if (element instanceof ClassPathContainer.RequiredProjectWrapper) {
+			ClassPathContainer.RequiredProjectWrapper wrapper= (ClassPathContainer.RequiredProjectWrapper) element;
+			fPackageExplorer.tryToReveal(wrapper.getProject());
+		}
+	}
+}
---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/explorer/LayoutAction.java
@@ -0,0 +1,39 @@
+package com.redhat.ceylon.eclipse.code.explorer;
+
+import org.eclipse.jdt.internal.ui.IJavaHelpContextIds;
+import org.eclipse.jdt.internal.ui.JavaPluginImages;
+import org.eclipse.jdt.internal.ui.packageview.PackagesMessages;
+import org.eclipse.jface.action.Action;
+import org.eclipse.ui.PlatformUI;
+
+class LayoutAction extends Action {
+
+	private boolean fIsFlatLayout;
+	private PackageExplorerPart fPackageExplorer;
+
+	public LayoutAction(PackageExplorerPart packageExplorer, boolean flat) {
+		super("", AS_RADIO_BUTTON); //$NON-NLS-1$
+
+		fIsFlatLayout= flat;
+		fPackageExplorer= packageExplorer;
+		if (fIsFlatLayout) {
+			setText(PackagesMessages.LayoutActionGroup_flatLayoutAction_label);
+			JavaPluginImages.setLocalImageDescriptors(this, "flatLayout.gif"); //$NON-NLS-1$
+			PlatformUI.getWorkbench().getHelpSystem().setHelp(this, IJavaHelpContextIds.LAYOUT_FLAT_ACTION);
+		} else {
+			setText(PackagesMessages.LayoutActionGroup_hierarchicalLayoutAction_label);
+			JavaPluginImages.setLocalImageDescriptors(this, "hierarchicalLayout.gif"); //$NON-NLS-1$
+			PlatformUI.getWorkbench().getHelpSystem().setHelp(this, IJavaHelpContextIds.LAYOUT_HIERARCHICAL_ACTION);
+		}
+		setChecked(packageExplorer.isFlatLayout() == fIsFlatLayout);
+	}
+
+	/*
+	 * @see org.eclipse.jface.action.IAction#run()
+	 */
+	@Override
+	public void run() {
+		if (fPackageExplorer.isFlatLayout() != fIsFlatLayout)
+			fPackageExplorer.setFlatLayout(fIsFlatLayout);
+	}
+}
