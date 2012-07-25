186bbaa215dacefbdb073c2916e86b71f641b834
Gavin King <gavin@hibernate.org>
2012-07-24 19:23:33 -0500

    improve algorithm for finding refined dec

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/EditorAnnotationService.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/EditorAnnotationService.java
@@ -94,21 +94,42 @@ public class EditorAnnotationService implements TreeLifecycleListener {
             return null;
         }
         else {
-            //TODO: improve this:
-            ClassOrInterface etd = ((ClassOrInterface) dec.getContainer())
-            		.getExtendedTypeDeclaration();
+            //TODO: this algorithm is a bit arbitrary
+        	//first get the superclass of the declaring class 
+            ClassOrInterface td = (ClassOrInterface) dec.getContainer();
+			ClassOrInterface etd = td.getExtendedTypeDeclaration();
             if (etd==null) {
                 return null;
             }
             else {
+            	//look for a member declared or inherited by 
+            	//the superclass
                 Declaration refined = etd.getMember(dec.getName(), null); //TODO: pass signature?
                 if (refined==null) {
-                    //this is the case where the declaration
-                    //refines a concrete member of an interface
-                    refined = dec.getRefinedDeclaration();
-                    if (refined!=null && refined.equals(dec)) {
-                        refined = null;
-                    }
+                    //then the declaration might refine a
+                    //member of a satisfied interface
+                	List<Declaration> allRefined = td.getInheritedMembers(dec.getName());
+                	if (allRefined.isEmpty()) {
+                		//the declaration does not refine
+                		//anything
+                		return null;
+                	}
+                	else if (allRefined.size()==1) {
+                		//the declaration refines exactly one
+                		//member of a satisfied interface 
+                		return allRefined.get(0);
+                	}
+                	else {
+                		//the declaration directly refines two 
+                		//different supertype members, so lets
+                		//just return the topmost refined
+                		//declaration, because at least we know
+                		//it is something unique!
+                		refined = dec.getRefinedDeclaration();
+                		if (refined!=null && refined.equals(dec)) {
+                			refined = null;
+                		}
+                	}
                 }
                 return refined;
             }
