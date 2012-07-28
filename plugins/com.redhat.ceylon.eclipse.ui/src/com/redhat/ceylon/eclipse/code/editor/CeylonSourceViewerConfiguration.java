5bce8e46427a47e2f8382f864f632786f0933dd0
Gavin King <gavin@hibernate.org>
2012-07-28 14:46:45 -0500

    make popups look more 3-D

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/CeylonSourceViewerConfiguration.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/CeylonSourceViewerConfiguration.java
@@ -250,7 +250,7 @@ public class CeylonSourceViewerConfiguration extends TextSourceViewerConfigurati
 			@Override
 			public IInformationControl createInformationControl(Shell parent) {
 				return new OutlinePopup(editor, parent, 
-						SWT.RESIZE, SWT.V_SCROLL | SWT.H_SCROLL);
+						SWT.RESIZE, SWT.V_SCROLL | SWT.H_SCROLL | SWT.BORDER);
 			}
 		});
         presenter.setDocumentPartitioning(getConfiguredDocumentPartitioning(sourceViewer));
@@ -300,7 +300,7 @@ public class CeylonSourceViewerConfiguration extends TextSourceViewerConfigurati
 			@Override
 			public IInformationControl createInformationControl(Shell parent) {
 				return new HierarchyPopup(editor, parent, 
-						SWT.RESIZE, SWT.V_SCROLL | SWT.H_SCROLL);
+						SWT.RESIZE, SWT.V_SCROLL | SWT.H_SCROLL | SWT.BORDER);
 			}
 		});
         presenter.setDocumentPartitioning(getConfiguredDocumentPartitioning(sourceViewer));
