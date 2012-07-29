0de18eff68088b0593db75053cba05da55f15165
Gavin King <gavin@hibernate.org>
2012-07-29 16:50:08 -0500

    auto-add imports in Create Subtype/Instance proposals for #146

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/quickfix/CreateObjectProposal.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/quickfix/CreateObjectProposal.java
@@ -1,11 +1,14 @@
 package com.redhat.ceylon.eclipse.code.quickfix;
 
 import static com.redhat.ceylon.eclipse.code.outline.CeylonLabelProvider.CORRECTION;
+import static com.redhat.ceylon.eclipse.code.quickfix.CeylonQuickFixAssistant.getIndent;
+import static com.redhat.ceylon.eclipse.code.quickfix.CeylonQuickFixAssistant.importType;
+import static com.redhat.ceylon.eclipse.code.quickfix.CreateSubtypeProposal.subtypeDeclaration;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 import org.eclipse.core.resources.IFile;
-
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.ltk.core.refactoring.DocumentChange;
@@ -14,12 +17,14 @@ import org.eclipse.text.edits.InsertEdit;
 import org.eclipse.text.edits.MultiTextEdit;
 
 import com.redhat.ceylon.compiler.typechecker.model.ClassOrInterface;
+import com.redhat.ceylon.compiler.typechecker.model.Declaration;
 import com.redhat.ceylon.compiler.typechecker.model.IntersectionType;
 import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
 import com.redhat.ceylon.compiler.typechecker.tree.Node;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.Statement;
 import com.redhat.ceylon.eclipse.code.editor.Util;
+import com.redhat.ceylon.eclipse.code.quickfix.CreateSubtypeProposal.CreateSubtype;
 import com.redhat.ceylon.eclipse.util.FindStatementVisitor;
 
 class CreateObjectProposal extends ChangeCorrectionProposal {
@@ -58,13 +63,19 @@ class CreateObjectProposal extends ChangeCorrectionProposal {
                 TextChange change = new DocumentChange("Create Object", doc);
                 change.setEdit(new MultiTextEdit());
                 Integer offset = s.getStartIndex();
-                String name = type.getDeclaration().getName().replace("&", "").replace("<", "").replace(">", "");
-                String dec = CreateSubtypeProposal.subtypeDeclaration(type, true)
-                        .replace("$className", "my" + name) + "\n";
-                dec = dec.replaceAll("\n", "\n" + CeylonQuickFixAssistant.getIndent(node, doc));
+                String name = type.getDeclaration().getName().replace("&", "")
+                		.replace("<", "").replace(">", "");
+                CreateSubtype cs = subtypeDeclaration(type, true);
+                int shift=0;
+            	HashSet<Declaration> already = new HashSet<Declaration>();
+                for (ProducedType pt: cs.getImportedTypes()) {
+                	shift+=importType(change, pt, cu, already);
+                }
+				String dec = cs.getDefinition().replace("$className", "my" + name) + "\n";
+                dec = dec.replaceAll("\n", "\n" + getIndent(node, doc));
                 change.addEdit(new InsertEdit(offset,dec));
                 proposals.add(new CreateObjectProposal(type, 
-                        offset+7, name.length()+2, file, change));
+                        offset+7+shift, name.length()+2, file, change));
             }
         }
     }
