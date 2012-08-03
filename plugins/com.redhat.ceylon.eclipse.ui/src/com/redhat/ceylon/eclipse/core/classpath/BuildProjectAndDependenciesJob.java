74eb78549ad1db141ee5c1c6a04290a633fdc7e9
Gavin King <gavin@hibernate.org>
2012-08-03 02:06:47 -0500

    schedule initial builds via the classpath initializer (attempt to fix a bug where sometimes the initial full build never ran)

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/classpath/BuildProjectAndDependenciesJob.java
@@ -0,0 +1,50 @@
+package com.redhat.ceylon.eclipse.core.classpath;
+
+import static org.eclipse.core.resources.IncrementalProjectBuilder.FULL_BUILD;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IBuildConfiguration;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+
+public class BuildProjectAndDependenciesJob extends Job {
+	protected final IProject project;
+
+	public BuildProjectAndDependenciesJob(String name, IProject project) {
+		super(name);
+		this.project = project;
+	}
+
+	@Override
+	protected IStatus run(IProgressMonitor monitor) {
+		if (reallyRun()) {
+			try {
+				List<IBuildConfiguration> configs = new ArrayList<IBuildConfiguration>();
+				configs.add(project.getBuildConfig(IBuildConfiguration.DEFAULT_CONFIG_NAME));
+				for (IProject p: project.getReferencingProjects()) {
+					if (p.isOpen()) {
+						configs.add(p.getBuildConfig(IBuildConfiguration.DEFAULT_CONFIG_NAME));
+						//    	            					project.getWorkspace().build(FULL_BUILD, monitor);
+						//    	            					break;
+					} 
+				}
+				project.getWorkspace().build(configs.toArray(new IBuildConfiguration[1]), 
+						FULL_BUILD, false, monitor);    	            			
+			}
+			catch (CoreException e) {
+				e.printStackTrace();
+			}
+		}
+		return Status.OK_STATUS;
+	}
+
+	protected boolean reallyRun() {
+		return true;
+	}
+}
