f2d55e63f051c850b1293d3caecd0e58e4398e9a
Max Rydahl Andersen <max@jboss.org>
2012-09-07 22:15:21 +0200

    fix dependencies and make test run against/with JDT

--- tests/com.redhat.ceylon.eclipse.ui.test/src/com/redhat/ceylon/eclipse/ui/test/CeylonEditorTest.java
+++ tests/com.redhat.ceylon.eclipse.ui.test/src/com/redhat/ceylon/eclipse/ui/test/CeylonEditorTest.java
@@ -10,17 +10,43 @@
  ************************************************************************************/
 package com.redhat.ceylon.eclipse.ui.test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.SubProgressMonitor;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.internal.ui.util.CoreUtility;
+import org.eclipse.jdt.launching.IVMInstall;
+import org.eclipse.jdt.launching.JavaRuntime;
+import org.eclipse.jdt.launching.LibraryLocation;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
 import org.junit.Test;
+
+import com.redhat.ceylon.eclipse.code.wizard.*;
+import com.redhat.ceylon.eclipse.core.builder.CeylonNature;
 
 /**
  * 
@@ -29,20 +55,61 @@ import org.junit.Test;
  */
 public class CeylonEditorTest {
 
+	/** basic test that setup a ceylon project **/
 	@Test
-	public void testOpenEditor() {
+	public void testCreateProjectAddNature() throws CoreException {
 
-		File fileToOpen = new File("externalfile.xml");
-
-		IFileStore fileStore = EFS.getLocalFileSystem().getStore(
-				fileToOpen.toURI());
-		IWorkbenchPage page = PlatformUI.getWorkbench()
-				.getActiveWorkbenchWindow().getActivePage();
+		String name = "test-ceylon";
 		
-		try {
-			IDE.openEditorOnFileStore(page, fileStore);
-		} catch (PartInitException e) {
-			fail("editor failed to open file");
+		IProject project = createJavaProject(name);
+		
+		IPath outputPath = new Path(project.getName()+ "/ceylon-output").makeAbsolute();
+
+		List<String> repositoryPaths = new ArrayList<>();
+
+		CeylonNature nature = new CeylonNature(outputPath, repositoryPaths, true, false);
+		nature.addToProject(project);
+		
+		assertEquals(2,project.getDescription().getNatureIds().length);
+		
+
+	}
+
+	private IProject createJavaProject(String name) throws CoreException,
+			JavaModelException {
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IProject project = root.getProject(name);
+		project.create(null);
+		project.open(null);
+		
+		IProjectDescription description = project.getDescription();
+		description.setNatureIds(new String[] { JavaCore.NATURE_ID });
+		project.setDescription(description, null);
+		
+		IJavaProject javaProject = JavaCore.create(project); 
+		
+		IFolder binFolder = project.getFolder("bin");
+		binFolder.create(false, true, null);
+		javaProject.setOutputLocation(binFolder.getFullPath(), null);
+		
+		List<IClasspathEntry> entries = new ArrayList<IClasspathEntry>();
+		IVMInstall vmInstall = JavaRuntime.getDefaultVMInstall();
+		LibraryLocation[] locations = JavaRuntime.getLibraryLocations(vmInstall);
+		for (LibraryLocation element : locations) {
+		 entries.add(JavaCore.newLibraryEntry(element.getSystemLibraryPath(), null, null));
 		}
+		//add libs to project class path
+		javaProject.setRawClasspath(entries.toArray(new IClasspathEntry[entries.size()]), null);
+		
+		IFolder sourceFolder = project.getFolder("src");
+		sourceFolder.create(false, true, null);
+		
+		IPackageFragmentRoot packageroot = javaProject.getPackageFragmentRoot(sourceFolder);
+		IClasspathEntry[] oldEntries = javaProject.getRawClasspath();
+		IClasspathEntry[] newEntries = new IClasspathEntry[oldEntries.length + 1];
+		System.arraycopy(oldEntries, 0, newEntries, 0, oldEntries.length);
+		newEntries[oldEntries.length] = JavaCore.newSourceEntry(packageroot.getPath());
+		javaProject.setRawClasspath(newEntries, null);
+		return project;
 	}
 }
