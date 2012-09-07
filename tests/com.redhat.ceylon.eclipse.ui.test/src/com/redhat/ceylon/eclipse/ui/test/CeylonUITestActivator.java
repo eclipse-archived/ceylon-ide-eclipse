da2f62377bc26908940103755acc13a9d6a81faa
Max Rydahl Andersen <max@jboss.org>
2012-09-07 17:29:37 +0200

    initial testsetup layout

---
+++ tests/com.redhat.ceylon.eclipse.ui.test/src/com/redhat/ceylon/eclipse/ui/test/CeylonUITestActivator.java
@@ -0,0 +1,45 @@
+package com.redhat.ceylon.eclipse.ui.test;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+public class CeylonUITestActivator extends Plugin {
+
+	// The shared instance
+	private static CeylonUITestActivator plugin;
+	
+	
+	/**
+	 * The constructor
+	 */
+	public CeylonUITestActivator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static CeylonUITestActivator getDefault() {
+		return plugin;
+	}
+
+}
