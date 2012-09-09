c90f271750d0a89ab6febd970bae95e52ddc9e71
thradec <thradec@gmail.com>
2012-09-09 20:42:11 +0200

    Issue #381: Propose doc annotations (revert FindContainerVisitor)

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/util/FindContainerVisitor.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/util/FindContainerVisitor.java
@@ -5,91 +5,56 @@ import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
 
 public class FindContainerVisitor extends Visitor {
-
-    private Node node;
-    private Tree.StatementOrArgument container;
-    private Tree.StatementOrArgument currentContainer;
-
-    public FindContainerVisitor(Node node) {
-        this.node = node;
-    }
-
-    public Tree.Declaration getDeclaration() {
-        if (container instanceof Tree.Declaration) {
-            return (Tree.Declaration) container;
-        } else {
-            return null;
-        }
-    }
-
-    public Tree.StatementOrArgument getStatementOrArgument() {
-        return container;
-    }
-
-    @Override
-    public void visit(Tree.ModuleDescriptor that) {
-        Tree.StatementOrArgument d = currentContainer;
-        currentContainer = that;
-        super.visit(that);
-        currentContainer = d;
-    }
-    
-    @Override
-    public void visit(Tree.PackageDescriptor that) {
-        Tree.StatementOrArgument d = currentContainer;
-        currentContainer = that;
-        super.visit(that);
-        currentContainer = d;
-    }    
-
-    @Override
-    public void visit(Tree.ObjectDefinition that) {
-        Tree.StatementOrArgument d = currentContainer;
-        currentContainer = that;
-        super.visit(that);
-        currentContainer = d;
-    }
-
-    @Override
-    public void visit(Tree.AttributeGetterDefinition that) {
-        Tree.StatementOrArgument d = currentContainer;
-        currentContainer = that;
-        super.visit(that);
-        currentContainer = d;
-    }
-
-    @Override
-    public void visit(Tree.MethodDefinition that) {
-        Tree.StatementOrArgument d = currentContainer;
-        currentContainer = that;
-        super.visit(that);
-        currentContainer = d;
-    }
-
-    @Override
-    public void visit(Tree.ClassDefinition that) {
-        Tree.StatementOrArgument d = currentContainer;
-        currentContainer = that;
-        super.visit(that);
-        currentContainer = d;
-    }
-
-    @Override
-    public void visit(Tree.InterfaceDefinition that) {
-        Tree.StatementOrArgument d = currentContainer;
-        currentContainer = that;
-        super.visit(that);
-        currentContainer = d;
-    }
-
-    @Override
-    public void visitAny(Node node) {
-        if (this.node == node) {
-            container = currentContainer;
-        }
-        if (container == null) {
-            super.visitAny(node);
-        }
-    }
-    
+	Node node;
+	Tree.Declaration declaration;
+	Tree.Declaration currentDeclaration;
+	public Tree.Declaration getDeclaration() {
+		return declaration;
+	}
+	public FindContainerVisitor(Node node) {
+		this.node=node;
+	}
+	@Override
+	public void visit(Tree.ObjectDefinition that) {
+		Tree.Declaration d = currentDeclaration;
+		currentDeclaration = that;
+		super.visit(that);
+		currentDeclaration = d;
+	}
+	@Override
+	public void visit(Tree.AttributeGetterDefinition that) {
+		Tree.Declaration d = currentDeclaration;
+		currentDeclaration = that;
+		super.visit(that);
+		currentDeclaration = d;
+	}
+	@Override
+	public void visit(Tree.MethodDefinition that) {
+		Tree.Declaration d = currentDeclaration;
+		currentDeclaration = that;
+		super.visit(that);
+		currentDeclaration = d;
+	}
+	@Override
+	public void visit(Tree.ClassDefinition that) {
+		Tree.Declaration d = currentDeclaration;
+		currentDeclaration = that;
+		super.visit(that);
+		currentDeclaration = d;
+	}
+	@Override
+	public void visit(Tree.InterfaceDefinition that) {
+		Tree.Declaration d = currentDeclaration;
+		currentDeclaration = that;
+		super.visit(that);
+		currentDeclaration = d;
+	}
+	public void visitAny(Node node) {
+		if (this.node==node) {
+			declaration=currentDeclaration;
+		}
+		if (declaration==null) {
+			super.visitAny(node);
+		}
+	}
 }
