79f185bbc35ee5333289f00193ac00430a36a930
thradec <thradec@gmail.com>
2012-12-19 20:51:35 +0100

    Ceylon Test Plugin (preferencies initializer)

---
+++ plugins/com.redhat.ceylon.test.eclipse.plugin/src/com/redhat/ceylon/test/eclipse/plugin/CeylonTestPreferenceInitializer.java
@@ -0,0 +1,23 @@
+package com.redhat.ceylon.test.eclipse.plugin;
+
+import static com.redhat.ceylon.test.eclipse.plugin.CeylonTestPlugin.PREF_SHOW_TESTS_ELAPSED_TIME;
+import static com.redhat.ceylon.test.eclipse.plugin.CeylonTestPlugin.PREF_SHOW_TESTS_GROUPED_BY_PACKAGES;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+public class CeylonTestPreferenceInitializer extends AbstractPreferenceInitializer {
+
+    @Override
+    public void initializeDefaultPreferences() {
+        IPreferenceStore preferenceStore = CeylonTestPlugin.getDefault().getPreferenceStore();
+
+        if (!preferenceStore.contains(PREF_SHOW_TESTS_ELAPSED_TIME)) {
+            preferenceStore.setValue(PREF_SHOW_TESTS_ELAPSED_TIME, true);
+        }
+        if (!preferenceStore.contains(PREF_SHOW_TESTS_GROUPED_BY_PACKAGES)) {
+            preferenceStore.setValue(PREF_SHOW_TESTS_GROUPED_BY_PACKAGES, true);
+        }
+    }
+
+}
