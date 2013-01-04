da6958bc0d2b11f55eb6ecb0a5725b86902ec2a2
thradec <thradec@gmail.com>
2013-01-04 12:25:58 +0100

    Issue #476 CeylonRunPropertyTester for disable launch shortcuts

--- plugins/com.redhat.ceylon.test.eclipse.plugin/src/com/redhat/ceylon/test/eclipse/plugin/launch/CeylonTestPropertyTester.java
+++ plugins/com.redhat.ceylon.test.eclipse.plugin/src/com/redhat/ceylon/test/eclipse/plugin/launch/CeylonTestPropertyTester.java
@@ -1,14 +1,6 @@
 package com.redhat.ceylon.test.eclipse.plugin.launch;
 
-import static com.redhat.ceylon.test.eclipse.plugin.CeylonTestUtil.isCeylonFile;
-import static com.redhat.ceylon.test.eclipse.plugin.CeylonTestUtil.isCeylonProject;
-
 import org.eclipse.core.expressions.PropertyTester;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IPackageFragment;
-import org.eclipse.ui.IFileEditorInput;
 
 public class CeylonTestPropertyTester extends PropertyTester {
 
@@ -17,20 +9,8 @@ public class CeylonTestPropertyTester extends PropertyTester {
     @Override
     public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
         if (CAN_LAUNCH_AS_CEYLON_TEST_PROPERTY.equals(property)) {
-            if (receiver instanceof IJavaProject) {
-                IProject project = ((IJavaProject) receiver).getProject();
-                return isCeylonProject(project);
-            } else if (receiver instanceof IPackageFragment) {
-                IPackageFragment packageFragment = (IPackageFragment) receiver;
-                IProject project = packageFragment.getJavaProject().getProject();
-                return isCeylonProject(project);
-            } else if (receiver instanceof IFile) {
-                IFile file = (IFile) receiver;
-                return isCeylonFile(file);
-            } else if( receiver instanceof IFileEditorInput) {
-                IFileEditorInput fileEditorInput = (IFileEditorInput) receiver;
-                return isCeylonFile(fileEditorInput.getFile());
-            }
+            // TODO test that receiver is testable method/class
+            return true;
         }
         return false;
     }
