7ed704b26d7cc6cfc1f4e2a65230452962a1edb7
Stephane Epardaud <stef@epardaud.fr>
2013-01-25 14:29:08 +0100

    Updated boxing visitors to latest compiler API
    This needs heavy refactoring

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/builder/IDEBoxingVisitor.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/builder/IDEBoxingVisitor.java
@@ -2,17 +2,22 @@ package com.redhat.ceylon.eclipse.core.builder;
 
 import com.redhat.ceylon.compiler.java.codegen.BoxingVisitor;
 import com.redhat.ceylon.compiler.typechecker.model.Declaration;
+import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
 
 public class IDEBoxingVisitor extends BoxingVisitor {
 
 	@Override
 	protected boolean isBooleanTrue(Declaration decl) {
-		return decl.getUnit().getLanguageModuleDeclaration("true") == decl;
+		return IDEBoxingUtil.isBooleanTrue(decl);
 	}
 
 	@Override
 	protected boolean isBooleanFalse(Declaration decl) {
-		return decl.getUnit().getLanguageModuleDeclaration("false") == decl;
+        return IDEBoxingUtil.isBooleanFalse(decl);
 	}
 
+    @Override
+    protected boolean hasErasure(ProducedType type) {
+        return IDEBoxingUtil.hasErasure(type);
+    }
 }
