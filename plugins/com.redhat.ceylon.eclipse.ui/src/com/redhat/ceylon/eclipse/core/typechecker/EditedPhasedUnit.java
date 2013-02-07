64fb4da9d17e3b7e6d0d942180ce93fdf39ca0bd
David Festal <david.festal@serli.com>
2013-02-07 20:39:46 +0100

    Use specific PhasedUnits / Units for In-editor working copies

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/typechecker/EditedPhasedUnit.java
@@ -0,0 +1,33 @@
+package com.redhat.ceylon.eclipse.core.typechecker;
+
+import java.util.List;
+
+import org.antlr.runtime.CommonToken;
+
+import com.redhat.ceylon.compiler.typechecker.TypeChecker;
+import com.redhat.ceylon.compiler.typechecker.analyzer.ModuleManager;
+import com.redhat.ceylon.compiler.typechecker.context.PhasedUnit;
+import com.redhat.ceylon.compiler.typechecker.io.VirtualFile;
+import com.redhat.ceylon.compiler.typechecker.model.Package;
+import com.redhat.ceylon.compiler.typechecker.model.Unit;
+import com.redhat.ceylon.compiler.typechecker.tree.Tree.CompilationUnit;
+import com.redhat.ceylon.eclipse.core.model.EditedSourceFile;
+
+public class EditedPhasedUnit extends IdePhasedUnit {
+
+    public EditedPhasedUnit(VirtualFile unitFile, VirtualFile srcDir,
+            CompilationUnit cu, Package p, ModuleManager moduleManager,
+            TypeChecker typeChecker, List<CommonToken> tokenStream) {
+        super(unitFile, srcDir, cu, p, moduleManager, typeChecker, tokenStream);
+    }
+    
+    public EditedPhasedUnit(PhasedUnit other) {
+        super(other);
+    }
+
+    @Override
+    public Unit createUnit() {
+        return new EditedSourceFile(this);
+    }
+
+}
