820874315d68c5a9070686eb5f9140c9ed4d9858
David Festal <david.festal@serli.com>
2013-02-12 17:21:26 +0100

    clean

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/model/JavaUnit.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/model/JavaUnit.java
@@ -1,7 +1,4 @@
 package com.redhat.ceylon.eclipse.core.model;
 
 public abstract class JavaUnit extends IdeUnit implements IJavaModelAware, IResourceAware {
-    public JavaUnit() {
-        String s = "";
-    }
 }
