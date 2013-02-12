7b782cced31fb949c9babbea2beca10e7c9ddcb0
David Festal <david.festal@serli.com>
2013-02-12 17:28:07 +0100

    Uncomplete implementation of CeylonBinaryUnit

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/model/CeylonBinaryUnit.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/model/CeylonBinaryUnit.java
@@ -1,12 +1,21 @@
 package com.redhat.ceylon.eclipse.core.model;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+import net.lingala.zip4j.exception.ZipException;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.jdt.core.IClassFile;
 import org.eclipse.jdt.core.ITypeRoot;
 
 import com.redhat.ceylon.compiler.typechecker.context.PhasedUnit;
+import com.redhat.ceylon.compiler.typechecker.model.Module;
+import com.redhat.ceylon.compiler.typechecker.model.Package;
 import com.redhat.ceylon.eclipse.core.typechecker.ExternalPhasedUnit;
+import com.redhat.ceylon.eclipse.util.CarUtils;
 
 /*
  * Created inside the JDTModelLoader.getCompiledUnit() function if the unit is a ceylon one
@@ -34,6 +43,23 @@ public class CeylonBinaryUnit extends CeylonUnit implements IJavaModelAware {
     protected void setPhasedUnitIfNecessary() {
         // Look into the mapping.txt of the module archive, and get the name of the source unit
         // Then get the PhasedUnits related to this module, and search for the relative path in it.
-        // Then set in inot the Weakef with createPhasedUnit
+        // Then set it into the WeakRef with createPhasedUnit
+        
+        String[] splittedPath = getFullPath().split("!");
+        if (splittedPath.length == 2) {
+            String carPath = splittedPath[0];
+            try {
+                Properties mapping = CarUtils.retrieveMappingFile(new File(carPath));
+                String sourceFileRelativePath = mapping.getProperty(splittedPath[1]);
+                Package pkg = getPackage();
+                if (pkg != null) {
+                    Module module = pkg.getModule();
+                    // TODO : retrieve the PhasedUnits object related to this module
+                    // get the PhasedUnit object through its src-relative path
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
