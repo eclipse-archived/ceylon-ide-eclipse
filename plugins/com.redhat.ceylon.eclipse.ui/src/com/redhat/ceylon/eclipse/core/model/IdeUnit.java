1ffbe9d34c0be2aee4cb833fc3d7ca6e0d12dd99
David Festal <david.festal@serli.com>
2013-02-13 17:57:21 +0100

    clean imports

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/model/IdeUnit.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/model/IdeUnit.java
@@ -1,8 +1,5 @@
 package com.redhat.ceylon.eclipse.core.model;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IResource;
-
 import com.redhat.ceylon.compiler.typechecker.model.Unit;
 
 public abstract class IdeUnit extends Unit {
