4444e2adc24c81eaa46f9737b7226c9e5057751f
thradec <thradec@gmail.com>
2013-05-16 20:29:42 +0200

    #171 Visual cue for initialiser sections (semi-transparent annotation color)

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/CeylonRangeIndicator.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/CeylonRangeIndicator.java
@@ -2,20 +2,75 @@ package com.redhat.ceylon.eclipse.code.editor;
 
 import static com.redhat.ceylon.eclipse.code.parse.CeylonTokenColorer.getCurrentThemeColor;
 
+import org.eclipse.jface.text.source.Annotation;
+import org.eclipse.jface.text.source.IAnnotationPresentation;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.PaletteData;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 
-public class CeylonRangeIndicator extends AbstractAnnotationWithPatternPresentation {
+public class CeylonRangeIndicator extends Annotation implements IAnnotationPresentation {
 
     @Override
-    protected Color getPatternForegroundColor() {
-        return getCurrentThemeColor("rangeIndicatorAnnotation");
+    public int getLayer() {
+        return DEFAULT_LAYER;
     }
 
     @Override
-    protected Color getPatternBackgroundColor() {
-        return Display.getCurrent().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND);
+    public void paint(GC gc, Canvas canvas, Rectangle bounds) {
+        Point canvasSize = canvas.getSize();
+        int x = 0;
+        int y = bounds.y;
+        int w = canvasSize.x;
+        int h = bounds.height;
+
+        if (y + h > canvasSize.y) {
+            h = canvasSize.y - y;
+        }
+        if (y < 0) {
+            h = h + y;
+            y = 0;
+        }
+        if (h <= 0) {
+            return;
+        }
+
+        Image patternImage = getPatternImage(canvas);
+        gc.drawImage(patternImage, 0, 0, w, h, x, y, w, h);
+        patternImage.dispose();
+
+        gc.setBackground(getCurrentThemeColor("rangeIndicatorAnnotation"));
+        gc.fillRectangle(x, bounds.y, w, 1);
+        gc.fillRectangle(x, bounds.y + bounds.height - 1, w, 1);
+    }
+
+    private Image getPatternImage(Control control) {
+        Point size = control.getSize();
+        Display display = control.getDisplay();
+        Color c1 = getCurrentThemeColor("rangeIndicatorAnnotation");
+        Color c2 = Display.getCurrent().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND);
+
+        RGB rgbs[] = new RGB[] {
+                new RGB(c1.getRed(), c1.getGreen(), c1.getBlue()),
+                new RGB(c2.getRed(), c2.getGreen(), c2.getBlue()) };
+
+        ImageData imageData = new ImageData(size.x, size.y, 1, new PaletteData(rgbs));
+
+        for (int y = 0; y < size.y; y++) {
+            for (int x = 0; x < size.x; x++) {
+                imageData.setPixel(x, y, (x + y) % 2);
+            }
+        }
+
+        return new Image(display, imageData);
     }
 
 }
