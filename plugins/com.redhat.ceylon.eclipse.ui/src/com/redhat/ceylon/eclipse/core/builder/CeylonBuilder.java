b24b7840bc94afa6e38ab22574cefb10917009ab
Enrique Zamudio <alterbass@gmail.com>
2013-07-16 00:55:26 -0500

    Work on ceylon/ceylon-js#219 show js backend compilation errors

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/builder/CeylonBuilder.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/builder/CeylonBuilder.java
@@ -12,9 +12,16 @@ import static org.eclipse.core.resources.IResource.DEPTH_INFINITE;
 import static org.eclipse.core.resources.IResource.DEPTH_ZERO;
 import static org.eclipse.core.runtime.SubProgressMonitor.PREPEND_MAIN_LABEL_TO_SUBTASK;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,13 +31,18 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.NestingKind;
+import javax.tools.Diagnostic;
 import javax.tools.DiagnosticListener;
 import javax.tools.FileObject;
 import javax.tools.JavaFileObject;
+import javax.tools.JavaFileObject.Kind;
 
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
@@ -102,6 +114,8 @@ import com.redhat.ceylon.compiler.typechecker.parser.CeylonLexer;
 import com.redhat.ceylon.compiler.typechecker.parser.CeylonParser;
 import com.redhat.ceylon.compiler.typechecker.parser.LexError;
 import com.redhat.ceylon.compiler.typechecker.parser.ParseError;
+import com.redhat.ceylon.compiler.typechecker.tree.AnalysisMessage;
+import com.redhat.ceylon.compiler.typechecker.tree.Message;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.redhat.ceylon.compiler.typechecker.util.ModuleManagerFactory;
 import com.redhat.ceylon.eclipse.code.editor.CeylonTaskUtil;
@@ -1444,6 +1458,174 @@ public class CeylonBuilder extends IncrementalProjectBuilder {
     	}.schedule();
     }
 
+    //This is bullshit. The IDE should have a nice way of converting
+    //compilation units into objects that can be shown in the problems
+    //view, and even a nice way of converting errors in nodes in the
+    //typechecker into errors that can be shown in the problems view.
+    //Right now I'm using this for the js compiler errors but I expect
+    //to see other people wanting this and probably implementing their own
+    //solution again.
+    public class CeylonCompilationError implements Diagnostic<JavaFileObject> {
+
+        private final AnalysisMessage err;
+        private final IProject project;
+        private final JavaFileObject jf;
+        private final IFile file;
+        private int spos=-1, epos=-1;
+
+        public CeylonCompilationError(IProject proj, AnalysisMessage error) {
+            err = error;
+            this.project = proj;
+            file = project.getFile(err.getTreeNode().getUnit().getFullPath());
+            jf = new JavaFileObject() {
+                int spos = -1;
+                int epos = -1;
+                @Override
+                public URI toUri() {
+                    try {
+                        return new URI(file.getFullPath().toString());
+                    } catch (URISyntaxException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }
+                
+                @Override
+                public Writer openWriter() throws IOException {
+                    return null;
+                }
+                
+                @Override
+                public Reader openReader(boolean ignoreEncodingErrors) throws IOException {
+                    return null;
+                }
+                
+                @Override
+                public OutputStream openOutputStream() throws IOException {
+                    return null;
+                }
+                
+                @Override
+                public InputStream openInputStream() throws IOException {
+                    return null;
+                }
+                
+                @Override
+                public String getName() {
+                    return file.getLocation().toOSString();
+                }
+                
+                @Override
+                public long getLastModified() {
+                    return file.getModificationStamp();
+                }
+                
+                @Override
+                public CharSequence getCharContent(boolean ignoreEncodingErrors)
+                        throws IOException {
+                    return null;
+                }
+                
+                @Override
+                public boolean delete() {
+                    return false;
+                }
+                
+                @Override
+                public boolean isNameCompatible(String simpleName, Kind kind) {
+                    return false;
+                }
+                
+                @Override
+                public NestingKind getNestingKind() {
+                    return NestingKind.TOP_LEVEL;
+                }
+                
+                @Override
+                public Kind getKind() {
+                    return Kind.SOURCE;
+                }
+                
+                @Override
+                public Modifier getAccessLevel() {
+                    return Modifier.FINAL;
+                }
+            };
+        }
+
+        @Override
+        public javax.tools.Diagnostic.Kind getKind() {
+            return Diagnostic.Kind.ERROR;
+        }
+
+        @Override
+        public JavaFileObject getSource() {
+            return jf;
+        }
+
+        private void calculatePositions() {
+            int ln = 1;
+            try (BufferedReader reader = new BufferedReader(new java.io.InputStreamReader(file.getContents()))) {
+                String line = reader.readLine();
+                while (ln < err.getLine()) {
+                    spos += line.length()+1;
+                    ln++;
+                    line = reader.readLine();
+                }
+                spos += err.getTreeNode().getToken().getCharPositionInLine()+1;
+                epos = spos + err.getTreeNode().getToken().getText().length();
+                System.out.println("spos:" + spos + " epos:" + epos);
+            } catch (IOException|CoreException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        @Override
+        public long getPosition() {
+            if (spos < 0) {
+                calculatePositions();
+            }
+            return spos;
+        }
+
+        @Override
+        public long getStartPosition() {
+            if (spos < 0) {
+                calculatePositions();
+            }
+            return spos;
+        }
+
+        @Override
+        public long getEndPosition() {
+            if (epos < 0) {
+                calculatePositions();
+            }
+            return epos;
+        }
+
+        @Override
+        public long getLineNumber() {
+            return err.getLine();
+        }
+
+        @Override
+        public long getColumnNumber() {
+            return err.getTreeNode().getToken().getCharPositionInLine();
+        }
+
+        @Override
+        public String getCode() {
+            return String.valueOf(err.getCode());
+        }
+
+        @Override
+        public String getMessage(Locale locale) {
+            return err.getMessage();
+        }
+        
+    }
+
     private boolean generateBinaries(IProject project, IJavaProject javaProject,
     		Collection<IFile> filesToCompile, TypeChecker typeChecker, 
     		IProgressMonitor monitor) throws CoreException {
@@ -1523,9 +1705,18 @@ public class CeylonBuilder extends IncrementalProjectBuilder {
                 try {
                     if (!jsc.generate()) {
                         System.out.println("Ceylon-JS compiler failed for " + project.getName());
-                        //TODO report backend errors
+                        CompileErrorReporter errorReporter = new CompileErrorReporter(project);
+                        //Report backend errors
+                        for (Message e : jsc.getErrors()) {
+                            if (e instanceof AnalysisMessage) {
+                                errorReporter.report(new CeylonCompilationError(project, (AnalysisMessage)e));
+                            }
+                        }
                         jsc.printErrors(System.out);
+                        errorReporter.failed();
                         return false;
+                    } else {
+                        System.out.println("compile ok to js");
                     }
                 } catch (IOException ex) {
                     ex.printStackTrace(printWriter);
