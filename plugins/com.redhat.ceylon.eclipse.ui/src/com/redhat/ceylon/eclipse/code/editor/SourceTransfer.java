1b3b4c58007e81c0786cee8978764146937220a8
Gavin King <gavin@hibernate.org>
2013-09-01 14:33:30 +0200

    ups add missing file

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/SourceTransfer.java
@@ -0,0 +1,51 @@
+package com.redhat.ceylon.eclipse.code.editor;
+
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.dnd.TransferData;
+
+public class SourceTransfer extends Transfer {
+    
+    public static final SourceTransfer INSTANCE = new SourceTransfer();
+
+    private static final String TYPE_NAME = "ceylon-source-transfer-format" + 
+            System.currentTimeMillis();
+    
+    private static final int TYPEID = registerType(TYPE_NAME);
+    
+    public static String text;
+    
+    @Override
+    public TransferData[] getSupportedTypes() {
+        return null;
+    }
+
+    public boolean isSupportedType(TransferData transferData){
+        if (transferData == null) return false;
+        int[] types = getTypeIds();
+        for (int i = 0; i < types.length; i++) {
+            if (transferData.type == types[i]) return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected int[] getTypeIds() {
+        return new int[] { TYPEID };
+    }
+
+    @Override
+    protected String[] getTypeNames() {
+        return new String[] { TYPE_NAME };
+    }
+
+    @Override
+    protected void javaToNative(Object object, TransferData transferData) {
+        text = (String) object;
+    }
+
+    @Override
+    protected String nativeToJava(TransferData transferData) {
+        return text;
+    }
+
+}
