1aa7893f6c48694075a1e84d72801169a9efeba2
Gavin King <gavin@hibernate.org>
2013-09-04 04:04:42 +0200

    clean up solution for #171, only show initializer of current element

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/hover/CeylonAnnotationHover.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/hover/CeylonAnnotationHover.java
@@ -3,8 +3,8 @@ package com.redhat.ceylon.eclipse.code.hover;
 import static com.redhat.ceylon.eclipse.util.AnnotationUtils.formatAnnotationList;
 import static com.redhat.ceylon.eclipse.util.AnnotationUtils.getAnnotationsForLine;
 
+import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 
 import org.eclipse.jface.text.source.Annotation;
 import org.eclipse.jface.text.source.IAnnotationHover;
@@ -13,27 +13,17 @@ import org.eclipse.jface.text.source.ISourceViewer;
 import com.redhat.ceylon.eclipse.code.editor.CeylonInitializerAnnotation;
 
 public class CeylonAnnotationHover implements IAnnotationHover {
-
+    
     @Override
     public String getHoverInfo(ISourceViewer sourceViewer, int lineNumber) {
         List<Annotation> annotations = getAnnotationsForLine(sourceViewer, lineNumber);
-
-        CeylonInitializerAnnotation deepestInitializerAnnotation = null;
-        ListIterator<Annotation> annotationIterator = annotations.listIterator();
-        while (annotationIterator.hasNext()) {
-            Annotation annotation = annotationIterator.next();
-            if (annotation instanceof CeylonInitializerAnnotation) {
-                annotationIterator.remove();
-                CeylonInitializerAnnotation initializerAnnotation = (CeylonInitializerAnnotation) annotation;
-                if (deepestInitializerAnnotation == null || deepestInitializerAnnotation.getDepth() < initializerAnnotation.getDepth()) {
-                    deepestInitializerAnnotation = initializerAnnotation;
+        if (annotations.size()>1) {
+            for (Iterator<Annotation> iter=annotations.iterator(); iter.hasNext();) {
+                if (iter.next() instanceof CeylonInitializerAnnotation) {
+                    iter.remove();
                 }
             }
         }
-        if (annotations.isEmpty()) {
-            annotations.add(deepestInitializerAnnotation);
-        }
-
         return formatAnnotationList(annotations);
     }
 
