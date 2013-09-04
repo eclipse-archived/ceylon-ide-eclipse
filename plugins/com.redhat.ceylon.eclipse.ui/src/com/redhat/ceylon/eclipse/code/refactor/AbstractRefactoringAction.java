375ef696126c1623c0339119d06dc7373e81240d
Gavin King <gavin@hibernate.org>
2013-09-04 22:07:56 +0200

    move to own unit

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/refactor/AbstractRefactoringAction.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/refactor/AbstractRefactoringAction.java
@@ -1,12 +1,7 @@
 package com.redhat.ceylon.eclipse.code.refactor;
 
-import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.ltk.core.refactoring.Refactoring;
-import org.eclipse.ltk.core.refactoring.RefactoringStatus;
 import org.eclipse.ltk.ui.refactoring.RefactoringWizard;
-import org.eclipse.ltk.ui.refactoring.RefactoringWizardOpenOperation;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.texteditor.ITextEditor;
 import org.eclipse.ui.texteditor.TextEditorAction;
@@ -39,9 +34,9 @@ abstract class AbstractRefactoringAction extends TextEditorAction {
             }
         }
         if (refactoring.isEnabled()) {
-            new RefactoringStarter().activate(refactoring, createWizard(refactoring),
+            new RefactoringStarter().activate(createWizard(refactoring),
                             getTextEditor().getSite().getShell(),
-                            refactoring.getName(), false);
+                            refactoring.getName(), 4);
         }
         else {
             MessageDialog.openWarning(getTextEditor().getEditorSite().getShell(), 
@@ -54,41 +49,4 @@ abstract class AbstractRefactoringAction extends TextEditorAction {
     
     abstract String message();
     
-}
-
-/**
- * Simple class to wrap the processing flow for user-initiated refactorings. Copied from a similar class in JDT/UI.
- * 
- * @author rfuhrer
- */
-class RefactoringStarter {
-    // TODO re-enable support for the "Save Helper", which had too many claws in the JDT/UI to excise conveniently.
-    // private RefactoringSaveHelper fSaveHelper= new RefactoringSaveHelper();
-    private RefactoringStatus fStatus;
-
-    public void activate(Refactoring refactoring, RefactoringWizard wizard, Shell parent, String dialogTitle,
-            boolean mustSaveEditors) {
-        if (!canActivate(mustSaveEditors, parent))
-            return;
-        try {
-            RefactoringWizardOpenOperation op= new RefactoringWizardOpenOperation(wizard);
-            int result= op.run(parent, dialogTitle);
-            fStatus= op.getInitialConditionCheckingStatus();
-            if (result == IDialogConstants.CANCEL_ID
-                    || result == RefactoringWizardOpenOperation.INITIAL_CONDITION_CHECKING_FAILED) {
-                /* fSaveHelper.triggerBuild() */
-            }
-        } catch (InterruptedException e) {
-            // do nothing. User action got cancelled
-        }
-    }
-
-    public RefactoringStatus getInitialConditionCheckingStatus() {
-        return fStatus;
-    }
-
-    private boolean canActivate(boolean mustSaveEditors, Shell shell) {
-        // return !mustSaveEditors || fSaveHelper.saveEditors(shell);
-        return true;
-    }
 }
