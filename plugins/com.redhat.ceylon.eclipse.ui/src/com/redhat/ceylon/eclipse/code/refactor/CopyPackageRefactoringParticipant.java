b0f2b5317b9cee345127edd3391de2073a5c1db5
Gavin King <gavin@hibernate.org>
2013-09-05 23:53:22 +0200

    reimplement all the refactoring participants w/o the regex-based search

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/refactor/CopyPackageRefactoringParticipant.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/refactor/CopyPackageRefactoringParticipant.java
@@ -2,10 +2,10 @@ package com.redhat.ceylon.eclipse.code.refactor;
 
 import static com.redhat.ceylon.compiler.typechecker.tree.Util.formatPath;
 import static com.redhat.ceylon.eclipse.core.builder.CeylonBuilder.getProjectTypeChecker;
-import static org.eclipse.search.ui.text.FileTextSearchScope.newSearchScope;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.regex.Pattern;
+import java.util.List;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
@@ -18,22 +18,17 @@ import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.ltk.core.refactoring.CompositeChange;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
-import org.eclipse.ltk.core.refactoring.TextChange;
 import org.eclipse.ltk.core.refactoring.TextFileChange;
 import org.eclipse.ltk.core.refactoring.participants.CheckConditionsContext;
 import org.eclipse.ltk.core.refactoring.participants.CopyParticipant;
 import org.eclipse.ltk.core.refactoring.participants.ReorgExecutionLog;
-import org.eclipse.search.core.text.TextSearchEngine;
-import org.eclipse.search.core.text.TextSearchMatchAccess;
-import org.eclipse.search.core.text.TextSearchRequestor;
-import org.eclipse.search.ui.text.FileTextSearchScope;
 import org.eclipse.text.edits.MultiTextEdit;
 import org.eclipse.text.edits.ReplaceEdit;
-import org.eclipse.text.edits.TextEditGroup;
 
 import com.redhat.ceylon.compiler.typechecker.context.PhasedUnit;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.ImportPath;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
+import com.redhat.ceylon.eclipse.core.vfs.IFileVirtualFile;
 
 public class CopyPackageRefactoringParticipant extends CopyParticipant {
 
@@ -66,50 +61,39 @@ public class CopyPackageRefactoringParticipant extends CopyParticipant {
                 ReorgExecutionLog executionLog = getArguments().getExecutionLog();
                 final String newName = executionLog.getNewName(javaPackageFragment);
                 newPackage = dest.getPackageFragment(newName).getResource();
-                IResource[] roots = { newPackage };  // limit to files in the tree being pasted!
-                String[] fileNamePatterns = { "*.ceylon" };
-                FileTextSearchScope scope = newSearchScope(roots , fileNamePatterns, false);
                 final String oldName = javaPackageFragment.getElementName();
                 final IProject project = javaPackageFragment.getJavaProject().getProject();
                 
                 final HashMap<IFile,Change> changes= new HashMap<IFile,Change>();
-                TextSearchRequestor collector= new TextSearchRequestor() {
-                    public boolean acceptPatternMatch(final TextSearchMatchAccess matchAccess) throws CoreException {
-                        String relPath = javaPackageFragment.getResource().getProjectRelativePath().removeFirstSegments(1)
-                                .append(matchAccess.getFile().getProjectRelativePath()
-                                        .removeFirstSegments(newPackage.getProjectRelativePath().segmentCount()))
-                                .toPortableString();
-                        PhasedUnit phasedUnit= getProjectTypeChecker(project).getPhasedUnitFromRelativePath(relPath);
+                for (PhasedUnit phasedUnit: getProjectTypeChecker(project).getPhasedUnits().getPhasedUnits()) {
+                    final List<ReplaceEdit> edits = new ArrayList<ReplaceEdit>();
+                    if (phasedUnit.getPackage().getNameAsString().startsWith(oldName)) {
                         phasedUnit.getCompilationUnit().visit(new Visitor() {
                             @Override
                             public void visit(ImportPath that) {
                                 super.visit(that);
                                 if (formatPath(that.getIdentifiers()).equals(oldName)) {
-                                    IFile file = matchAccess.getFile();
-                                    TextFileChange change = (TextFileChange) changes.get(file);
-                                    if (change == null) {
-                                        TextChange textChange= getTextChange(file);
-                                        if (textChange != null) {
-                                            return;
-                                            //return false; // don't try to merge changes
-                                        }
-                                        change= new TextFileChange(file.getName(), file);
-                                        change.setEdit(new MultiTextEdit());
-                                        changes.put(file, change);
-                                    }
-                                    ReplaceEdit edit = new ReplaceEdit(that.getStartIndex(), oldName.length(), newName);
-                                    change.addEdit(edit);
-                                    //display the change in the UI
-                                    change.addTextEditGroup(new TextEditGroup("Rename package reference '" + 
-                                            oldName + "' to '" + newName + "' in " + file.getName(), edit));
+                                    edits.add(new ReplaceEdit(that.getStartIndex(), oldName.length(), newName));
                                 }
                             }
                         });
-                        return true;
+                        if (!edits.isEmpty()) {
+                            try {
+                                IFile file = ((IFileVirtualFile) phasedUnit.getUnitFile()).getFile();
+                                TextFileChange change= new TextFileChange(file.getName(), 
+                                        getMovedFile(newName, file));
+                                change.setEdit(new MultiTextEdit());
+                                changes.put(file, change);
+                                for (ReplaceEdit edit: edits) {
+                                    change.addEdit(edit);
+                                }
+                            }       
+                            catch (Exception e) { 
+                                e.printStackTrace(); 
+                            }
+                        }
                     }
-                };
-                Pattern pattern = Pattern.compile("\\b"+oldName.replace(".", "\\.")+"\\b");
-                TextSearchEngine.create().search(scope, collector, pattern, pm);
+                }
                 
                 if (changes.isEmpty())
                     return null;
@@ -146,4 +130,11 @@ public class CopyPackageRefactoringParticipant extends CopyParticipant {
         
     }
 
+    private IFile getMovedFile(final String newName, IFile file) {
+        String oldPath = javaPackageFragment.getElementName().replace('.', '/');
+        String newPath = newName.replace('.', '/');
+        String replaced = file.getProjectRelativePath().toString()
+                .replace(oldPath, newPath);
+        return file.getProject().getFile(replaced);
+    }
 }
--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/refactor/RenamePackageRefactoringParticipant.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/refactor/RenamePackageRefactoringParticipant.java
@@ -2,40 +2,29 @@ package com.redhat.ceylon.eclipse.code.refactor;
 
 import static com.redhat.ceylon.compiler.typechecker.tree.Util.formatPath;
 import static com.redhat.ceylon.eclipse.core.builder.CeylonBuilder.getProjectTypeChecker;
-import static org.eclipse.search.ui.text.FileTextSearchScope.newSearchScope;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.regex.Pattern;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jdt.core.IPackageFragment;
-import org.eclipse.jdt.core.IPackageFragmentRoot;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.ltk.core.refactoring.CompositeChange;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
-import org.eclipse.ltk.core.refactoring.TextChange;
 import org.eclipse.ltk.core.refactoring.TextFileChange;
 import org.eclipse.ltk.core.refactoring.participants.CheckConditionsContext;
 import org.eclipse.ltk.core.refactoring.participants.RenameParticipant;
-import org.eclipse.search.core.text.TextSearchEngine;
-import org.eclipse.search.core.text.TextSearchMatchAccess;
-import org.eclipse.search.core.text.TextSearchRequestor;
-import org.eclipse.search.ui.text.FileTextSearchScope;
 import org.eclipse.text.edits.MultiTextEdit;
 import org.eclipse.text.edits.ReplaceEdit;
-import org.eclipse.text.edits.TextEditGroup;
 
 import com.redhat.ceylon.compiler.typechecker.context.PhasedUnit;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.ImportPath;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
+import com.redhat.ceylon.eclipse.core.vfs.IFileVirtualFile;
 
 public class RenamePackageRefactoringParticipant extends RenameParticipant {
 
@@ -60,49 +49,39 @@ public class RenamePackageRefactoringParticipant extends RenameParticipant {
 	@Override
 	public Change createChange(IProgressMonitor pm) throws CoreException {
 		
-        final IProject project = javaPackageFragment.getJavaProject().getProject();
 		final String newName = getArguments().getNewName();
-		IResource[] roots = getSourceDirs(project);  // limit to source dirs in the current project
-		String[] fileNamePatterns = { "*.ceylon" };
-		FileTextSearchScope scope = newSearchScope(roots, fileNamePatterns, false);
 		final String oldName = javaPackageFragment.getElementName();
+        final IProject project = javaPackageFragment.getJavaProject().getProject();
 		
         final HashMap<IFile,Change> changes= new HashMap<IFile,Change>();
-		TextSearchRequestor collector= new TextSearchRequestor() {
-			public boolean acceptPatternMatch(final TextSearchMatchAccess matchAccess) throws CoreException {
-			    String relPath = matchAccess.getFile().getProjectRelativePath().removeFirstSegments(1).toPortableString();
-                PhasedUnit phasedUnit= getProjectTypeChecker(project).getPhasedUnitFromRelativePath(relPath);
-			    phasedUnit.getCompilationUnit().visit(new Visitor() {
-			        @Override
-			        public void visit(ImportPath that) {
-			            super.visit(that);
-			            if (formatPath(that.getIdentifiers()).equals(oldName)) {
-		                    IFile file = matchAccess.getFile();
-		                    TextFileChange change = (TextFileChange) changes.get(file);
-		                    if (change == null) {
-		                        TextChange textChange= getTextChange(file);
-		                        if (textChange != null) {
-		                            return;
-		                            //return false; // don't try to merge changes
-		                        }
-		                        change= new TextFileChange(file.getName(), getMovedFile(newName, file));
-		                        change.setEdit(new MultiTextEdit());
-		                        changes.put(file, change);
-		                    }
-		                    ReplaceEdit edit = new ReplaceEdit(that.getStartIndex(), oldName.length(), newName);
-		                    change.addEdit(edit);
-		                    //display the change in the UI
-		                    change.addTextEditGroup(new TextEditGroup("Rename package reference '" + 
-		                            oldName + "' to '" + newName + "' in " + file.getName(), edit));
-			            }
-			        }
-                });
-				return true;
-			}
-		};
-        Pattern pattern = Pattern.compile("\\b"+oldName.replace(".", "\\.")+"\\b");
-		TextSearchEngine.create().search(scope, collector, pattern, pm);
-		
+        for (PhasedUnit phasedUnit: getProjectTypeChecker(project).getPhasedUnits().getPhasedUnits()) {
+            final List<ReplaceEdit> edits = new ArrayList<ReplaceEdit>();
+            phasedUnit.getCompilationUnit().visit(new Visitor() {
+                @Override
+                public void visit(ImportPath that) {
+                    super.visit(that);
+                    if (formatPath(that.getIdentifiers()).equals(oldName)) {
+                        edits.add(new ReplaceEdit(that.getStartIndex(), oldName.length(), newName));
+                    }
+                }
+            });
+            if (!edits.isEmpty()) {
+                try {
+                    IFile file = ((IFileVirtualFile) phasedUnit.getUnitFile()).getFile();
+                    TextFileChange change= new TextFileChange(file.getName(), 
+                            getMovedFile(newName, file));
+                    change.setEdit(new MultiTextEdit());
+                    changes.put(file, change);
+                    for (ReplaceEdit edit: edits) {
+                        change.addEdit(edit);
+                    }
+                }       
+                catch (Exception e) { 
+                    e.printStackTrace(); 
+                }
+            }
+        }
+                
 		if (changes.isEmpty())
 			return null;
 		
@@ -113,20 +92,7 @@ public class RenamePackageRefactoringParticipant extends RenameParticipant {
 		return result;
 	}
 	
-    static IResource[] getSourceDirs(IProject project)
-            throws JavaModelException {
-        IPackageFragmentRoot[] paths = JavaCore.create(project).getAllPackageFragmentRoots();
-        List<IResource> list = new ArrayList<IResource>();
-		for (int i=0; i<paths.length; i++) {
-		    if (paths[i].getKind()==IPackageFragmentRoot.K_SOURCE) {
-		        IResource r = paths[i].getResource();
-		        if (r.exists()) list.add(r);
-		    }
-		}
-		return list.toArray(new IResource[0]);
-    }
-
-	IFile getMovedFile(final String newName, IFile file) {
+	private IFile getMovedFile(final String newName, IFile file) {
 		String oldPath = javaPackageFragment.getElementName().replace('.', '/');
         String newPath = newName.replace('.', '/');
         String replaced = file.getProjectRelativePath().toString()
