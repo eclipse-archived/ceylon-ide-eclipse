810b1f0650b26d52b26e161c27eac1cf7e78bb40
Gavin King <gavin@hibernate.org>
2013-09-06 00:53:38 +0200

    handle Moved Java elements, and fix bug with Renamed aliased Java types #610

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/refactor/MoveFileRefactoringParticipant.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/refactor/MoveFileRefactoringParticipant.java
@@ -13,6 +13,11 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IMember;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.ltk.core.refactoring.CompositeChange;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
@@ -28,8 +33,6 @@ import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.BaseMemberOrTypeExpression;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.BaseType;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.ImportMemberOrType;
-import com.redhat.ceylon.compiler.typechecker.tree.Tree.QualifiedMemberOrTypeExpression;
-import com.redhat.ceylon.compiler.typechecker.tree.Tree.QualifiedType;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
 import com.redhat.ceylon.eclipse.core.vfs.IFileVirtualFile;
 
@@ -40,7 +43,8 @@ public class MoveFileRefactoringParticipant extends MoveParticipant {
     @Override
     protected boolean initialize(Object element) {
         file = (IFile) element;
-        return file.getFileExtension().equals("ceylon");
+        return file.getFileExtension().equals("ceylon")||
+                file.getFileExtension().equals("java");
     }
 
     @Override
@@ -58,67 +62,118 @@ public class MoveFileRefactoringParticipant extends MoveParticipant {
     public Change createChange(IProgressMonitor pm) throws CoreException,
             OperationCanceledException {
         final IProject project = file.getProject();
-        final String newName = ((IFolder) getArguments().getDestination()).getProjectRelativePath()
-                .removeFirstSegments(1).toPortableString().replace('/', '.');
+        final String newName = ((IFolder) getArguments().getDestination())
+                .getProjectRelativePath().removeFirstSegments(1)
+                .toPortableString().replace('/', '.');
         String movedRelFilePath = file.getProjectRelativePath()
                 .removeFirstSegments(1).toPortableString();
         String movedRelPath = file.getParent().getProjectRelativePath()
                 .removeFirstSegments(1).toPortableString();
         final String oldName = movedRelPath.replace('/', '.');
-        PhasedUnit movedPhasedUnit= getProjectTypeChecker(project).getPhasedUnitFromRelativePath(movedRelFilePath);
-        final List<Declaration> declarations = movedPhasedUnit.getDeclarations();
-        if (newName.equals(oldName)) return null;
-
+        
         final HashMap<IFile,Change> changes= new HashMap<IFile,Change>();
-        for (PhasedUnit phasedUnit: getProjectTypeChecker(project).getPhasedUnits().getPhasedUnits()) {
-            final Map<Declaration,String> imports = new HashMap<Declaration,String>();
-            phasedUnit.getCompilationUnit().visit(new Visitor() {
-                @Override
-                public void visit(ImportMemberOrType that) {
-                    super.visit(that);
-                    visitIt(that.getIdentifier(), that.getDeclarationModel());
-                }
-                @Override
-                public void visit(QualifiedMemberOrTypeExpression that) {
-                    super.visit(that);
-                    visitIt(that.getIdentifier(), that.getDeclaration());
-                }
-                @Override
-                public void visit(BaseMemberOrTypeExpression that) {
-                    super.visit(that);
-                    visitIt(that.getIdentifier(), that.getDeclaration());
-                }
-                @Override
-                public void visit(BaseType that) {
-                    super.visit(that);
-                    visitIt(that.getIdentifier(), that.getDeclarationModel());
-                }
-                @Override
-                public void visit(QualifiedType that) {
-                    super.visit(that);
-                    visitIt(that.getIdentifier(), that.getDeclarationModel());
-                }
-                protected void visitIt(Tree.Identifier id, Declaration dec) {
-                    if (dec!=null && declarations.contains(dec)) {
-                        imports.put(dec, id.getText());
+        
+        if (file.getFileExtension().equals("java")) {
+            ICompilationUnit jcu = (ICompilationUnit) JavaCore.create(file);
+            final IType[] types = jcu.getTypes();
+            for (PhasedUnit phasedUnit: getProjectTypeChecker(project)
+                    .getPhasedUnits().getPhasedUnits()) {
+                final Map<Declaration,String> imports = new HashMap<Declaration,String>();
+                phasedUnit.getCompilationUnit().visit(new Visitor() {
+                    @Override
+                    public void visit(ImportMemberOrType that) {
+                        super.visit(that);
+                        visitIt(that.getIdentifier(), that.getDeclarationModel());
                     }
-                }
-            });
-            try {
-                IFile file = ((IFileVirtualFile) phasedUnit.getUnitFile()).getFile();
-                TextFileChange change= new TextFileChange(file.getName(), file);
-                change.setEdit(new MultiTextEdit());
-                changes.put(file, change);
-                if (!imports.isEmpty()) {
-                    List<TextEdit> edits = importEditForMove(phasedUnit.getCompilationUnit(), 
-                            imports.keySet(), imports.values(), newName, oldName);
-                    for (TextEdit edit: edits) {
-                        change.addEdit(edit);
+//                    @Override
+//                    public void visit(QualifiedMemberOrTypeExpression that) {
+//                        super.visit(that);
+//                        visitIt(that.getIdentifier(), that.getDeclaration());
+//                    }
+                    @Override
+                    public void visit(BaseMemberOrTypeExpression that) {
+                        super.visit(that);
+                        visitIt(that.getIdentifier(), that.getDeclaration());
                     }
-                }
+                    @Override
+                    public void visit(BaseType that) {
+                        super.visit(that);
+                        visitIt(that.getIdentifier(), that.getDeclarationModel());
+                    }
+//                    @Override
+//                    public void visit(QualifiedType that) {
+//                        super.visit(that);
+//                        visitIt(that.getIdentifier(), that.getDeclarationModel());
+//                    }
+                    protected void visitIt(Tree.Identifier id, Declaration dec) {
+                        for (IType type: types) {
+                            if (dec!=null && dec.getQualifiedNameString()
+                                    .equals(getQualifiedName(type))) {
+                               imports.put(dec, id.getText());
+                            }
+                        }
+                    }
+                    protected String getQualifiedName(IMember dec) {
+                        IJavaElement parent = dec.getParent();
+                        if (parent instanceof ICompilationUnit) {
+                            return parent.getParent().getElementName() + "::" + 
+                                    dec.getElementName();
+                        }
+                        else if (dec.getDeclaringType()!=null) {
+                            return getQualifiedName(dec.getDeclaringType()) + "." + 
+                                    dec.getElementName();
+                        }
+                        else {
+                            return "@";
+                        }
+                    }
+                });
+                collectEdits(newName, oldName, changes, phasedUnit, imports);
             }
-            catch (Exception e) { 
-                e.printStackTrace(); 
+        }
+        
+        else {
+            PhasedUnit movedPhasedUnit= getProjectTypeChecker(project)
+                    .getPhasedUnitFromRelativePath(movedRelFilePath);
+            final List<Declaration> declarations = movedPhasedUnit.getDeclarations();
+            if (newName.equals(oldName)) return null;
+
+            for (PhasedUnit phasedUnit: getProjectTypeChecker(project)
+                    .getPhasedUnits().getPhasedUnits()) {
+                final Map<Declaration,String> imports = new HashMap<Declaration,String>();
+                phasedUnit.getCompilationUnit().visit(new Visitor() {
+                    @Override
+                    public void visit(ImportMemberOrType that) {
+                        super.visit(that);
+                        visitIt(that.getIdentifier(), that.getDeclarationModel());
+                    }
+//                    @Override
+//                    public void visit(QualifiedMemberOrTypeExpression that) {
+//                        super.visit(that);
+//                        visitIt(that.getIdentifier(), that.getDeclaration());
+//                    }
+                    @Override
+                    public void visit(BaseMemberOrTypeExpression that) {
+                        super.visit(that);
+                        visitIt(that.getIdentifier(), that.getDeclaration());
+                    }
+                    @Override
+                    public void visit(BaseType that) {
+                        super.visit(that);
+                        visitIt(that.getIdentifier(), that.getDeclarationModel());
+                    }
+//                    @Override
+//                    public void visit(QualifiedType that) {
+//                        super.visit(that);
+//                        visitIt(that.getIdentifier(), that.getDeclarationModel());
+//                    }
+                    protected void visitIt(Tree.Identifier id, Declaration dec) {
+                        if (dec!=null && declarations.contains(dec)) {
+                            imports.put(dec, id.getText());
+                        }
+                    }
+                });
+                collectEdits(newName, oldName, changes, phasedUnit, imports);
             }
         }
 
@@ -130,6 +185,27 @@ public class MoveFileRefactoringParticipant extends MoveParticipant {
             result.add(change);
         }
         return result;
+    }
+    
+    private void collectEdits(final String newName, final String oldName,
+            final HashMap<IFile, Change> changes, PhasedUnit phasedUnit,
+            final Map<Declaration, String> imports) {
+        try {
+            IFile file = ((IFileVirtualFile) phasedUnit.getUnitFile()).getFile();
+            TextFileChange change= new TextFileChange(file.getName(), file);
+            change.setEdit(new MultiTextEdit());
+            changes.put(file, change);
+            if (!imports.isEmpty()) {
+                List<TextEdit> edits = importEditForMove(phasedUnit.getCompilationUnit(), 
+                        imports.keySet(), imports.values(), newName, oldName);
+                for (TextEdit edit: edits) {
+                    change.addEdit(edit);
+                }
+            }
+        }
+        catch (Exception e) { 
+            e.printStackTrace(); 
+        }
     }
 
 }
