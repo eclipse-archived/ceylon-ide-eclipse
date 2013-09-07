962966c9c75242534fc81617c8b9828971bd838a
Gavin King <gavin@hibernate.org>
2013-09-07 13:01:48 +0200

    big cleanup of and bugfix to copy file participant

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/refactor/CopyFileRefactoringParticipant.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/refactor/CopyFileRefactoringParticipant.java
@@ -17,7 +17,6 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.ltk.core.refactoring.Change;
-import org.eclipse.ltk.core.refactoring.CompositeChange;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
 import org.eclipse.ltk.core.refactoring.TextFileChange;
 import org.eclipse.ltk.core.refactoring.participants.CheckConditionsContext;
@@ -34,11 +33,10 @@ import com.redhat.ceylon.compiler.typechecker.model.Declaration;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.BaseMemberOrTypeExpression;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.BaseType;
-import com.redhat.ceylon.compiler.typechecker.tree.Tree.CompilationUnit;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.ImportMemberOrType;
-import com.redhat.ceylon.compiler.typechecker.tree.Tree.ImportPath;
+import com.redhat.ceylon.compiler.typechecker.tree.Tree.ModuleDescriptor;
+import com.redhat.ceylon.compiler.typechecker.tree.Tree.PackageDescriptor;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
-import com.redhat.ceylon.eclipse.core.vfs.IFileVirtualFile;
 
 public class CopyFileRefactoringParticipant extends CopyParticipant {
 
@@ -63,168 +61,95 @@ public class CopyFileRefactoringParticipant extends CopyParticipant {
     }
 
     public Change createChange(IProgressMonitor pm) throws CoreException {
+        IFolder dest = (IFolder) getArguments().getDestination();
+        final String newName = dest.getProjectRelativePath()
+                .removeFirstSegments(1).toPortableString()
+                .replace('/', '.');
+        IFile newFile = dest.getFile(file.getName());
+        String relFilePath = file.getProjectRelativePath()
+                .removeFirstSegments(1).toPortableString();
+        String relPath = file.getProjectRelativePath()
+                .removeFirstSegments(1).removeLastSegments(1)
+                .toPortableString();
+        final String oldName = relPath.replace('/', '.');
+        final IProject project = file.getProject();
+
+        PhasedUnit phasedUnit = getProjectTypeChecker(project)
+                .getPhasedUnitFromRelativePath(relFilePath);
+        final List<ReplaceEdit> edits = new ArrayList<ReplaceEdit>();                
+        final List<Declaration> declarations = phasedUnit.getDeclarations();
+        final Map<Declaration,String> imports = new HashMap<Declaration,String>();
+        phasedUnit.getCompilationUnit().visit(new Visitor() {
+            @Override
+            public void visit(ImportMemberOrType that) {
+                super.visit(that);
+                visitIt(that.getIdentifier(), that.getDeclarationModel());
+            }
+            @Override
+            public void visit(BaseMemberOrTypeExpression that) {
+                super.visit(that);
+                visitIt(that.getIdentifier(), that.getDeclaration());
+            }
+            @Override
+            public void visit(BaseType that) {
+                super.visit(that);
+                visitIt(that.getIdentifier(), that.getDeclarationModel());
+            }
+            @Override
+            public void visit(ModuleDescriptor that) {
+                super.visit(that);
+                visitIt(that.getImportPath());
+            }
+            @Override
+            public void visit(PackageDescriptor that) {
+                super.visit(that);
+                visitIt(that.getImportPath());
+            }
+            private void visitIt(Tree.ImportPath importPath) {
+                if (formatPath(importPath.getIdentifiers()).equals(oldName)) {
+                    edits.add(new ReplaceEdit(importPath.getStartIndex(), 
+                            oldName.length(), newName));
+                }
+            }
+            private void visitIt(Tree.Identifier id, Declaration dec) {
+                if (dec!=null && !declarations.contains(dec)) {
+                    String pn = dec.getUnit().getPackage().getNameAsString();
+                    if (!pn.equals(newName) && !pn.isEmpty() && 
+                            !pn.equals("ceylon.language")) {
+                        imports.put(dec, id.getText());
+                    }
+                }
+            }
+        });
+
         try {
-        Change change = new Change() {
-            IFile newFile;
-            @Override
-            public Change perform(IProgressMonitor pm) throws CoreException {
-                IFolder dest = (IFolder) getArguments().getDestination();
-                final String newName = dest.getProjectRelativePath()
-                        .removeFirstSegments(1).toPortableString()
-                        .replace('/', '.');
-                newFile = dest.getFile(file.getName());
-                String relFilePath = file.getProjectRelativePath()
-                        .removeFirstSegments(1).toPortableString();
-                String relPath = file.getProjectRelativePath()
-                        .removeFirstSegments(1).removeLastSegments(1)
-                        .toPortableString();
-                final String oldName = relPath.replace('/', '.');
-                final IProject project = file.getProject();
-                
-                final HashMap<IFile,Change> changes= new HashMap<IFile,Change>();
-                PhasedUnit phasedUnit = getProjectTypeChecker(project)
-                        .getPhasedUnitFromRelativePath(relFilePath);
-                final List<ReplaceEdit> edits = new ArrayList<ReplaceEdit>();
-                if (phasedUnit.getPackage().getNameAsString().startsWith(oldName)) {
-                    phasedUnit.getCompilationUnit().visit(new Visitor() {
-                        @Override
-                        public void visit(ImportPath that) {
-                            super.visit(that);
-                            if (formatPath(that.getIdentifiers()).equals(oldName)) {
-                                edits.add(new ReplaceEdit(that.getStartIndex(), oldName.length(), newName));
-                            }
-                        }
-                    });
-                    if (!edits.isEmpty()) {
-                        try {
-                            IFile file = ((IFileVirtualFile) phasedUnit.getUnitFile()).getFile();
-                            TextFileChange change= new TextFileChange(file.getName(), newFile);
-                            change.setEdit(new MultiTextEdit());
-                            changes.put(file, change);
-                            for (ReplaceEdit edit: edits) {
-                                change.addEdit(edit);
-                            }
-                        }       
-                        catch (Exception e) { 
-                            e.printStackTrace(); 
-                        }
-                    }
-                }
-                
-                final List<Declaration> declarations = phasedUnit.getDeclarations();
-                final Map<Declaration,String> imports = new HashMap<Declaration,String>();
-                phasedUnit.getCompilationUnit().visit(new Visitor() {
-                    @Override
-                    public void visit(ImportMemberOrType that) {
-                        super.visit(that);
-                        visitIt(that.getIdentifier(), that.getDeclarationModel());
-                    }
-//                    @Override
-//                    public void visit(QualifiedMemberOrTypeExpression that) {
-//                        super.visit(that);
-//                        visitIt(that.getIdentifier(), that.getDeclaration());
-//                    }
-                    @Override
-                    public void visit(BaseMemberOrTypeExpression that) {
-                        super.visit(that);
-                        visitIt(that.getIdentifier(), that.getDeclaration());
-                    }
-                    @Override
-                    public void visit(BaseType that) {
-                        super.visit(that);
-                        visitIt(that.getIdentifier(), that.getDeclarationModel());
-                    }
-//                    @Override
-//                    public void visit(QualifiedType that) {
-//                        super.visit(that);
-//                        visitIt(that.getIdentifier(), that.getDeclarationModel());
-//                    }
-                    protected void visitIt(Tree.Identifier id, Declaration dec) {
-                        if (dec!=null && !declarations.contains(dec)) {
-                            String pn = dec.getUnit().getPackage().getNameAsString();
-                            if (!pn.equals(newName) && !pn.isEmpty() && 
-                                    !pn.equals("ceylon.language")) {
-                                imports.put(dec, id.getText());
-                            }
-                        }
-                    }
-                });
-                collectEdits(newName, oldName, changes, phasedUnit, imports);
-                
-                if (changes.isEmpty())
-                    return null;
-                
-                CompositeChange result= new CompositeChange("Ceylon source changes");
-                for (Change change: changes.values()) {
-                    result.add(change);
-                }
-                result.perform(pm);
-                return null;
+            TextFileChange change = new TextFileChange(file.getName(), newFile);
+            Tree.CompilationUnit cu = phasedUnit.getCompilationUnit();
+            change.setEdit(new MultiTextEdit());
+            for (ReplaceEdit edit: edits) {
+                change.addEdit(edit);
             }
-
-            @Override
-            public String getName() {
-                return "Copy Ceylon Package";
-            }
-
-            @Override
-            public void initializeValidationData(IProgressMonitor pm) {}
-
-            @Override
-            public RefactoringStatus isValid(IProgressMonitor pm)
-                    throws CoreException, OperationCanceledException {
-                return new RefactoringStatus();
-            }
-
-            @Override
-            public Object getModifiedElement() {
-                return newFile;
-            }
-
-            private void collectEdits(final String newName, final String oldName,
-                    final HashMap<IFile, Change> changes, PhasedUnit phasedUnit,
-                    final Map<Declaration, String> imports) {
-                try {
-                    CompilationUnit cu = phasedUnit.getCompilationUnit();
-                    IFile file = ((IFileVirtualFile) phasedUnit.getUnitFile()).getFile();
-                    TextFileChange change= new TextFileChange(file.getName(), newFile);
-                    change.setEdit(new MultiTextEdit());
-                    changes.put(file, change);
-                    if (!imports.isEmpty()) {
-                        List<InsertEdit> edits = importEdit(cu, 
-                                imports.keySet(), imports.values(), null);
-                        for (TextEdit edit: edits) {
-                            change.addEdit(edit);
-                        }
-                    }
-                    Tree.Import toDelete = findImportNode(cu, newName);
-                    if (toDelete!=null) {
-                        change.addEdit(new DeleteEdit(toDelete.getStartIndex(), 
-                                toDelete.getStopIndex()-toDelete.getStartIndex()+1));
-                    }
-                }
-                catch (Exception e) { 
-                    e.printStackTrace(); 
+            if (!imports.isEmpty()) {
+                List<InsertEdit> edits1 = importEdit(cu, 
+                        imports.keySet(), imports.values(), null);
+                for (TextEdit edit: edits1) {
+                    change.addEdit(edit);
                 }
             }
-            
-        };
-        change.setEnabled(true);
-        return change;
+            Tree.Import toDelete = findImportNode(cu, newName);
+            if (toDelete!=null) {
+                change.addEdit(new DeleteEdit(toDelete.getStartIndex(), 
+                        toDelete.getStopIndex()-toDelete.getStartIndex()+1));
+            }
+            if (change.getEdit().hasChildren()) {
+                return change;
+            }
         }
-        catch (Exception e) {
-            e.printStackTrace();
-            return null;
+        catch (Exception e) { 
+            e.printStackTrace(); 
         }
-        
+
+        return null;
     }
 
-//
-//    private IFile getMovedFile(final String newName, IFile file) {
-//        String oldPath = file.getParent().getElementName().replace('.', '/');
-//        String newPath = newName.replace('.', '/');
-//        String replaced = file.getProjectRelativePath().toString()
-//                .replace(oldPath, newPath);
-//        return file.getProject().getFile(replaced);
-//    }
 }
