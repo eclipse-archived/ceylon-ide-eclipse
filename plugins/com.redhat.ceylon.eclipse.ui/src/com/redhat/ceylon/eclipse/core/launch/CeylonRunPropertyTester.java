5d59db9b49c263490ff522400b6684998f392c98
Akber Choudhry <akber@dgwave.com>
2013-09-14 00:28:05 -0400

    Merge with master and re-use app property

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/launch/CeylonRunPropertyTester.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/launch/CeylonRunPropertyTester.java
@@ -14,7 +14,6 @@ public class CeylonRunPropertyTester extends PropertyTester {
 
     private static final String CAN_LAUNCH_AS_CEYLON_APP_PROPERTY = "canLaunchAsCeylonApp";
     private static final String CAN_LAUNCH_AS_CEYLON_JS_APP_PROPERTY = "canLaunchAsCeylonJsApp";
-    private static final String CAN_LAUNCH_AS_CEYLON_MODULE_PROPERTY = "canLaunchAsCeylonModule";
     private static final String CEYLON_FILE_EXTENSION = "ceylon";
 
     @Override
@@ -34,30 +33,18 @@ public class CeylonRunPropertyTester extends PropertyTester {
                 IFileEditorInput fileEditorInput = (IFileEditorInput) receiver;
                 return isCeylonFile(fileEditorInput.getFile()) && isBuilderEnabled(fileEditorInput.getFile().getProject(), property);
             }
-        } else if (CAN_LAUNCH_AS_CEYLON_MODULE_PROPERTY.equals(property)) {
-            if (receiver instanceof IJavaProject) {
-                IProject project = ((IJavaProject) receiver).getProject();
-                return isCeylonProject(project) && isBuilderEnabled(project, property);
-            } else if (receiver instanceof IPackageFragment) {
-                IPackageFragment packageFragment = (IPackageFragment) receiver;
-                return isCeylonModule(packageFragment) && isBuilderEnabled(packageFragment.getJavaProject().getProject(), property);
-            } 
         }
         return false;
     }
     
     private boolean isBuilderEnabled(IProject project, String property) {
-        if (CAN_LAUNCH_AS_CEYLON_APP_PROPERTY.equals(property) || CAN_LAUNCH_AS_CEYLON_MODULE_PROPERTY.equals(property)) {
+        if (CAN_LAUNCH_AS_CEYLON_APP_PROPERTY.equals(property)) {
             return CeylonBuilder.compileToJava(project);
         } else if (CAN_LAUNCH_AS_CEYLON_JS_APP_PROPERTY.equals(property)) {
             return CeylonBuilder.compileToJs(project);
         }
         return false;
     }
-
-    private boolean isCeylonModule(IPackageFragment packageFragment) {
-		return (packageFragment.getClassFile("module_.class")  != null && packageFragment.getClassFile("run_.class") != null);
-	}
 
 	private boolean isCeylonProject(IProject project) {
         return project.isOpen() && CeylonNature.isEnabled(project);
