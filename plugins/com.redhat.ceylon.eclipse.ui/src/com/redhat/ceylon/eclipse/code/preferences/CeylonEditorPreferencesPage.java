cf3829c897bd6b0b7134ebdc62ba0462691fdbf4
Gavin King <gavin@hibernate.org>
2013-09-14 16:00:59 +0200

    fix #631

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/preferences/CeylonEditorPreferencesPage.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/preferences/CeylonEditorPreferencesPage.java
@@ -23,6 +23,7 @@ import org.eclipse.jface.preference.ScaleFieldEditor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -185,7 +186,62 @@ public class CeylonEditorPreferencesPage
         addField(autoActivationChars);
         autoActivationDelay = new ScaleFieldEditor(AUTO_ACTIVATION_DELAY, 
                 "Auto-activation delay", 
-                getFieldEditorParent());
+                getFieldEditorParent()) {
+            Label label;
+            @Override
+            protected void adjustForNumColumns(int numColumns) {
+                ((GridData) scale.getLayoutData()).horizontalSpan = numColumns - 2;
+            }
+            @Override
+            protected void doFillIntoGrid(Composite parent,
+                    int numColumns) {
+                super.doFillIntoGrid(parent, numColumns-1);
+//                ((GridData) getScaleControl().getLayoutData()).grabExcessHorizontalSpace=false;
+                label = new Label(parent, SWT.SHADOW_IN);
+                getScaleControl().addSelectionListener(new SelectionListener() {
+                    @Override
+                    public void widgetSelected(SelectionEvent e) {
+                        label.setText(scale.getSelection() + " ms");
+                    }
+                    @Override
+                    public void widgetDefaultSelected(SelectionEvent e) {
+                        label.setText(scale.getSelection() + " ms");
+                    }
+                });
+                GridData gd = new GridData();
+                label.setLayoutData(gd);
+            }
+            @Override
+            public int getNumberOfControls() {
+                return super.getNumberOfControls()+1;
+            }
+            @Override
+            protected void createControl(Composite parent) {
+                GridLayout layout = new GridLayout();
+                layout.numColumns = getNumberOfControls();
+                layout.marginWidth = 0;
+                layout.marginHeight = 0;
+                layout.horizontalSpacing = HORIZONTAL_GAP;
+                parent.setLayout(layout);
+                doFillIntoGrid(parent, layout.numColumns);
+            }
+            @Override
+            protected void doLoad() {
+                super.doLoad();
+                if (label != null) {
+                    int value = getPreferenceStore().getInt(getPreferenceName());
+                    label.setText(value + " ms       ");
+                }
+            }
+            @Override
+            protected void doLoadDefault() {
+                super.doLoadDefault();
+                if (label != null) {
+                    int value = getPreferenceStore().getDefaultInt(getPreferenceName());
+                    label.setText(value + " ms       ");
+                }
+            }
+        };
         //autoActivationDelay.setValidRange(1, 9999);
         autoActivationDelay.setMinimum(1);
         autoActivationDelay.setMaximum(2000);
