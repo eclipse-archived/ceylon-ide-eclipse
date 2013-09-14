fe9d90984ffe000b7ebe7ecce364059e8a2f15f7
Gavin King <gavin@hibernate.org>
2013-09-14 02:15:44 +0200

    tabs to spaces

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/FoldingUpdater.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/FoldingUpdater.java
@@ -37,8 +37,8 @@ import com.redhat.ceylon.eclipse.code.parse.CeylonParseController;
  * @author Gavin King
  */
 public class FoldingUpdater {
-	
-	// Maps new annotations to positions
+    
+    // Maps new annotations to positions
     protected HashMap<Annotation,Position> newAnnotations = new HashMap<Annotation, Position>();
 
     // Lists the new annotations, which are the keys for newAnnotations
@@ -58,10 +58,10 @@ public class FoldingUpdater {
     // in the language-specific concrete subtype
 
     public FoldingUpdater(CeylonSourceViewer sourceViewer) {
-		this.sourceViewer = sourceViewer;
-	}
+        this.sourceViewer = sourceViewer;
+    }
 
-	/**
+    /**
      * Make a folding annotation that corresponds to the extent of text
      * represented by a given program entity. Usually, this will be an
      * AST node, but it can be anything for which the language's
@@ -70,7 +70,7 @@ public class FoldingUpdater {
      * @param n an Object representing a program entity
      */
     public void makeAnnotation(Object n) {
-    	makeAnnotation(n, false);
+        makeAnnotation(n, false);
     }
     
     /**
@@ -82,185 +82,185 @@ public class FoldingUpdater {
      * @param n an Object representing a program entity
      */
     public void makeAnnotation(Object n, boolean collapsed) {
-		makeAnnotation(getStartOffset(n), getLength(n), collapsed);
+        makeAnnotation(getStartOffset(n), getLength(n), collapsed);
     }
 
     /**
      * Make a folding annotation that corresponds to the given range of text.
      * 
-     * @param start		The starting offset of the text range
-     * @param len		The length of the text range
+     * @param start        The starting offset of the text range
+     * @param len        The length of the text range
      */
     public ProjectionAnnotation makeAnnotation(int start, int len) {
-		ProjectionAnnotation annotation= new ProjectionAnnotation();
-		len = advanceToEndOfLine(start, len);
-		newAnnotations.put(annotation, new Position(start, len));
-		annotations.add(annotation);
-		return annotation;
+        ProjectionAnnotation annotation= new ProjectionAnnotation();
+        len = advanceToEndOfLine(start, len);
+        newAnnotations.put(annotation, new Position(start, len));
+        annotations.add(annotation);
+        return annotation;
     }
 
-	protected int advanceToEndOfLine(int start, int len) {
-		IDocument doc = sourceViewer.getDocument();
-		try {
-			int line = doc.getLineOfOffset(start+len);
-			while (start+len<doc.getLength() && 
-					Character.isWhitespace(doc.get(start+len,1).charAt(0)) &&
-					doc.getLineOfOffset(start+len)==line) {
-				len++;
-			}
-		}
-		catch (BadLocationException e) {
-			e.printStackTrace();
-		}
-		return len;
-	}
+    protected int advanceToEndOfLine(int start, int len) {
+        IDocument doc = sourceViewer.getDocument();
+        try {
+            int line = doc.getLineOfOffset(start+len);
+            while (start+len<doc.getLength() && 
+                    Character.isWhitespace(doc.get(start+len,1).charAt(0)) &&
+                    doc.getLineOfOffset(start+len)==line) {
+                len++;
+            }
+        }
+        catch (BadLocationException e) {
+            e.printStackTrace();
+        }
+        return len;
+    }
     
     /**
      * Make a folding annotation that corresponds to the given range of text.
      * 
-     * @param start		The starting offset of the text range
-     * @param len		The length of the text range
+     * @param start        The starting offset of the text range
+     * @param len        The length of the text range
      */
     public void makeAnnotation(int start, int len, boolean collapsed) {
-		ProjectionAnnotation annotation= new ProjectionAnnotation(collapsed);
-		len = advanceToEndOfLine(start, len);
-		newAnnotations.put(annotation, new Position(start, len));
-		annotations.add(annotation);
+        ProjectionAnnotation annotation= new ProjectionAnnotation(collapsed);
+        len = advanceToEndOfLine(start, len);
+        newAnnotations.put(annotation, new Position(start, len));
+        annotations.add(annotation);
     }
 
-	/**
-	 * Update the folding structure for a source text, where the text and its
-	 * AST are represented by a given parse controller and the folding structure
-	 * is represented by annotations in a given annotation model.
-	 * 
-	 * This is the principal routine of the folding updater.
-	 * 
-	 * The implementation provided here makes use of a local class
-	 * FoldingUpdateStrategy, to which the task of updating the folding
-	 * structure is delegated.
-	 * 
-	 * updateFoldingStructure is synchronized because, at least on file opening,
-	 * it can be called more than once before the first invocation has completed.
-	 * This can lead to inconsistent calculations resulting in the absence of
-	 * folding annotations in newly opened files.
-	 * 
-	 * @param parseController		A parse controller through which the AST for
-	 *								the source text can be accessed
-	 * @param annotationModel		A structure of projection annotations that
-	 *								represent the foldable elements in the source
-	 *								text
-	 */
-	public synchronized void updateFoldingStructure(CeylonParseController parseController, 
-			ProjectionAnnotationModel annotationModel) {
-		if (parseController != null)
-			this.parseController = parseController;
-		
-		try {
-			Node ast = parseController.getRootNode();
-			
-			if (ast == null) {
-				// We can't create annotations without an AST
-				return;
-			}
-		
-			// But, since here we have the AST ...
-			sendVisitorToAST(newAnnotations, annotations, ast);
+    /**
+     * Update the folding structure for a source text, where the text and its
+     * AST are represented by a given parse controller and the folding structure
+     * is represented by annotations in a given annotation model.
+     * 
+     * This is the principal routine of the folding updater.
+     * 
+     * The implementation provided here makes use of a local class
+     * FoldingUpdateStrategy, to which the task of updating the folding
+     * structure is delegated.
+     * 
+     * updateFoldingStructure is synchronized because, at least on file opening,
+     * it can be called more than once before the first invocation has completed.
+     * This can lead to inconsistent calculations resulting in the absence of
+     * folding annotations in newly opened files.
+     * 
+     * @param parseController        A parse controller through which the AST for
+     *                                the source text can be accessed
+     * @param annotationModel        A structure of projection annotations that
+     *                                represent the foldable elements in the source
+     *                                text
+     */
+    public synchronized void updateFoldingStructure(CeylonParseController parseController, 
+            ProjectionAnnotationModel annotationModel) {
+        if (parseController != null)
+            this.parseController = parseController;
+        
+        try {
+            Node ast = parseController.getRootNode();
+            
+            if (ast == null) {
+                // We can't create annotations without an AST
+                return;
+            }
+        
+            // But, since here we have the AST ...
+            sendVisitorToAST(newAnnotations, annotations, ast);
 
-			// Update the annotation model if there have been changes
-			// but not otherwise (since update leads to redrawing of the	
-			// source in the editor, which is likely to be unwelcome if
-			// there haven't been any changes relevant to folding)
-			boolean updateNeeded = false;
-			if (oldAnnotationsList == null) {
-				// Should just be the first time through
-				updateNeeded = true;
-			} 
-			else {
-				// Check to see whether the current and previous annotations
-				// differ in any significant way; if not, then there's no
-				// reason to update the annotation model.
-				// Note:  This test may be implemented in various ways that may
-				// be more or less simple, efficient, correct, etc.  (The
-				// default test provided below is simplistic although quick and
-				// usually effective.)
-				updateNeeded = differ(oldAnnotationsList, annotations);
-			}
-			if (updateNeeded) {
-				// Save the current annotations to compare for changes the next time
-				oldAnnotationsList = new ArrayList<Annotation>();
-				for (int i = 0; i < annotations.size(); i++) {
-					oldAnnotationsList.add(annotations.get(i));	
-				}
-			} else {
-			}
-		
-			// Need to curtail calls to modifyAnnotations() because these lead to calls
-			// to fireModelChanged(), which eventually lead to calls to updateFoldingStructure,
-			// which lead back here, which would lead to another call to modifyAnnotations()
-			// (unless those were curtailed)
-			if (updateNeeded) {
-				annotationModel.modifyAnnotations(oldAnnotationsArray, newAnnotations, null);
-				// Capture the latest set of annotations in a form that can be used the next
-				// time that it is necessary to modify the annotations
-				oldAnnotationsArray = (Annotation[]) annotations.toArray(new Annotation[annotations.size()]);
-			}
+            // Update the annotation model if there have been changes
+            // but not otherwise (since update leads to redrawing of the    
+            // source in the editor, which is likely to be unwelcome if
+            // there haven't been any changes relevant to folding)
+            boolean updateNeeded = false;
+            if (oldAnnotationsList == null) {
+                // Should just be the first time through
+                updateNeeded = true;
+            } 
+            else {
+                // Check to see whether the current and previous annotations
+                // differ in any significant way; if not, then there's no
+                // reason to update the annotation model.
+                // Note:  This test may be implemented in various ways that may
+                // be more or less simple, efficient, correct, etc.  (The
+                // default test provided below is simplistic although quick and
+                // usually effective.)
+                updateNeeded = differ(oldAnnotationsList, annotations);
+            }
+            if (updateNeeded) {
+                // Save the current annotations to compare for changes the next time
+                oldAnnotationsList = new ArrayList<Annotation>();
+                for (int i = 0; i < annotations.size(); i++) {
+                    oldAnnotationsList.add(annotations.get(i));    
+                }
+            } else {
+            }
+        
+            // Need to curtail calls to modifyAnnotations() because these lead to calls
+            // to fireModelChanged(), which eventually lead to calls to updateFoldingStructure,
+            // which lead back here, which would lead to another call to modifyAnnotations()
+            // (unless those were curtailed)
+            if (updateNeeded) {
+                annotationModel.modifyAnnotations(oldAnnotationsArray, newAnnotations, null);
+                // Capture the latest set of annotations in a form that can be used the next
+                // time that it is necessary to modify the annotations
+                oldAnnotationsArray = (Annotation[]) annotations.toArray(new Annotation[annotations.size()]);
+            }
 
-			newAnnotations.clear();
-			annotations.clear();			
-		} 
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	}	
+            newAnnotations.clear();
+            annotations.clear();            
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }    
 
 
-	/**
-	 * A method to test whether there has been a significant change in the folding
-	 * annotations for a source text.  The method works by comparing two lists of
-	 * annotations, nominally the "old" and "new" annotations.  It returns true iff
-	 * there is considered to be a "significant" difference in the two lists, where
-	 * the meaning of "significant" is defined by the implementation of this method.
-	 * 
-	 * The default implementation provided here is a simplistic test of the difference
-	 * between two lists, considering only their size.  This may work well enough much
-	 * of the time as the comparisons between lists should be made very frequently,
-	 * actually more frequently than the rate at which the typical human user will
-	 * edit the program text so as to affect the AST so as to affect the lists.  Thus
-	 * most changes of lists will entail some change in the number of elements at some
-	 * point that will be observed here.  This will not work for certain very rapid
-	 * edits of source text (e.g., rapid replacement of elements).
-	 * 
-	 * This method should be overridden in language-specific implementations of the
-	 * folding updater where a more sophisticated test is desired.	
-	 * 	
-	 * @param list1		A list of annotations (nominally the "old" annotations)
-	 * @param list2		A list of annotations (nominally the "new" annotations)
-	 * @return			true iff there has been a "significant" difference in the
-	 * 					two given lists of annotations
-	 * 
-	 */
-	protected boolean differ(List<Annotation> list1, List<Annotation> list2) {
-		if (list1.size() != list2.size()) {
-			return true;
-		}
-		return false;
-	}
+    /**
+     * A method to test whether there has been a significant change in the folding
+     * annotations for a source text.  The method works by comparing two lists of
+     * annotations, nominally the "old" and "new" annotations.  It returns true iff
+     * there is considered to be a "significant" difference in the two lists, where
+     * the meaning of "significant" is defined by the implementation of this method.
+     * 
+     * The default implementation provided here is a simplistic test of the difference
+     * between two lists, considering only their size.  This may work well enough much
+     * of the time as the comparisons between lists should be made very frequently,
+     * actually more frequently than the rate at which the typical human user will
+     * edit the program text so as to affect the AST so as to affect the lists.  Thus
+     * most changes of lists will entail some change in the number of elements at some
+     * point that will be observed here.  This will not work for certain very rapid
+     * edits of source text (e.g., rapid replacement of elements).
+     * 
+     * This method should be overridden in language-specific implementations of the
+     * folding updater where a more sophisticated test is desired.    
+     *     
+     * @param list1        A list of annotations (nominally the "old" annotations)
+     * @param list2        A list of annotations (nominally the "new" annotations)
+     * @return            true iff there has been a "significant" difference in the
+     *                     two given lists of annotations
+     * 
+     */
+    protected boolean differ(List<Annotation> list1, List<Annotation> list2) {
+        if (list1.size() != list2.size()) {
+            return true;
+        }
+        return false;
+    }
 
-	/**
-	 * Send a visitor to an AST representing a program in order to construct the
-	 * folding annotations.  Both the visitor type and the AST node type are language-
-	 * dependent, so this method is abstract.
-	 * 
-	 * @param newAnnotations	A map of annotations to text positions
-	 * @param annotations		A listing of the annotations in newAnnotations, that is,
-	 * 							a listing of keys to the map of text positions
-	 * @param ast				An Object that will be taken to represent an AST node
-	 */
-	public void sendVisitorToAST(HashMap<Annotation,Position> newAnnotations, 
-	        final List<Annotation> annotations, Object ast) {
+    /**
+     * Send a visitor to an AST representing a program in order to construct the
+     * folding annotations.  Both the visitor type and the AST node type are language-
+     * dependent, so this method is abstract.
+     * 
+     * @param newAnnotations    A map of annotations to text positions
+     * @param annotations        A listing of the annotations in newAnnotations, that is,
+     *                             a listing of keys to the map of text positions
+     * @param ast                An Object that will be taken to represent an AST node
+     */
+    public void sendVisitorToAST(HashMap<Annotation,Position> newAnnotations, 
+            final List<Annotation> annotations, Object ast) {
         for (CommonToken token: getTokens()) {
             int type = token.getType();
-			if (type==MULTI_COMMENT ||
+            if (type==MULTI_COMMENT ||
                 type==STRING_LITERAL ||
                 type==ASTRING_LITERAL||
                 type==VERBATIM_STRING||
@@ -271,14 +271,14 @@ public class FoldingUpdater {
                 }
             }
         }
-		Tree.CompilationUnit cu = (Tree.CompilationUnit) ast;
+        Tree.CompilationUnit cu = (Tree.CompilationUnit) ast;
         new Visitor() {
             @Override 
             public void visit(Tree.ImportList importList) {
                 super.visit(importList);
                 if (!importList.getImports().isEmpty()) {
                     ProjectionAnnotation ann = foldIfNecessary(importList);
-					if (ann!=null) {
+                    if (ann!=null) {
                         ann.markCollapsed();
                     }
                 }
@@ -288,25 +288,25 @@ public class FoldingUpdater {
                 super.visit(that);
                 foldIfNecessary(that);
             }*/
-			@Override 
-			public void visit(Tree.Body that) {
+            @Override 
+            public void visit(Tree.Body that) {
                 super.visit(that);
                 if (that.getToken()!=null) { //for "else if"
                     foldIfNecessary(that);
                 }
-			}
+            }
             @Override 
             public void visit(Tree.NamedArgumentList that) {
                 super.visit(that);
                 foldIfNecessary(that);
             }
-			@Override 
-			public void visit(Tree.ModuleDescriptor that) {
+            @Override 
+            public void visit(Tree.ModuleDescriptor that) {
                 super.visit(that);
                 foldIfNecessary(that);
-			}
-		}.visit(cu);
-	}
+            }
+        }.visit(cu);
+    }
 
     private ProjectionAnnotation foldIfNecessary(Node node) {
         CommonToken token = (CommonToken) node.getToken();
@@ -315,7 +315,7 @@ public class FoldingUpdater {
             return makeAnnotation(token, endToken);
         }
         else {
-        	return null;
+            return null;
         }
     }
     
