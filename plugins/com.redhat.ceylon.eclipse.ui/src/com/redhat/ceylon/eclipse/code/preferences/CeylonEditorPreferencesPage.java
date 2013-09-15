18ee19ca47765f50aaaf4e7999818f3268a35a31
Gavin King <gavin@hibernate.org>
2013-09-15 19:56:52 +0200

    improve presentation of preference/properties pages

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/preferences/CeylonEditorPreferencesPage.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/preferences/CeylonEditorPreferencesPage.java
@@ -22,7 +22,6 @@ import static com.redhat.ceylon.eclipse.code.editor.CeylonSourceViewerConfigurat
 
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.preference.BooleanFieldEditor;
-import org.eclipse.jface.preference.FieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.RadioGroupFieldEditor;
 import org.eclipse.jface.preference.ScaleFieldEditor;
@@ -133,7 +132,7 @@ public class CeylonEditorPreferencesPage
     protected Control createContents(Composite parent) {
         Link textEditorsLink = new Link(parent, 0);
         textEditorsLink.setLayoutData(GridDataFactory.swtDefaults().align(SWT.FILL, SWT.CENTER).indent(0, 6).create());
-        textEditorsLink.setText("Ceylon preferences. See '<a>Text Editors</a>' for general editor preferences.");
+        textEditorsLink.setText("See '<a>Text Editors</a>' for general editor preferences.");
         textEditorsLink.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent e) {
@@ -152,7 +151,12 @@ public class CeylonEditorPreferencesPage
                         "selectFont:com.redhat.ceylon.eclipse.ui.editorFont");
             }
         });
-        Group composite = new Group(parent, SWT.SHADOW_ETCHED_IN);
+
+        Label sep = new Label(parent, SWT.SEPARATOR | SWT.HORIZONTAL);
+        GridData sgd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+        sep.setLayoutData(sgd);
+
+        Composite composite = new Composite(parent, SWT.NONE);
         //composite.setText("Ceylon editor settings");
         GridData gd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
         gd.grabExcessHorizontalSpace=true;
@@ -165,46 +169,48 @@ public class CeylonEditorPreferencesPage
 
     @Override
     protected void createFieldEditors() {
+        otherSection();
         autocompletionSection();
+        autocloseSection();
         bracketHighlightingSection();        
         foldingSection();
-        autocloseSection();
-        otherSection();
     }
 
     private void bracketHighlightingSection() {
-        addField(new LabelFieldEditor("Bracket highlighting:",
-                getFieldEditorParent()));
+//        addField(new LabelFieldEditor("Bracket highlighting:",
+//                getFieldEditorParent()));
+        Composite group = createGroup(1, "Bracket highlighting");
         matchingBracket = new BooleanFieldEditor(MATCHING_BRACKET, 
                 "Highlight matching bracket", 
-                getFieldEditorParent());
+                getFieldEditorParent(group));
         matchingBracket.load();
         addField(matchingBracket);
         currentBracket = new BooleanFieldEditor(SELECTED_BRACKET, 
                 "Highlight selected bracket", 
-                getFieldEditorParent());
+                getFieldEditorParent(group));
         currentBracket.load();
         addField(currentBracket);
         enclosingBrackets = new BooleanFieldEditor(ENCLOSING_BRACKETS, 
                 "Highlight enclosing brackets", 
-                getFieldEditorParent());
+                getFieldEditorParent(group));
         enclosingBrackets.load();
         addField(enclosingBrackets);
 //        super.createDescriptionLabel(getFieldEditorParent()).setText("Autocompletion");
-        addField(new SpacerFieldEditor(getFieldEditorParent()));
+//        addField(new SpacerFieldEditor(getFieldEditorParent()));
     }
 
     private void autocompletionSection() {
-        addField(new LabelFieldEditor("Autocompletion:",
-                getFieldEditorParent()));
+//        addField(new LabelFieldEditor("Autocompletion:",
+//                getFieldEditorParent()));
+        Composite group = createGroup(1, "Autocompletion");
         autoInsert = new BooleanFieldEditor(AUTO_INSERT, 
                 "Auto-insert unique completions", 
-                getFieldEditorParent());
+                getFieldEditorParent(group));
         autoInsert.load();
         addField(autoInsert);
         autoActivation = new BooleanFieldEditor(AUTO_ACTIVATION, 
                 "Auto-activate completions list", 
-                getFieldEditorParent());
+                getFieldEditorParent(group));
         autoActivation.load();
         addField(autoActivation);
         String letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -213,12 +219,12 @@ public class CeylonEditorPreferencesPage
                 new String[][] { new String[] {"period", "."}, 
                                  new String[] {"letters", letters },
                                  new String[] {"both", "." + letters } }, 
-                getFieldEditorParent());
+                getFieldEditorParent(group));
         autoActivationChars.load();
         addField(autoActivationChars);
         autoActivationDelay = new ScaleWithLabelFieldEditor(AUTO_ACTIVATION_DELAY, 
                 "Auto-activation delay", 
-                getFieldEditorParent());
+                getFieldEditorParent(group));
         //autoActivationDelay.setValidRange(1, 9999);
         autoActivationDelay.setMinimum(1);
         autoActivationDelay.setMaximum(2000);
@@ -226,44 +232,58 @@ public class CeylonEditorPreferencesPage
         addField(autoActivationDelay);
         linkedMode = new BooleanFieldEditor(LINKED_MODE, 
                 "Use linked mode to complete argument lists", 
-                getFieldEditorParent());
+                getFieldEditorParent(group));
         linkedMode.load();
         addField(linkedMode);
-        addField(new SpacerFieldEditor(getFieldEditorParent()));        
+//        addField(new SpacerFieldEditor(getFieldEditorParent()));        
+    }
+
+    private Composite createGroup(int cols, String text) {
+        Composite parent = getFieldEditorParent();
+        Group group = new Group(parent, SWT.NONE);
+        group.setText(text);
+        GridLayout layout = new GridLayout(cols, true);
+        group.setLayout(layout);
+        GridData gd = new GridData();
+        gd.horizontalSpan=3;
+        group.setLayoutData(gd);
+        return group;
     }
 
     private void foldingSection() {
-        addField(new LabelFieldEditor("Folding:",
-                getFieldEditorParent()));
+//        addField(new LabelFieldEditor("Folding:",
+//                getFieldEditorParent()));
+        Composite group = createGroup(2, "Source Folding");
         autoFoldImports = new BooleanFieldEditor(AUTO_FOLD_IMPORTS, 
                 "Automatically fold import lists", 
-                getFieldEditorParent());
+                getFieldEditorParent(group));
         autoFoldImports.load();
         addField(autoFoldImports);
         autoFoldComments = new BooleanFieldEditor(AUTO_FOLD_COMMENTS, 
                 "Automatically fold comments", 
-                getFieldEditorParent());
+                getFieldEditorParent(group));
         autoFoldComments.load();
         addField(autoFoldComments);
-        addField(new SpacerFieldEditor(getFieldEditorParent()));
+//        addField(new SpacerFieldEditor(getFieldEditorParent()));
     }
 
     private void otherSection() {
-        addField(new LabelFieldEditor("Other:",
-                getFieldEditorParent()));
+//        addField(new LabelFieldEditor("Other:",
+//                getFieldEditorParent()));
+        Composite group = createGroup(1, "General");
         linkedModeRename = new BooleanFieldEditor(LINKED_MODE_RENAME, 
                 "Use linked mode for rename", 
-                getFieldEditorParent());
+                getFieldEditorParent(group));
         linkedModeRename.load();
         addField(linkedModeRename);
         smartCaret = new BooleanFieldEditor(SUB_WORD_NAVIGATION, 
                 "Smart caret positioning inside identifiers", 
-                getFieldEditorParent());
+                getFieldEditorParent(group));
         smartCaret.load();
         addField(smartCaret);
         pasteCorrectIndent = new BooleanFieldEditor(PASTE_CORRECT_INDENTATION, 
                 "Correct indentation of pasted code", 
-                getFieldEditorParent());
+                getFieldEditorParent(group));
         pasteCorrectIndent.load();
         addField(pasteCorrectIndent);
     }
@@ -275,15 +295,9 @@ public class CeylonEditorPreferencesPage
     }
 
     private void autocloseSection() {
-        addField(new LabelFieldEditor("Automatically close:",
-                getFieldEditorParent()));
-        Composite parent = getFieldEditorParent();
-        Composite group = new Composite(parent, SWT.NONE);
-        GridLayout layout = new GridLayout(3, true);
-        group.setLayout(layout);
-        GridData gd = new GridData();
-        gd.horizontalSpan=3;
-        group.setLayoutData(gd);
+//        addField(new LabelFieldEditor("Automatically close:",
+//                getFieldEditorParent()));
+        Composite group = createGroup(3, "Automatically close");
         closeParens = new BooleanFieldEditor(CLOSE_PARENS, 
                 "Parentheses", 
                 getFieldEditorParent(group));
@@ -314,55 +328,55 @@ public class CeylonEditorPreferencesPage
                 getFieldEditorParent(group));
         closeQuotes.load();
         addField(closeQuotes);
-        addField(new SpacerFieldEditor(getFieldEditorParent()));
+//        addField(new SpacerFieldEditor(getFieldEditorParent()));
     }
 
 }
 
-class LabelFieldEditor extends FieldEditor {
-
-    private Label label;
-
-    // All labels can use the same preference name since they don't
-    // store any preference.
-    public LabelFieldEditor(String value, Composite parent) {
-        super("label", value, parent);
-    }
-
-    // Adjusts the field editor to be displayed correctly
-    // for the given number of columns.
-    protected void adjustForNumColumns(int numColumns) {
-        ((GridData) label.getLayoutData()).horizontalSpan = numColumns;
-    }
-
-    // Fills the field editor's controls into the given parent.
-    protected void doFillIntoGrid(Composite parent, int numColumns) {
-        label = getLabelControl(parent);
-        
-        GridData gridData = new GridData();
-        gridData.horizontalSpan = numColumns;
-        gridData.horizontalAlignment = GridData.FILL;
-        gridData.grabExcessHorizontalSpace = false;
-        gridData.verticalAlignment = GridData.CENTER;
-        gridData.grabExcessVerticalSpace = false;
-        
-        label.setLayoutData(gridData);
-    }
-
-    // Returns the number of controls in the field editor.
-    public int getNumberOfControls() {
-        return 1;
-    }
-
-    // Labels do not persist any preferences, so these methods are empty.
-    protected void doLoad() {}
-    protected void doLoadDefault() {}
-    protected void doStore() {}
-}
-
-class SpacerFieldEditor extends LabelFieldEditor {
-    // Implemented as an empty label field editor.
-    public SpacerFieldEditor(Composite parent) {
-        super("", parent);
-    }
-}
+//class LabelFieldEditor extends FieldEditor {
+//
+//    private Label label;
+//
+//    // All labels can use the same preference name since they don't
+//    // store any preference.
+//    public LabelFieldEditor(String value, Composite parent) {
+//        super("label", value, parent);
+//    }
+//
+//    // Adjusts the field editor to be displayed correctly
+//    // for the given number of columns.
+//    protected void adjustForNumColumns(int numColumns) {
+//        ((GridData) label.getLayoutData()).horizontalSpan = numColumns;
+//    }
+//
+//    // Fills the field editor's controls into the given parent.
+//    protected void doFillIntoGrid(Composite parent, int numColumns) {
+//        label = getLabelControl(parent);
+//        
+//        GridData gridData = new GridData();
+//        gridData.horizontalSpan = numColumns;
+//        gridData.horizontalAlignment = GridData.FILL;
+//        gridData.grabExcessHorizontalSpace = false;
+//        gridData.verticalAlignment = GridData.CENTER;
+//        gridData.grabExcessVerticalSpace = false;
+//        
+//        label.setLayoutData(gridData);
+//    }
+//
+//    // Returns the number of controls in the field editor.
+//    public int getNumberOfControls() {
+//        return 1;
+//    }
+//
+//    // Labels do not persist any preferences, so these methods are empty.
+//    protected void doLoad() {}
+//    protected void doLoadDefault() {}
+//    protected void doStore() {}
+//}
+//
+//class SpacerFieldEditor extends LabelFieldEditor {
+//    // Implemented as an empty label field editor.
+//    public SpacerFieldEditor(Composite parent) {
+//        super("", parent);
+//    }
+//}
