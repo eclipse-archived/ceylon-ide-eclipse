a426cd5e58302e945354e0036088a339c65c2a0c
Gavin King <gavin@hibernate.org>
2013-09-15 18:56:44 +0200

    improve popups for managing module repo paths addresses #647

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/preferences/CeylonRepoPreferencesBlock.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/preferences/CeylonRepoPreferencesBlock.java
@@ -1,6 +1,5 @@
 package com.redhat.ceylon.eclipse.code.preferences;
 
-import static com.redhat.ceylon.cmr.maven.AetherUtils.getDefaultMavenSettings;
 import static com.redhat.ceylon.compiler.typechecker.TypeChecker.LANGUAGE_MODULE_VERSION;
 import static org.eclipse.jface.layout.GridDataFactory.swtDefaults;
 
@@ -16,14 +15,10 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.jdt.internal.ui.wizards.NewWizardMessages;
 import org.eclipse.jdt.internal.ui.wizards.TypedElementSelectionValidator;
 import org.eclipse.jdt.internal.ui.wizards.TypedViewerFilter;
 import org.eclipse.jdt.internal.ui.wizards.buildpaths.FolderSelectionDialog;
-import org.eclipse.jface.dialogs.Dialog;
-import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.IInputValidator;
-import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.ViewerFilter;
@@ -33,16 +28,13 @@ import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
@@ -61,9 +53,7 @@ import com.redhat.ceylon.eclipse.ui.CeylonResources;
 public class CeylonRepoPreferencesBlock {
 
     public interface ValidationCallback {
-
         void validationResultChange(boolean isValid, String message);
-
     }
 
     private IProject project;
@@ -113,7 +103,8 @@ public class CeylonRepoPreferencesBlock {
 
     public void performDefaults() {
         systemRepoText.setText("${ceylon.repo}");
-        outputRepoText.setText(Repositories.withConfig(ConfigParser.loadDefaultConfig(null)).getOutputRepository().getUrl());
+        outputRepoText.setText(Repositories.withConfig(ConfigParser.loadDefaultConfig(null))
+                .getOutputRepository().getUrl());
 
         projectLocalRepos = new ArrayList<String>();
         projectRemoteRepos = new ArrayList<String>();
@@ -232,7 +223,8 @@ public class CeylonRepoPreferencesBlock {
         outputRepoBrowseButton.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent e) {
-                IResource result = openSelectRelativeFolderDialog(composite);
+                IResource result = openSelectRelativeFolderDialog(composite, "Output Repository",
+                        "Select a workspace folder for compiled module archives");
                 if (result != null) {
                     String outputRepoUrl = "." + File.separator + result.getFullPath().removeFirstSegments(1);
                     outputRepoText.setText(outputRepoUrl);
@@ -263,7 +255,8 @@ public class CeylonRepoPreferencesBlock {
         addRepoButton.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent e) {
-                IResource result = openSelectRelativeFolderDialog(buttons);
+                IResource result = openSelectRelativeFolderDialog(buttons, "Add Repository",
+                        "Select a module repository in the workspace");
                 if( result != null ) {
                     String repo = "." + File.separator + result.getFullPath().removeFirstSegments(1);
                     addProjectRepo(repo, 0, true);
@@ -321,14 +314,18 @@ public class CeylonRepoPreferencesBlock {
                     public String isValid(String val) {
                         try {
                             // FIXME: we might want to validate it more than that: for example to check that it's http/https
-                            new URI(val);
+                            URI uri = new URI(val);
+                            if (!uri.isAbsolute()) {
+                                return "Unsupported URI: missing scheme";
+                            }
                             return null;
                         } catch (URISyntaxException e) {
                             return "Invalid URI: " + e.getReason();
                         }
                     }
                 };
-                InputDialog input = new InputDialog(buttons.getShell(), "Add Remote Repository", "Enter a remote repository URI", "http://", inputValidator);
+                InputDialog input = new InputDialog(buttons.getShell(), "Add Remote Repository", 
+                        "Enter URI of remote module repository", "http://", inputValidator);
                 int result = input.open();
                 if (result == InputDialog.OK) {
                 	int index = projectLocalRepos.size() + globalLookupRepos.size() + projectRemoteRepos.size();
@@ -504,7 +501,8 @@ public class CeylonRepoPreferencesBlock {
         return false;
     }
 
-    private IResource openSelectRelativeFolderDialog(Control control) {
+    private IResource openSelectRelativeFolderDialog(Control control, String title, 
+            String description) {
         IWorkspaceRoot root = project.getWorkspace().getRoot();
 
         Class<?>[] acceptedClasses= new Class[] { IProject.class, IFolder.class };
@@ -535,9 +533,9 @@ public class CeylonRepoPreferencesBlock {
 
         try {
             FolderSelectionDialog dialog= new FolderSelectionDialog(control.getShell(), lp, cp);
-            dialog.setTitle(NewWizardMessages.BuildPathsBlock_ChooseOutputFolderDialog_title);
+            dialog.setTitle(title);
             dialog.setValidator(validator);
-            dialog.setMessage(NewWizardMessages.BuildPathsBlock_ChooseOutputFolderDialog_description);
+            dialog.setMessage(description);
             dialog.addFilter(filter);
             dialog.setInput(root);
             dialog.setInitialSelection(container);
@@ -603,100 +601,6 @@ public class CeylonRepoPreferencesBlock {
             outputRepoUrl = outputRepoUrl.substring(2);
         }
         return project.getFolder(outputRepoUrl);
-    }
-    
-    private static class AetherRepositoryDialog extends Dialog {
-
-        private Label infoLabel;
-        private Text valueText;
-        private Text errorText;
-        private Button browseButton;
-        private String value = "";
-
-        public AetherRepositoryDialog(Shell shell) {
-            super(shell);
-        }
-
-        @Override
-        protected void configureShell(Shell shell) {
-            super.configureShell(shell);
-            shell.setText("Add Aether Repository");
-        }
-
-        @Override
-        protected void createButtonsForButtonBar(Composite parent) {
-            createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
-            createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
-        }
-
-        @Override
-        protected Control createDialogArea(Composite parent) {
-            GridLayout layout = new GridLayout(2, false);
-            layout.marginHeight = convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_MARGIN);
-            layout.marginWidth = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_MARGIN);
-            layout.verticalSpacing = convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_SPACING);
-            layout.horizontalSpacing = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_SPACING);
-
-            Composite composite = new Composite(parent, SWT.NONE);
-            composite.setLayout(layout);
-            composite.setLayoutData(swtDefaults().grab(true, true).align(SWT.FILL, SWT.FILL).create());
-
-            infoLabel = new Label(composite, SWT.LEFT | SWT.WRAP);
-            infoLabel.setText("Enter maven settings.xml file or leave it empty for defaults");
-            infoLabel.setLayoutData(swtDefaults().align(SWT.FILL, SWT.CENTER).span(2, 1).grab(true, false).minSize(convertHorizontalDLUsToPixels(IDialogConstants.MINIMUM_MESSAGE_AREA_WIDTH), 0).create());
-
-            valueText = new Text(composite, SWT.SINGLE | SWT.BORDER);
-            valueText.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL | GridData.HORIZONTAL_ALIGN_FILL));
-            valueText.addModifyListener(new ModifyListener() {
-                public void modifyText(ModifyEvent e) {
-                    value = valueText.getText();
-
-                    boolean isValid = true;
-                    if (!value.isEmpty()) {
-                        File f = new File(value);
-                        if (!f.exists() || !f.isFile()) {
-                            isValid = false;
-                        }
-                    }
-
-                    errorText.setText(isValid ? "" : "Invalid path to settings.xml");
-                    errorText.setVisible(!isValid);
-                    getButton(IDialogConstants.OK_ID).setEnabled(isValid);
-                }
-            });
-
-            browseButton = new Button(composite, SWT.PUSH);
-            browseButton.setText("Browse...");
-            browseButton.addSelectionListener(new SelectionAdapter() {
-                @Override
-                public void widgetSelected(SelectionEvent e) {
-                    String settingsXml = getDefaultMavenSettings();
-                    FileDialog fileDialog = new FileDialog(getShell(), SWT.SHEET);
-                    fileDialog.setFileName("settings.xml");
-                    fileDialog.setFilterPath(settingsXml.replace("settings.xml", ""));
-                    fileDialog.setFilterExtensions(new String[] { "*.xml" });
-                    String result = fileDialog.open();
-                    if (result != null) {
-                        valueText.setText(result);
-                    }
-                }
-            });
-            setButtonLayoutData(browseButton);
-
-            errorText = new Text(composite, SWT.READ_ONLY | SWT.WRAP);
-            errorText.setVisible(false);
-            errorText.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL | GridData.HORIZONTAL_ALIGN_FILL));
-            errorText.setBackground(errorText.getDisplay().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND));
-
-            applyDialogFont(composite);
-
-            return composite;
-        }
-
-        public String getValue() {
-            return value;
-        }
-
     }    
 
 }
