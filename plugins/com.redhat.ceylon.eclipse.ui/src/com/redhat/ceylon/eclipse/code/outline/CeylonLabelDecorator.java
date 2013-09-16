86773f9324619a7bda2bee88482dc7b08abef805
Gavin King <gavin@hibernate.org>
2013-09-16 11:58:29 +0200

    make types toplevel

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/outline/CeylonLabelDecorator.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/outline/CeylonLabelDecorator.java
@@ -4,38 +4,18 @@ package com.redhat.ceylon.eclipse.code.outline;
 import static com.redhat.ceylon.eclipse.ui.CeylonResources.CEYLON_ERR;
 import static com.redhat.ceylon.eclipse.ui.CeylonResources.CEYLON_WARN;
 
-import java.util.HashMap;
-
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.ListenerList;
-import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.layout.PixelConverter;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
-import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.viewers.IBaseLabelProvider;
 import org.eclipse.jface.viewers.IDecoration;
 import org.eclipse.jface.viewers.ILabelDecorator;
 import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.LabelProviderChangedEvent;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.dnd.DragSource;
-import org.eclipse.swt.dnd.DropTarget;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Caret;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.ScrollBar;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.Widget;
 
 import com.redhat.ceylon.eclipse.ui.CeylonPlugin;
 
@@ -268,166 +248,5 @@ public class CeylonLabelDecorator implements ILabelDecorator {
         else if (adornmentFlags == ERRORTICK_WARNING) {
             decoration.addOverlay(registry.getDescriptor(CEYLON_ERR));
         }
-    }
-}
-
-class ImageImageDescriptor extends ImageDescriptor {
-    private Image fImage;
-
-    public ImageImageDescriptor(Image image) {
-        super();
-        fImage= image;
-    }
-
-    public ImageData getImageData() {
-        return fImage.getImageData();
-    }
-
-    public boolean equals(Object obj) {
-        return (obj != null) && getClass().equals(obj.getClass()) && 
-        		fImage.equals(((ImageImageDescriptor)obj).fImage);
-    }
-
-    public int hashCode() {
-        return fImage.hashCode();
-    }
-}
-
-/**
- * A registry that maps <code>ImageDescriptors</code> to <code>Image</code>.
- */
-class ImageDescriptorRegistry {
-    private HashMap<ImageDescriptor,Image> fRegistry= new HashMap<ImageDescriptor,Image>(10);
-
-    private Display fDisplay;
-
-    /**
-     * Creates a new image descriptor registry for the current or 
-     * default display, respectively.
-     */
-    public ImageDescriptorRegistry() {
-        this(SWTUtil.getStandardDisplay());
-    }
-
-    /**
-     * Creates a new image descriptor registry for the given display. 
-     * All images managed by this registry will be disposed when the 
-     * display gets disposed.
-     * 
-     * @param display
-     *            the display the images managed by this registry are allocated for
-     */
-    public ImageDescriptorRegistry(Display display) {
-        fDisplay= display;
-        Assert.isNotNull(fDisplay);
-        hookDisplay();
-    }
-
-    /**
-     * Returns the image associated with the given image descriptor.
-     * 
-     * @param descriptor
-     *            the image descriptor for which the registry manages an image
-     * @return the image associated with the image descriptor or <code>null</code> 
-     * if the image descriptor can't create the requested image.
-     */
-    public Image get(ImageDescriptor descriptor) {
-        if (descriptor == null)
-            descriptor= ImageDescriptor.getMissingImageDescriptor();
-        Image result= (Image) fRegistry.get(descriptor);
-        if (result != null)
-            return result;
-        Assert.isTrue(fDisplay == SWTUtil.getStandardDisplay(), "Allocating image for wrong display."); //$NON-NLS-1$
-        result= descriptor.createImage();
-        if (result != null)
-            fRegistry.put(descriptor, result);
-        return result;
-    }
-
-    /**
-     * Disposes all images managed by this registry.
-     */
-    public void dispose() {
-        for(Image image: fRegistry.values()) {
-            image.dispose();
-        }
-        fRegistry.clear();
-    }
-
-    private void hookDisplay() {
-        fDisplay.disposeExec(new Runnable() {
-            public void run() {
-                dispose();
-            }
-        });
-    }
-}
-
-class SWTUtil {
-    /**
-     * Returns the standard display to be used. The method first checks, if the thread calling this method has an associated display. If so, this display is
-     * returned. Otherwise the method returns the default display.
-     */
-    public static Display getStandardDisplay() {
-        Display display;
-        display= Display.getCurrent();
-        if (display == null)
-            display= Display.getDefault();
-        return display;
-    }
-
-    /**
-     * Returns the shell for the given widget. If the widget doesn't represent a SWT object that manage a shell, <code>null</code> is returned.
-     * 
-     * @return the shell for the given widget
-     */
-    public static Shell getShell(Widget widget) {
-        if (widget instanceof Control)
-            return ((Control) widget).getShell();
-        if (widget instanceof Caret)
-            return ((Caret) widget).getParent().getShell();
-        if (widget instanceof DragSource)
-            return ((DragSource) widget).getControl().getShell();
-        if (widget instanceof DropTarget)
-            return ((DropTarget) widget).getControl().getShell();
-        if (widget instanceof Menu)
-            return ((Menu) widget).getParent().getShell();
-        if (widget instanceof ScrollBar)
-            return ((ScrollBar) widget).getParent().getShell();
-        return null;
-    }
-
-    /**
-     * Returns a width hint for a button control.
-     */
-    public static int getButtonWidthHint(Button button) {
-        button.setFont(JFaceResources.getDialogFont());
-        PixelConverter converter= new PixelConverter(button);
-        int widthHint= converter.convertHorizontalDLUsToPixels(IDialogConstants.BUTTON_WIDTH);
-        return Math.max(widthHint, button.computeSize(SWT.DEFAULT, SWT.DEFAULT, true).x);
-    }
-
-    /**
-     * Sets width and height hint for the button control. <b>Note:</b> This is a NOP if the button's layout data is not an instance of <code>GridData</code>.
-     * 
-     * @param button
-     *            the button for which to set the dimension hint
-     */
-    public static void setButtonDimensionHint(Button button) {
-        Assert.isNotNull(button);
-        Object gd= button.getLayoutData();
-        if (gd instanceof GridData) {
-            ((GridData) gd).widthHint= getButtonWidthHint(button);
-            ((GridData) gd).horizontalAlignment= GridData.FILL;
-        }
-    }
-
-    public static int getTableHeightHint(Table table, int rows) {
-        if (table.getFont().equals(JFaceResources.getDefaultFont()))
-            table.setFont(JFaceResources.getDialogFont());
-        int result= table.getItemHeight() * rows + table.getHeaderHeight();
-        if (table.getLinesVisible())
-            result+= table.getGridLineWidth() * (rows - 1);
-        return result;
     }
 }
