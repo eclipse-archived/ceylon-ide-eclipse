57a9880a8ed931f3af1f22cea596901ec4648ca4
Gavin King <gavin@hibernate.org>
2013-09-18 00:11:55 +0200

    content viewer to get syntax highlighting in Restore From Local History

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/viewer/CeylonTextViewerCreator.java
@@ -0,0 +1,15 @@
+package com.redhat.ceylon.eclipse.code.viewer;
+
+import org.eclipse.compare.CompareConfiguration;
+import org.eclipse.compare.IViewerCreator;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.widgets.Composite;
+
+public class CeylonTextViewerCreator implements IViewerCreator {
+
+    @Override
+    public Viewer createViewer(Composite parent, CompareConfiguration config) {
+        return new CeylonTextViewer(parent);
+    }
+
+}
