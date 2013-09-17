cf2e11cba610abac852fdc77b38822b5dc2676b7
Gavin King <gavin@hibernate.org>
2013-09-17 16:56:02 +0200

    pull in some stuff from JDT internal

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/html/SingleCharReader.java
@@ -0,0 +1,55 @@
+package com.redhat.ceylon.eclipse.code.html;
+
+import java.io.IOException;
+import java.io.Reader;
+
+
+/**
+ * <p>
+ * Moved into this package from <code>org.eclipse.jface.internal.text.revisions</code>.</p>
+ */
+public abstract class SingleCharReader extends Reader {
+
+	/**
+	 * @see Reader#read()
+	 */
+	public abstract int read() throws IOException;
+
+	/**
+	 * @see Reader#read(char[],int,int)
+	 */
+	public int read(char cbuf[], int off, int len) throws IOException {
+		int end= off + len;
+		for (int i= off; i < end; i++) {
+			int ch= read();
+			if (ch == -1) {
+				if (i == off)
+					return -1;
+				return i - off;
+			}
+			cbuf[i]= (char)ch;
+		}
+		return len;
+	}
+
+	/**
+	 * @see Reader#ready()
+	 */
+    public boolean ready() throws IOException {
+		return true;
+	}
+
+	/**
+	 * Returns the readable content as string.
+	 * @return the readable content as string
+	 * @exception IOException in case reading fails
+	 */
+	public String getString() throws IOException {
+		StringBuffer buf= new StringBuffer();
+		int ch;
+		while ((ch= read()) != -1) {
+			buf.append((char)ch);
+		}
+		return buf.toString();
+	}
+}
