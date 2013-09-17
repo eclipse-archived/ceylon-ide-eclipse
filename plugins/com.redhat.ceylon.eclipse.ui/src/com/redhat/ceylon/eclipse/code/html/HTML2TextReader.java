c2f256e13afe2b5e5a879fc907c1948d70f70666
Gavin King <gavin@hibernate.org>
2013-09-17 18:42:46 +0200

    unexternalize the strings

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/html/HTML2TextReader.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/html/HTML2TextReader.java
@@ -235,7 +235,7 @@ public class HTML2TextReader extends SubstitutionTextReader {
 
 		if ("li".equals(html)) //$NON-NLS-1$
 			// FIXME: this hard-coded prefix does not work for RTL languages, see https://bugs.eclipse.org/bugs/show_bug.cgi?id=91682
-			return LINE_DELIM + HTMLMessages.getString("HTML2TextReader.listItemPrefix"); //$NON-NLS-1$
+			return LINE_DELIM + "\t- ";
 
 		if ("/b".equals(html)) { //$NON-NLS-1$
 			stopBold();
