eclipse-archived/ceylon-ide-eclipse

commit 9f5a0cbd69070cd32280200cb3a1a21466c0ec3b

Fixes due to API changes in ArtifactContext (ceylon/ceylon-module-resolver#63)

plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/classpath/CeylonClasspathContainer.java
@@ -457,34 +457,22 @@ public class CeylonClasspathContainer implements IClasspathContainer {
 	public static IPath getSourceArchive(RepositoryManager provider,
 			Module module) {
         ArtifactContext ctx = new ArtifactContext(module.getNameAsString(), 
-        		module.getVersion());
-		ctx.setSuffix(ArtifactContext.SRC);
+        		module.getVersion(), ArtifactContext.SRC, ArtifactContext.MAVEN_SRC);
 		File srcArtifact = provider.getArtifact(ctx);
 		if (srcArtifact!=null) {
-		    if (srcArtifact.getPath().endsWith(".src")) {
+		    if (srcArtifact.getPath().endsWith(ArtifactContext.SRC)
+		    		|| srcArtifact.getPath().endsWith(ArtifactContext.MAVEN_SRC)) {
 	            return new Path(srcArtifact.getPath());
 		    }
 		}
-        ctx.setSuffix("-sources.jar");
-        srcArtifact = provider.getArtifact(ctx);
-        if (srcArtifact!=null) {
-            return new Path(srcArtifact.getPath());
-        }
 		return null;
 	}
 
 	public static IPath getModuleArchive(RepositoryManager provider,
 			Module module) {
         ArtifactContext ctx = new ArtifactContext(module.getNameAsString(), 
-        		module.getVersion());
-		// try first with .car
-		ctx.setSuffix(ArtifactContext.CAR);
+        		module.getVersion(), ArtifactContext.CAR, ArtifactContext.JAR);
 		File moduleArtifact = provider.getArtifact(ctx);
-		if (moduleArtifact==null){
-			// try with .jar
-			ctx.setSuffix(ArtifactContext.JAR);
-			moduleArtifact = provider.getArtifact(ctx);
-		}
 		if (moduleArtifact!=null) {
 			return new Path(moduleArtifact.getPath());
 		}

commit b051255c21c96c418587098ae7cd23f3a9aee880

Fix for the maven source artifact download broken recently by 9f5a0cbd69070cd32280200cb3a1a21466c0ec3b

plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/classpath/CeylonClasspathContainer.java
@@ -456,15 +456,26 @@ public class CeylonClasspathContainer implements IClasspathContainer {
 
 	public static IPath getSourceArchive(RepositoryManager provider,
 			Module module) {
+	    // BEWARE : here the request to the provider is done in 2 steps, because if
+	    // we do this in a single step, the Aether repo might return the .jar
+	    // archive as a default result when not finding it with the .src extension.
+	    // In this case it will not try the second extension (-sources.jar). 
         ArtifactContext ctx = new ArtifactContext(module.getNameAsString(), 
-        		module.getVersion(), ArtifactContext.SRC, ArtifactContext.MAVEN_SRC);
+        		module.getVersion(), ArtifactContext.SRC);
 		File srcArtifact = provider.getArtifact(ctx);
 		if (srcArtifact!=null) {
-		    if (srcArtifact.getPath().endsWith(ArtifactContext.SRC)
-		    		|| srcArtifact.getPath().endsWith(ArtifactContext.MAVEN_SRC)) {
+		    if (srcArtifact.getPath().endsWith(ArtifactContext.SRC)) {
 	            return new Path(srcArtifact.getPath());
 		    }
 		}
+        ctx = new ArtifactContext(module.getNameAsString(), 
+                module.getVersion(), ArtifactContext.MAVEN_SRC);
+        srcArtifact = provider.getArtifact(ctx);
+        if (srcArtifact!=null) {
+            if (srcArtifact.getPath().endsWith(ArtifactContext.MAVEN_SRC)) {
+                return new Path(srcArtifact.getPath());
+            }
+        }
 		return null;
 	}
 