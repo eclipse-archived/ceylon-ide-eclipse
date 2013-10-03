0fba2406a7ef0fb6355e02bd216d903819bce78a
Gavin King <gavin@hibernate.org>
2013-10-03 13:48:24 +0200

    sanitize HTML in backend errors

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/util/AnnotationUtils.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/util/AnnotationUtils.java
@@ -66,12 +66,12 @@ public class AnnotationUtils {
      */
     public static String formatAnnotationList(List<Annotation> annotations) {
         if (annotations!=null && !annotations.isEmpty()) {
-        	if (annotations.size()==1) {
-        		return AnnotationUtils.formatSingleMessage(annotations.get(0));
-        	}
-        	else {
-        		return AnnotationUtils.formatMultipleMessages(annotations);
-        	}
+            if (annotations.size()==1) {
+                return AnnotationUtils.formatSingleMessage(annotations.get(0));
+            }
+            else {
+                return AnnotationUtils.formatMultipleMessages(annotations);
+            }
         }
         return null;
     }
@@ -80,48 +80,48 @@ public class AnnotationUtils {
      * @return true, if the given Annotation and Position are redundant, given the annotation
      * information in the given Map
      */
-	public static boolean addAndCheckDuplicateAnnotation(Map<Integer, List<Object>> map, 
-			Annotation annotation, Position position) {
-	    List<Object> annotationsAtPosition;
+    public static boolean addAndCheckDuplicateAnnotation(Map<Integer, List<Object>> map, 
+            Annotation annotation, Position position) {
+        List<Object> annotationsAtPosition;
 
-	    if (!map.containsKey(position.offset)) {
-			annotationsAtPosition = new ArrayList<Object>();
-			map.put(position.offset, annotationsAtPosition);
-		} else {
-		    annotationsAtPosition = map.get(position.offset);
-		}
+        if (!map.containsKey(position.offset)) {
+            annotationsAtPosition = new ArrayList<Object>();
+            map.put(position.offset, annotationsAtPosition);
+        } else {
+            annotationsAtPosition = map.get(position.offset);
+        }
 
-		// TODO this should call out to a language extension point first to see if the language can resolve duplicates
-		
-		// Check to see if an error code is present on the marker / annotation
-		Integer errorCode = -1;
+        // TODO this should call out to a language extension point first to see if the language can resolve duplicates
+        
+        // Check to see if an error code is present on the marker / annotation
+        Integer errorCode = -1;
 
-		if (annotation instanceof CeylonAnnotation) {
-			errorCode = ((CeylonAnnotation) annotation).getId();
-		} 
-		else if (annotation instanceof MarkerAnnotation) {
-			errorCode = ((MarkerAnnotation) annotation).getMarker()
-					.getAttribute(MarkerCreator.ERROR_CODE_KEY, -1);
-		}
-		
-		// Fall back to comparing the text associated with this annotation
-		if (errorCode == -1) {
-			if (!annotationsAtPosition.contains(annotation.getText())) {
-			    annotationsAtPosition.add(annotation.getText());
-				return false;
-			}			
-		} 
-		else if (!annotationsAtPosition.contains(errorCode)) {
-		    annotationsAtPosition.add(errorCode);
-			return false;	
-		}
+        if (annotation instanceof CeylonAnnotation) {
+            errorCode = ((CeylonAnnotation) annotation).getId();
+        } 
+        else if (annotation instanceof MarkerAnnotation) {
+            errorCode = ((MarkerAnnotation) annotation).getMarker()
+                    .getAttribute(MarkerCreator.ERROR_CODE_KEY, -1);
+        }
+        
+        // Fall back to comparing the text associated with this annotation
+        if (errorCode == -1) {
+            if (!annotationsAtPosition.contains(annotation.getText())) {
+                annotationsAtPosition.add(annotation.getText());
+                return false;
+            }            
+        } 
+        else if (!annotationsAtPosition.contains(errorCode)) {
+            annotationsAtPosition.add(errorCode);
+            return false;    
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * @return the list of Annotations that reside at the given line for the given ISourceViewer
-	 */
+    /**
+     * @return the list of Annotations that reside at the given line for the given ISourceViewer
+     */
     public static List<Annotation> getAnnotationsForLine(ISourceViewer viewer, final int line) {
         final IDocument document= viewer.getDocument();
         IPositionPredicate posPred= new IPositionPredicate() {
@@ -150,7 +150,7 @@ public class AnnotationUtils {
     public static boolean positionIsAtLine(Position position, IDocument document, int line) {
         if (position.getOffset() > -1 && position.getLength() > -1) {
             try {
-            	// RMF 11/10/2006 - This used to add 1 to the line computed by the document,
+                // RMF 11/10/2006 - This used to add 1 to the line computed by the document,
                 // which appears to be bogus. First, it didn't work right (annotation hovers
                 // never appeared); second, the line passed in comes from the Eclipse
                 // framework, so it should be consistent (wrt the index base) with what the
@@ -183,54 +183,54 @@ public class AnnotationUtils {
      * debugger breakpoint annotations and source folding annotations)
      */
     public static List<Annotation> getAnnotations(ISourceViewer viewer, IPositionPredicate posPred) {
-		IAnnotationModel model = getAnnotationModel(viewer);
-		if (model == null)
-			return null;
-		List<Annotation> annotations = new ArrayList<Annotation>();
-		Iterator<?> iterator = model.getAnnotationIterator();
+        IAnnotationModel model = getAnnotationModel(viewer);
+        if (model == null)
+            return null;
+        List<Annotation> annotations = new ArrayList<Annotation>();
+        Iterator<?> iterator = model.getAnnotationIterator();
 
-		Map<Integer, List<Object>> map = new HashMap<Integer, List<Object>>();
+        Map<Integer, List<Object>> map = new HashMap<Integer, List<Object>>();
 
-		while (iterator.hasNext()) {
-			Annotation annotation = (Annotation) iterator.next();
-			Position position = model.getPosition(annotation);
-			
-			if (annotation instanceof MarkerAnnotation) {
-				try {
-					if (((MarkerAnnotation) annotation).getMarker().getType()
-							.equals(PROBLEM_MARKER_ID)) {
-						continue;
-					}
-				} 
-				catch (CoreException e) {
-					e.printStackTrace();
-					continue;
-				}
-			}
-			if (position == null)
-				continue;
-			if (!posPred.matchPosition(position))
-				continue;
-			if (annotation instanceof AnnotationBag) {
-				AnnotationBag bag = (AnnotationBag) annotation;
-				for (Iterator<?> e = bag.iterator(); e.hasNext(); ) {
-					Annotation bagAnnotation = (Annotation) e.next();
-					position = model.getPosition(bagAnnotation);
-					if (position != null
-							&& includeAnnotation(bagAnnotation, position)
-							&& !addAndCheckDuplicateAnnotation(map, bagAnnotation, position))
-						annotations.add(bagAnnotation);
+        while (iterator.hasNext()) {
+            Annotation annotation = (Annotation) iterator.next();
+            Position position = model.getPosition(annotation);
+            
+            if (annotation instanceof MarkerAnnotation) {
+                try {
+                    if (((MarkerAnnotation) annotation).getMarker().getType()
+                            .equals(PROBLEM_MARKER_ID)) {
+                        continue;
+                    }
+                } 
+                catch (CoreException e) {
+                    e.printStackTrace();
+                    continue;
+                }
+            }
+            if (position == null)
+                continue;
+            if (!posPred.matchPosition(position))
+                continue;
+            if (annotation instanceof AnnotationBag) {
+                AnnotationBag bag = (AnnotationBag) annotation;
+                for (Iterator<?> e = bag.iterator(); e.hasNext(); ) {
+                    Annotation bagAnnotation = (Annotation) e.next();
+                    position = model.getPosition(bagAnnotation);
+                    if (position != null
+                            && includeAnnotation(bagAnnotation, position)
+                            && !addAndCheckDuplicateAnnotation(map, bagAnnotation, position))
+                        annotations.add(bagAnnotation);
 
-				}
-			} else {
-				if (includeAnnotation(annotation, position)
-						&& !addAndCheckDuplicateAnnotation(map, annotation, position)) {
-					annotations.add(annotation);
-				}
-			}
-		}
-		return annotations;
-	}
+                }
+            } else {
+                if (includeAnnotation(annotation, position)
+                        && !addAndCheckDuplicateAnnotation(map, annotation, position)) {
+                    annotations.add(annotation);
+                }
+            }
+        }
+        return annotations;
+    }
 
     /**
      * Check preferences, etc., to determine whether this annotation is actually showing.
@@ -250,108 +250,108 @@ public class AnnotationUtils {
         return formatAnnotationList(getAnnotationsForLine(sourceViewer, lineNumber));
     }
 
-	public static void addMessageImageAndLabel(Annotation message,
-			StringBuffer buffer) {
-		URL icon = null;
-		String text = null;
-	    if (message instanceof CeylonAnnotation) {
-	    	text = HTMLPrinter.convertToHTMLContent(message.getText());
-	    	icon = getProblemIcon(((CeylonAnnotation) message).getSeverity());
-	    }
-	    else if (message instanceof CeylonInitializerAnnotation) {
+    public static void addMessageImageAndLabel(Annotation message,
+            StringBuffer buffer) {
+        URL icon = null;
+        String text = null;
+        if (message instanceof CeylonAnnotation) {
+            text = HTMLPrinter.convertToHTMLContent(message.getText());
+            icon = getProblemIcon(((CeylonAnnotation) message).getSeverity());
+        }
+        else if (message instanceof CeylonInitializerAnnotation) {
             text = message.getText();
             icon = CeylonHover.fileUrl("information.gif");
         }
-	    else if (message instanceof RefinementAnnotation) {
-	    	Declaration dec = ((RefinementAnnotation) message).getDeclaration();
-	    	icon = dec.isFormal() ? CeylonHover.fileUrl("implm_co.gif") : CeylonHover.fileUrl("over_co.gif");
-			text = "refines&nbsp;&nbsp;<tt>" + HTMLPrinter.convertToHTMLContent(CeylonContentProposer.getDescriptionFor(dec))
-					+ "</tt>&nbsp;&nbsp;declared by&nbsp;&nbsp;<tt><b>" + ((TypeDeclaration) dec.getContainer()).getName() + 
-					"</b></tt>";
-	    }
-	    else if (message instanceof MarkerAnnotation) {
-	    	try {
-		    	Integer sev = (Integer)((MarkerAnnotation) message).getMarker()
-		    			.getAttribute(IMarker.SEVERITY);
-				icon = getProblemIcon(sev);
-				text = "[Backend error] " + ((MarkerAnnotation) message).getMarker()
-						.getAttribute(IMarker.MESSAGE);
-			} 
-	    	catch (CoreException e) {
-				e.printStackTrace();
-			}
-	    }
-	    else if (message!=null && SEARCH_ANNOTATION_TYPE.equals(message.getType())) {
-	    	text = "<b>Search result</b>";
-	    	icon = CeylonHover.fileUrl("find_obj.gif");
-	    }
+        else if (message instanceof RefinementAnnotation) {
+            Declaration dec = ((RefinementAnnotation) message).getDeclaration();
+            icon = dec.isFormal() ? CeylonHover.fileUrl("implm_co.gif") : CeylonHover.fileUrl("over_co.gif");
+            text = "refines&nbsp;&nbsp;<tt>" + HTMLPrinter.convertToHTMLContent(CeylonContentProposer.getDescriptionFor(dec))
+                    + "</tt>&nbsp;&nbsp;declared by&nbsp;&nbsp;<tt><b>" + ((TypeDeclaration) dec.getContainer()).getName() + 
+                    "</b></tt>";
+        }
+        else if (message instanceof MarkerAnnotation) {
+            try {
+                Integer sev = (Integer)((MarkerAnnotation) message).getMarker()
+                        .getAttribute(IMarker.SEVERITY);
+                icon = getProblemIcon(sev);
+                String msg = (String)((MarkerAnnotation) message).getMarker()
+                        .getAttribute(IMarker.MESSAGE);
+                text = "[Backend error] " + HTMLPrinter.convertToHTMLContent(msg).replace("\n", "<br/>");
+            } 
+            catch (CoreException e) {
+                e.printStackTrace();
+            }
+        }
+        else if (message!=null && SEARCH_ANNOTATION_TYPE.equals(message.getType())) {
+            text = "<b>Search result</b>";
+            icon = CeylonHover.fileUrl("find_obj.gif");
+        }
         else if (message!=null && TODO_ANNOTATION_TYPE.equals(message.getType())) {
             text = "<b>Task</b><p>" + message.getText() + "</p>";
             icon = CeylonHover.fileUrl("tasks_tsk.gif");
         }
-	    if (icon!=null) {
-	    	CeylonHover.addImageAndLabel(buffer, null, icon.toExternalForm(), 16, 16, text, 20, 2);
-	    }
-	}
+        if (icon!=null) {
+            CeylonHover.addImageAndLabel(buffer, null, icon.toExternalForm(), 16, 16, text, 20, 2);
+        }
+    }
 
-	public static URL getProblemIcon(Integer severity) {
-		if (severity==null) {
-			return null;
-		}
-		if (severity.intValue()==SEVERITY_ERROR) {
-			return CeylonHover.fileUrl("error_obj.gif");
-		}
-		else if (severity.intValue()==SEVERITY_WARNING) {
-			return CeylonHover.fileUrl("warning_obj.gif");
-		}
-		else {
-			return null;
-		}
-	}
+    public static URL getProblemIcon(Integer severity) {
+        if (severity==null) {
+            return null;
+        }
+        if (severity.intValue()==SEVERITY_ERROR) {
+            return CeylonHover.fileUrl("error_obj.gif");
+        }
+        else if (severity.intValue()==SEVERITY_WARNING) {
+            return CeylonHover.fileUrl("warning_obj.gif");
+        }
+        else {
+            return null;
+        }
+    }
 
-	/**
-	 * Formats a message as HTML text.
-	 */
-	public static String formatSingleMessage(Annotation message) {
-	    StringBuffer buffer= new StringBuffer();
-	    HTMLPrinter.insertPageProlog(buffer, 0, getStyleSheet());
-	    addMessageImageAndLabel(message, buffer);
-	    HTMLPrinter.addPageEpilog(buffer);
-	    return buffer.toString();
-	}
+    /**
+     * Formats a message as HTML text.
+     */
+    public static String formatSingleMessage(Annotation message) {
+        StringBuffer buffer= new StringBuffer();
+        HTMLPrinter.insertPageProlog(buffer, 0, getStyleSheet());
+        addMessageImageAndLabel(message, buffer);
+        HTMLPrinter.addPageEpilog(buffer);
+        return buffer.toString();
+    }
 
-	/**
-	 * Formats several messages as HTML text.
-	 */
-	public static String formatMultipleMessages(List<Annotation> messages) {
-	    StringBuffer buffer= new StringBuffer();
-	    HTMLPrinter.insertPageProlog(buffer, 0, getStyleSheet());
-	    CeylonHover.addImageAndLabel(buffer, null, CeylonHover.fileUrl("errorwarning_tab.gif").toExternalForm(),
-	    		16, 16, "Multiple messages at this line:", 20, 2);
-	    buffer.append("<hr/>");
-	    for (Annotation message: messages) {
-	    	addMessageImageAndLabel(message, buffer);
-	    }
-	    HTMLPrinter.addPageEpilog(buffer);
-	    return buffer.toString();
-	}
-	
-	private static String fgStyleSheet;
+    /**
+     * Formats several messages as HTML text.
+     */
+    public static String formatMultipleMessages(List<Annotation> messages) {
+        StringBuffer buffer= new StringBuffer();
+        HTMLPrinter.insertPageProlog(buffer, 0, getStyleSheet());
+        CeylonHover.addImageAndLabel(buffer, null, CeylonHover.fileUrl("errorwarning_tab.gif").toExternalForm(),
+                16, 16, "Multiple messages at this line:", 20, 2);
+        buffer.append("<hr/>");
+        for (Annotation message: messages) {
+            addMessageImageAndLabel(message, buffer);
+        }
+        HTMLPrinter.addPageEpilog(buffer);
+        return buffer.toString();
+    }
+    
+    private static String fgStyleSheet;
 
-	public static String getStyleSheet() {
-		if (fgStyleSheet == null)
-			fgStyleSheet= CeylonHover.loadStyleSheet() ;
-		//Color c = CeylonTokenColorer.getCurrentThemeColor("messageHover");
-		//String color = toHexString(c.getRed()) + toHexString(c.getGreen()) + toHexString(c.getBlue());
-		String css= fgStyleSheet; //+ "body { background-color: #" + color + " }";
-		if (css != null) {
-			FontData fontData= JFaceResources.getFontRegistry()
-					.getFontData(PreferenceConstants.APPEARANCE_JAVADOC_FONT)[0];
-			css= HTMLPrinter.convertTopLevelFont(css, fontData);
-		}
-
-		return css;
-	}
+    public static String getStyleSheet() {
+        if (fgStyleSheet == null)
+            fgStyleSheet= CeylonHover.loadStyleSheet() ;
+        //Color c = CeylonTokenColorer.getCurrentThemeColor("messageHover");
+        //String color = toHexString(c.getRed()) + toHexString(c.getGreen()) + toHexString(c.getBlue());
+        String css= fgStyleSheet; //+ "body { background-color: #" + color + " }";
+        if (css != null) {
+            FontData fontData= JFaceResources.getFontRegistry()
+                    .getFontData(PreferenceConstants.APPEARANCE_JAVADOC_FONT)[0];
+            css= HTMLPrinter.convertTopLevelFont(css, fontData);
+        }
+        return css;
+    }
 
 }
 
