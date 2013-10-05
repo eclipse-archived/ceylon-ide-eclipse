151998f2e81ca09245a2367f526f828eb4431259
Gavin King <gavin@hibernate.org>
2013-10-06 01:04:47 +0200

    squash npe

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/outline/CeylonOutlineBuilder.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/outline/CeylonOutlineBuilder.java
@@ -80,7 +80,11 @@ public class CeylonOutlineBuilder extends Visitor {
 	    IFile file = cpc.getProject()==null || cpc.getPath()==null ? null :
 	            cpc.getProject().getFile(cpc.getPath());
 	    Tree.CompilationUnit rootNode = cpc.getRootNode();
-	    if (rootNode==null || rootNode.getUnit()==null) return null;
+	    if (rootNode==null || 
+	        rootNode.getUnit()==null || 
+	        rootNode.getStartIndex()==null) {
+	        return null;
+	    }
 	    CeylonOutlineNode modelRoot = createTopItem(rootNode, file);
 	    itemStack.push(modelRoot);
 		try {
