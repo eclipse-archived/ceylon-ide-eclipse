c9db9cabbcc0b7277d4cd35ce7f7e3694b45d01a
Gavin King <gavin@hibernate.org>
2013-10-09 11:21:49 +0200

    add buttons to switch between list/tree view in search results

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/ui/CeylonResources.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/ui/CeylonResources.java
@@ -96,4 +96,6 @@ public interface CeylonResources {
     public static final String PACKAGE_MODE = "package_mode";
     public static final String UNIT_MODE = "unit_mode";
     
+    public static final String FLAT_MODE = "flat_mode";
+    public static final String TREE_MODE = "tree_mode";
 }
