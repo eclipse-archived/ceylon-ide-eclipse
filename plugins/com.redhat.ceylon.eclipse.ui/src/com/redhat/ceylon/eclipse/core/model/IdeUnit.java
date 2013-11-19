7fbc9338a9d3e0e422bcaeb3abf879cc566cb64c
David Festal <david.festal@serli.com>
2013-11-19 14:47:28 +0100

    add a module getter

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/model/IdeUnit.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/model/IdeUnit.java
@@ -1,6 +1,10 @@
 package com.redhat.ceylon.eclipse.core.model;
 
 import com.redhat.ceylon.compiler.typechecker.model.Unit;
+import com.redhat.ceylon.eclipse.core.model.loader.JDTModule;
 
 public abstract class IdeUnit extends Unit {
+    public JDTModule getModule() {
+        return (JDTModule) getPackage().getModule();
+    }
 }
