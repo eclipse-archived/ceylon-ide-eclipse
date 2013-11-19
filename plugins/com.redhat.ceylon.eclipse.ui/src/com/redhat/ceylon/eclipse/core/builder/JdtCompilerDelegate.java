6d50de35e08e5d7931a367a00ea3d8d46ddd3799
David Festal <david.festal@serli.com>
2013-11-19 12:55:13 +0100

    Add PhasedUnits for the modules defined in source files (as required by the compiler)

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/builder/JdtCompilerDelegate.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/builder/JdtCompilerDelegate.java
@@ -3,27 +3,46 @@ package com.redhat.ceylon.eclipse.core.builder;
 import static com.redhat.ceylon.compiler.java.util.Util.quoteIfJavaKeyword;
 import static com.redhat.ceylon.compiler.typechecker.io.impl.Helper.computeRelativePath;
 
+import java.io.File;
 import java.lang.ref.WeakReference;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
+import javax.tools.JavaFileManager;
+import javax.tools.JavaFileObject;
+
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.internal.core.util.Util;
 
 import com.redhat.ceylon.compiler.java.codegen.CeylonCompilationUnit;
 import com.redhat.ceylon.compiler.java.loader.CeylonClassReader;
 import com.redhat.ceylon.compiler.java.tools.CeylonLog;
+import com.redhat.ceylon.compiler.java.tools.CeylonPhasedUnit;
 import com.redhat.ceylon.compiler.java.tools.LanguageCompiler.CompilerDelegate;
 import com.redhat.ceylon.compiler.loader.SourceDeclarationVisitor;
 import com.redhat.ceylon.compiler.loader.mirror.ClassMirror;
 import com.redhat.ceylon.compiler.typechecker.TypeChecker;
+import com.redhat.ceylon.compiler.typechecker.analyzer.AnalysisError;
 import com.redhat.ceylon.compiler.typechecker.analyzer.ModuleManager;
 import com.redhat.ceylon.compiler.typechecker.context.PhasedUnit;
 import com.redhat.ceylon.compiler.typechecker.context.PhasedUnits;
 import com.redhat.ceylon.compiler.typechecker.io.VirtualFile;
+import com.redhat.ceylon.compiler.typechecker.model.Unit;
+import com.redhat.ceylon.compiler.typechecker.parser.RecognitionError;
+import com.redhat.ceylon.compiler.typechecker.tree.Message;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.CompilationUnit;
+import com.redhat.ceylon.eclipse.core.model.ProjectSourceFile;
 import com.redhat.ceylon.eclipse.core.model.loader.JDTModelLoader;
+import com.sun.tools.javac.file.JavacFileManager;
 import com.sun.tools.javac.tree.JCTree.JCCompilationUnit;
 import com.sun.tools.javac.util.Context;
 import com.sun.tools.javac.util.Names;
+import com.sun.tools.javac.util.Position;
+import com.sun.tools.javac.util.Position.LineMap;
 
 final class JdtCompilerDelegate implements CompilerDelegate {
 	private final JDTModelLoader modelLoader;
@@ -60,8 +79,44 @@ final class JdtCompilerDelegate implements CompilerDelegate {
     
     @Override
     public void visitModules(PhasedUnits phasedUnits) {
-        // Do nothing : 
-        //   alreadyDone by the IncrementalBuilder before calling the Compiler for binary generation
+        Set<ProjectSourceFile> compiledModules = new HashSet<>();  
+        for (PhasedUnit pu : phasedUnits.getPhasedUnits()) {
+            Unit unit = pu.getPackage().getModule().getUnit();
+            if (unit instanceof ProjectSourceFile) {
+                compiledModules.add((ProjectSourceFile) unit);
+            }
+        }
+        for (ProjectSourceFile compiledModule : compiledModules) {
+            PhasedUnit pu = compiledModule.getPhasedUnit();
+            boolean hasErrors = false;
+            for (Message e : pu.getCompilationUnit().getErrors()) {
+                if ( (e instanceof RecognitionError) || 
+                     (e instanceof AnalysisError)) {
+                    hasErrors = true;
+                }
+            }
+            if (! hasErrors) {
+                IFile moduleResource = compiledModule.getFileResource();
+                File moduleFile = moduleResource.getRawLocation().toFile();
+                Context context = contextRef.get();
+                assert(context != null);
+                JavacFileManager fileManager = (JavacFileManager) context.get(JavaFileManager.class);
+                Iterator<? extends JavaFileObject> files = fileManager.getJavaFileObjects(moduleFile).iterator();
+                if (files.hasNext()) {
+                    JavaFileObject fileObject = files.next();                    
+                    char[] chars = new char[0];
+                    try {
+                        chars = Util.getResourceContentsAsCharArray(moduleResource);
+                    } catch (JavaModelException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    }
+                    LineMap map = Position.makeLineMap(chars, chars.length, false);
+                    CeylonPhasedUnit cpu = new CeylonPhasedUnit(pu, fileObject, map);
+                    phasedUnits.addPhasedUnit(pu.getUnitFile(), cpu);
+                }
+            }
+        }
     }
 
     @Override
