aeff782c77639b0cfdec87d8b40d1929725079a4
David Festal <david.festal@serli.com>
2014-01-13 17:44:01 +0100

    New feature and plugin to provide Android compatibility

---
+++ plugins/com.redhat.ceylon.eclipse.android.plugin/src/com/redhat/ceylon/eclipse/android/plugin/CeylonAndroidPlugin.java
@@ -0,0 +1,44 @@
+package com.redhat.ceylon.eclipse.android.plugin;
+
+import java.io.IOException;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+public class CeylonAndroidPlugin extends AbstractUIPlugin {
+
+    public static final String PLUGIN_ID = "com.redhat.ceylon.eclipse.android.plugin";
+        
+    private static CeylonAndroidPlugin plugin;
+    
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+    
+    public static CeylonAndroidPlugin getDefault() {
+        return plugin;
+    }
+
+    public static void logInfo(String msg) {
+        plugin.getLog().log(new Status(IStatus.INFO, PLUGIN_ID, msg));
+    }
+
+    public static void logInfo(String msg, IOException e) {
+        plugin.getLog().log(new Status(IStatus.INFO, PLUGIN_ID, msg, e));
+    }
+
+    public static void logError(String msg, Exception e) {
+        plugin.getLog().log(new Status(IStatus.ERROR, PLUGIN_ID, msg, e));
+    }
+
+}
