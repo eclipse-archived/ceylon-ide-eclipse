ca7a6ae265a434dd9100aba8f1d0db8ed4aeb2b7
gavinking <gavin@ceylon-lang.org>
2014-02-07 12:16:13 +0100

    correctly handle generics

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/correct/ChangeRefiningTypeProposal.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/correct/ChangeRefiningTypeProposal.java
@@ -3,6 +3,7 @@ package com.redhat.ceylon.eclipse.code.correct;
 import static com.redhat.ceylon.eclipse.util.FindUtils.findDeclaration;
 
 import java.util.Collection;
+import java.util.Collections;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
@@ -10,6 +11,8 @@ import org.eclipse.ltk.core.refactoring.TextFileChange;
 import org.eclipse.text.edits.ReplaceEdit;
 
 import com.redhat.ceylon.compiler.typechecker.model.Declaration;
+import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
+import com.redhat.ceylon.compiler.typechecker.model.TypeDeclaration;
 import com.redhat.ceylon.compiler.typechecker.model.TypedDeclaration;
 import com.redhat.ceylon.compiler.typechecker.tree.Node;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
@@ -20,11 +23,14 @@ public class ChangeRefiningTypeProposal {
 	        Tree.CompilationUnit cu, Collection<ICompletionProposal> proposals,
 	        Node node) {
 	    Tree.Declaration decNode = findDeclaration(cu, node);
-	    Declaration rd = decNode.getDeclarationModel()
-	    		.getRefinedDeclaration();
+	    Declaration dec = decNode.getDeclarationModel();
+		Declaration rd = dec.getRefinedDeclaration();
 	    if (rd instanceof TypedDeclaration) {
-	    	String type = ((TypedDeclaration) rd).getType()
-	    			.getProducedTypeName(decNode.getUnit());
+	    	ProducedType supertype = ((TypeDeclaration) dec.getContainer()).getType()
+	    			.getSupertype((TypeDeclaration) rd.getContainer());
+	    	ProducedType t = ((TypedDeclaration) rd).getProducedTypedReference(supertype, 
+	    			Collections.<ProducedType>emptyList()).getType();
+	    	String type = t.getProducedTypeName(decNode.getUnit());
 	    	TextFileChange change = new TextFileChange("Change Type", file);
 	    	change.setEdit(new ReplaceEdit(node.getStartIndex(), 
 	    			node.getStopIndex()-node.getStartIndex()+1, 
