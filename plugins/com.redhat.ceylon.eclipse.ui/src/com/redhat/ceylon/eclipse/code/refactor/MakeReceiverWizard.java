e9b44312e00e9ca85156afda4aa54f1511fb96be
gavinking <gavin@ceylon-lang.org>
2014-02-20 21:19:44 +0100

    new Make Receiver refactoring

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/refactor/MakeReceiverWizard.java
@@ -0,0 +1,20 @@
+package com.redhat.ceylon.eclipse.code.refactor;
+
+import org.eclipse.ltk.ui.refactoring.RefactoringWizard;
+
+public class MakeReceiverWizard extends RefactoringWizard {
+    public MakeReceiverWizard(MakeReceiverRefactoring refactoring) {
+        super(refactoring, DIALOG_BASED_USER_INTERFACE
+                | PREVIEW_EXPAND_FIRST_NODE);
+        setDefaultPageTitle(refactoring.getName());
+    }
+    
+    @Override
+    protected void addUserInputPages() {
+        addPage(new MakeReceiverInputPage(getRefactoring().getName()));
+    }
+    
+//    public ExtractFunctionRefactoring getExtractFunctionRefactoring() {
+//        return (ExtractFunctionRefactoring) getRefactoring();
+//    }
+}
--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/util/FindUtils.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/util/FindUtils.java
@@ -3,9 +3,12 @@ package com.redhat.ceylon.eclipse.util;
 import static com.redhat.ceylon.compiler.typechecker.model.Util.isOverloadedVersion;
 
 import com.redhat.ceylon.compiler.typechecker.model.Declaration;
+import com.redhat.ceylon.compiler.typechecker.model.Scope;
 import com.redhat.ceylon.compiler.typechecker.tree.Node;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
+import com.redhat.ceylon.compiler.typechecker.tree.Tree.CompilationUnit;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.Statement;
+import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
 
 public class FindUtils {
 
@@ -39,6 +42,29 @@ public class FindUtils {
         }
         else {
             return d;
+        }
+    }
+
+    public static Tree.Declaration getContainer(final Declaration dec,
+            Tree.CompilationUnit rootNode) {
+        class FindContainer extends Visitor {
+            final Scope container = dec.getContainer();
+            Tree.Declaration result;
+            @Override
+            public void visit(Tree.Declaration that) {
+                super.visit(that);
+                if (that.getDeclarationModel().equals(container)) {
+                    result = that;
+                }
+            }
+        }
+        FindContainer fc = new FindContainer();
+        rootNode.visit(fc);
+        if (fc.result instanceof Tree.Declaration) {
+            return (Tree.Declaration) fc.result;
+        }
+        else {
+            return null;
         }
     }
 
