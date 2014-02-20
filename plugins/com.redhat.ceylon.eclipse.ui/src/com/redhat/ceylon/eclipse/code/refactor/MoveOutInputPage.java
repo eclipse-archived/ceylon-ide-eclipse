436939e8394a738ed8dfb783218f0aebf1ff990e
gavinking <gavin@ceylon-lang.org>
2014-02-20 16:19:10 +0100

    allow customization of parameter name for #140

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/refactor/MoveOutInputPage.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/refactor/MoveOutInputPage.java
@@ -2,11 +2,18 @@ package com.redhat.ceylon.eclipse.code.refactor;
 
 import org.eclipse.ltk.ui.refactoring.UserInputWizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+import com.redhat.ceylon.eclipse.code.parse.CeylonTokenColorer;
 
 public class MoveOutInputPage extends UserInputWizardPage {
     public MoveOutInputPage(String name) {
@@ -19,6 +26,19 @@ public class MoveOutInputPage extends UserInputWizardPage {
         GridLayout layout = new GridLayout();
         layout.numColumns = 2;
         result.setLayout(layout);
+        Label label = new Label(result, SWT.RIGHT);  
+        label.setText("Parameter name: ");
+        final Text text = new Text(result, SWT.SINGLE|SWT.BORDER);
+        text.setText(getMoveOutRefactoring().getNewName());
+        text.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
+        text.addModifyListener(new ModifyListener() {
+            @Override
+            public void modifyText(ModifyEvent event) {
+                String name = text.getText();
+                validateIdentifier(name);
+                getMoveOutRefactoring().setNewName(name);
+            }
+        });
         final Button checkbox = new Button(result, SWT.CHECK);
         checkbox.setText("Make referenced locals shared");
         checkbox.addSelectionListener(new SelectionListener() {
@@ -29,11 +49,28 @@ public class MoveOutInputPage extends UserInputWizardPage {
             @Override
             public void widgetDefaultSelected(SelectionEvent event) {}
         });
+        text.addKeyListener(new SubwordIterator(text));
+        text.selectAll();
+        text.setFocus();
     }
 
     private MoveOutRefactoring getMoveOutRefactoring() {
         return (MoveOutRefactoring) getRefactoring();
     }
 
-
+    void validateIdentifier(String name) {
+        if (!name.matches("^[a-z_]\\w*$")) {
+            setErrorMessage("Not a legal Ceylon identifier");
+            setPageComplete(false);
+        }
+        else if (CeylonTokenColorer.keywords.contains(name)) {
+            setErrorMessage("'" + name + "' is a Ceylon keyword");
+            setPageComplete(false);
+        }
+        else {
+            setErrorMessage(null);
+            setPageComplete(true);
+        }
+    }
+    
 }
