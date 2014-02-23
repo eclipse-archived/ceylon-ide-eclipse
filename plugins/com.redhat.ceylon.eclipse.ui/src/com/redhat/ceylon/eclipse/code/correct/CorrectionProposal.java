2aa9e1c639a01ef46f2a8775365e7001b0f1ad39
gavinking <gavin@ceylon-lang.org>
2014-02-23 15:54:11 +0100

    get rid of nasty TextChangeOperation

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/correct/CorrectionProposal.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/correct/CorrectionProposal.java
@@ -10,44 +10,33 @@ package com.redhat.ceylon.eclipse.code.correct;
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 
+import static com.redhat.ceylon.eclipse.code.editor.EditorUtil.getCurrentEditor;
+import static com.redhat.ceylon.eclipse.code.editor.EditorUtil.performChange;
 import static com.redhat.ceylon.eclipse.code.outline.CeylonLabelProvider.MINOR_CHANGE;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IRewriteTarget;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.jface.text.contentassist.ICompletionProposalExtension5;
 import org.eclipse.jface.text.contentassist.ICompletionProposalExtension6;
 import org.eclipse.jface.text.contentassist.IContextInformation;
-import org.eclipse.jface.text.link.LinkedModeModel;
 import org.eclipse.jface.util.Util;
 import org.eclipse.jface.viewers.StyledString;
 import org.eclipse.ltk.core.refactoring.Change;
-import org.eclipse.ltk.core.refactoring.IUndoManager;
 import org.eclipse.ltk.core.refactoring.NullChange;
-import org.eclipse.ltk.core.refactoring.RefactoringCore;
-import org.eclipse.ltk.core.refactoring.RefactoringStatus;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.StyledText;
-import org.eclipse.swt.events.TraverseEvent;
-import org.eclipse.swt.events.TraverseListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.IEditorPart;
 
 
 /**
  * A quick fix/assist proposal based on a {@link Change}.
  */
 class CorrectionProposal 
-        implements ICompletionProposal, ICompletionProposalExtension5, ICompletionProposalExtension6 {
+        implements ICompletionProposal, ICompletionProposalExtension5, 
+                   ICompletionProposalExtension6 {
 
     private static final NullChange COMPUTING_CHANGE = 
             new NullChange("ChangeCorrectionProposal computing...");
@@ -90,103 +79,10 @@ class CorrectionProposal
     @Override
     public void apply(IDocument document) {
         try {
-            performChange(JavaPlugin.getActivePage().getActiveEditor(), 
-                    document, getChange(), getName());
+            performChange(getCurrentEditor(), document, getChange(), getName());
         } 
         catch (CoreException e) {
             e.printStackTrace();
-        }
-    }
-
-    /**
-     * Performs the change associated with this proposal.
-     * <p>
-     * Subclasses may extend, but must call the super implementation.
-     * 
-     * @param activeEditor the editor currently active or <code>null</code> if no editor is active
-     * @param document the document of the editor currently active or <code>null</code> if no editor
-     *            is visible
-     * @throws CoreException when the invocation of the change failed
-     */
-    protected static void performChange(IEditorPart activeEditor, 
-            IDocument document, Change change, String name) 
-                    throws CoreException {
-        StyledText disabledStyledText= null;
-        TraverseListener traverseBlocker= null;
-        
-        IRewriteTarget rewriteTarget= null;
-        try {
-            if (change != null) {
-                if (document != null) {
-                    LinkedModeModel.closeAllModels(document);
-                }
-                if (activeEditor != null) {
-                    rewriteTarget= (IRewriteTarget) activeEditor.getAdapter(IRewriteTarget.class);
-                    if (rewriteTarget != null) {
-                        rewriteTarget.beginCompoundChange();
-                    }
-                    /*
-                     * Workaround for https://bugs.eclipse.org/bugs/show_bug.cgi?id=195834#c7 :
-                     * During change execution, an EventLoopProgressMonitor can process the event queue while the text
-                     * widget has focus. When that happens and the user e.g. pressed a key, the event is prematurely
-                     * delivered to the text widget and screws up the document. Change execution fails or performs
-                     * wrong changes.
-                     * 
-                     * The fix is to temporarily disable the text widget.
-                     */
-                    Object control= activeEditor.getAdapter(Control.class);
-                    if (control instanceof StyledText) {
-                        disabledStyledText= (StyledText) control;
-                        if (disabledStyledText.getEditable()) {
-                            disabledStyledText.setEditable(false);
-                            traverseBlocker= new TraverseListener() {
-                                public void keyTraversed(TraverseEvent e) {
-                                    e.doit= true;
-                                    e.detail= SWT.TRAVERSE_NONE;
-                                }
-                            };
-                            disabledStyledText.addTraverseListener(traverseBlocker);
-                        } else {
-                            disabledStyledText= null;
-                        }
-                    }
-                }
-
-                change.initializeValidationData(new NullProgressMonitor());
-                RefactoringStatus valid= change.isValid(new NullProgressMonitor());
-                if (valid.hasFatalError()) {
-                    IStatus status= new Status(IStatus.ERROR, JavaPlugin.getPluginId(), IStatus.ERROR,
-                        valid.getMessageMatchingSeverity(RefactoringStatus.FATAL), null);
-                    throw new CoreException(status);
-                } else {
-                    IUndoManager manager= RefactoringCore.getUndoManager();
-                    Change undoChange;
-                    boolean successful= false;
-                    try {
-                        manager.aboutToPerformChange(change);
-                        undoChange= change.perform(new NullProgressMonitor());
-                        successful= true;
-                    } finally {
-                        manager.changePerformed(change, successful);
-                    }
-                    if (undoChange != null) {
-                        undoChange.initializeValidationData(new NullProgressMonitor());
-                        manager.addUndo(name, undoChange);
-                    }
-                }
-            }
-        } finally {
-            if (disabledStyledText != null) {
-                disabledStyledText.setEditable(true);
-                disabledStyledText.removeTraverseListener(traverseBlocker);
-            }
-            if (rewriteTarget != null) {
-                rewriteTarget.endCompoundChange();
-            }
-
-            if (change != null) {
-                change.dispose();
-            }
         }
     }
 
