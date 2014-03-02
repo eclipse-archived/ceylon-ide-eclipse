df97133c1879ba2eae8e943b442d901512fc2888
gavinking <gavin@ceylon-lang.org>
2014-03-02 16:10:20 +0100

    linked mode for Specify Type quick assist

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/correct/AssignToLocalProposal.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/correct/AssignToLocalProposal.java
@@ -14,10 +14,13 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.jface.text.contentassist.IContextInformation;
 import org.eclipse.jface.text.link.LinkedModeModel;
 import org.eclipse.jface.text.link.ProposalPosition;
 import org.eclipse.ltk.core.refactoring.TextChange;
 import org.eclipse.ltk.core.refactoring.TextFileChange;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.text.edits.InsertEdit;
 import org.eclipse.text.edits.MultiTextEdit;
 
@@ -28,9 +31,9 @@ import com.redhat.ceylon.compiler.typechecker.tree.Node;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.Annotation;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.Primary;
-import com.redhat.ceylon.eclipse.code.complete.CompletionProposal;
 import com.redhat.ceylon.eclipse.code.editor.CeylonEditor;
 import com.redhat.ceylon.eclipse.code.editor.EditorUtil;
+import com.redhat.ceylon.eclipse.code.parse.CeylonParseController;
 import com.redhat.ceylon.eclipse.util.FindUtils;
 
 class AssignToLocalProposal extends CorrectionProposal {
@@ -40,6 +43,7 @@ class AssignToLocalProposal extends CorrectionProposal {
     private final int length;
     private final int exitPos;
     private final ProducedType resultType;
+    private String text;
     
     AssignToLocalProposal(int offset, int length, int exitPos, 
             ProducedType resultType, IFile file, TextChange change) {
@@ -49,6 +53,57 @@ class AssignToLocalProposal extends CorrectionProposal {
         this.file=file;
         this.offset=offset;
         this.length=length;
+        text = "value";
+    }
+    
+    private class TypeCompletionProposal 
+            implements ICompletionProposal {
+        
+        private final String text;
+        private final Image image;
+        private final int offset;
+        
+        private TypeCompletionProposal(int offset, String text, 
+                Image image) {
+            this.text=text;
+            this.image = image;
+            this.offset = offset;
+        }
+        
+        @Override
+        public Image getImage() {
+            return image;
+        }
+        
+        @Override
+        public Point getSelection(IDocument document) {
+            return new Point(offset + text.length(), 0);
+        }
+        
+        public void apply(IDocument document) {
+            try {
+                document.replace(offset, 
+                        AssignToLocalProposal.this.text.length(), text);
+                AssignToLocalProposal.this.text = text;
+            } 
+            catch (BadLocationException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        public String getDisplayString() {
+            return text;
+        }
+        
+        public String getAdditionalProposalInfo() {
+            return null;
+        }
+        
+        @Override
+        public IContextInformation getContextInformation() {
+            return null;
+        }
+        
     }
     
     @Override
@@ -56,18 +111,18 @@ class AssignToLocalProposal extends CorrectionProposal {
         super.apply(document);
         gotoLocation(file, offset, length);
         if (!isTypeUnknown(resultType)) {
-            LinkedModeModel linkedModeModel = new LinkedModeModel();
+            final LinkedModeModel linkedModeModel = new LinkedModeModel();
             CeylonEditor editor = (CeylonEditor) EditorUtil.getCurrentEditor();
-            Unit unit = editor.getParseController().getRootNode().getUnit();
+            CeylonParseController cpc = editor.getParseController();
+            Unit unit = cpc.getRootNode().getUnit();
             List<ProducedType> supertypes = resultType.getSupertypes();
             ICompletionProposal[] proposals = 
                     new ICompletionProposal[supertypes.size()];
             for (int i=0; i<supertypes.size(); i++) {
                 ProducedType type = supertypes.get(i);
                 String typeName = type.getProducedTypeName(unit);
-                proposals[i] = new CompletionProposal(offset-1, "value", 
-                        getImageForDeclaration(type.getDeclaration()),
-                        typeName, typeName);
+                proposals[i] = new TypeCompletionProposal(offset-6, typeName, 
+                        getImageForDeclaration(type.getDeclaration()));
             }
             ProposalPosition linkedPosition = 
                     new ProposalPosition(document, offset-6, 5, 0, proposals);
@@ -145,8 +200,9 @@ class AssignToLocalProposal extends CorrectionProposal {
             else {
                 return;
             }
+            Integer stopIndex = expanse.getStopIndex();
             if (currentOffset<expanse.getStartIndex() || 
-                currentOffset>expanse.getStopIndex()+1) {
+                currentOffset>stopIndex+1) {
                 return;
             }
             String name = guessName(expression);
@@ -157,11 +213,11 @@ class AssignToLocalProposal extends CorrectionProposal {
             String terminal = expanse.getEndToken().getText();
             int exitPos;
             if (!terminal.equals(";")) {
-                change.addEdit(new InsertEdit(expanse.getStopIndex()+1, ";"));
-                exitPos = expanse.getStopIndex()+2;
+                change.addEdit(new InsertEdit(stopIndex+1, ";"));
+                exitPos = stopIndex+2;
             }
             else {
-                exitPos = expanse.getStopIndex()+1;
+                exitPos = stopIndex+1;
             }
             proposals.add(new AssignToLocalProposal(offset+6, name.length(), exitPos, 
                     resultType, file, change));
