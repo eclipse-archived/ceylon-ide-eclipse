4fd47701c8fb0b89c18a53bb77413d3ef50f9277
gavinking <gavin@ceylon-lang.org>
2014-03-02 10:29:41 +0100

    switch to "wizard"-style dialog for Safe Delete

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/refactor/DeleteWizard.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/refactor/DeleteWizard.java
@@ -1,20 +1,20 @@
 package com.redhat.ceylon.eclipse.code.refactor;
 
+import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ltk.ui.refactoring.RefactoringWizard;
 
 public class DeleteWizard extends RefactoringWizard {
     public DeleteWizard(AbstractRefactoring refactoring) {
-        super(refactoring, DIALOG_BASED_USER_INTERFACE
+        super(refactoring, WIZARD_BASED_USER_INTERFACE
                 | PREVIEW_EXPAND_FIRST_NODE);
         setDefaultPageTitle(refactoring.getName());
+        setWindowTitle("Safe Delete");
     }
     
     @Override
     protected void addUserInputPages() {
+        ((WizardDialog) getContainer()).setPageSize(200, 200);
         addPage(new DeleteInputPage(getRefactoring().getName()));
     }
 
-//    public AbstractRefactoring getRenameRefactoring() {
-//        return (AbstractRefactoring) getRefactoring();
-//    }
 }
