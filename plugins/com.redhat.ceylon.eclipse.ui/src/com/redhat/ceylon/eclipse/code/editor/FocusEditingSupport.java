6a1fe373f070d741deb6b59d7583944fe51ce4f6
gavinking <gavin@ceylon-lang.org>
2014-03-03 10:33:44 +0100

    more refactoring / improvement for linked mode stuff

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/FocusEditingSupport.java
@@ -0,0 +1,24 @@
+package com.redhat.ceylon.eclipse.code.editor;
+
+import org.eclipse.jface.text.DocumentEvent;
+import org.eclipse.jface.text.IEditingSupport;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.swt.widgets.Shell;
+
+public class FocusEditingSupport implements IEditingSupport {
+    private final CeylonEditor editor;
+
+    public FocusEditingSupport(CeylonEditor editor) {
+        this.editor = editor;
+    }
+
+    public boolean ownsFocusShell() {
+        Shell editorShell = editor.getSite().getShell();
+        Shell activeShell = editorShell.getDisplay().getActiveShell();
+        return editorShell == activeShell;
+    }
+
+    public boolean isOriginator(DocumentEvent event, IRegion subjectRegion) {
+        return false; //leave on external modification outside positions
+    }
+}
