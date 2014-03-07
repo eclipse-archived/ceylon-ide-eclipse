4d9b24bbf635236adcc07608069f2956c25ca0d5
gavinking <gavin@ceylon-lang.org>
2014-03-07 16:41:31 +0100

    dispose

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/NewMenuItems.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/NewMenuItems.java
@@ -1,32 +1,37 @@
 package com.redhat.ceylon.eclipse.code.editor;
 
 
+import static org.eclipse.ui.PlatformUI.getWorkbench;
+
 import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.actions.CompoundContributionItem;
 import org.eclipse.ui.actions.NewWizardMenu;
 
 public class NewMenuItems extends CompoundContributionItem {
     
+    private NewWizardMenu menu;
+
     public NewMenuItems() {}
-    
-    public NewMenuItems(String id) {
-        super(id);
-    }
+
+    public NewMenuItems(String id) {}
     
     @Override
     public IContributionItem[] getContributionItems() {
+        menu = new NewWizardMenu(getWorkbench().getActiveWorkbenchWindow());
         String newId = ActionFactory.NEW.getId();
         MenuManager newMenu = new MenuManager("New", newId);
         newMenu.setActionDefinitionId("org.eclipse.ui.file.newQuickMenu");
         newMenu.add(new Separator(newId));
-        NewWizardMenu menu = new NewWizardMenu(PlatformUI.getWorkbench().getActiveWorkbenchWindow());
         newMenu.add(menu);
-        //TODO: clean up menu
         return new IContributionItem[] { newMenu };
+    }
+    
+    @Override
+    public void dispose() {
+        menu.dispose();
     }
     
 }
