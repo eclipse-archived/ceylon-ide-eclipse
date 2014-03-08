f2cb67edfeefd6bdff1e04a4ed55cb6bc23243e4
gavinking <gavin@ceylon-lang.org>
2014-03-08 12:31:54 +0100

    cache icons

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/open/OpenDeclarationAction.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/open/OpenDeclarationAction.java
@@ -1,18 +1,24 @@
 package com.redhat.ceylon.eclipse.code.open;
 
+import static com.redhat.ceylon.eclipse.code.editor.EditorUtil.getSelectionText;
+import static com.redhat.ceylon.eclipse.code.editor.Navigation.gotoDeclaration;
 import static com.redhat.ceylon.eclipse.ui.CeylonPlugin.PLUGIN_ID;
+import static org.eclipse.ui.PlatformUI.getWorkbench;
 
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.texteditor.ITextEditor;
 
-import com.redhat.ceylon.eclipse.code.editor.EditorUtil;
-import com.redhat.ceylon.eclipse.code.editor.Navigation;
 import com.redhat.ceylon.eclipse.ui.CeylonPlugin;
 import com.redhat.ceylon.eclipse.ui.CeylonResources;
 
 public class OpenDeclarationAction extends Action {
+    
+    private static final ImageDescriptor ICON = CeylonPlugin.getInstance().getImageRegistry()
+            .getDescriptor(CeylonResources.CEYLON_OPEN_DECLARATION);
+    
     private final IEditorPart editor;
     
     public OpenDeclarationAction(IEditorPart editor) {
@@ -23,25 +29,24 @@ public class OpenDeclarationAction extends Action {
         super(text);
         this.editor = editor;
         setActionDefinitionId(PLUGIN_ID + ".action.openDeclaration");
-        setImageDescriptor(CeylonPlugin.getInstance().getImageRegistry()
-                .getDescriptor(CeylonResources.CEYLON_OPEN_DECLARATION));
+        setImageDescriptor(ICON);
     }
     
     @Override
     public void run() {
-        Shell shell = CeylonPlugin.getInstance().getWorkbench()
-                .getActiveWorkbenchWindow().getShell();
-        OpenCeylonDeclarationDialog dialog = new OpenCeylonDeclarationDialog(shell, editor);
+        Shell shell = getWorkbench().getActiveWorkbenchWindow().getShell();
+        OpenCeylonDeclarationDialog dialog = 
+                new OpenCeylonDeclarationDialog(shell, editor);
         dialog.setTitle("Open Ceylon Declaration");
         dialog.setMessage("Select a Ceylon declaration to open:");
         if (editor instanceof ITextEditor) {
-            dialog.setInitialPattern(EditorUtil.getSelectionText((ITextEditor) editor));
+            dialog.setInitialPattern(getSelectionText((ITextEditor) editor));
         }
         dialog.open();
         Object[] types = dialog.getResult();
         if (types != null && types.length > 0) {
             DeclarationWithProject dwp = (DeclarationWithProject) types[0];
-            Navigation.gotoDeclaration(dwp.getDeclaration(), dwp.getProject(), editor);
+            gotoDeclaration(dwp.getDeclaration(), dwp.getProject(), editor);
         }
     }
 
