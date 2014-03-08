7a264411791868e9a4bbcfd7402551793cc89c4b
gavinking <gavin@ceylon-lang.org>
2014-03-08 12:31:54 +0100

    cache icons

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/open/OpenDeclarationInHierarchyAction.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/open/OpenDeclarationInHierarchyAction.java
@@ -1,16 +1,15 @@
 package com.redhat.ceylon.eclipse.code.open;
 
+import static com.redhat.ceylon.eclipse.code.editor.EditorUtil.getSelectionText;
 import static com.redhat.ceylon.eclipse.code.outline.HierarchyView.showHierarchyView;
 import static com.redhat.ceylon.eclipse.ui.CeylonPlugin.PLUGIN_ID;
+import static org.eclipse.ui.PlatformUI.getWorkbench;
 
 import org.eclipse.jface.action.Action;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.texteditor.ITextEditor;
-
-import com.redhat.ceylon.eclipse.code.editor.EditorUtil;
-import com.redhat.ceylon.eclipse.ui.CeylonPlugin;
 
 public class OpenDeclarationInHierarchyAction extends Action {
     
@@ -30,13 +29,13 @@ public class OpenDeclarationInHierarchyAction extends Action {
     
     @Override
     public void run() {
-        Shell shell = CeylonPlugin.getInstance().getWorkbench()
-                .getActiveWorkbenchWindow().getShell();
-        OpenCeylonDeclarationDialog dialog = new OpenCeylonDeclarationDialog(shell, editor);
+        Shell shell = getWorkbench().getActiveWorkbenchWindow().getShell();
+        OpenCeylonDeclarationDialog dialog = 
+                new OpenCeylonDeclarationDialog(shell, editor);
         dialog.setTitle("Open in Type Hierarchy View");
         dialog.setMessage("Select a Ceylon declaration to open:");
         if (editor instanceof ITextEditor) {
-            dialog.setInitialPattern(EditorUtil.getSelectionText((ITextEditor) editor));
+            dialog.setInitialPattern(getSelectionText((ITextEditor) editor));
         }
         dialog.open();
         Object[] types = dialog.getResult();
