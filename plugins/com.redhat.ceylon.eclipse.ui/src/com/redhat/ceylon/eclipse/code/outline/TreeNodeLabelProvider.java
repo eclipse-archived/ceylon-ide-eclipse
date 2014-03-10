2fe39a12ffc7251404e5e9cf1ad263d59595acb9
gavinking <gavin@ceylon-lang.org>
2014-03-10 15:15:23 +0100

    tree-based view of references

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/outline/TreeNodeLabelProvider.java
@@ -0,0 +1,63 @@
+package com.redhat.ceylon.eclipse.code.outline;
+
+import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.StyledCellLabelProvider;
+import org.eclipse.jface.viewers.StyledString;
+import org.eclipse.jface.viewers.TreeNode;
+import org.eclipse.jface.viewers.ViewerCell;
+import org.eclipse.swt.graphics.Image;
+
+public class TreeNodeLabelProvider extends StyledCellLabelProvider 
+        implements DelegatingStyledCellLabelProvider.IStyledLabelProvider, 
+                   ILabelProvider {
+    
+    private CeylonLabelProvider ceylonLabelProvider = 
+            new CeylonLabelProvider(true);
+
+    protected Object unwrap(Object element) {
+        return ((TreeNode) element).getValue();
+    }
+
+    @Override
+    public String getText(Object element) {
+        return ceylonLabelProvider.getText(unwrap(element));
+    }
+
+    @Override
+    public StyledString getStyledText(Object element) {
+        return ceylonLabelProvider.getStyledText(unwrap(element));
+    }
+
+    @Override
+    public Image getImage(Object element) {
+        return ceylonLabelProvider.getImage(unwrap(element));
+    }
+    
+    @Override
+    public void addListener(ILabelProviderListener listener) {
+        ceylonLabelProvider.addListener(listener);
+    }
+    
+    @Override
+    public void removeListener(ILabelProviderListener listener) {
+        ceylonLabelProvider.removeListener(listener);
+    }
+
+    @Override
+    public boolean isLabelProperty(Object element, String property) {
+        return false;
+    }
+    
+    @Override
+    public void update(ViewerCell cell) {
+        Object element = cell.getElement();
+        StyledString styledText = getStyledText(element);
+        cell.setText(styledText.toString());
+        cell.setStyleRanges(styledText.getStyleRanges());
+        cell.setImage(getImage(element));
+        super.update(cell);
+    }
+    
+}
