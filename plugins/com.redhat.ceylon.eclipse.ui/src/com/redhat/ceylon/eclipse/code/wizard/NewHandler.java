599a1f62f355bc626d31a2177c65a3c456595718
gavinking <gavin@ceylon-lang.org>
2014-03-14 20:39:08 +0100

    much nicer impl of New shortcuts

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/wizard/NewHandler.java
@@ -0,0 +1,36 @@
+package com.redhat.ceylon.eclipse.code.wizard;
+
+import static org.eclipse.ui.PlatformUI.getWorkbench;
+
+import java.util.Hashtable;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExecutableExtension;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.internal.actions.NewWizardShortcutAction;
+import org.eclipse.ui.wizards.IWizardDescriptor;
+
+public class NewHandler extends AbstractHandler implements IExecutableExtension {
+    
+    private String wizardId;
+    
+    @Override
+    public Object execute(ExecutionEvent event) 
+            throws ExecutionException {
+        IWorkbench workbench = getWorkbench();
+        IWizardDescriptor descriptor = workbench.getNewWizardRegistry().findWizard(wizardId);
+        new NewWizardShortcutAction(workbench.getActiveWorkbenchWindow(), descriptor).run();
+        return null;
+    }
+    @Override
+    public void setInitializationData(IConfigurationElement config,
+            String propertyName, Object data) throws CoreException {
+        if (propertyName.equals("class")) {
+            wizardId = (String) ((Hashtable) data).get("wizardId");
+        }
+    }
+}
