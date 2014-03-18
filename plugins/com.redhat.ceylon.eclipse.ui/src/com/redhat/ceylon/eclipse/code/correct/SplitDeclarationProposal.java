94d716df778b5cb86ee003830291a66a90c5eb77
gavinking <gavin@ceylon-lang.org>
2014-03-18 15:12:37 +0100

    Split declaration for parameters (quite useful!)

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/correct/SplitDeclarationProposal.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/correct/SplitDeclarationProposal.java
@@ -3,7 +3,10 @@ package com.redhat.ceylon.eclipse.code.correct;
 import static com.redhat.ceylon.eclipse.code.correct.ImportProposals.applyImports;
 import static com.redhat.ceylon.eclipse.code.correct.ImportProposals.importType;
 import static com.redhat.ceylon.eclipse.code.correct.SpecifyTypeProposal.inferType;
+import static com.redhat.ceylon.eclipse.util.Indents.getDefaultIndent;
+import static com.redhat.ceylon.eclipse.util.Indents.getDefaultLineDelimiter;
 import static com.redhat.ceylon.eclipse.util.Indents.getIndent;
+import static com.redhat.ceylon.eclipse.util.Nodes.getReferencedNode;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -16,6 +19,7 @@ import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.ltk.core.refactoring.TextChange;
 import org.eclipse.ltk.core.refactoring.TextFileChange;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.text.edits.DeleteEdit;
 import org.eclipse.text.edits.InsertEdit;
 import org.eclipse.text.edits.MultiTextEdit;
 import org.eclipse.text.edits.ReplaceEdit;
@@ -23,10 +27,10 @@ import org.eclipse.text.edits.ReplaceEdit;
 import com.redhat.ceylon.compiler.typechecker.model.Declaration;
 import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
 import com.redhat.ceylon.compiler.typechecker.model.TypedDeclaration;
+import com.redhat.ceylon.compiler.typechecker.tree.Node;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.ParameterList;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.Type;
-import com.redhat.ceylon.eclipse.util.Indents;
 
 class SplitDeclarationProposal extends CorrectionProposal {
     
@@ -40,33 +44,68 @@ class SplitDeclarationProposal extends CorrectionProposal {
             Tree.TypedDeclaration decNode) {
         TypedDeclaration dec = decNode.getDeclarationModel();
         if (dec==null) return;
-        if (dec.isParameter() || dec.isToplevel()) return;
+        if (dec.isToplevel()) return;
         Tree.Identifier id = decNode.getIdentifier();
         if (id==null || id.getToken()==null) return;
-        String params = "";
-        if (decNode instanceof Tree.MethodDeclaration) {
-            List<ParameterList> pls = ((Tree.MethodDeclaration) decNode).getParameterLists();
-            if (pls.isEmpty()) {
-                return;
-            } 
-            else {
-                Integer start = pls.get(0).getStartIndex();
-                Integer end = pls.get(pls.size()-1).getStopIndex();
-                try {
-                    params = doc.get(start, end-start+1);
-                } 
-                catch (BadLocationException e) {
-                    e.printStackTrace();
+        int idStartOffset = id.getStartIndex();
+        int idEndOffset = id.getStopIndex()+1;
+        int startOffset = decNode.getStartIndex();
+        int paramsEndOffset = idEndOffset;
+        String paramsString = "";
+        String typeString;
+        try {
+            int typeStartOffset = decNode.getType().getStartIndex();
+            int typeEndOffset = decNode.getType().getStopIndex()+1;
+            typeString = doc.get(typeStartOffset, typeEndOffset-typeStartOffset);
+            if (decNode instanceof Tree.MethodDeclaration) {
+                List<ParameterList> pls = 
+                        ((Tree.MethodDeclaration) decNode).getParameterLists();
+                if (pls.isEmpty()) {
                     return;
+                } 
+                else {
+                    int paramsOffset = pls.get(0).getStartIndex();
+                    paramsEndOffset = pls.get(pls.size()-1).getStopIndex()+1;
+                    paramsString = doc.get(paramsOffset, paramsEndOffset-paramsOffset);
                 }
             }
+        } 
+        catch (BadLocationException e) {
+            e.printStackTrace();
+            return;
         }
         TextChange change = new TextFileChange("Split Declaration", file);
         change.setEdit(new MultiTextEdit());
-        Integer offset = id.getStopIndex()+1;
-        change.addEdit(new InsertEdit(offset, params+";" + 
-                Indents.getDefaultLineDelimiter(doc) + getIndent(decNode, doc) + 
-                dec.getName()));
+        String delim = getDefaultLineDelimiter(doc);
+        String indent = getIndent(decNode, doc);
+        if (dec.isParameter()) {
+            //TODO: does not handle default args correctly for callable parameters
+            change.addEdit(new DeleteEdit(startOffset, idStartOffset-startOffset));
+            change.addEdit(new DeleteEdit(idEndOffset, paramsEndOffset-idEndOffset));
+            Node containerNode = (Tree.Declaration) 
+                    getReferencedNode((Declaration) dec.getContainer(), cu);
+            Tree.Body body;
+            if (containerNode instanceof Tree.ClassDefinition) {
+                body = ((Tree.ClassDefinition) containerNode).getClassBody();
+            }
+            else if (containerNode instanceof Tree.MethodDefinition) {
+                body = ((Tree.MethodDefinition) containerNode).getBlock();
+            }
+            else {
+                return;
+            }
+            String text = delim + indent + getDefaultIndent() +
+                    typeString + " " + dec.getName() + paramsString + ";";
+            if (body.getStopIndex()==body.getStartIndex()+1) {
+                text += delim + indent;
+            }
+            change.addEdit(new InsertEdit(body.getStartIndex()+1, text));
+        }
+        else {
+            String text = paramsString +";" + 
+                    delim + indent + dec.getName();
+            change.addEdit(new InsertEdit(idEndOffset, text));
+        }
         Type type = decNode.getType();
         int il;
         if (type instanceof Tree.LocalModifier) {
@@ -88,7 +127,7 @@ class SplitDeclarationProposal extends CorrectionProposal {
         else {
             il=0;
         }
-        proposals.add(new SplitDeclarationProposal(dec, offset+il, change));
+        proposals.add(new SplitDeclarationProposal(dec, idEndOffset+il, change));
     }
 
 	static void addSplitDeclarationProposals(
@@ -98,14 +137,14 @@ class SplitDeclarationProposal extends CorrectionProposal {
 	        Tree.AttributeDeclaration attDecNode = (Tree.AttributeDeclaration) decNode;
 	        Tree.SpecifierOrInitializerExpression sie = 
 	                attDecNode.getSpecifierOrInitializerExpression();
-	        if (sie!=null) {
+	        if (sie!=null || decNode.getDeclarationModel().isParameter()) {
 	            addSplitDeclarationProposal(doc, cu, proposals, file, attDecNode);
 	        }
 	    }
 	    if (decNode instanceof Tree.MethodDeclaration) {
 	        Tree.MethodDeclaration methDecNode = (Tree.MethodDeclaration) decNode;
 	        Tree.SpecifierExpression sie = methDecNode.getSpecifierExpression();
-	        if (sie!=null) {
+	        if (sie!=null || decNode.getDeclarationModel().isParameter()) {
 	            addSplitDeclarationProposal(doc, cu, proposals, file, methDecNode);
 	        }
 	    }
