6d50218f792978ca33f0587edf6d58ecc3eac2fb
gavinking <gavin@ceylon-lang.org>
2014-03-25 19:53:36 +0100

    finally, a complete solution to the issues withsyntax highlighting in linked mode

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/PresentationDamageRepairer.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/PresentationDamageRepairer.java
@@ -3,6 +3,7 @@ package com.redhat.ceylon.eclipse.code.editor;
 import static com.redhat.ceylon.eclipse.util.Highlights.getColoring;
 import static com.redhat.ceylon.eclipse.util.Highlights.getInterpolationColoring;
 import static com.redhat.ceylon.eclipse.util.Highlights.getMemberColoring;
+import static com.redhat.ceylon.eclipse.util.Nodes.getTokenIndexAtCharacter;
 
 import java.util.Iterator;
 import java.util.List;
@@ -27,10 +28,10 @@ import org.eclipse.jface.text.presentation.IPresentationRepairer;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyleRange;
+import org.eclipse.swt.graphics.Color;
 
 import com.redhat.ceylon.compiler.typechecker.parser.CeylonLexer;
 import com.redhat.ceylon.compiler.typechecker.parser.CeylonParser;
-import com.redhat.ceylon.eclipse.util.Nodes;
 
 class PresentationDamageRepairer implements IPresentationDamager, 
         IPresentationRepairer {
@@ -39,34 +40,57 @@ class PresentationDamageRepairer implements IPresentationDamager,
     private final CeylonEditor editor;
     private IDocument document;
     
-    PresentationDamageRepairer(ISourceViewer sourceViewer, CeylonEditor editor) {
+    PresentationDamageRepairer(ISourceViewer sourceViewer, 
+            CeylonEditor editor) {
         this.editor = editor;
     }
     
     public IRegion getDamageRegion(ITypedRegion partition, 
-            DocumentEvent event, boolean documentPartitioningChanged) {
+            DocumentEvent event, 
+            boolean documentPartitioningChanged) {
 
         if (tokens==null) {
             //parse and color the whole document the first time!
             return partition;
         }
-        
-        Position linkedPosition =
-                getLinkedPosition(event.getOffset(), event.getLength());
-        if (linkedPosition!=null) {
-            return new Region(linkedPosition.getOffset(), 
-                    linkedPosition.getLength());
+                
+        if (editor!=null && editor.isInLinkedMode()) {
+            Position linkedPosition =
+                    getLinkedPosition(event.getOffset(), 
+                            event.getLength());
+            if (linkedPosition == null) {
+                return partition;
+            }
+            else {
+                return new Region(linkedPosition.getOffset(), 
+                        linkedPosition.getLength());
+            }
         }
-        
+
         if (noTextChange(event)) {
             //it was a change to annotations - don't reparse
             return new Region(event.getOffset(), 
                     event.getLength());
         }
         
-        int i = Nodes.getTokenIndexAtCharacter(tokens, event.getOffset()-1);
-        if (i<0) i=-i;
-        CommonToken t = tokens.get(i);
+        Region tokenRegion = 
+                getContainingTokenRegion(event);
+        if (tokenRegion == null) {
+            //the change is totally within a token,
+            //and doesn't break it, return the
+            //token extent
+            return partition;
+        }
+        else {
+            return tokenRegion;
+        }
+    }
+    
+    private Region getContainingTokenRegion(DocumentEvent event) {
+        int tokenIndex = 
+                getTokenIndexAtCharacter(tokens, event.getOffset()-1);
+        if (tokenIndex<0) tokenIndex=-tokenIndex;
+        CommonToken t = tokens.get(tokenIndex);
         if (isWithinExistingToken(event, t)) {
             if (isWithinTokenChange(event, t)) {
                 //the edit just changes the text inside
@@ -76,8 +100,7 @@ class PresentationDamageRepairer implements IPresentationDamager,
                         event.getText().length());
             }
         }
-        
-        return partition;
+        return null;
     }
 
     public boolean isWithinExistingToken(DocumentEvent event, 
@@ -171,11 +194,15 @@ class PresentationDamageRepairer implements IPresentationDamager,
     
     public void createPresentation(TextPresentation presentation, 
             ITypedRegion damage) {
-        ANTLRStringStream input = new ANTLRStringStream(document.get());
-        CeylonLexer lexer = new CeylonLexer(input);
-        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+        ANTLRStringStream input = 
+                new ANTLRStringStream(document.get());
+        CeylonLexer lexer = 
+                new CeylonLexer(input);
+        CommonTokenStream tokenStream = 
+                new CommonTokenStream(lexer);
         
-        CeylonParser parser = new CeylonParser(tokenStream);
+        CeylonParser parser = 
+                new CeylonParser(tokenStream);
         try {
             parser.compilationUnit();
         }
@@ -280,36 +307,37 @@ class PresentationDamageRepairer implements IPresentationDamager,
             TextAttribute attribute, int startOffset, int endOffset,
             int extraStyle) {
         
+        Color foreground = attribute==null ? 
+                null : attribute.getForeground();
+        Color background = attribute==null ? 
+                null : attribute.getBackground();
+        int fontStyle = attribute==null ? 
+                extraStyle : attribute.getStyle()|extraStyle;
         StyleRange styleRange = new StyleRange(startOffset, 
                 endOffset-startOffset,
-                attribute==null ? null : attribute.getForeground(),
-                attribute==null ? null : attribute.getBackground(),
-                attribute==null ? extraStyle : attribute.getStyle()|extraStyle);
+                foreground, background, fontStyle);
         
         presentation.addStyleRange(styleRange);
         
     }
 
     private Position getLinkedPosition(int offset, int length) {
-        if (editor!=null) {
-            LinkedModeModel linkedMode = editor.getLinkedMode();
-            if (linkedMode!=null &&
-                    (linkedMode.anyPositionContains(offset) ||
-                     linkedMode.anyPositionContains(offset+length))) {
-                try {
-                    for (Position p: 
-                        document.getPositions(linkedMode.toString())) {
-                        if (!p.isDeleted()) {
-                            if (p.includes(offset) && 
+        LinkedModeModel linkedMode = editor.getLinkedMode();
+        if (linkedMode.anyPositionContains(offset) ||
+                linkedMode.anyPositionContains(offset+length)) {
+            try {
+                for (Position p: 
+                    document.getPositions(linkedMode.toString())) {
+                    if (!p.isDeleted()) {
+                        if (p.includes(offset) && 
                                 p.includes(offset+length)) {
-                                return p;
-                            }
+                            return p;
                         }
                     }
                 }
-                catch (BadPositionCategoryException e) {
-                    e.printStackTrace();
-                }
+            }
+            catch (BadPositionCategoryException e) {
+                e.printStackTrace();
             }
         }
         return null;
