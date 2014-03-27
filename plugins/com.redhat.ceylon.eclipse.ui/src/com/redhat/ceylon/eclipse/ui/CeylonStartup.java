892e7b49c100ed2174735a4ca1f96b0bc1521f26
David Festal <david.festal@serli.com>
2014-03-27 14:34:56 +0100

    Fix #919 by moving the code into CeylonStartup, which is explicitly done to run code just after the workbench is fully initialized.

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/ui/CeylonStartup.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/ui/CeylonStartup.java
@@ -3,15 +3,23 @@ package com.redhat.ceylon.eclipse.ui;
 import static com.redhat.ceylon.eclipse.ui.CeylonPlugin.PLUGIN_ID;
 import static org.eclipse.ui.PlatformUI.getWorkbench;
 
-import org.eclipse.swt.widgets.Display;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IExecutionListener;
+import org.eclipse.core.commands.NotHandledException;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IPerspectiveDescriptor;
 import org.eclipse.ui.IPerspectiveListener;
 import org.eclipse.ui.IStartup;
 import org.eclipse.ui.IWindowListener;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.commands.ICommandService;
 import org.eclipse.ui.contexts.IContextActivation;
 import org.eclipse.ui.contexts.IContextService;
+
+import com.redhat.ceylon.eclipse.code.editor.EditorUtil;
+import com.redhat.ceylon.eclipse.code.editor.RecentFilesPopup;
 
 public class CeylonStartup implements IStartup {
 
@@ -63,7 +71,7 @@ public class CeylonStartup implements IStartup {
 
     @Override
     public void earlyStartup() {
-        Display.getDefault().asyncExec(new Runnable() {
+        getWorkbench().getDisplay().asyncExec(new Runnable() {
             @Override
             public void run() {
                 WindowListener listener = new WindowListener();
@@ -73,6 +81,28 @@ public class CeylonStartup implements IStartup {
                 }
             }
         });
+
+        getWorkbench().getDisplay().asyncExec(new Runnable() {
+            @Override
+            public void run() {
+                ICommandService commandService = (ICommandService) getWorkbench().getService( ICommandService.class );
+                commandService.addExecutionListener(new IExecutionListener() {
+                    public void notHandled(final String commandId, final NotHandledException exception) {}
+                    public void postExecuteFailure(final String commandId, final ExecutionException exception) {}
+                    public void postExecuteSuccess(final String commandId, final Object returnValue) {
+                        if (commandId.equals("org.eclipse.ui.file.save")) {
+                            IEditorPart ed = EditorUtil.getCurrentEditor();
+                            if (ed!=null) {
+                                RecentFilesPopup.addToHistory(EditorUtil.getFile(ed.getEditorInput()));
+                            }
+                        }
+                    }
+                    public void preExecute(final String commandId, final ExecutionEvent event) {}
+
+                } );
+            }
+        });
+        
     }
 
 }
