79ca37cd947b3128c2038858b75da39547820429
David Festal <david.festal@serli.com>
2014-04-09 15:01:56 +0200

    Add a IUnit interface

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/model/IdeUnit.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/model/IdeUnit.java
@@ -2,7 +2,7 @@ package com.redhat.ceylon.eclipse.core.model;
 
 import com.redhat.ceylon.compiler.typechecker.model.Unit;
 
-public abstract class IdeUnit extends Unit {
+public abstract class IdeUnit extends Unit implements IUnit {
     public JDTModule getModule() {
         return (JDTModule) getPackage().getModule();
     }
