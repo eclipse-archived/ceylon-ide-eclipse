eac2a1a501e2c78821bd29502fdd1d491f4b6635
David Festal <david.festal@serli.com>
2014-04-16 18:57:40 +0200

    new SourceArchiveEditorInput for source archive files. This allows managing breakpoints, and also opened editor retrieval, more cleanly.

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/SourceArchiveEditorInput.java
@@ -0,0 +1,32 @@
+package com.redhat.ceylon.eclipse.code.editor;
+
+import java.net.URI;
+
+import org.eclipse.core.internal.resources.Resource;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.ui.part.FileEditorInput;
+
+import com.redhat.ceylon.eclipse.core.external.ExternalSourceArchiveManager;
+
+public class SourceArchiveEditorInput extends FileEditorInput {
+    public SourceArchiveEditorInput(IFile file) {
+        super(file);
+    }
+    
+    @Override
+    public IPath getPath() {
+        return ExternalSourceArchiveManager.toFullPath(getFile());
+    }
+    
+    @Override
+    public URI getURI() {
+        return ((Resource) getFile()).getStore().toURI();
+    }
+    
+    @Override
+    public String getToolTipText() {
+        IPath sourceArchiveFullPath = ExternalSourceArchiveManager.toFullPath(getFile());
+        return sourceArchiveFullPath != null ? sourceArchiveFullPath.makeRelative().toOSString() : super.getToolTipText();
+    }
+}
