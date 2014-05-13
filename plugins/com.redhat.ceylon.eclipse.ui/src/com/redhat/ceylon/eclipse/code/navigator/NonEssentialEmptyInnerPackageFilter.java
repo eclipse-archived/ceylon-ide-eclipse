f7d7020409e318b0da2909d11ea773ce0e5e671b
davidfestal <david.festal@serli.com>
2014-05-13 14:25:08 +0200

    Correctly manage Navigator filters and disable JDT empty package filters

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/navigator/NonEssentialEmptyInnerPackageFilter.java
@@ -0,0 +1,29 @@
+package com.redhat.ceylon.eclipse.code.navigator;
+
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jface.viewers.Viewer;
+
+public class NonEssentialEmptyInnerPackageFilter extends NonEssentialElementsFilter {
+
+	public NonEssentialEmptyInnerPackageFilter() {
+		super();
+	}
+
+	@Override
+	protected boolean doSelect(Viewer viewer, Object parent, Object element) {
+		if (element instanceof IPackageFragment) {
+			IPackageFragment pkg= (IPackageFragment)element;
+			try {
+				if (pkg.isDefaultPackage()) {
+					return pkg.hasChildren() || hasUnfilteredResources(viewer, pkg);
+				}
+				return !pkg.hasSubpackages() || pkg.hasChildren() || hasUnfilteredResources(viewer, pkg);
+			} catch (JavaModelException e) {
+				return false;
+			}
+		}
+	
+		return true;
+	}
+}
---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/navigator/NonEssentialEmptyPackageFilter.java
@@ -0,0 +1,25 @@
+package com.redhat.ceylon.eclipse.code.navigator;
+
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jface.viewers.Viewer;
+
+public class NonEssentialEmptyPackageFilter extends NonEssentialElementsFilter {
+
+	public NonEssentialEmptyPackageFilter() {
+		super();
+	}
+
+	@Override
+	protected boolean doSelect(Viewer viewer, Object parent, Object element) {
+		if (element instanceof IPackageFragment) {
+			IPackageFragment pkg= (IPackageFragment)element;
+			try {
+				return pkg.hasChildren() || hasUnfilteredResources(viewer, pkg);
+			} catch (JavaModelException e) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
