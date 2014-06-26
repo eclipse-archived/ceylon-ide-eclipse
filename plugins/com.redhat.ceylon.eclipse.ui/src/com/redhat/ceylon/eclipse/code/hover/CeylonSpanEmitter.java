2d1a99b98f194ad723e9cc3586d73138211709aa
gavinking <gavin@ceylon-lang.org>
2014-06-26 03:41:43 +0200

    make functions look like functions

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/hover/CeylonSpanEmitter.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/hover/CeylonSpanEmitter.java
@@ -2,6 +2,7 @@ package com.redhat.ceylon.eclipse.code.hover;
 
 import com.github.rjeschke.txtmark.SpanEmitter;
 import com.redhat.ceylon.compiler.typechecker.model.Declaration;
+import com.redhat.ceylon.compiler.typechecker.model.Method;
 import com.redhat.ceylon.compiler.typechecker.model.Module;
 import com.redhat.ceylon.compiler.typechecker.model.Scope;
 import com.redhat.ceylon.compiler.typechecker.model.Unit;
@@ -18,38 +19,42 @@ final class CeylonSpanEmitter implements SpanEmitter {
 
     @Override
     public void emitSpan(StringBuilder out, String content) {
-        String linkName;
+        String linkDescription;
         String linkTarget; 
         
         int indexOf = content.indexOf("|");
         boolean hasNoPlainText = indexOf == -1;
         if (hasNoPlainText) {
-            linkName = content;
+            int sep = content.indexOf("::");
+            linkDescription = sep<0 ? content : content.substring(sep+2);
             linkTarget = content;
         }
         else {
-            linkName = content.substring(0, indexOf);
+            linkDescription = content.substring(0, indexOf);
             linkTarget = content.substring(indexOf+1, content.length()); 
         }
         
-        String href = resolveLink(linkTarget, linkScope, unit);
+        Declaration decl = resolveLink(linkTarget, linkScope, unit);
+        String href = decl != null ? HTML.link(decl) : null;
         if (href != null) {
             out.append("<a ").append(href).append(">");
         }
         if (hasNoPlainText) {
-        	out.append("<code>");
+            out.append("<code>");
         }
-        int sep = linkName.indexOf("::");
-        out.append(sep<0?linkName:linkName.substring(sep+2));
+        out.append(linkDescription);
         if (hasNoPlainText) {
-        	out.append("</code>");
+            if (decl instanceof Method) {
+                out.append("()");
+            }
+            out.append("</code>");
         }
         if (href != null) {
             out.append("</a>");
         }
     }
 
-    static String resolveLink(String linkTarget, Scope linkScope, Unit unit) {
+    static Declaration resolveLink(String linkTarget, Scope linkScope, Unit unit) {
         String declName;
         Scope scope = null;
         int pkgSeparatorIndex = linkTarget.indexOf("::");
@@ -83,12 +88,6 @@ final class CeylonSpanEmitter implements SpanEmitter {
             }
         }
     
-        if (decl != null) {
-            String href = HTML.link(decl);
-            return href;
-        }
-        else {
-            return null;
-        }
+        return decl;
     }
 }
