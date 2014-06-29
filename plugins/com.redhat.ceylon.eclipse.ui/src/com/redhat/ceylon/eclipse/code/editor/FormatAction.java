eclipse-archived/ceylon-ide-eclipse

commit ad23ba0650c7d290e8e1c536b97909112410eaba

Add preference to format on save
Implements and closes #997.

plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/FormatAction.java
@@ -38,10 +38,16 @@ import com.redhat.ceylon.eclipse.util.Nodes;
 
 final class FormatAction extends Action {
     private final CeylonEditor editor;
+    private final boolean respectSelection;
 
     FormatAction(CeylonEditor editor) {
+        this(editor, false);
+    }
+    
+    FormatAction(CeylonEditor editor, boolean respectSelection) {
         super(null);
         this.editor = editor;
+        this.respectSelection = respectSelection;
     }
     
     /**
@@ -112,7 +118,7 @@ final class FormatAction extends Action {
     public void run() {
         IDocument document = editor.getCeylonSourceViewer().getDocument();
         final ITextSelection ts = getSelection(editor);
-        final boolean selected = ts.getLength() > 0;
+        final boolean selected = respectSelection && ts.getLength() > 0;
         final CeylonParseController pc = editor.getParseController();
         final Node node;
         if (selected) {

plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/CeylonEditor.java
@@ -15,6 +15,7 @@ import static com.redhat.ceylon.eclipse.code.editor.CeylonSourceViewerConfigurat
 import static com.redhat.ceylon.eclipse.code.editor.CeylonSourceViewerConfiguration.NORMALIZE_NL;
 import static com.redhat.ceylon.eclipse.code.editor.CeylonSourceViewerConfiguration.NORMALIZE_WS;
 import static com.redhat.ceylon.eclipse.code.editor.CeylonSourceViewerConfiguration.STRIP_TRAILING_WS;
+import static com.redhat.ceylon.eclipse.code.editor.CeylonSourceViewerConfiguration.FORMAT;
 import static com.redhat.ceylon.eclipse.code.editor.CeylonSourceViewerConfiguration.configCompletionPopup;
 import static com.redhat.ceylon.eclipse.code.editor.EditorActionIds.ADD_BLOCK_COMMENT;
 import static com.redhat.ceylon.eclipse.code.editor.EditorActionIds.CORRECT_INDENTATION;
@@ -1505,6 +1506,7 @@ public class CeylonEditor extends TextEditor {
         boolean normalizeNl = prefs.getBoolean(NORMALIZE_NL);
         boolean stripTrailingWs = prefs.getBoolean(STRIP_TRAILING_WS);
         boolean cleanImports = prefs.getBoolean(CLEAN_IMPORTS);
+        boolean format = prefs.getBoolean(FORMAT);
         if (cleanImports) {
             try {
                 CleanImportsHandler.cleanImports(this, doc);
@@ -1513,7 +1515,9 @@ public class CeylonEditor extends TextEditor {
                 e.printStackTrace();
             }
         }
-        if (normalizeWs || normalizeNl || stripTrailingWs) {
+        if (format) {
+            new FormatAction(this, /* respectSelection = */ false).run();
+        } else if (normalizeWs || normalizeNl || stripTrailingWs) {
             normalize(viewer, doc, normalizeWs, normalizeNl, stripTrailingWs);
         }
         super.doSave(progressMonitor);

plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/CeylonSourceViewerConfiguration.java
@@ -74,6 +74,7 @@ public class CeylonSourceViewerConfiguration extends TextSourceViewerConfigurati
     public static final String NORMALIZE_NL = "normalizedNl";
     public static final String STRIP_TRAILING_WS = "stripTrailingWs";
     public static final String CLEAN_IMPORTS = "cleanImports";
+    public static final String FORMAT = "format";
     
     protected final CeylonEditor editor;
     
@@ -131,6 +132,7 @@ public class CeylonSourceViewerConfiguration extends TextSourceViewerConfigurati
         preferenceStore.setDefault(NORMALIZE_NL, false);
         preferenceStore.setDefault(STRIP_TRAILING_WS, false);
         preferenceStore.setDefault(CLEAN_IMPORTS, false);
+        preferenceStore.setDefault(FORMAT, false);
         preferenceStore.setDefault(CLOSE_PARENS, true);
         preferenceStore.setDefault(CLOSE_BRACKETS, true);
         preferenceStore.setDefault(CLOSE_ANGLES, true);

plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/preferences/CeylonEditorPreferencesPage.java
@@ -11,6 +11,7 @@ import static com.redhat.ceylon.eclipse.code.editor.CeylonSourceViewerConfigurat
 import static com.redhat.ceylon.eclipse.code.editor.CeylonSourceViewerConfiguration.AUTO_ACTIVATION_DELAY;
 import static com.redhat.ceylon.eclipse.code.editor.CeylonSourceViewerConfiguration.AUTO_INSERT;
 import static com.redhat.ceylon.eclipse.code.editor.CeylonSourceViewerConfiguration.CLEAN_IMPORTS;
+import static com.redhat.ceylon.eclipse.code.editor.CeylonSourceViewerConfiguration.FORMAT;
 import static com.redhat.ceylon.eclipse.code.editor.CeylonSourceViewerConfiguration.CLOSE_ANGLES;
 import static com.redhat.ceylon.eclipse.code.editor.CeylonSourceViewerConfiguration.CLOSE_BACKTICKS;
 import static com.redhat.ceylon.eclipse.code.editor.CeylonSourceViewerConfiguration.CLOSE_BRACES;
@@ -75,6 +76,7 @@ public class CeylonEditorPreferencesPage
     BooleanFieldEditor normalizeNl;
     BooleanFieldEditor stripTrailingWs;
     BooleanFieldEditor cleanImports;
+    BooleanFieldEditor format;
     BooleanFieldEditor autoFoldImports;
     BooleanFieldEditor autoFoldComments;
     BooleanFieldEditor closeParens;
@@ -107,6 +109,7 @@ public class CeylonEditorPreferencesPage
         normalizeNl.store();
         stripTrailingWs.store();
         cleanImports.store();
+        format.store();
         autoFoldImports.store();
         autoFoldComments.store();
         closeAngles.store();
@@ -138,6 +141,7 @@ public class CeylonEditorPreferencesPage
         normalizeNl.loadDefault();
         stripTrailingWs.loadDefault();
         cleanImports.loadDefault();
+        format.loadDefault();
         autoFoldImports.loadDefault();
         autoFoldComments.loadDefault();
         closeAngles.loadDefault();
@@ -447,6 +451,11 @@ public class CeylonEditorPreferencesPage
                 parent);
         cleanImports.load();
         addField(cleanImports);
+        format = new BooleanFieldEditor(FORMAT,
+                "Format",
+                parent);
+        format.load();
+        addField(format);
     }
     
     protected Composite getFieldEditorParent(Composite group) {

commit 6d42f1941b5eae54e2d3af1d7f600d4cd50d913d

Fix for #986 (broke in ad23ba0)

plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/FormatAction.java
@@ -41,7 +41,7 @@ final class FormatAction extends Action {
     private final boolean respectSelection;
 
     FormatAction(CeylonEditor editor) {
-        this(editor, false);
+        this(editor, true);
     }
     
     FormatAction(CeylonEditor editor, boolean respectSelection) {