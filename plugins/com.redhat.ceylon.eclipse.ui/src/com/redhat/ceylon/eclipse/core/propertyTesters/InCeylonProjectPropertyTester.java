4d80b7786260dcf636fbb2310b211c658cf6de4a
davidfestal <david.festal@serli.com>
2014-07-25 11:54:54 +0200

    Fix #831

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/propertyTesters/InCeylonProjectPropertyTester.java
@@ -0,0 +1,25 @@
+package com.redhat.ceylon.eclipse.core.propertyTesters;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.jdt.core.IJavaElement;
+
+import com.redhat.ceylon.eclipse.core.builder.CeylonNature;
+
+public class InCeylonProjectPropertyTester extends PropertyTester {
+
+    private static final String IS_IN_CEYLON_PROJECT_PROPERTY = "isInCeylonProject";
+
+    @Override
+    public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
+        if (!(receiver instanceof IJavaElement)) {
+            return false;
+        }
+        IJavaElement javaElement = (IJavaElement) receiver;
+        
+        if (IS_IN_CEYLON_PROJECT_PROPERTY.equals(property)) {
+            return CeylonNature.isEnabled(javaElement.getJavaProject().getProject());
+        }
+        return false;
+    }
+
+}
