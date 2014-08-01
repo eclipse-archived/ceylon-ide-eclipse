fc054031bacec8a2f2780547e48dbf9639ad1896
davidfestal <david.festal@serli.com>
2014-08-01 15:16:26 +0200

    Fix #1059

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/model/JDTModuleManager.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/model/JDTModuleManager.java
@@ -237,7 +237,9 @@ public class JDTModuleManager extends LazyModuleManager {
                 if(moduleNameString.equals(Module.DEFAULT_MODULE_NAME)){
                     // Add the list of source package fragment roots
                     for (IPackageFragmentRoot root : javaProject.getPackageFragmentRoots()) {
-                        if (root.exists()) {
+                        if (root.exists() 
+                        		&& root.isConsistent() 
+                        		&& javaProject.isOnClasspath(root)) {
                             IClasspathEntry entry = root.getResolvedClasspathEntry();
                             if (entry.getEntryKind() == IClasspathEntry.CPE_SOURCE && !root.isExternal()) {
                                 roots.add(root);
