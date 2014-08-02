b4a032670454ffc8cd44e8185d242d16dc306d51
gavinking <gavin@ceylon-lang.org>
2014-08-02 22:51:38 +0200

    catch an exception coming from deep inside JDT

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/complete/ProposalComparator.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/complete/ProposalComparator.java
@@ -11,8 +11,8 @@ import com.redhat.ceylon.compiler.typechecker.model.NothingType;
 import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
 import com.redhat.ceylon.compiler.typechecker.model.TypedDeclaration;
 
-final class ProposalComparator implements
-        Comparator<DeclarationWithProximity> {
+final class ProposalComparator 
+        implements Comparator<DeclarationWithProximity> {
     private final String prefix;
     private final ProducedType type;
 
@@ -22,79 +22,85 @@ final class ProposalComparator implements
     }
 
     public int compare(DeclarationWithProximity x, DeclarationWithProximity y) {
-        boolean xbt = x.getDeclaration() instanceof NothingType;
-        boolean ybt = y.getDeclaration() instanceof NothingType;
-        if (xbt&&ybt) {
-            return 0;
-        }
-        if (xbt&&!ybt) {
-            return 1;
-        }
-        if (ybt&&!xbt) {
-            return -1;
-        }
-        ProducedType xtype = getResultType(x.getDeclaration());
-        ProducedType ytype = getResultType(y.getDeclaration());
-        boolean xbottom = xtype!=null && xtype.getDeclaration() instanceof NothingType;
-        boolean ybottom = ytype!=null && ytype.getDeclaration() instanceof NothingType;
-        if (xbottom && !ybottom) {
-            return 1;
-        }
-        if (ybottom && !xbottom) {
-            return -1;
-        }
-        String xName = x.getName();
-        String yName = y.getName();
-        if (!prefix.isEmpty() && isUpperCase(prefix.charAt(0))) {
+        try {
+            boolean xbt = x.getDeclaration() instanceof NothingType;
+            boolean ybt = y.getDeclaration() instanceof NothingType;
+            if (xbt&&ybt) {
+                return 0;
+            }
+            if (xbt&&!ybt) {
+                return 1;
+            }
+            if (ybt&&!xbt) {
+                return -1;
+            }
+            ProducedType xtype = getResultType(x.getDeclaration());
+            ProducedType ytype = getResultType(y.getDeclaration());
+            boolean xbottom = xtype!=null && xtype.getDeclaration() instanceof NothingType;
+            boolean ybottom = ytype!=null && ytype.getDeclaration() instanceof NothingType;
+            if (xbottom && !ybottom) {
+                return 1;
+            }
+            if (ybottom && !xbottom) {
+                return -1;
+            }
+            String xName = x.getName();
+            String yName = y.getName();
+            if (!prefix.isEmpty() && isUpperCase(prefix.charAt(0))) {
+                if (isLowerCase(xName.charAt(0)) && 
+                    isUpperCase(yName.charAt(0))) {
+                    return 1;
+                }
+                else if (isUpperCase(xName.charAt(0)) && 
+                         isLowerCase(yName.charAt(0))) {
+                    return -1;
+                }
+            }
+            if (type!=null) {
+                boolean xassigns = xtype!=null && xtype.isSubtypeOf(type);
+                boolean yassigns = ytype!=null && ytype.isSubtypeOf(type);
+                if (xassigns && !yassigns) {
+                    return -1;
+                }
+                if (yassigns && !xassigns) {
+                    return 1;
+                }
+                if (xassigns && yassigns) {
+                    boolean xtd = x.getDeclaration() instanceof TypedDeclaration;
+                    boolean ytd = y.getDeclaration() instanceof TypedDeclaration;
+                    if (xtd && !ytd) {
+                        return -1;
+                    }
+                    if (ytd && !xtd) {
+                        return 1;
+                    }
+                }
+            }
+            if (x.getProximity()!=y.getProximity()) {
+                return new Integer(x.getProximity()).compareTo(y.getProximity());
+            }
+            //if (!prefix.isEmpty() && isLowerCase(prefix.charAt(0))) {
             if (isLowerCase(xName.charAt(0)) && 
                     isUpperCase(yName.charAt(0))) {
-                return 1;
+                return -1;
             }
             else if (isUpperCase(xName.charAt(0)) && 
                     isLowerCase(yName.charAt(0))) {
-                return -1;
-            }
-        }
-        if (type!=null) {
-            boolean xassigns = xtype!=null && xtype.isSubtypeOf(type);
-            boolean yassigns = ytype!=null && ytype.isSubtypeOf(type);
-            if (xassigns && !yassigns) {
-                return -1;
-            }
-            if (yassigns && !xassigns) {
                 return 1;
             }
-            if (xassigns && yassigns) {
-                boolean xtd = x.getDeclaration() instanceof TypedDeclaration;
-                boolean ytd = y.getDeclaration() instanceof TypedDeclaration;
-                if (xtd && !ytd) {
-                    return -1;
-                }
-                if (ytd && !xtd) {
-                    return 1;
-                }
+            int nc = xName.compareTo(yName);
+            if (nc==0) {
+                String xqn = x.getDeclaration().getQualifiedNameString();
+                String yqn = y.getDeclaration().getQualifiedNameString();
+                return xqn.compareTo(yqn);
+            }
+            else {
+                return nc;
             }
         }
-        if (x.getProximity()!=y.getProximity()) {
-            return new Integer(x.getProximity()).compareTo(y.getProximity());
-        }
-        //if (!prefix.isEmpty() && isLowerCase(prefix.charAt(0))) {
-        if (isLowerCase(xName.charAt(0)) && 
-                isUpperCase(yName.charAt(0))) {
-            return -1;
-        }
-        else if (isUpperCase(xName.charAt(0)) && 
-                isLowerCase(yName.charAt(0))) {
-            return 1;
-        }
-        int nc = xName.compareTo(yName);
-        if (nc==0) {
-            String xqn = x.getDeclaration().getQualifiedNameString();
-            String yqn = y.getDeclaration().getQualifiedNameString();
-            return xqn.compareTo(yqn);
-        }
-        else {
-            return nc;
+        catch (Exception e) {
+            e.printStackTrace();
+            return 0;
         }
     }
 }
