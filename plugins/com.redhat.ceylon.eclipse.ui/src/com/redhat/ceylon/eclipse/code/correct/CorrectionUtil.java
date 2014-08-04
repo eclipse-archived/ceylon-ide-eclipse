76fe863d844e3f4e489fe0984c843161f185324f
gavinking <gavin@ceylon-lang.org>
2014-08-04 15:35:21 +0200

    make package-private

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/correct/CorrectionUtil.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/correct/CorrectionUtil.java
@@ -17,7 +17,7 @@ import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.redhat.ceylon.eclipse.code.editor.CeylonEditor;
 import com.redhat.ceylon.eclipse.code.parse.CeylonParseController;
 
-public class CorrectionUtil {
+class CorrectionUtil {
     
     static int getLevenshteinDistance(String x, String y) {
         
