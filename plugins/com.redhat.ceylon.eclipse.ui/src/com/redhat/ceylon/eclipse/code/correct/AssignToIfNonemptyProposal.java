643684451d0675d0be12417169e17e79b39b64e8
gavinking <gavin@ceylon-lang.org>
2014-08-10 23:33:56 +0200

    assign to if (nonempty) proposal

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/correct/AssignToIfNonemptyProposal.java
@@ -0,0 +1,88 @@
+package com.redhat.ceylon.eclipse.code.correct;
+
+import static com.redhat.ceylon.eclipse.code.complete.LinkedModeCompletionProposal.getNameProposals;
+
+import java.util.Collection;
+
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.jface.text.link.ProposalPosition;
+import org.eclipse.ltk.core.refactoring.DocumentChange;
+import org.eclipse.text.edits.InsertEdit;
+import org.eclipse.text.edits.MultiTextEdit;
+import org.eclipse.text.edits.ReplaceEdit;
+
+import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
+import com.redhat.ceylon.compiler.typechecker.model.Unit;
+import com.redhat.ceylon.compiler.typechecker.tree.Node;
+import com.redhat.ceylon.compiler.typechecker.tree.Tree;
+import com.redhat.ceylon.eclipse.util.LinkedMode;
+
+class AssignToIfNonemptyProposal extends LocalProposal {
+
+    protected DocumentChange createChange(IDocument document, Node expanse,
+            Integer stopIndex) {
+        DocumentChange change = 
+                new DocumentChange("Assign to If Nonempty", document);
+        change.setEdit(new MultiTextEdit());
+        change.addEdit(new InsertEdit(offset, "if (nonempty " + initialName + " = "));
+
+        String terminal = expanse.getEndToken().getText();
+        if (!terminal.equals(";")) {
+            change.addEdit(new InsertEdit(stopIndex+1, ") {}"));
+            exitPos = stopIndex+11;
+        }
+        else {
+            change.addEdit(new ReplaceEdit(stopIndex, 1, ") {}"));
+            exitPos = stopIndex+10;
+        }
+        return change;
+    }
+    
+    public AssignToIfNonemptyProposal(Tree.CompilationUnit cu, 
+            Node node, int currentOffset) {
+        super(cu, node, currentOffset);
+    }
+    
+    protected void addLinkedPositions(IDocument document, Unit unit)
+            throws BadLocationException {
+//        ProposalPosition typePosition = 
+//        		new ProposalPosition(document, offset, 5, 1, 
+//        				getSupertypeProposals(offset, unit, 
+//        						type, true, "value"));
+        
+        ProposalPosition namePosition = 
+        		new ProposalPosition(document, offset+13, initialName.length(), 0, 
+        				getNameProposals(offset+13, 0, nameProposals));
+        
+//        LinkedMode.addLinkedPosition(linkedModeModel, typePosition);
+        LinkedMode.addLinkedPosition(linkedModeModel, namePosition);
+    }
+    
+    @Override
+    String[] computeNameProposals(Node expression) {
+        return super.computeNameProposals(expression);
+    }
+    
+    @Override
+    public String getDisplayString() {
+        return "Assign expression to 'if (nonempty)' condition";
+    }
+
+    @Override
+    boolean isEnabled(ProducedType resultType) {
+        return rootNode.getUnit().isPossiblyEmptyType(resultType);
+    }
+
+    static void addAssignToIfNonemptyProposal(Tree.CompilationUnit cu, 
+            Collection<ICompletionProposal> proposals,
+            Node node, int currentOffset) {
+        AssignToIfNonemptyProposal prop = 
+                new AssignToIfNonemptyProposal(cu, node, currentOffset);
+        if (prop.isEnabled()) {
+            proposals.add(prop);
+        }
+    }
+
+}
