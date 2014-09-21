e868f0d06ae2ec217522d5c8bc105210aee6a653
gavinking <gavin@ceylon-lang.org>
2014-09-21 13:22:52 +0200

    linked mode proposals of package members in import completion #643

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/complete/PackageCompletions.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/complete/PackageCompletions.java
@@ -1,27 +1,43 @@
 package com.redhat.ceylon.eclipse.code.complete;
 
+import static com.redhat.ceylon.eclipse.code.complete.CeylonCompletionProcessor.NO_COMPLETIONS;
 import static com.redhat.ceylon.eclipse.code.complete.CompletionUtil.fullPath;
 import static com.redhat.ceylon.eclipse.code.complete.CompletionUtil.isModuleDescriptor;
+import static com.redhat.ceylon.eclipse.code.editor.CeylonSourceViewerConfiguration.LINKED_MODE;
 import static com.redhat.ceylon.eclipse.code.hover.DocumentationHover.getDocumentationFor;
-import static com.redhat.ceylon.eclipse.code.outline.CeylonLabelProvider.MODULE;
-import static com.redhat.ceylon.eclipse.code.outline.CeylonLabelProvider.PACKAGE;
+import static com.redhat.ceylon.eclipse.code.outline.CeylonLabelProvider.getImageForDeclaration;
 import static com.redhat.ceylon.eclipse.core.builder.CeylonBuilder.getPackageName;
+import static com.redhat.ceylon.eclipse.ui.CeylonResources.MODULE;
+import static com.redhat.ceylon.eclipse.ui.CeylonResources.PACKAGE;
 import static com.redhat.ceylon.eclipse.util.Escaping.escapePackageName;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.jface.text.contentassist.IContextInformation;
+import org.eclipse.jface.text.link.ILinkedModeListener;
+import org.eclipse.jface.text.link.LinkedModeModel;
+import org.eclipse.jface.text.link.ProposalPosition;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.editors.text.EditorsUI;
 
+import com.redhat.ceylon.compiler.typechecker.model.Declaration;
 import com.redhat.ceylon.compiler.typechecker.model.ImportList;
 import com.redhat.ceylon.compiler.typechecker.model.Module;
 import com.redhat.ceylon.compiler.typechecker.model.Package;
 import com.redhat.ceylon.compiler.typechecker.model.Unit;
+import com.redhat.ceylon.compiler.typechecker.model.Util;
 import com.redhat.ceylon.compiler.typechecker.tree.Node;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
+import com.redhat.ceylon.eclipse.code.editor.CeylonEditor;
 import com.redhat.ceylon.eclipse.code.parse.CeylonParseController;
+import com.redhat.ceylon.eclipse.util.EditorUtil;
+import com.redhat.ceylon.eclipse.util.LinkedMode;
 
 public class PackageCompletions {
 
@@ -65,6 +81,71 @@ public class PackageCompletions {
             }
             else {
                 return new Point(offset+completed.length()-prefix.length()-len, 0);
+            }
+        }
+        
+        @Override
+        public void apply(IDocument document) {
+            super.apply(document);
+            if (withBody && 
+                    EditorsUI.getPreferenceStore()
+                             .getBoolean(LINKED_MODE)) {
+                final LinkedModeModel linkedModeModel = new LinkedModeModel();
+                final Point selection = getSelection(document);
+                List<ICompletionProposal> proposals = new ArrayList<ICompletionProposal>();
+                for (final Declaration d: p.getMembers()) {
+                    if (Util.isResolvable(d) && d.isShared()) {
+                        proposals.add(new ICompletionProposal() {
+                            @Override
+                            public Point getSelection(IDocument document) {
+                                return null;
+                            }
+                            @Override
+                            public Image getImage() {
+                                return getImageForDeclaration(d);
+                            }
+                            
+                            @Override
+                            public String getDisplayString() {
+                                return d.getName();
+                            }
+                            
+                            @Override
+                            public IContextInformation getContextInformation() {
+                                return null;
+                            }
+                            
+                            @Override
+                            public String getAdditionalProposalInfo() {
+                                return null;
+                            }
+                            
+                            @Override
+                            public void apply(IDocument document) {
+                                try {
+                                    document.replace(selection.x, selection.y, 
+                                            d.getName());
+                                }
+                                catch (BadLocationException e) {
+                                    e.printStackTrace();
+                                }
+                                linkedModeModel.exit(ILinkedModeListener.UPDATE_CARET);
+                            }
+                        });
+                    }
+                }
+                ProposalPosition linkedPosition = 
+                        new ProposalPosition(document, selection.x, selection.y, 0, 
+                                proposals.toArray(NO_COMPLETIONS));
+                try {
+                    LinkedMode.addLinkedPosition(linkedModeModel, linkedPosition);
+                    LinkedMode.installLinkedMode((CeylonEditor) EditorUtil.getCurrentEditor(), 
+                            document, linkedModeModel, this, new LinkedMode.NullExitPolicy(),
+                            -1, 0);
+                }
+                catch (BadLocationException ble) {
+                    ble.printStackTrace();
+                }
             }
         }
 
