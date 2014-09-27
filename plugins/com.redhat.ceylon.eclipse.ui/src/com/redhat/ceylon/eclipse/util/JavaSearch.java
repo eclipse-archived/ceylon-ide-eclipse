37001a89833ea873976a005cddfe31719e13b9dc
gavinking <gavin@ceylon-lang.org>
2014-09-27 02:55:37 +0200

    work around strange behavior of SearchPattern

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/util/JavaSearch.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/util/JavaSearch.java
@@ -57,8 +57,15 @@ public class JavaSearch {
             }
         }
         else {
-            return createPattern(pattern, CLASS_AND_INTERFACE, 
+            SearchPattern searchPattern = createPattern(pattern, CLASS_AND_INTERFACE, 
                     limitTo, R_EXACT_MATCH);
+            //weirdly, ALL_OCCURRENCES doesn't return all occurrences
+            if (limitTo==IJavaSearchConstants.ALL_OCCURRENCES) {
+                searchPattern = createOrPattern(createPattern(pattern, CLASS_AND_INTERFACE, 
+                    IJavaSearchConstants.IMPLEMENTORS, R_EXACT_MATCH),
+                    searchPattern);
+            }
+            return searchPattern;
         }
     }
 
