48fc2eacb4aebf5da3b2eae03f6941b2f32b5bf9
Lucas Werkmeister <mail@lucaswerkmeister.de>
2014-10-03 16:51:29 +0200

    Update formatter config space tab (preview, phrasing)
    Preview again converted with
    
        xclip -o | while IFS='' read line; do echo '+ "'"${line//\"/\\\"}"'\n"'; done
    
    from 29e6c54. (This preview code is a bit more contrived than the others
    because it needs to contain a lot of language features.)

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/style/FormatterTabSpace.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/style/FormatterTabSpace.java
@@ -11,11 +11,55 @@ import org.eclipse.swt.widgets.Group;
 public class FormatterTabSpace extends FormatterTabPage {
 
     private final String PREVIEW =
-            "import ceylon.collection { MutableList, freq=frequencies }\n\n\n"
-            + "shared class Example(str) {"
-            + "shared String str; \n"
-            + "value test => 0;\n"
-            + "}";
+            "import ceylon.file {\n"
+            + "    pp=parsePath,\n"
+            + "    File,\n"
+            + "    Reader\n"
+            + "}\n"
+            + "import ceylon.collection {\n"
+            + "    LL=LinkedList\n"
+            + "}\n"
+            + "\n"
+            + "by (\"Someone\")\n"
+            + "throws (`class Exception`, \"If anything goes wrong\")\n"
+            + "shared class WithLineNumbersReader(Reader r)\n"
+            + "        satisfies Reader & Iterable<String,Null> {\n"
+            + "    \n"
+            + "    LL<String> lines = LL<String>();\n"
+            + "    variable Integer lineNum = 0;\n"
+            + "    \n"
+            + "    close() => r.close();\n"
+            + "    shared actual Byte[] readBytes(Integer max) {\n"
+            + "        throw AssertionError(\"Can't read bytes from line-oriented reader\");\n"
+            + "    }\n"
+            + "    shared actual Iterator<String> iterator() => lines.iterator();\n"
+            + "    shared actual String? readLine() {\n"
+            + "        if (exists line = r.readLine()) {\n"
+            + "            value ret = lineNum.string + \"t\" + line;\n"
+            + "            lines.add(ret);\n"
+            + "            lineNum++;\n"
+            + "            return ret;\n"
+            + "        } else {\n"
+            + "            return null;\n"
+            + "        }\n"
+            + "    }\n"
+            + "}\n"
+            + "\n"
+            + "void run() {\n"
+            + "    assert (is File f = pp(process.arguments.first else nothing).resource);\n"
+            + "    try (r = WithLineNumbersReader(f.Reader())) {\n"
+            + "        variable String? line = \"\";\n"
+            + "        while (line exists) {\n"
+            + "            line = r.readLine();\n"
+            + "        }\n"
+            + "        for (l in r) {\n"
+            + "            print(l);\n"
+            + "        }\n"
+            + "    } catch (Exception e) {\n"
+            + "        e.printStackTrace();\n"
+            + "    }\n"
+            + "    print({ \"Here\", \"have\", \"an\", \"iterable\", \"enumeration\" });\n"
+            + "}\n";
 
     private CeylonPreview ceylonPreview;
 
@@ -29,13 +73,13 @@ public class FormatterTabSpace extends FormatterTabPage {
 
         final Group generalGroup = createGroup(numColumns, composite,
                 "Spacing Options");
-        createCheckboxPref(generalGroup, numColumns, "Space before method or class positional argument list",
+        createCheckboxPref(generalGroup, numColumns, "Space before positional argument list",
                 FORMATTER_space_BeforeMethodOrClassPositionalArgumentList, FALSE_TRUE);
         createCheckboxPref(generalGroup, numColumns, "Space before resource list",
                 FORMATTER_space_BeforeResourceList, FALSE_TRUE);
         createCheckboxPref(generalGroup, numColumns, "Space before catch variable",
                 FORMATTER_space_BeforeCatchVariable, FALSE_TRUE);
-        createCheckboxPref(generalGroup, numColumns, "Space before 'satisfies' and 'of'",
+        createCheckboxPref(generalGroup, numColumns, "Space around satisfied and case types",
                 FORMATTER_space_AroundSatisfiesOf, FALSE_TRUE);
         createCheckboxPref(generalGroup, numColumns, "Space around '=' for import alias",
                 FORMATTER_space_AroundImportAliasEqualsSign, FALSE_TRUE);
@@ -72,7 +116,7 @@ public class FormatterTabSpace extends FormatterTabPage {
         createCheckboxPref(generalGroup, 1, "Before )",
                 FORMATTER_space_BeforeValueIteratorClosingParenthesis, FALSE_TRUE);
         
-        createLabel(2, generalGroup, "Sequence Enumeration");
+        createLabel(2, generalGroup, "Iterable Enumeration");
         createCheckboxPref(generalGroup, 1, "After {",
                 FORMATTER_space_AfterSequenceEnumerationOpeningBrace, FALSE_TRUE);         
         createCheckboxPref(generalGroup, 1, "Before }",
