b2ade08e2d241ca4ac414f95e2ad939f9431e9e1
gavinking <gavin@ceylon-lang.org>
2014-11-15 15:04:04 -0300

    add new quick fix to change assert to if

---
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/correct/ChangeToIfProposal.java
@@ -0,0 +1,70 @@
+package com.redhat.ceylon.eclipse.code.correct;
+
+import static com.redhat.ceylon.eclipse.util.Indents.getDefaultIndent;
+import static com.redhat.ceylon.eclipse.util.Indents.getDefaultLineDelimiter;
+import static com.redhat.ceylon.eclipse.util.Indents.getIndent;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.Region;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.ltk.core.refactoring.TextFileChange;
+import org.eclipse.text.edits.InsertEdit;
+import org.eclipse.text.edits.MultiTextEdit;
+import org.eclipse.text.edits.ReplaceEdit;
+
+import com.redhat.ceylon.compiler.typechecker.tree.Tree;
+import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
+
+public class ChangeToIfProposal {
+
+    static void addChangeToIfProposal(Collection<ICompletionProposal> proposals,
+            IDocument doc, IFile file, Tree.CompilationUnit rootNode,
+            final Tree.Statement statement) {
+        if (statement instanceof Tree.Assertion) {
+            Tree.ConditionList conditionList = 
+                    ((Tree.Assertion) statement).getConditionList();
+            if (conditionList!=null) {
+                class FindBodyVisitor extends Visitor {
+                    Tree.Body result;
+                    @Override
+                    public void visit(Tree.Body that) {
+                        if (that.getStatements().contains(statement)) {
+                            result = that;
+                        }
+                        else {
+                            super.visit(that);
+                        }
+                    }
+                }
+                FindBodyVisitor fbv = new FindBodyVisitor();
+                fbv.visit(rootNode);
+                List<Tree.Statement> statements = fbv.result.getStatements();
+                Tree.Statement last = statements.get(statements.size()-1);
+                boolean isLast = statement==last;
+                TextFileChange change = 
+                        new TextFileChange("Change Assert To If", file);
+                change.setEdit(new MultiTextEdit());
+                int begin = statement.getStartIndex();
+                int end = conditionList.getStartIndex();
+                change.addEdit(new ReplaceEdit(begin, end-begin, "if "));
+                change.addEdit(new ReplaceEdit(statement.getStopIndex(), 1, 
+                        isLast ? " {}" : " {"));
+                for (int i=statements.indexOf(statement)+1; i<statements.size(); i++) {
+                    change.addEdit(new InsertEdit(statements.get(i).getStartIndex(), 
+                            getDefaultIndent()));
+                }
+                if (!isLast) {
+                    change.addEdit(new InsertEdit(last.getStopIndex()+1, 
+                            getDefaultLineDelimiter(doc) + getIndent(last, doc) + "}"));
+                }
+                proposals.add(new CorrectionProposal("Change 'assert' to 'if'", change, 
+                        new Region(statement.getStopIndex()-2, 0)));
+            }
+        }
+    }
+
+}
