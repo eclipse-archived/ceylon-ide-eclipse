8d5b4bd5217e12676aeefdf1aeab81f097a8f8f5
davidfestal <david.festal@serli.com>
2014-11-20 21:48:30 +0100

    Basic variable filtering oin Variables View (drop all the variables whose name contains a $)

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/debug/CeylonStackFrameContentProvider.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/debug/CeylonStackFrameContentProvider.java
@@ -1,25 +1,23 @@
 package com.redhat.ceylon.eclipse.core.debug;
 
-import org.eclipse.debug.internal.ui.viewers.model.provisional.IChildrenCountUpdate;
-import org.eclipse.debug.internal.ui.viewers.model.provisional.IChildrenUpdate;
-import org.eclipse.debug.internal.ui.viewers.model.provisional.IHasChildrenUpdate;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.internal.ui.viewers.model.provisional.IPresentationContext;
+import org.eclipse.debug.internal.ui.viewers.model.provisional.IViewerUpdate;
+import org.eclipse.jdt.debug.core.IJavaThread;
 import org.eclipse.jdt.internal.debug.ui.variables.JavaStackFrameContentProvider;
 
 public class CeylonStackFrameContentProvider extends JavaStackFrameContentProvider {
-
     @Override
-    public void update(IChildrenCountUpdate[] updates) {
-        super.update(updates);
+    protected Object[] getAllChildren(Object parent, IPresentationContext context, IViewerUpdate monitor) throws CoreException {
+        try {
+            Object[] variables = super.getAllChildren(parent, context, monitor);
+            return CeylonContentProviderFilter.filterVariables(variables, context);
+        } catch (CoreException e) {
+            if (e.getStatus().getCode() == IJavaThread.ERR_THREAD_NOT_SUSPENDED) {
+                monitor.cancel();
+                return EMPTY;
+            }
+            throw e;
+        }
     }
-
-    @Override
-    public void update(IChildrenUpdate[] updates) {
-        super.update(updates);
-    }
-
-    @Override
-    public void update(IHasChildrenUpdate[] updates) {
-        super.update(updates);
-    }
-
 }
