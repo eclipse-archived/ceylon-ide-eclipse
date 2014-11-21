09cad46f561ee0e73bedab45291cd76ff75f8e90
davidfestal <david.festal@serli.com>
2014-11-21 22:52:44 +0100

    Apply Ceylon-aware variable filtering only in real Ceylon contexts

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/debug/CeylonStackFrameContentProvider.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/debug/CeylonStackFrameContentProvider.java
@@ -1,5 +1,7 @@
 package com.redhat.ceylon.eclipse.core.debug;
 
+import static com.redhat.ceylon.eclipse.core.debug.CeylonPresentationContext.isCeylonContext;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.internal.ui.viewers.model.provisional.IPresentationContext;
 import org.eclipse.debug.internal.ui.viewers.model.provisional.IViewerUpdate;
@@ -7,11 +9,36 @@ import org.eclipse.jdt.debug.core.IJavaThread;
 import org.eclipse.jdt.internal.debug.ui.variables.JavaStackFrameContentProvider;
 
 public class CeylonStackFrameContentProvider extends JavaStackFrameContentProvider {
+
+    @Override
+    protected Object[] getChildren(Object parent, int index, int length, IPresentationContext context, IViewerUpdate monitor) throws CoreException {
+        return super.getChildren(parent, index, length, new CeylonPresentationContext(context, monitor), monitor);
+    }
+        
+    @Override
+    protected int getChildCount(Object element, IPresentationContext context, IViewerUpdate monitor) throws CoreException {
+        return super.getChildCount(element, new CeylonPresentationContext(context, monitor), monitor);
+    }
+    
+    @Override
+    protected boolean hasChildren(Object element, IPresentationContext context, IViewerUpdate monitor) throws CoreException {
+        if (isCeylonContext(monitor)) {
+            return getChildCount(element, context, monitor) > 0;
+        } else {
+            return super.hasChildren(element, context, monitor);
+        }
+    }
+    
     @Override
     protected Object[] getAllChildren(Object parent, IPresentationContext context, IViewerUpdate monitor) throws CoreException {
         try {
             Object[] variables = super.getAllChildren(parent, context, monitor);
-            return CeylonContentProviderFilter.filterVariables(variables, context);
+            if (context instanceof CeylonPresentationContext) {
+                if (((CeylonPresentationContext) context).isCeylonContext()) {
+                    return CeylonContentProviderFilter.filterVariables(variables, context);
+                }
+            }
+            return variables;
         } catch (CoreException e) {
             if (e.getStatus().getCode() == IJavaThread.ERR_THREAD_NOT_SUSPENDED) {
                 monitor.cancel();
--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/debug/CeylonVariableContentProvider.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/debug/CeylonVariableContentProvider.java
@@ -1,20 +1,43 @@
 package com.redhat.ceylon.eclipse.core.debug;
 
+import static com.redhat.ceylon.eclipse.core.debug.CeylonContentProviderFilter.filterVariables;
+import static com.redhat.ceylon.eclipse.core.debug.CeylonPresentationContext.isCeylonContext;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.internal.ui.viewers.model.provisional.IPresentationContext;
 import org.eclipse.debug.internal.ui.viewers.model.provisional.IViewerUpdate;
 import org.eclipse.jdt.internal.debug.ui.variables.JavaVariableContentProvider;
 
 public class CeylonVariableContentProvider extends JavaVariableContentProvider {
+
     @Override
-    protected Object[] getAllChildren(Object parent, IPresentationContext context) throws CoreException {
-        Object[] variables = super.getAllChildren(parent, context);
-        return CeylonContentProviderFilter.filterVariables(variables, context);
+    protected Object[] getChildren(Object parent, int index, int length, IPresentationContext context, IViewerUpdate monitor) throws CoreException {
+        return super.getChildren(parent, index, length, new CeylonPresentationContext(context, monitor), monitor);
+    }
+        
+    @Override
+    protected int getChildCount(Object element, IPresentationContext context, IViewerUpdate monitor) throws CoreException {
+        return super.getChildCount(element, new CeylonPresentationContext(context, monitor), monitor);
     }
     
     @Override
-    protected boolean hasChildren(Object element, IPresentationContext context,
-            IViewerUpdate monitor) throws CoreException {
-        return getAllChildren(element, context).length > 0;
+    protected boolean hasChildren(Object element, IPresentationContext context, IViewerUpdate monitor) throws CoreException {
+        if (isCeylonContext(monitor)) {
+            return getChildCount(element, context, monitor) > 0;
+        } else {
+            return super.hasChildren(element, context, monitor);
+        }
+    }
+    
+    @Override
+    protected Object[] getAllChildren(Object parent,
+            IPresentationContext context) throws CoreException {
+        Object[] variables = super.getAllChildren(parent, context);
+        if (context instanceof CeylonPresentationContext) {
+            if (((CeylonPresentationContext) context).isCeylonContext()) {
+                return filterVariables(variables, context);
+            }
+        }
+        return variables;
     }
 }
