29f55c1f95967eccb527c3eb4047d9c02b532b7a
davidfestal <david.festal@serli.com>
2014-11-24 18:05:38 +0100

    Manage quoted variables (because having the name of a Java keyword)

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/debug/CeylonContentProviderFilter.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/debug/CeylonContentProviderFilter.java
@@ -7,14 +7,21 @@ import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.internal.ui.viewers.model.provisional.IPresentationContext;
 import org.eclipse.jdt.debug.core.IJavaVariable;
 
+import com.redhat.ceylon.compiler.java.codegen.Naming;
+
 public class CeylonContentProviderFilter {
     public static Object[] filterVariables(Object[] variables, IPresentationContext context) throws DebugException {
         List<Object> keep = new ArrayList<Object>(variables.length);
         for (int i = 0; i < variables.length; i++) {
             boolean filter = false;
             if (variables[i] instanceof IJavaVariable){
-                IJavaVariable var = (IJavaVariable)variables[i];
-                if (var.getName().contains("$")){
+                String name = ((IJavaVariable)variables[i]).getName();
+                if (name.charAt(0) == '$') {
+                    if (Naming.isJavaKeyword(name, 1, name.length())) {
+                        name = name.substring(1);
+                    }
+                }
+                if (name.contains("$")){
                     filter = true;
                 }
             }
