329e69c41c8c313d9b829c03828b57557a1d92c5
davidfestal <david.festal@serli.com>
2014-12-10 23:05:15 +0100

    Precise mapping from generated `IJavaElement`s to Ceylon source decls

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/util/JavaSearch.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/util/JavaSearch.java
@@ -1,6 +1,11 @@
 package com.redhat.ceylon.eclipse.util;
 
 import static com.redhat.ceylon.compiler.java.codegen.CodegenUtil.getJavaNameOfDeclaration;
+import static com.redhat.ceylon.compiler.loader.AbstractModelLoader.CEYLON_CEYLON_ANNOTATION;
+import static com.redhat.ceylon.compiler.loader.AbstractModelLoader.CEYLON_LOCAL_DECLARATION_ANNOTATION;
+import static com.redhat.ceylon.compiler.loader.AbstractModelLoader.CEYLON_METHOD_ANNOTATION;
+import static com.redhat.ceylon.compiler.loader.AbstractModelLoader.CEYLON_NAME_ANNOTATION;
+import static com.redhat.ceylon.compiler.loader.AbstractModelLoader.CEYLON_OBJECT_ANNOTATION;
 import static com.redhat.ceylon.eclipse.core.builder.CeylonBuilder.getCeylonClassesOutputFolder;
 import static com.redhat.ceylon.eclipse.core.builder.CeylonBuilder.getProjectTypeChecker;
 import static com.redhat.ceylon.eclipse.core.builder.CeylonBuilder.isExplodeModulesEnabled;
@@ -10,23 +15,36 @@ import static org.eclipse.jdt.core.search.IJavaSearchConstants.METHOD;
 import static org.eclipse.jdt.core.search.SearchPattern.R_EXACT_MATCH;
 import static org.eclipse.jdt.core.search.SearchPattern.createOrPattern;
 import static org.eclipse.jdt.core.search.SearchPattern.createPattern;
+import static org.eclipse.jdt.internal.core.util.Util.getUnresolvedJavaElement;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.jdt.core.Flags;
+import org.eclipse.jdt.core.IAnnotatable;
+import org.eclipse.jdt.core.IAnnotation;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IMember;
+import org.eclipse.jdt.core.IMemberValuePair;
 import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.compiler.CharOperation;
 import org.eclipse.jdt.core.search.IJavaSearchConstants;
 import org.eclipse.jdt.core.search.SearchEngine;
 import org.eclipse.jdt.core.search.SearchPattern;
 import org.eclipse.jdt.core.search.SearchRequestor;
+import org.eclipse.jdt.internal.compiler.env.IBinaryType;
+import org.eclipse.jdt.internal.compiler.lookup.MethodBinding;
+import org.eclipse.jdt.internal.compiler.lookup.ReferenceBinding;
+import org.eclipse.jdt.internal.core.JavaElement;
 import org.eclipse.jdt.internal.corext.util.SearchUtils;
 
 import com.redhat.ceylon.compiler.typechecker.TypeChecker;
@@ -35,9 +53,14 @@ import com.redhat.ceylon.compiler.typechecker.model.Declaration;
 import com.redhat.ceylon.compiler.typechecker.model.Method;
 import com.redhat.ceylon.compiler.typechecker.model.Module;
 import com.redhat.ceylon.compiler.typechecker.model.Modules;
+import com.redhat.ceylon.compiler.typechecker.model.Package;
+import com.redhat.ceylon.compiler.typechecker.model.Scope;
+import com.redhat.ceylon.compiler.typechecker.model.Specification;
 import com.redhat.ceylon.compiler.typechecker.model.Value;
 import com.redhat.ceylon.eclipse.core.builder.CeylonBuilder;
 import com.redhat.ceylon.eclipse.core.builder.CeylonNature;
+import com.redhat.ceylon.eclipse.core.model.JDTModelLoader;
+import com.redhat.ceylon.eclipse.core.model.JDTModelLoader.ActionOnResolvedGeneratedType;
 import com.redhat.ceylon.eclipse.core.model.JDTModule;
 
 public class JavaSearch {
@@ -174,6 +197,74 @@ public class JavaSearch {
         }
     }
     
+    private static boolean isCeylon(IMember element) {
+        if (element instanceof IAnnotatable) {
+            IAnnotation ceylonAnnotation = ((IAnnotatable) element).getAnnotation(CEYLON_CEYLON_ANNOTATION);
+            return ceylonAnnotation.exists();
+        }
+        return false;
+    }
+
+    private static boolean isCeylonObject(IMember element) {
+        if (element instanceof IAnnotatable) {
+            IAnnotation ceylonAnnotation = ((IAnnotatable) element).getAnnotation(CEYLON_OBJECT_ANNOTATION);
+            return ceylonAnnotation.exists();
+        }
+        return false;
+    }
+    
+    private static boolean isCeylonMethod(IMember element) {
+        if (element instanceof IAnnotatable) {
+            IAnnotation ceylonAnnotation = ((IAnnotatable) element).getAnnotation(CEYLON_METHOD_ANNOTATION);
+            return ceylonAnnotation.exists();
+        }
+        return false;
+    }
+
+    private static String getCeylonNameAnnotationValue(IMember element) {
+        if (element instanceof IAnnotatable) {
+            IAnnotation nameAnnotation = ((IAnnotatable) element).getAnnotation(CEYLON_NAME_ANNOTATION);
+            if (nameAnnotation.exists()) {
+                try {
+                    for (IMemberValuePair mvp : nameAnnotation.getMemberValuePairs()) {
+                        if ("value".equals(mvp.getMemberName())) {
+                            Object value = mvp.getValue();
+                            if ((value instanceof String) 
+                                && ! "".equals(value)) {
+                                return (String) value;
+                            }
+                        }
+                    }
+                } catch (JavaModelException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    private static String getLocalDeclarationQualifier(IMember element) {
+        if (element instanceof IAnnotatable) {
+            IAnnotation nameAnnotation = ((IAnnotatable) element).getAnnotation(CEYLON_LOCAL_DECLARATION_ANNOTATION);
+            if (nameAnnotation.exists()) {
+                try {
+                    for (IMemberValuePair mvp : nameAnnotation.getMemberValuePairs()) {
+                        if ("qualifier".equals(mvp.getMemberName())) {
+                            Object value = mvp.getValue();
+                            if ((value instanceof String) 
+                                    && ! "".equals(value)) {
+                                return (String) value;
+                            }
+                        }
+                    }
+                } catch (JavaModelException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+    
     /*
      * returns null if it's a method with no Ceylon equivalent
      * (internal getter of a Ceylon object value)
@@ -196,52 +287,166 @@ public class JavaSearch {
                 name = "hash";
             }
         } else if (dec instanceof IType) {
+            IType type = (IType) dec;
+            String nameAnnotationValue = getCeylonNameAnnotationValue(type);
+            if (nameAnnotationValue != null) {
+                return nameAnnotationValue;
+            }
+            
             if (name.endsWith("_")) {
-                name = name.substring(0, name.length()-1);
+                if (isCeylonObject(type) ||
+                        isCeylonMethod(type)) {
+                    name = name.substring(0, name.length()-1);
+                }
             }
         }
         return name;
     }
 
-    public static String getCeylonQualifiedName(IMember dec) {
-        IPackageFragment packageFragment = (IPackageFragment) 
-                dec.getAncestor(IJavaElement.PACKAGE_FRAGMENT);
-        IType type = (IType) dec.getAncestor(IJavaElement.TYPE);
-        
-        String qualifier = packageFragment.getElementName();
-        if (! qualifier.isEmpty()) {
-            qualifier += "::";
-        }
-        String name = getCeylonSimpleName(dec);
-        if (name == null) {
-            // getter of a Ceylon object value
-            return getCeylonQualifiedName(type);
-        }
-        
-        if (dec!=type) {
-            String fullyQualifiedTypeName = type.getFullyQualifiedName();
-            String[] parts = fullyQualifiedTypeName.split("\\.");
-            String typeName = parts.length == 0 ? fullyQualifiedTypeName : parts[parts.length-1];
-            if (typeName.endsWith("$impl")) {
-                typeName = typeName.substring(0, typeName.length()-5);
-            }
-            if (typeName.endsWith(name + "_")) {
-                return qualifier + name;
-            }
-            else {
-                if (typeName.length()>2 && 
-                        !Character.isUpperCase(typeName.charAt(0))
-                        && typeName.endsWith("_")) {
-                    // case of an object value
-                    typeName = typeName.substring(0, typeName.length()-1);
+    public static boolean elementEqualsDeclaration(IMember typeOrMethod, Declaration declaration) {
+        Map<String, Map<String, Object>> memberAnnotations = new HashMap<>();
+        if (typeOrMethod instanceof IAnnotatable) {
+            try {
+                for (IAnnotation annotation : ((IAnnotatable) typeOrMethod).getAnnotations()) {
+                    String annotationName = annotation.getElementName();
+                    if (! memberAnnotations.containsKey(annotationName)) {
+                        memberAnnotations.put(annotationName, new HashMap<String, Object>());
+                    }
+                    Map<String, Object> annotationMembers = memberAnnotations.get(annotationName);
+                    for (IMemberValuePair pair : annotation.getMemberValuePairs()) {
+                        annotationMembers.put(pair.getMemberName(), pair.getValue());
+                    }
                 }
-                return qualifier + 
-                        typeName + '.' + name;
+            } catch (JavaModelException e) {
+                e.printStackTrace();
             }
         }
-        else {
-            return qualifier + name;
+        
+        Map<String, Map<String, Object>> typeAnnotations = new HashMap<>();
+        if (typeOrMethod instanceof IType) {
+            IType type = (IType) typeOrMethod;
+            try {
+                for (IAnnotation annotation : ((IAnnotatable) type).getAnnotations()) {
+                    String annotationName = annotation.getElementName();
+                    if (! typeAnnotations.containsKey(annotationName)) {
+                        typeAnnotations.put(annotationName, new HashMap<String, Object>());
+                    }
+                    Map<String, Object> annotationMembers = typeAnnotations.get(annotationName);
+                    for (IMemberValuePair pair : annotation.getMemberValuePairs()) {
+                        annotationMembers.put(pair.getMemberName(), pair.getValue());
+                    }
+                }
+            } catch (JavaModelException e) {
+                e.printStackTrace();
+            }
         }
+
+        String javaElementSimpleName = getCeylonSimpleName(typeOrMethod);
+        String ceylonDeclarationSimpleName = declaration.getName();
+        if (! javaElementSimpleName.equals(ceylonDeclarationSimpleName)) {
+            return false;
+        }
+        
+        if (typeOrMethod instanceof IType) {
+            String localDeclarationQualifier = getLocalDeclarationQualifier(typeOrMethod);
+            if (localDeclarationQualifier != null) {
+                if (! localDeclarationQualifier.equals(declaration.getQualifier())) {
+                    return false;
+                }
+            }
+            if (isCeylonObject(typeOrMethod) 
+                    && !(declaration instanceof Value)) {
+                return false;
+            }
+        }
+        
+        // TODO : also manage the case of local declarations that are 
+        // wrongly top-level (specific retrieval of the parent IMember)
+        
+        IMember declaringElement = typeOrMethod.getDeclaringType();
+        if (declaringElement == null) {
+            if (typeOrMethod instanceof IType)  {
+                IType type = (IType) typeOrMethod;
+                try {
+                    if (type.isLocal() ||type.isAnonymous() || false) {
+                        final MethodBinding[] enclosingMethodBinding = new MethodBinding[1];
+                        JDTModelLoader.doOnResolvedGeneratedType(type, new ActionOnResolvedGeneratedType() {
+                            @Override
+                            public void doWithBinding(IType classModel, ReferenceBinding classBinding,
+                                    IBinaryType binaryType) {
+                                char[] enclosingMethodSignature = binaryType.getEnclosingMethod();
+                                if (enclosingMethodSignature != null 
+                                        && enclosingMethodSignature.length > 0) {
+                                    ReferenceBinding enclosingType = classBinding.enclosingType();
+                                    if (enclosingType != null) {
+                                        for (MethodBinding method : enclosingType.methods()) {
+                                            if (CharOperation.equals(
+                                                    CharOperation.concat(method.selector, method.signature()), 
+                                                    enclosingMethodSignature)) {
+                                                enclosingMethodBinding[0] = method;
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        });
+
+                        if (enclosingMethodBinding[0] != null) {
+                            IMethod enclosingMethod = (IMethod) getUnresolvedJavaElement(enclosingMethodBinding[0], null, null);
+                            if (enclosingMethod != null && !enclosingMethod.isResolved()) {
+                                enclosingMethod = (IMethod) ((JavaElement)enclosingMethod).resolved(enclosingMethodBinding[0]);
+                                if (enclosingMethod != null) {
+                                    declaringElement = enclosingMethod;
+                                }
+                            }
+                        }
+                    }
+                } catch (JavaModelException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            if ((typeOrMethod instanceof IMethod) 
+                    && declaringElement.getElementName() != null
+                    && declaringElement.getElementName().equals(typeOrMethod.getElementName() + "_")
+                    && isCeylonMethod(declaringElement)) {
+                declaringElement = null;
+            }
+        }
+        
+        if (declaringElement != null) {
+            declaringElement = toCeylonDeclarationElement(declaringElement);
+            Declaration ceylonContainingDeclaration = null;
+            if (! declaration.isToplevel()) {
+                Scope scope = declaration.getContainer();
+                while (!(scope instanceof Package)) {
+                    if (scope instanceof Declaration) {
+                        ceylonContainingDeclaration = (Declaration) scope;
+                        break;
+                    }
+                    if (scope instanceof Specification) {
+                        ceylonContainingDeclaration = ((Specification) scope).getDeclaration();
+                        break;
+                    }
+                    scope = scope.getContainer();
+                }
+            }
+            
+            if (ceylonContainingDeclaration != null) {
+                return elementEqualsDeclaration(declaringElement, 
+                        (Declaration) ceylonContainingDeclaration);
+            }
+        } else {
+            Scope scope = declaration.getScope();
+            if (scope instanceof Package) {
+                String ceylonDeclarationPkgName = scope.getQualifiedNameString();
+                IPackageFragment pkgFragment = (IPackageFragment) typeOrMethod.getAncestor(IJavaElement.PACKAGE_FRAGMENT);
+                String pkgFragmentName = pkgFragment != null ? pkgFragment.getElementName() : null;
+                return ceylonDeclarationPkgName.equals(pkgFragmentName);
+            }
+        }
+        return false;
     }
 
     public static IProject[] getProjectsToSearch(IProject project) {
@@ -253,12 +458,58 @@ public class JavaSearch {
         }
     }
 
+    public static IMember toCeylonDeclarationElement(IMember typeOrMethod) {
+        if (typeOrMethod instanceof IMethod) {
+            IMethod method = (IMethod)typeOrMethod;
+            try {
+                if ("get_".equals(method.getElementName())
+                        && ((method.getFlags() & Flags.AccStatic) > 0)) {
+                    IType parentType = method.getDeclaringType();
+                    if (parentType != null) {
+                        IAnnotation objectAnnotation = parentType.getAnnotation(com.redhat.ceylon.compiler.java.metadata.Object.class.getName());
+                        if (objectAnnotation.exists()) {
+                            return toCeylonDeclarationElement(parentType);
+                        }
+                    }
+                }
+            } catch (JavaModelException e) {
+                e.printStackTrace();
+            }
+            
+        }
+        if (typeOrMethod instanceof IType) {
+            IType type = (IType) typeOrMethod;
+            String fullyQualifiedTypeName = type.getFullyQualifiedName();
+            if (fullyQualifiedTypeName.endsWith("$impl")) {
+                IType interfaceType = null;
+                try {
+                    interfaceType = type.getJavaProject().findType(
+                            fullyQualifiedTypeName.substring(0, fullyQualifiedTypeName.length() - 5),
+                            (IProgressMonitor) null);
+                } catch (JavaModelException e) {
+                    e.printStackTrace();
+                }
+                if (interfaceType != null) {
+                    return toCeylonDeclarationElement(interfaceType);
+                }
+            }
+        }
+        
+        return typeOrMethod;
+    }
+    
     public static Declaration toCeylonDeclaration(IJavaElement javaElement,
             List<? extends PhasedUnit> phasedUnits) {
-        String ceylonQualifiedName = getCeylonQualifiedName((IMember) javaElement);
+        if (! (javaElement instanceof IMember)) {
+            return null;
+        }
+        IMember declarationElement = toCeylonDeclarationElement((IMember)javaElement);
+        if (!isCeylon(declarationElement)) {
+            return null;
+        }
         for (PhasedUnit pu: phasedUnits) {
             for (Declaration declaration: pu.getDeclarations()) {
-                if (declaration.getQualifiedNameString().equals(ceylonQualifiedName)) {
+                if (elementEqualsDeclaration(declarationElement, declaration)) {
                     return declaration;
                 }
             }
