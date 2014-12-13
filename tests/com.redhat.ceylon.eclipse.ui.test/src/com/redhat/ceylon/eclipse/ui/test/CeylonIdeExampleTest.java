4de89e2375ec80766969b9305030312cfe6ddc9b
gavinking <gavin@ceylon-lang.org>
2014-12-13 22:47:55 +0100

    fix test

--- tests/com.redhat.ceylon.eclipse.ui.test/src/com/redhat/ceylon/eclipse/ui/test/CeylonIdeExampleTest.java
+++ tests/com.redhat.ceylon.eclipse.ui.test/src/com/redhat/ceylon/eclipse/ui/test/CeylonIdeExampleTest.java
@@ -50,7 +50,7 @@ public class CeylonIdeExampleTest {
 		IProject project = createJavaProject(name);
 		
 		String systemRepo = CeylonBuilder.getCeylonSystemRepo(project);
-		CeylonNature nature = new CeylonNature(systemRepo, true, false, true, false, true);
+		CeylonNature nature = new CeylonNature(systemRepo, true, false, true, false, true, null);
 		nature.addToProject(project);
 		
 		assertEquals(2,project.getDescription().getNatureIds().length);
