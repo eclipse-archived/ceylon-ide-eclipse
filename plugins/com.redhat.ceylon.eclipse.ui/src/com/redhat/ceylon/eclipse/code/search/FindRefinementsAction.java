471efc470526950fe1cffdc1f4979b617cdeb34e
gavinking <gavin@ceylon-lang.org>
2015-03-16 18:43:37 +0100

    very minor

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/search/FindRefinementsAction.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/search/FindRefinementsAction.java
@@ -29,7 +29,7 @@ public class FindRefinementsAction extends AbstractFindAction {
                 Referenceable referencedDeclaration) {
             //TODO: very ugly!!
             Declaration declaration = (Declaration)
-                    new FindReferencesVisitor((Declaration)referencedDeclaration)
+                    new FindReferencesVisitor(referencedDeclaration)
                             .getDeclaration();
             FindRefinementsVisitor frv = new FindRefinementsVisitor(declaration);
             cu.visit(frv);
