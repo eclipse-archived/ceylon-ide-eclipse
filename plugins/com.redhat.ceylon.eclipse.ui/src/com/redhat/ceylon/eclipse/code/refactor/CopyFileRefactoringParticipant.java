2d7f691cc685b39f52bdcaf69adea8f4a0d854bd
gavinking <gavin@ceylon-lang.org>
2015-03-24 00:09:54 +0100

    fix prob with inferred anon fun param types cleanup

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/refactor/CopyFileRefactoringParticipant.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/refactor/CopyFileRefactoringParticipant.java
@@ -4,6 +4,7 @@ import static com.redhat.ceylon.compiler.typechecker.tree.Util.formatPath;
 import static com.redhat.ceylon.eclipse.code.correct.ImportProposals.findImportNode;
 import static com.redhat.ceylon.eclipse.code.correct.ImportProposals.importEdits;
 import static com.redhat.ceylon.eclipse.core.builder.CeylonBuilder.getProjectTypeChecker;
+import static com.redhat.ceylon.eclipse.util.EditorUtil.getDocument;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,6 +15,7 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.ltk.core.refactoring.Change;
@@ -32,6 +34,7 @@ import com.redhat.ceylon.compiler.typechecker.TypeChecker;
 import com.redhat.ceylon.compiler.typechecker.context.PhasedUnit;
 import com.redhat.ceylon.compiler.typechecker.model.Declaration;
 import com.redhat.ceylon.compiler.typechecker.model.Module;
+import com.redhat.ceylon.compiler.typechecker.model.Unit;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.BaseMemberOrTypeExpression;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.BaseType;
@@ -39,7 +42,6 @@ import com.redhat.ceylon.compiler.typechecker.tree.Tree.ImportMemberOrType;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.ModuleDescriptor;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.PackageDescriptor;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
-import com.redhat.ceylon.eclipse.util.EditorUtil;
 
 public class CopyFileRefactoringParticipant extends CopyParticipant {
 
@@ -61,29 +63,38 @@ public class CopyFileRefactoringParticipant extends CopyParticipant {
 
     @Override
     public RefactoringStatus checkConditions(IProgressMonitor pm,
-            CheckConditionsContext context) throws OperationCanceledException {
+            CheckConditionsContext context) 
+                    throws OperationCanceledException {
         return new RefactoringStatus();
     }
 
-    public Change createChange(IProgressMonitor pm) throws CoreException {
+    public Change createChange(IProgressMonitor pm) 
+            throws CoreException {
         try {
             IFolder dest = (IFolder) 
                     getArguments().getDestination();
-            final String newName = dest.getProjectRelativePath()
-                    .removeFirstSegments(1).toPortableString()
-                    .replace('/', '.');
+            final String newName = 
+                    dest.getProjectRelativePath()
+                        .removeFirstSegments(1)
+                        .toPortableString()
+                        .replace('/', '.');
             IFile newFile = dest.getFile(file.getName());
-            String relFilePath = file.getProjectRelativePath()
-                    .removeFirstSegments(1).toPortableString();
-            String relPath = file.getProjectRelativePath()
-                    .removeFirstSegments(1).removeLastSegments(1)
-                    .toPortableString();
-            final String oldName = relPath.replace('/', '.');
+            IPath path = file.getProjectRelativePath();
+            String relFilePath = 
+                    path.removeFirstSegments(1)
+                        .toPortableString();
+            String relPath = 
+                    path.removeFirstSegments(1)
+                        .removeLastSegments(1)
+                        .toPortableString();
+            final String oldName = 
+                    relPath.replace('/', '.');
             final IProject project = file.getProject();
             
             if (newName.equals(oldName)) return null;
 
-            TypeChecker tc = getProjectTypeChecker(project);
+            TypeChecker tc = 
+                    getProjectTypeChecker(project);
             if (tc==null) return null;
             PhasedUnit phasedUnit = 
                     tc.getPhasedUnitFromRelativePath(relFilePath);
@@ -124,17 +135,25 @@ public class CopyFileRefactoringParticipant extends CopyParticipant {
                     visitIt(that.getImportPath());
                 }
                 private void visitIt(Tree.ImportPath importPath) {
-                    String path = formatPath(importPath.getIdentifiers());
+                    String path = 
+                            formatPath(importPath.getIdentifiers());
                     if (path.equals(oldName)) {
-                        edits.add(new ReplaceEdit(importPath.getStartIndex(), 
-                                oldName.length(), newName));
+                        int start = importPath.getStartIndex();
+                        int len = oldName.length();
+                        edits.add(new ReplaceEdit(start, len, newName));
                     }
                 }
                 private void visitIt(Tree.Identifier id, Declaration dec) {
-                    if (dec!=null && !declarations.contains(dec)) {
-                        String pn = dec.getUnit().getPackage().getNameAsString();
-                        if (pn.equals(oldName) && !pn.isEmpty() && 
-                                !pn.equals(Module.LANGUAGE_MODULE_NAME)) {
+                    if (dec!=null && 
+                            //TODO: superflous
+                            !declarations.contains(dec)) {
+                        Unit unit = dec.getUnit();
+                        String packageName = 
+                                unit.getPackage().getNameAsString();
+                        if (packageName.equals(oldName) && 
+                            !packageName.isEmpty() && 
+                            !packageName.equals(Module.LANGUAGE_MODULE_NAME) &&
+                            !unit.equals(id.getUnit())) {
                             imports.put(dec, id.getText());
                         }
                     }
@@ -143,7 +162,8 @@ public class CopyFileRefactoringParticipant extends CopyParticipant {
 
             try {
                 TextFileChange change = 
-                        new TextFileChange(file.getName(), newFile);
+                        new TextFileChange(file.getName(), 
+                                newFile);
                 Tree.CompilationUnit cu = 
                         phasedUnit.getCompilationUnit();
                 change.setEdit(new MultiTextEdit());
@@ -151,9 +171,11 @@ public class CopyFileRefactoringParticipant extends CopyParticipant {
                     change.addEdit(edit);
                 }
                 if (!imports.isEmpty()) {
-                    List<InsertEdit> list = importEdits(cu, 
-                            imports.keySet(), imports.values(), 
-                            null, EditorUtil.getDocument(change));
+                    List<InsertEdit> list = 
+                            importEdits(cu, 
+                                imports.keySet(), 
+                                imports.values(), 
+                                null, getDocument(change));
                     for (TextEdit edit: list) {
                         change.addEdit(edit);
                     }
@@ -161,8 +183,10 @@ public class CopyFileRefactoringParticipant extends CopyParticipant {
                 Tree.Import toDelete = 
                         findImportNode(cu, newName);
                 if (toDelete!=null) {
-                    change.addEdit(new DeleteEdit(toDelete.getStartIndex(), 
-                            toDelete.getStopIndex()-toDelete.getStartIndex()+1));
+                    int start = toDelete.getStartIndex();
+                    int stop = toDelete.getStopIndex();
+                    change.addEdit(new DeleteEdit(start, 
+                            stop-start+1));
                 }
                 if (change.getEdit().hasChildren()) {
                     return change;
