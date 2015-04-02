8170732125c5e7f7056eb61160444c6606ad2dcc
davidfestal <david.festal@serli.com>
2015-04-02 11:12:11 +0200

    ceylon/ceylon-ide-eclipse#1291 : Show the dependency overrides in the Module Dependencies graph.

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/moduledependencies/DependencyGraphView.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/moduledependencies/DependencyGraphView.java
@@ -54,6 +54,7 @@ import org.eclipse.zest.layouts.algorithms.HorizontalTreeLayoutAlgorithm;
 import org.eclipse.zest.layouts.algorithms.TreeLayoutAlgorithm;
 
 import com.redhat.ceylon.compiler.typechecker.model.Module;
+import com.redhat.ceylon.compiler.typechecker.model.ModuleImport;
 import com.redhat.ceylon.eclipse.code.editor.CeylonEditor;
 import com.redhat.ceylon.eclipse.code.navigator.SourceModuleNode;
 import com.redhat.ceylon.eclipse.core.builder.CeylonBuilder;
@@ -64,6 +65,7 @@ import com.redhat.ceylon.eclipse.core.model.ModuleDependencies;
 import com.redhat.ceylon.eclipse.core.model.ModuleDependencies.Dependency;
 import com.redhat.ceylon.eclipse.core.model.ModuleDependencies.ModuleReference;
 import com.redhat.ceylon.eclipse.core.model.ModuleDependencies.ModuleWeakReference;
+import com.redhat.ceylon.eclipse.ui.CeylonPlugin;
 import com.redhat.ceylon.eclipse.ui.CeylonResources;
 import com.redhat.ceylon.eclipse.util.EditorUtil;
 
@@ -131,21 +133,76 @@ public class DependencyGraphView extends ViewPart implements IShowInTarget, ICey
             return "";
         }
         
+        private String importPrefix(boolean optional, boolean export, boolean initialUpperCase) {
+            if (optional) {
+                return (initialUpperCase ? "O" : "o") + "ptional import";
+            } else if (export) {
+                return (initialUpperCase ? "E" : "e") + "xported import";
+            } else {
+                return (initialUpperCase ? "L" : "l") + "ocal import";
+            }
+        }
+        
         @Override
         public void selfStyleConnection(Object element,
                 GraphConnection connection) {
             connection.setLineWidth(1);
+            
+            String tooltipText = "";
+            boolean overriding = false;
             if (element instanceof Dependency) {
-                if (((Dependency) element).optional) {
-                    connection
-                    .setConnectionStyle(ZestStyles.CONNECTIONS_DOT);
-                    connection.setTooltip(new Label("Optional"));
+                Dependency dep = (Dependency) element;
+                if (dep.optional) {
+                    connection.setConnectionStyle(ZestStyles.CONNECTIONS_DOT);
                 }
-                if (((Dependency) element).exported) {
+                if (dep.exported) {
                     connection.setLineWidth(4);
-                    connection.setTooltip(new Label("Exported"));
+                }
+                tooltipText = importPrefix(dep.optional, dep.exported, true);
+                Object source = dep.getSource();
+                Object target = dep.getTarget();
+                if (source instanceof ModuleWeakReference 
+                        && target instanceof ModuleWeakReference) {
+                    Module sourceMod = ((ModuleWeakReference) source).get();
+                    Module targetMod = ((ModuleWeakReference) target).get();
+                    if (sourceMod != null 
+                            && targetMod != null) {
+                        for (ModuleImport mi : sourceMod.getImports()) {
+                            if (mi.getModule().equals(targetMod)) {
+                                ModuleImport overridenImport = mi.getOverridenModuleImport();
+                                if (overridenImport != null) {
+                                    overriding = true;
+                                    tooltipText += "\nOverrides the import of '" + 
+                                    overridenImport.getModule().getNameAsString() + 
+                                    "', version " + overridenImport.getModule().getVersion();
+                                }
+                                break;
+                            }
+                        }
+                        List<ModuleImport> overridenModuleImports = sourceMod.getOverridenImports();
+                        if (overridenModuleImports != null) {
+                            boolean isChanged = true;
+                            for (ModuleImport mi : overridenModuleImports) {
+                                if (mi.getModule().equals(targetMod) &&
+                                        mi.isExport() == dep.exported &&
+                                        mi.isOptional() == dep.optional) {
+                                    isChanged = false;
+                                    break;
+                                }
+                            }
+                            if (isChanged) {
+                                overriding = true;
+                                tooltipText += "\nModified by the dependency overrides of the importing module";
+                            }
+                        }
+                    }
                 }
             }
+            if (overriding) {
+                connection.setImage(CeylonPlugin.imageRegistry.get(CeylonResources.CEYLON_DELETE_IMPORT));
+            }
+            Label tooltip = new Label(tooltipText);
+            connection.setTooltip(tooltip);
             connection.setConnectionStyle(ZestStyles.CONNECTIONS_DIRECTED);
             Connection figure = connection.getConnectionFigure();
             PolygonDecoration decoration = new PolygonDecoration();
@@ -180,7 +237,7 @@ public class DependencyGraphView extends ViewPart implements IShowInTarget, ICey
                         node.setHighlightColor(node.getDisplay()
                                 .getSystemColor(SWT.COLOR_DARK_GRAY));
                         node.setTooltip(new Label(
-                                "Unavailable module\n(not visible from any project module)"));
+                                "Unresolved module\n(not visible from any project module)"));
                     }
                     else if (module.isJava()) {
                         node.setBackgroundColor(node.getDisplay()
@@ -293,7 +350,6 @@ public class DependencyGraphView extends ViewPart implements IShowInTarget, ICey
         List<String> projectNames = new ArrayList<String>(projectMap.keySet());
 //        projectNames.add("");
         Collections.sort(projectNames);
-        
         int selectedIndex = projectCombo.getSelectionIndex();
         String selectedProjectName = selectedIndex != -1 ? projectCombo.getItem(selectedIndex) : "";
         
