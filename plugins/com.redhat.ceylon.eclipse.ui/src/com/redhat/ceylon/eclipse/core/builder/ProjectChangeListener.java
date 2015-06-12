9ec08ee37b85112deafbac466e74ef5c63873085
davidfestal <david.festal@serli.com>
2015-06-12 22:34:11 +0200

    Revert "add import"
    This reverts commit 960f62a7cc7732a819c6b7f3cb600d1dd9f5c38e.

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/builder/ProjectChangeListener.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/builder/ProjectChangeListener.java
@@ -1,6 +1,5 @@
 package com.redhat.ceylon.eclipse.core.builder;
 
-import static com.redhat.ceylon.eclipse.core.builder.ceylonModelGetter_.ceylonModelGetter;
 import static com.redhat.ceylon.eclipse.core.classpath.CeylonClasspathUtil.getCeylonClasspathContainers;
 import static org.eclipse.core.resources.ResourcesPlugin.getWorkspace;
 
@@ -30,18 +29,18 @@ public class ProjectChangeListener implements IResourceChangeListener {
                     }
                     if (resource instanceof IProject && delta.getKind()==IResourceDelta.REMOVED) {
                         CeylonBuilder.removeProject((IProject) resource);
-                        ceylonModelGetter().removeProject((IProject) resource); 
+                        ceylonModelGetter_.ceylonModelGetter().removeProject((IProject) resource); 
                     }
                     else if (resource instanceof IProject && (delta.getFlags() & IResourceDelta.OPEN) != 0) {
                         final IProject project = (IProject) resource;
                         if (!project.isOpen()) {
                             CeylonBuilder.removeProject(project);
-                            ceylonModelGetter().removeProject((IProject) resource); 
+                            ceylonModelGetter_.ceylonModelGetter().removeProject((IProject) resource); 
                         }
                         else if (CeylonNature.isEnabled(project)) {
                             IJavaProject javaProject = JavaCore.create(project);
                             if (javaProject != null) {
-                                ceylonModelGetter().addProject((IProject) resource); 
+                                ceylonModelGetter_.ceylonModelGetter().removeProject((IProject) resource); 
                                 //List<CeylonApplicationModulesContainer> cpContainers = 
                                 getCeylonClasspathContainers(javaProject);
                                 /*for (CeylonApplicationModulesContainer container : cpContainers) {
