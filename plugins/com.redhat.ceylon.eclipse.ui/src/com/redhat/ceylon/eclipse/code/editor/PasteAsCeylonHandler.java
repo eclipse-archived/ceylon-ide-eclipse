3549e8945df7c4b619a7f384833f80a6b178a1b7
Bastien Jansen <bastien.jansen@gmx.com>
2015-09-07 22:22:37 +0200

    Use settings for the code converter.

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/PasteAsCeylonHandler.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/editor/PasteAsCeylonHandler.java
@@ -2,16 +2,15 @@ package com.redhat.ceylon.eclipse.code.editor;
 
 import static com.redhat.ceylon.eclipse.util.EditorUtil.getCurrentEditor;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.StringWriter;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
@@ -21,12 +20,15 @@ import org.eclipse.swt.dnd.Clipboard;
 import org.eclipse.swt.dnd.TextTransfer;
 import org.eclipse.text.edits.ReplaceEdit;
 
+import com.redhat.ceylon.eclipse.code.refactor.AbstractHandler;
+import com.redhat.ceylon.eclipse.core.model.modelJ2C;
+import com.redhat.ceylon.eclipse.ui.CeylonPlugin;
+import com.redhat.ceylon.eclipse.util.EditorUtil;
+import com.redhat.ceylon.ide.common.model.CeylonIdeConfig;
+
 import ceylon.tool.converter.java2ceylon.Java8Lexer;
 import ceylon.tool.converter.java2ceylon.Java8Parser;
 import ceylon.tool.converter.java2ceylon.JavaToCeylonConverter;
-import com.redhat.ceylon.eclipse.code.refactor.AbstractHandler;
-import com.redhat.ceylon.eclipse.ui.CeylonPlugin;
-import com.redhat.ceylon.eclipse.util.EditorUtil;
 
 public class PasteAsCeylonHandler extends AbstractHandler {
 
@@ -37,7 +39,9 @@ public class PasteAsCeylonHandler extends AbstractHandler {
 		String javaCode = (String) cb.getContents(TextTransfer.getInstance());
 
 		try {
-			ceylonCode = transformJavaToCeylon(javaCode);
+		    IProject project = ((CeylonEditor) getCurrentEditor()).getParseController().getProject();
+		    CeylonIdeConfig<IProject> ideConfig = modelJ2C.ceylonModel().getProject(project).getIdeConfiguration();
+			ceylonCode = transformJavaToCeylon(javaCode, ideConfig);
 		} catch (IOException e) {
 			CeylonPlugin.log(IStatus.ERROR, "Could not transform Java code", e);
 		}
@@ -47,7 +51,7 @@ public class PasteAsCeylonHandler extends AbstractHandler {
 		return null;
 	}
 
-	private String transformJavaToCeylon(String javaCode) throws IOException {
+	private String transformJavaToCeylon(String javaCode, CeylonIdeConfig<IProject> ideConfig) throws IOException {
 		ANTLRInputStream input = new ANTLRInputStream(javaCode);
 		Java8Lexer lexer = new Java8Lexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -55,7 +59,12 @@ public class PasteAsCeylonHandler extends AbstractHandler {
 		ParserRuleContext tree = parser.compilationUnit();
 
 		StringWriter out = new StringWriter();
-		JavaToCeylonConverter converter = new JavaToCeylonConverter(out, true, true);
+		
+		JavaToCeylonConverter converter = new JavaToCeylonConverter(out, 
+		        ideConfig.getConverterConfig().getTransformGetters(),
+		        ideConfig.getConverterConfig().getUseVariableInParameters());
+		
+		ideConfig.save();
 		tree.accept(converter);
 
 		String ceylonCode = out.toString();
