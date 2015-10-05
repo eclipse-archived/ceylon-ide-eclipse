b503017bcb0cf5319a44b59918c149da3d1ca0e7
gavinking <gavin@ceylon-lang.org>
2015-10-05 14:17:23 +0200

    cleanup

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/correct/ConvertToBlockProposal.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/correct/ConvertToBlockProposal.java
@@ -13,94 +13,145 @@ import org.eclipse.text.edits.InsertEdit;
 import org.eclipse.text.edits.MultiTextEdit;
 import org.eclipse.text.edits.ReplaceEdit;
 
-import com.redhat.ceylon.model.typechecker.model.Function;
-import com.redhat.ceylon.model.typechecker.model.Value;
 import com.redhat.ceylon.compiler.typechecker.tree.Node;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
+import com.redhat.ceylon.model.typechecker.model.Function;
+import com.redhat.ceylon.model.typechecker.model.Value;
 
 class ConvertToBlockProposal extends CorrectionProposal {
     
-    ConvertToBlockProposal(String desc, int offset, TextChange change) {
+    private ConvertToBlockProposal(String desc, int offset, 
+            TextChange change) {
         super(desc, change, new Region(offset, 0));
     }
     
     static void addConvertToBlockProposal(IDocument doc,
-            Collection<ICompletionProposal> proposals, IFile file,
-            Node decNode) {
-        TextChange change = new TextFileChange("Convert to Block", file);
+            Collection<ICompletionProposal> proposals, 
+            IFile file, Node decNode) {
+        TextChange change = 
+                new TextFileChange("Convert to Block", 
+                        file);
         change.setEdit(new MultiTextEdit());
         int offset;
-        int len;
+        int length;
         String semi;
         boolean isVoid;
         String addedKeyword = null;
         String desc = "Convert => to block";
         if (decNode instanceof Tree.MethodDeclaration) {
-            Tree.MethodDeclaration md = (Tree.MethodDeclaration) decNode;
+            Tree.MethodDeclaration md = 
+                    (Tree.MethodDeclaration) decNode;
             Function dm = md.getDeclarationModel();
-            if (dm==null || dm.isParameter()) return;
+            if (dm==null || dm.isParameter()) {
+                return;
+            }
             isVoid = dm.isDeclaredVoid();
-            List<Tree.ParameterList> pls = md.getParameterLists();
+            List<Tree.ParameterList> pls = 
+                    md.getParameterLists();
             if (pls.isEmpty()) return;
             offset = pls.get(pls.size()-1).getEndIndex();
-            if (md.getTypeConstraintList()!=null) {
-                offset = md.getTypeConstraintList().getEndIndex();
+            Tree.TypeConstraintList tcl = 
+                    md.getTypeConstraintList();
+            if (tcl!=null) {
+                offset = tcl.getEndIndex();
             }
-            len = md.getSpecifierExpression().getExpression().getStartIndex() - offset;
+            length = 
+                    md.getSpecifierExpression()
+                        .getExpression()
+                        .getStartIndex() 
+                            - offset;
             semi = "";
         }
         else if (decNode instanceof Tree.AttributeDeclaration) {
-            Tree.AttributeDeclaration ad = (Tree.AttributeDeclaration) decNode;
+            Tree.AttributeDeclaration ad = 
+                    (Tree.AttributeDeclaration) decNode;
             Value dm = ad.getDeclarationModel();
-            if (dm==null || dm.isParameter()) return;
+            if (dm==null || dm.isParameter()) {
+                return;
+            }
             isVoid = false;
             offset = ad.getIdentifier().getEndIndex();
-            len = ad.getSpecifierOrInitializerExpression().getExpression().getStartIndex() - offset;
+            length = 
+                    ad.getSpecifierOrInitializerExpression()
+                        .getExpression()
+                        .getStartIndex() 
+                            - offset;
             semi = "";
         }
         else if (decNode instanceof Tree.AttributeSetterDefinition) {
-            Tree.AttributeSetterDefinition asd = (Tree.AttributeSetterDefinition) decNode;
+            Tree.AttributeSetterDefinition asd = 
+                    (Tree.AttributeSetterDefinition) decNode;
             isVoid = true;
             offset = asd.getIdentifier().getEndIndex();
-            len = asd.getSpecifierExpression().getExpression().getStartIndex() - offset;
+            length = 
+                    asd.getSpecifierExpression()
+                        .getExpression()
+                        .getStartIndex() 
+                            - offset;
             semi = "";
         }
         else if (decNode instanceof Tree.MethodArgument) {
-            Tree.MethodArgument ma = (Tree.MethodArgument) decNode;
+            Tree.MethodArgument ma = 
+                    (Tree.MethodArgument) decNode;
             Function dm = ma.getDeclarationModel();
-            if (dm==null) return;
+            if (dm==null) {
+                return;
+            }
             isVoid = dm.isDeclaredVoid();
             if (ma.getType().getToken()==null) {
                 addedKeyword = "function ";
             }
-            List<Tree.ParameterList> pls = ma.getParameterLists();
-            if (pls.isEmpty()) return;
+            List<Tree.ParameterList> pls = 
+                    ma.getParameterLists();
+            if (pls.isEmpty()) {
+                return;
+            }
             offset = pls.get(pls.size()-1).getEndIndex();
-            len = ma.getSpecifierExpression().getExpression().getStartIndex() - offset;
+            length = 
+                    ma.getSpecifierExpression()
+                        .getExpression()
+                        .getStartIndex() 
+                            - offset;
             semi = "";
         }
         else if (decNode instanceof Tree.AttributeArgument) {
-            Tree.AttributeArgument aa = (Tree.AttributeArgument) decNode;
+            Tree.AttributeArgument aa = 
+                    (Tree.AttributeArgument) decNode;
             isVoid = false;            
             if (aa.getType().getToken()==null) {
                 addedKeyword = "value ";
             }
             offset = aa.getIdentifier().getEndIndex();
-            len = aa.getSpecifierExpression().getExpression().getStartIndex() - offset;
+            length = 
+                    aa.getSpecifierExpression()
+                        .getExpression()
+                        .getStartIndex() 
+                            - offset;
             semi = "";
         }
         else if (decNode instanceof Tree.FunctionArgument) {
-            Tree.FunctionArgument fun = (Tree.FunctionArgument) decNode;
+            Tree.FunctionArgument fun = 
+                    (Tree.FunctionArgument) decNode;
             Function dm = fun.getDeclarationModel();
-            if (dm==null) return;
-            isVoid = dm.isDeclaredVoid();
-            List<Tree.ParameterList> pls = fun.getParameterLists();
-            if (pls.isEmpty()) return;
-            offset = pls.get(pls.size()-1).getEndIndex();
-            if (fun.getTypeConstraintList()!=null) {
-                offset = fun.getTypeConstraintList().getEndIndex();
+            if (dm==null) {
+                return;
             }
-            len = fun.getExpression().getStartIndex() - offset;
+            isVoid = dm.isDeclaredVoid();
+            List<Tree.ParameterList> pls = 
+                    fun.getParameterLists();
+            if (pls.isEmpty()) {
+                return;
+            }
+            offset = pls.get(pls.size()-1).getEndIndex();
+            Tree.TypeConstraintList tcl = 
+                    fun.getTypeConstraintList();
+            if (tcl!=null) {
+                offset = tcl.getEndIndex();
+            }
+            length = 
+                    fun.getExpression()
+                        .getStartIndex() 
+                            - offset;
             semi = ";";
             desc = "Convert anonymous function => to block";
         }
@@ -108,9 +159,11 @@ class ConvertToBlockProposal extends CorrectionProposal {
             return;
         }
         if (addedKeyword!=null) {
-            change.addEdit(new InsertEdit(decNode.getStartIndex(), addedKeyword));
+            int loc = decNode.getStartIndex();
+            change.addEdit(new InsertEdit(loc, addedKeyword));
         }
-        change.addEdit(new ReplaceEdit(offset, len, " {" + (isVoid?"":" return") + " "));
+        String text = " {" + (isVoid?"":" return") + " ";
+        change.addEdit(new ReplaceEdit(offset, length, text));
         change.addEdit(new InsertEdit(decNode.getEndIndex(), semi + " }"));
         proposals.add(new ConvertToBlockProposal(desc, offset + 3, change));
     }
