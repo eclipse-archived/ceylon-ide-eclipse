6edea8debcaf72a344038b6d07d1b8468c0f944a
gavinking <gavin@ceylon-lang.org>
2015-10-26 00:53:08 +0100

    cleanup

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/correct/CreateEnumProposal.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/correct/CreateEnumProposal.java
@@ -2,6 +2,8 @@ package com.redhat.ceylon.eclipse.code.correct;
 
 import static com.redhat.ceylon.eclipse.ui.CeylonResources.ATTRIBUTE;
 import static com.redhat.ceylon.eclipse.ui.CeylonResources.INTERFACE;
+import static com.redhat.ceylon.eclipse.util.EditorUtil.getDocument;
+import static com.redhat.ceylon.eclipse.util.Indents.getDefaultLineDelimiter;
 import static com.redhat.ceylon.eclipse.util.Indents.getIndent;
 import static com.redhat.ceylon.eclipse.util.Nodes.findDeclaration;
 
@@ -22,8 +24,6 @@ import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.redhat.ceylon.eclipse.core.model.ModifiableSourceFile;
 import com.redhat.ceylon.eclipse.core.typechecker.ModifiablePhasedUnit;
 import com.redhat.ceylon.eclipse.ui.CeylonResources;
-import com.redhat.ceylon.eclipse.util.EditorUtil;
-import com.redhat.ceylon.eclipse.util.Indents;
 import com.redhat.ceylon.eclipse.util.Nodes;
 
 class CreateEnumProposal extends CorrectionProposal {
@@ -39,181 +39,200 @@ class CreateEnumProposal extends CorrectionProposal {
             ProblemLocation problem, 
             Collection<ICompletionProposal> proposals, 
             IProject project) {
-            Node idn = Nodes.getIdentifyingNode(node);
-            if (idn==null) return;
-            String brokenName = idn.getText();
-            if (brokenName.isEmpty()) return;
-            Tree.Declaration dec = findDeclaration(rootNode, node);
-            if (dec instanceof Tree.ClassDefinition) {
-                Tree.ClassDefinition cd = (Tree.ClassDefinition) dec;
-                if (cd.getCaseTypes()!=null) {
-                    if (cd.getCaseTypes().getTypes().contains(node)) {
-                        addCreateEnumProposal(proposals, project, 
-                                "class " + brokenName + parameters(cd.getTypeParameterList()) +
-                                    parameters(cd.getParameterList()) +
-                                    " extends " + cd.getDeclarationModel().getName() + 
-                                    parameters(cd.getTypeParameterList()) + 
-                                    arguments(cd.getParameterList()) + " {}", 
-                                "class '"+ brokenName + parameters(cd.getTypeParameterList()) +
-                                parameters(cd.getParameterList()) + "'", 
-                                CeylonResources.CLASS, rootNode, cd);
-                    }
-                    if (cd.getCaseTypes().getBaseMemberExpressions().contains(node)) {
-                        addCreateEnumProposal(proposals, project, 
-                                "object " + brokenName + 
-                                    " extends " + cd.getDeclarationModel().getName() + 
-                                    parameters(cd.getTypeParameterList()) + 
-                                    arguments(cd.getParameterList()) + " {}", 
-                                "object '"+ brokenName + "'", 
-                                ATTRIBUTE, rootNode, cd);
-                    }
+        Node idn = Nodes.getIdentifyingNode(node);
+        if (idn==null) return;
+        String brokenName = idn.getText();
+        if (brokenName.isEmpty()) return;
+        Tree.Declaration dec = findDeclaration(rootNode, node);
+        if (dec instanceof Tree.ClassDefinition) {
+            Tree.ClassDefinition cd = (Tree.ClassDefinition) dec;
+            Tree.CaseTypes caseTypes = cd.getCaseTypes();
+            if (caseTypes!=null) {
+                Tree.TypeParameterList tpl = cd.getTypeParameterList();
+                if (caseTypes.getTypes().contains(node)) {
+                    addCreateEnumProposal(proposals, project, 
+                            "class " + brokenName + parameters(tpl) +
+                                parameters(cd.getParameterList()) +
+                                " extends " + cd.getDeclarationModel().getName() + 
+                                parameters(tpl) + 
+                                arguments(cd.getParameterList()) + " {}", 
+                            "class '"+ brokenName + parameters(tpl) +
+                            parameters(cd.getParameterList()) + "'", 
+                            CeylonResources.CLASS, rootNode, cd);
                 }
-            }
-            if (dec instanceof Tree.InterfaceDefinition) {
-                Tree.InterfaceDefinition cd = (Tree.InterfaceDefinition) dec;
-                if (cd.getCaseTypes()!=null) {
-                    if (cd.getCaseTypes().getTypes().contains(node)) {
-                        addCreateEnumProposal(proposals, project, 
-                                "interface " + brokenName + parameters(cd.getTypeParameterList()) +
-                                    " satisfies " + cd.getDeclarationModel().getName() + 
-                                    parameters(cd.getTypeParameterList()) + " {}", 
-                                "interface '"+ brokenName + parameters(cd.getTypeParameterList()) +  "'", 
-                                INTERFACE, rootNode, cd);
-                    }
-                    if (cd.getCaseTypes().getBaseMemberExpressions().contains(node)) {
-                        addCreateEnumProposal(proposals, project, 
-                                "object " + brokenName + 
-                                    " satisfies " + cd.getDeclarationModel().getName() + 
-                                    parameters(cd.getTypeParameterList()) + " {}", 
-                                "object '"+ brokenName + "'", 
-                                ATTRIBUTE, rootNode, cd);
-                    }
+                if (caseTypes.getBaseMemberExpressions().contains(node)) {
+                    addCreateEnumProposal(proposals, project, 
+                            "object " + brokenName + 
+                                " extends " + cd.getDeclarationModel().getName() + 
+                                parameters(tpl) + 
+                                arguments(cd.getParameterList()) + " {}", 
+                            "object '"+ brokenName + "'", 
+                            ATTRIBUTE, rootNode, cd);
                 }
             }
         }
-        
-    private static void addCreateEnumProposal(Collection<ICompletionProposal> proposals, 
-            String def, String desc, Image image, ModifiablePhasedUnit unit, 
+        if (dec instanceof Tree.InterfaceDefinition) {
+            Tree.InterfaceDefinition cd = (Tree.InterfaceDefinition) dec;
+            Tree.CaseTypes caseTypes = cd.getCaseTypes();
+            if (caseTypes!=null) {
+                Tree.TypeParameterList tpl = cd.getTypeParameterList();
+                if (caseTypes.getTypes().contains(node)) {
+                    addCreateEnumProposal(proposals, project, 
+                            "interface " + brokenName + parameters(tpl) +
+                                " satisfies " + cd.getDeclarationModel().getName() + 
+                                parameters(tpl) + " {}", 
+                            "interface '"+ brokenName + parameters(tpl) +  "'", 
+                            INTERFACE, rootNode, cd);
+                }
+                if (caseTypes.getBaseMemberExpressions().contains(node)) {
+                    addCreateEnumProposal(proposals, project, 
+                            "object " + brokenName + 
+                                " satisfies " + cd.getDeclarationModel().getName() + 
+                                parameters(tpl) + " {}", 
+                            "object '"+ brokenName + "'", 
+                            ATTRIBUTE, rootNode, cd);
+                }
+            }
+        }
+    }
+    
+    private static void addCreateEnumProposal(
+            Collection<ICompletionProposal> proposals, 
+            String def, String desc, Image image, 
+            ModifiablePhasedUnit unit, 
             Tree.Statement statement) {
         IFile file = unit.getResourceFile();
-        if (file == null) {
-            return;
+        if (file != null) {
+            TextFileChange change = 
+                    new TextFileChange("Create Enumerated", 
+                            file);
+            IDocument doc = getDocument(change);
+            String indent = getIndent(statement, doc);
+            String s = 
+                    indent + def + 
+                    getDefaultLineDelimiter(doc);
+            int offset = statement.getEndIndex()+1;
+            if (offset>doc.getLength()) {
+                offset = doc.getLength();
+                s = getDefaultLineDelimiter(doc) + s;
+            }
+            change.setEdit(new InsertEdit(offset, s));
+            proposals.add(new CreateEnumProposal(def, 
+                    "Create enumerated " + desc, 
+                    image, offset + def.indexOf("{}")+1, 
+                    change));
         }
-        TextFileChange change = new TextFileChange("Create Enumerated", file);
-        IDocument doc = EditorUtil.getDocument(change);
-        String indent = getIndent(statement, doc);
-        String s = indent + def + Indents.getDefaultLineDelimiter(doc);
-        int offset = statement.getEndIndex()+1;
-        if (offset>doc.getLength()) {
-            offset = doc.getLength();
-            s = Indents.getDefaultLineDelimiter(doc) + s;
-        }
-        change.setEdit(new InsertEdit(offset, s));
-        proposals.add(new CreateEnumProposal(def, 
-                "Create enumerated " + desc, 
-                image, offset + def.indexOf("{}")+1, change));
     }
 
-        private static void addCreateEnumProposal(
-                Collection<ICompletionProposal> proposals,
-                IProject project, String def, String desc, Image image, 
-                Tree.CompilationUnit rootNode, Tree.TypeDeclaration cd) {
-                TypecheckerUnit u = rootNode.getUnit();
-            if (u instanceof ModifiableSourceFile) {
-                ModifiableSourceFile cu = (ModifiableSourceFile) u;
-                    addCreateEnumProposal(proposals, def, desc, image, 
-                            cu.getPhasedUnit(), cd);
-            }
+    private static void addCreateEnumProposal(
+            Collection<ICompletionProposal> proposals,
+            IProject project, String def, String desc, Image image, 
+            Tree.CompilationUnit rootNode, Tree.TypeDeclaration cd) {
+            TypecheckerUnit u = rootNode.getUnit();
+        if (u instanceof ModifiableSourceFile) {
+            ModifiableSourceFile cu = (ModifiableSourceFile) u;
+                addCreateEnumProposal(proposals, def, desc, image, 
+                        cu.getPhasedUnit(), cd);
         }
+    }
 
-        private static String parameters(Tree.ParameterList pl) {
-            StringBuilder result = new StringBuilder();
-            if (pl==null ||
-                    pl.getParameters().isEmpty()) {
-                result.append("()");
-            }
-            else {
-                result.append("(");
-                int len = pl.getParameters().size(), i=0;
-                for (Tree.Parameter p: pl.getParameters()) {
-                    if (p!=null) {
-                        if (p instanceof Tree.ParameterDeclaration) {
-                            Tree.TypedDeclaration td = 
-                                    ((Tree.ParameterDeclaration) p).getTypedDeclaration();
-                            result.append(td.getType().getTypeModel().asString()) 
-                                    .append(" ")
-                                    .append(td.getIdentifier().getText());
-                        }
-                        else if (p instanceof Tree.InitializerParameter) {
-                            result.append(p.getParameterModel().getType().asString()) 
+    private static String parameters(Tree.ParameterList pl) {
+        StringBuilder result = new StringBuilder();
+        if (pl==null ||
+                pl.getParameters().isEmpty()) {
+            result.append("()");
+        }
+        else {
+            result.append("(");
+            int len = pl.getParameters().size(), i=0;
+            for (Tree.Parameter p: pl.getParameters()) {
+                if (p!=null) {
+                    if (p instanceof Tree.ParameterDeclaration) {
+                        Tree.ParameterDeclaration pd = 
+                                (Tree.ParameterDeclaration) p;
+                        Tree.TypedDeclaration td = pd.getTypedDeclaration();
+                        result.append(td.getType().getTypeModel().asString()) 
                                 .append(" ")
-                                .append(((Tree.InitializerParameter) p).getIdentifier().getText());
-                        }
-                        //TODO: easy to add back in:
-                        /*if (p instanceof Tree.FunctionalParameterDeclaration) {
-                            Tree.FunctionalParameterDeclaration fp = (Tree.FunctionalParameterDeclaration) p;
-                            for (Tree.ParameterList ipl: fp.getParameterLists()) {
-                                parameters(ipl, label);
-                            }
-                        }*/
+                                .append(td.getIdentifier().getText());
                     }
-                    if (++i<len) result.append(", ");
-                }
-                result.append(")");
-            }
-            return result.toString();
-        }
-        
-        private static String parameters(Tree.TypeParameterList tpl) {
-            StringBuilder result = new StringBuilder();
-            if (tpl!=null &&
-                    !tpl.getTypeParameterDeclarations().isEmpty()) {
-                result.append("<");
-                int len = tpl.getTypeParameterDeclarations().size(), i=0;
-                for (Tree.TypeParameterDeclaration p: tpl.getTypeParameterDeclarations()) {
-                    result.append(p.getIdentifier().getText());
-                    if (++i<len) result.append(", ");
-                }
-                result.append(">");
-            }
-            return result.toString();
-        }
-        
-        private static String arguments(Tree.ParameterList pl) {
-            StringBuilder result = new StringBuilder();
-            if (pl==null ||
-                    pl.getParameters().isEmpty()) {
-                result.append("()");
-            }
-            else {
-                result.append("(");
-                int len = pl.getParameters().size(), i=0;
-                for (Tree.Parameter p: pl.getParameters()) {
-                    if (p!=null) {
-                        Tree.Identifier id;
-                        if (p instanceof Tree.InitializerParameter) {
-                            id = ((Tree.InitializerParameter) p).getIdentifier();
-                        }
-                        else if (p instanceof Tree.ParameterDeclaration) {
-                            id = ((Tree.ParameterDeclaration) p).getTypedDeclaration().getIdentifier();
-                        }
-                        else {
-                            continue;
-                        }
-                        result.append(id.getText());
-                        //TODO: easy to add back in:
-                        /*if (p instanceof Tree.FunctionalParameterDeclaration) {
-                            Tree.FunctionalParameterDeclaration fp = (Tree.FunctionalParameterDeclaration) p;
-                            for (Tree.ParameterList ipl: fp.getParameterLists()) {
-                                parameters(ipl, label);
-                            }
-                        }*/
+                    else if (p instanceof Tree.InitializerParameter) {
+                        Tree.InitializerParameter ip = 
+                                (Tree.InitializerParameter) p;
+                        result.append(p.getParameterModel().getType().asString()) 
+                            .append(" ")
+                            .append(ip.getIdentifier().getText());
                     }
-                    if (++i<len) result.append(", ");
+                    //TODO: easy to add back in:
+                    /*if (p instanceof Tree.FunctionalParameterDeclaration) {
+                        Tree.FunctionalParameterDeclaration fp = (Tree.FunctionalParameterDeclaration) p;
+                        for (Tree.ParameterList ipl: fp.getParameterLists()) {
+                            parameters(ipl, label);
+                        }
+                    }*/
                 }
-                result.append(")");
+                if (++i<len) result.append(", ");
             }
-            return result.toString();
+            result.append(")");
         }
-        
+        return result.toString();
+    }
+    
+    private static String parameters(Tree.TypeParameterList tpl) {
+        StringBuilder result = new StringBuilder();
+        if (tpl!=null &&
+                !tpl.getTypeParameterDeclarations().isEmpty()) {
+            result.append("<");
+            int len = tpl.getTypeParameterDeclarations().size();
+            int i=0;
+            for (Tree.TypeParameterDeclaration p: 
+                    tpl.getTypeParameterDeclarations()) {
+                result.append(p.getIdentifier().getText());
+                if (++i<len) result.append(", ");
+            }
+            result.append(">");
+        }
+        return result.toString();
+    }
+    
+    private static String arguments(Tree.ParameterList pl) {
+        StringBuilder result = new StringBuilder();
+        if (pl==null ||
+                pl.getParameters().isEmpty()) {
+            result.append("()");
+        }
+        else {
+            result.append("(");
+            int len = pl.getParameters().size(), i=0;
+            for (Tree.Parameter p: pl.getParameters()) {
+                if (p!=null) {
+                    Tree.Identifier id;
+                    if (p instanceof Tree.InitializerParameter) {
+                        Tree.InitializerParameter ip = 
+                                (Tree.InitializerParameter) p;
+                        id = ip.getIdentifier();
+                    }
+                    else if (p instanceof Tree.ParameterDeclaration) {
+                        Tree.ParameterDeclaration pd = 
+                                (Tree.ParameterDeclaration) p;
+                        id = pd.getTypedDeclaration().getIdentifier();
+                    }
+                    else {
+                        continue;
+                    }
+                    result.append(id.getText());
+                    //TODO: easy to add back in:
+                    /*if (p instanceof Tree.FunctionalParameterDeclaration) {
+                        Tree.FunctionalParameterDeclaration fp = (Tree.FunctionalParameterDeclaration) p;
+                        for (Tree.ParameterList ipl: fp.getParameterLists()) {
+                            parameters(ipl, label);
+                        }
+                    }*/
+                }
+                if (++i<len) result.append(", ");
+            }
+            result.append(")");
+        }
+        return result.toString();
+    }
+    
 }
