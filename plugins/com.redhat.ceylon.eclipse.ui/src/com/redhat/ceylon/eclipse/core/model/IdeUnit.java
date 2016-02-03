34081262f652c0a31681ebb4cc4f0d1b3bf4d5f4
davidfestal <david.festal@serli.com>
2016-02-03 18:14:53 +0100

    allow `java.lang.Iterable` in for, and `java.lang.AutoCloseable` in try (ceylon/ceylon#4389) : fix on the `1.2.1-preparation` branch

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/model/IdeUnit.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/model/IdeUnit.java
@@ -1,13 +1,29 @@
 package com.redhat.ceylon.eclipse.core.model;
 
 import com.redhat.ceylon.compiler.typechecker.context.TypecheckerUnit;
+import com.redhat.ceylon.model.typechecker.model.Module;
+import com.redhat.ceylon.model.typechecker.model.Package;
 
 public abstract class IdeUnit extends TypecheckerUnit implements IUnit {
     public JDTModule getModule() {
-        return (JDTModule) getPackage().getModule();
+        Package p = getPackage();
+        return p != null ? (JDTModule) p.getModule() : null;
     }
 
     abstract public String getSourceFileName();
     abstract public String getSourceRelativePath();
     abstract public String getSourceFullPath();
+    @Override
+    public Package getJavaLangPackage() {
+        JDTModule currentModule = getModule();
+        if (currentModule != null) {
+            for (Module m : currentModule.getModuleSourceMapper()
+                    .getContext().getModules().getListOfModules()) {
+                if ("java.base".equals(m.getNameAsString())) {
+                    return m.getPackage("java.lang");
+                }
+            }
+        }
+        return super.getJavaLangPackage();
+    }
 }
