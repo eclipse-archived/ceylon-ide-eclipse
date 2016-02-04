a0d3ab30c2cbf42bc6d4d81ffb0d894ec7b6418d
davidfestal <david.festal@serli.com>
2016-02-04 17:13:33 +0100

    Fix #1613 (Enable Ceylon Builder does not sync source paths)
    Conflicts:
    	plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/builder/EnableCeylonNature.java

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/builder/EnableCeylonNature.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/core/builder/EnableCeylonNature.java
@@ -1,7 +1,17 @@
 package com.redhat.ceylon.eclipse.core.builder;
 
+import static com.redhat.ceylon.ide.common.util.toCeylonStringIterable_.toCeylonStringIterable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -9,6 +19,8 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
 import com.redhat.ceylon.eclipse.core.model.modelJ2C;
+import com.redhat.ceylon.ide.common.model.CeylonProject;
+import com.redhat.ceylon.ide.common.model.CeylonProjectConfig;
 
 
 public class EnableCeylonNature implements IWorkbenchWindowActionDelegate {
@@ -21,6 +33,25 @@ public class EnableCeylonNature implements IWorkbenchWindowActionDelegate {
     
     public void run(IAction action) {
         modelJ2C.ceylonModel().addProject(fProject);
+        CeylonProject<IProject> ceylonProject = modelJ2C.ceylonModel().getProject(fProject);
+        CeylonProjectConfig<IProject> config = ceylonProject.getConfiguration();
+        IJavaProject javaProject = JavaCore.create(fProject);
+        List<String> sourceFolders = new ArrayList<>();
+        IWorkspaceRoot workspaceRoot = fProject.getWorkspace().getRoot();
+        try {
+            for (IClasspathEntry root : javaProject.getRawClasspath()) {
+                if (CeylonBuilder.isCeylonSourceEntry(root)) {
+                    IFolder folder = workspaceRoot.getFolder(root.getPath());
+                    if (folder.isLinked()) {
+                        sourceFolders.add(folder.getLocation().toOSString());
+                    } else {
+                        sourceFolders.add(folder.getProjectRelativePath().toString());
+                    }
+                }
+            }
+        } catch (JavaModelException e) {}
+        config.setProjectSourceDirectories(toCeylonStringIterable(sourceFolders));
+        config.save();
         new CeylonNature().addToProject(fProject);
     }
     
