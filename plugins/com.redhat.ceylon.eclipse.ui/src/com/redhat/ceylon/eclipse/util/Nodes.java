5cabfb8c181167e3ee00bcc0e07c817070f141d9
Bastien Jansen <bastien.jansen@gmx.com>
2016-02-05 10:38:34 +0100

    Delegate from Nodes.java to nodes.ceylon

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/util/Nodes.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/util/Nodes.java
@@ -3,17 +3,12 @@ package com.redhat.ceylon.eclipse.util;
 import static com.redhat.ceylon.compiler.typechecker.parser.CeylonLexer.ASTRING_LITERAL;
 import static com.redhat.ceylon.compiler.typechecker.parser.CeylonLexer.AVERBATIM_STRING;
 import static com.redhat.ceylon.compiler.typechecker.parser.CeylonLexer.EOF;
-import static com.redhat.ceylon.compiler.typechecker.parser.CeylonLexer.LIDENTIFIER;
 import static com.redhat.ceylon.compiler.typechecker.parser.CeylonLexer.LINE_COMMENT;
 import static com.redhat.ceylon.compiler.typechecker.parser.CeylonLexer.MULTI_COMMENT;
 import static com.redhat.ceylon.compiler.typechecker.parser.CeylonLexer.STRING_END;
 import static com.redhat.ceylon.compiler.typechecker.parser.CeylonLexer.STRING_LITERAL;
-import static com.redhat.ceylon.compiler.typechecker.parser.CeylonLexer.UIDENTIFIER;
 import static com.redhat.ceylon.compiler.typechecker.parser.CeylonLexer.VERBATIM_STRING;
-import static com.redhat.ceylon.ide.common.util.toCeylonString_.toCeylonString;
-import static com.redhat.ceylon.ide.common.util.toJavaString_.toJavaString;
 
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -21,22 +16,15 @@ import java.util.Set;
 import org.antlr.runtime.CommonToken;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextSelection;
-import org.eclipse.jface.text.Region;
 
 import com.redhat.ceylon.compiler.typechecker.tree.Node;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.Statement;
-import com.redhat.ceylon.ide.common.model.BaseIdeModule;
-import com.redhat.ceylon.ide.common.model.CeylonBinaryUnit;
-import com.redhat.ceylon.ide.common.model.CeylonUnit;
-import com.redhat.ceylon.ide.common.model.ExternalSourceFile;
-import com.redhat.ceylon.ide.common.typechecker.ExternalPhasedUnit;
+import com.redhat.ceylon.ide.common.refactoring.DefaultRegion;
 import com.redhat.ceylon.ide.common.util.NodePrinter;
 import com.redhat.ceylon.ide.common.util.OccurrenceLocation;
 import com.redhat.ceylon.ide.common.util.nodes_;
 import com.redhat.ceylon.model.typechecker.model.Declaration;
-import com.redhat.ceylon.model.typechecker.model.FunctionOrValue;
-import com.redhat.ceylon.model.typechecker.model.ModelUtil;
 import com.redhat.ceylon.model.typechecker.model.Referenceable;
 import com.redhat.ceylon.model.typechecker.model.Unit;
 
@@ -133,30 +121,32 @@ public class Nodes {
 
     public static Iterator<CommonToken> getTokenIterator(List<CommonToken> tokens, 
             IRegion region) {
-        int regionOffset = region.getOffset();
-        int regionLength = region.getLength();
-        if (regionLength<=0) {
-            return Collections.<CommonToken>emptyList().iterator();
-        }
-        int regionEnd = regionOffset + regionLength - 1;
-        if (tokens==null) {
-            return null;
-        }
-        else {
-            int firstTokIdx = 
-                    getTokenIndexAtCharacter(tokens, regionOffset);
-            // getTokenIndexAtCharacter() answers the negative of the index of the
-            // preceding token if the given offset is not actually within a token.
-            if (firstTokIdx < 0) {
-                firstTokIdx= -firstTokIdx + 1;
-            }
-            int lastTokIdx = 
-                    getTokenIndexAtCharacter(tokens, regionEnd);
-            if (lastTokIdx < 0) {
-                lastTokIdx= -lastTokIdx;
-            }
-            return tokens.subList(firstTokIdx, lastTokIdx+1).iterator();
-        }
+//        int regionOffset = region.getOffset();
+//        int regionLength = region.getLength();
+//        if (regionLength<=0) {
+//            return Collections.<CommonToken>emptyList().iterator();
+//        }
+//        int regionEnd = regionOffset + regionLength - 1;
+//        if (tokens==null) {
+//            return null;
+//        }
+//        else {
+//            int firstTokIdx = 
+//                    getTokenIndexAtCharacter(tokens, regionOffset);
+//            // getTokenIndexAtCharacter() answers the negative of the index of the
+//            // preceding token if the given offset is not actually within a token.
+//            if (firstTokIdx < 0) {
+//                firstTokIdx= -firstTokIdx + 1;
+//            }
+//            int lastTokIdx = 
+//                    getTokenIndexAtCharacter(tokens, regionEnd);
+//            if (lastTokIdx < 0) {
+//                lastTokIdx= -lastTokIdx;
+//            }
+//            return tokens.subList(firstTokIdx, lastTokIdx+1).iterator();
+//        }
+        return delegate.getTokenIterator(tokens, 
+                new DefaultRegion(region.getOffset(), region.getLength()));
     }
 
     //
@@ -167,21 +157,22 @@ public class Nodes {
     //
     public static int getTokenIndexAtCharacter(List<CommonToken> tokens, int offset) {
         //search using bisection
-        int low = 0,
-                high = tokens.size();
-        while (high > low)
-        {
-            int mid = (high + low) / 2;
-            CommonToken midElement = (CommonToken) tokens.get(mid);
-            if (offset >= midElement.getStartIndex() &&
-                    offset <= midElement.getStopIndex())
-                return mid;
-            else if (offset < midElement.getStartIndex())
-                high = mid;
-            else low = mid + 1;
-        }
-        
-        return -(low - 1);
+//        int low = 0,
+//                high = tokens.size();
+//        while (high > low)
+//        {
+//            int mid = (high + low) / 2;
+//            CommonToken midElement = (CommonToken) tokens.get(mid);
+//            if (offset >= midElement.getStartIndex() &&
+//                    offset <= midElement.getStopIndex())
+//                return mid;
+//            else if (offset < midElement.getStartIndex())
+//                high = mid;
+//            else low = mid + 1;
+//        }
+//        
+//        return -(low - 1);
+        return (int) delegate.getTokenIndexAtCharacter(tokens, offset);
     }
     
     /** 
@@ -197,46 +188,48 @@ public class Nodes {
      * in all relevant compilation units.
      */
     public static Node getReferencedNode(Referenceable model) {
-        if (model==null) {
-            return null;
-        }
-        else {
-            Unit unit = model.getUnit();
-            if (unit instanceof CeylonUnit) {
-                CeylonUnit ceylonUnit = (CeylonUnit) unit;
-                return getReferencedNodeInUnit(model, 
-                        ceylonUnit.getCompilationUnit());
-            }
-            else {
-                return null;
-            }
-        }
+//        if (model==null) {
+//            return null;
+//        }
+//        else {
+//            Unit unit = model.getUnit();
+//            if (unit instanceof CeylonUnit) {
+//                CeylonUnit ceylonUnit = (CeylonUnit) unit;
+//                return getReferencedNodeInUnit(model, 
+//                        ceylonUnit.getCompilationUnit());
+//            }
+//            else {
+//                return null;
+//            }
+//        }
+        return delegate.getReferencedNode(model);
     }
 
     public static Referenceable getReferencedModel(Node node) {
-        if (node instanceof Tree.ImportPath) {
-            Tree.ImportPath importPath = (Tree.ImportPath) node;
-            return importPath.getModel();
-        }
-        else if (node instanceof Tree.DocLink) {
-            Tree.DocLink docLink = (Tree.DocLink) node;
-            if (docLink.getBase()==null) {
-                if (docLink.getModule()!=null) {
-                    return docLink.getModule(); 
-                }
-                if (docLink.getPkg()!=null) {
-                    return docLink.getPkg();
-                }
-            }
-        }
-        Referenceable dec = getReferencedDeclaration(node);
-        if (dec instanceof FunctionOrValue) {
-            FunctionOrValue mv = (FunctionOrValue) dec;
-            if (mv.isShortcutRefinement()) {
-                dec = mv.getRefinedDeclaration();
-            }
-        }
-        return dec;
+//        if (node instanceof Tree.ImportPath) {
+//            Tree.ImportPath importPath = (Tree.ImportPath) node;
+//            return importPath.getModel();
+//        }
+//        else if (node instanceof Tree.DocLink) {
+//            Tree.DocLink docLink = (Tree.DocLink) node;
+//            if (docLink.getBase()==null) {
+//                if (docLink.getModule()!=null) {
+//                    return docLink.getModule(); 
+//                }
+//                if (docLink.getPkg()!=null) {
+//                    return docLink.getPkg();
+//                }
+//            }
+//        }
+//        Referenceable dec = getReferencedDeclaration(node);
+//        if (dec instanceof FunctionOrValue) {
+//            FunctionOrValue mv = (FunctionOrValue) dec;
+//            if (mv.isShortcutRefinement()) {
+//                dec = mv.getRefinedDeclaration();
+//            }
+//        }
+//        return dec;
+        return delegate.getReferencedModel(node);
     }
 
     public static Referenceable getReferencedExplicitDeclaration(Node node, 
@@ -254,61 +247,62 @@ public class Nodes {
      */
     public static Node getReferencedNodeInUnit(Referenceable model,
             Tree.CompilationUnit rootNode) {
-        if (rootNode==null || model==null) {
-            return null;
-        }
-        else {
-            if (model instanceof Declaration) {
-                Declaration decl = (Declaration) model;
-                Unit unit = decl.getUnit();
-                if (unit != null 
-                        && ! unit.getFilename().toLowerCase().endsWith(".ceylon")) {
-                    boolean foundTheCeylonDeclaration = false;
-                    if (unit instanceof CeylonBinaryUnit) {
-                        BaseIdeModule module = (BaseIdeModule) unit.getPackage().getModule();
-                        String sourceRelativePath = toJavaString(module.toSourceUnitRelativePath(toCeylonString(unit.getRelativePath())));
-                        if (sourceRelativePath != null) {
-                            String ceylonSourceRelativePath = toJavaString(module.getCeylonDeclarationFile(toCeylonString(sourceRelativePath)));
-                            if (ceylonSourceRelativePath != null) {
-                                ExternalPhasedUnit externalPhasedUnit = module.getPhasedUnitFromRelativePath(ceylonSourceRelativePath);
-                                if (externalPhasedUnit != null) {
-                                    ExternalSourceFile sourceFile = externalPhasedUnit.getUnit();
-                                    if (sourceFile != null) {
-                                        for (Declaration sourceDecl : sourceFile.getDeclarations()) {
-                                            if (sourceDecl.getQualifiedNameString().equals(decl.getQualifiedNameString())) {
-                                                model = sourceDecl;
-                                                foundTheCeylonDeclaration = true;
-                                                break;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    if (! foundTheCeylonDeclaration) {
-                        if (decl.isNative() 
-                                && unit != null 
-                                && ! unit.getFilename().toLowerCase().endsWith(".ceylon")) {
-                            Declaration headerDeclaration = ModelUtil.getNativeHeader(decl.getContainer(), decl.getName());
-                            if (headerDeclaration != null) {
-                                List<Declaration> overloads = headerDeclaration.getOverloads();
-                                if (overloads != null) {
-                                    for (Declaration overload : overloads) {
-                                        if (overload.getNativeBackends().header()) {
-                                            model = overload;
-                                            foundTheCeylonDeclaration = true;
-                                            break;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            return findReferencedNode(rootNode, model);
-        }
+//        if (rootNode==null || model==null) {
+//            return null;
+//        }
+//        else {
+//            if (model instanceof Declaration) {
+//                Declaration decl = (Declaration) model;
+//                Unit unit = decl.getUnit();
+//                if (unit != null 
+//                        && ! unit.getFilename().toLowerCase().endsWith(".ceylon")) {
+//                    boolean foundTheCeylonDeclaration = false;
+//                    if (unit instanceof CeylonBinaryUnit) {
+//                        BaseIdeModule module = (BaseIdeModule) unit.getPackage().getModule();
+//                        String sourceRelativePath = toJavaString(module.toSourceUnitRelativePath(toCeylonString(unit.getRelativePath())));
+//                        if (sourceRelativePath != null) {
+//                            String ceylonSourceRelativePath = toJavaString(module.getCeylonDeclarationFile(toCeylonString(sourceRelativePath)));
+//                            if (ceylonSourceRelativePath != null) {
+//                                ExternalPhasedUnit externalPhasedUnit = module.getPhasedUnitFromRelativePath(ceylonSourceRelativePath);
+//                                if (externalPhasedUnit != null) {
+//                                    ExternalSourceFile sourceFile = externalPhasedUnit.getUnit();
+//                                    if (sourceFile != null) {
+//                                        for (Declaration sourceDecl : sourceFile.getDeclarations()) {
+//                                            if (sourceDecl.getQualifiedNameString().equals(decl.getQualifiedNameString())) {
+//                                                model = sourceDecl;
+//                                                foundTheCeylonDeclaration = true;
+//                                                break;
+//                                            }
+//                                        }
+//                                    }
+//                                }
+//                            }
+//                        }
+//                    }
+//                    if (! foundTheCeylonDeclaration) {
+//                        if (decl.isNative() 
+//                                && unit != null 
+//                                && ! unit.getFilename().toLowerCase().endsWith(".ceylon")) {
+//                            Declaration headerDeclaration = ModelUtil.getNativeHeader(decl.getContainer(), decl.getName());
+//                            if (headerDeclaration != null) {
+//                                List<Declaration> overloads = headerDeclaration.getOverloads();
+//                                if (overloads != null) {
+//                                    for (Declaration overload : overloads) {
+//                                        if (overload.getNativeBackends().header()) {
+//                                            model = overload;
+//                                            foundTheCeylonDeclaration = true;
+//                                            break;
+//                                        }
+//                                    }
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//            return findReferencedNode(rootNode, model);
+//        }
+        return delegate.getReferencedNodeInUnit(model, rootNode);
     }
 
     public static Node findReferencedNode(
@@ -317,31 +311,33 @@ public class Nodes {
     }
     
     public static String text(Node term, List<CommonToken> tokens) {
-        int start = term.getStartIndex();
-        int length = term.getDistance();
-        Region region = new Region(start, length);
-        StringBuilder exp = new StringBuilder();
-        for (Iterator<CommonToken> ti = 
-                getTokenIterator(tokens, region); 
-                ti.hasNext();) {
-            CommonToken token = ti.next();
-            int type = token.getType();
-            String text = token.getText();
-            if (type==LIDENTIFIER &&
-                    getTokenLength(token)>text.length()) {
-                exp.append("\\i");
-            }
-            else if (type==UIDENTIFIER &&
-                    getTokenLength(token)>text.length()) {
-                exp.append("\\I"); 
-            }
-            exp.append(text);
-        }
-        return exp.toString();
+//        int start = term.getStartIndex();
+//        int length = term.getDistance();
+//        Region region = new Region(start, length);
+//        StringBuilder exp = new StringBuilder();
+//        for (Iterator<CommonToken> ti = 
+//                getTokenIterator(tokens, region); 
+//                ti.hasNext();) {
+//            CommonToken token = ti.next();
+//            int type = token.getType();
+//            String text = token.getText();
+//            if (type==LIDENTIFIER &&
+//                    getTokenLength(token)>text.length()) {
+//                exp.append("\\i");
+//            }
+//            else if (type==UIDENTIFIER &&
+//                    getTokenLength(token)>text.length()) {
+//                exp.append("\\I"); 
+//            }
+//            exp.append(text);
+//        }
+//        return exp.toString();
+        return delegate.text(term, tokens);
     }
 
     public static int getTokenLength(CommonToken token) {
-        return token.getStopIndex()-token.getStartIndex()+1;
+//        return token.getStopIndex()-token.getStartIndex()+1;
+        return (int) delegate.getTokenLength(token);
     }
 
     public static String[] nameProposals(Node node) {
