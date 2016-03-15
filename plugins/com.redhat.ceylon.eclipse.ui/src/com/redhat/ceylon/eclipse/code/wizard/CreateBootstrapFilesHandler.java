1d03a10d16e091f26b905cfc63de58f30f208359
davidfestal <david.festal@serli.com>
2016-03-15 19:09:10 +0100

    Use a SWT-based dialog
    Conflicts:
    	plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/wizard/CreateBootstrapFilesHandler.java

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/wizard/CreateBootstrapFilesHandler.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/wizard/CreateBootstrapFilesHandler.java
@@ -7,10 +7,20 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
@@ -81,20 +91,64 @@ public class CreateBootstrapFilesHandler implements IWorkbenchWindowActionDelega
         return success;
     }
 
+    private class ComboDialog extends Dialog {
+    	Combo createBootstrapFilesVersionsCombo = null;
+		String bootstrapVersion;
+    	
+		public ComboDialog(Shell parentShell) {
+			super(parentShell);
+		}
+    	
+	    @Override
+	    public void create() {
+	        super.create();
+	        getShell().setText("Ceylon bootstrap files creation");
+	        setBlockOnOpen(true);
+	    }
+
+	    @Override
+	    protected Control createDialogArea(Composite parent) {
+	        Composite area = (Composite) super.createDialogArea(parent);
+
+	        Composite bootstrapComposite = new Composite(area, SWT.NONE);
+	        GridData bcgdb = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+	        bcgdb.grabExcessHorizontalSpace=true;
+	        bootstrapComposite.setLayoutData(bcgdb);
+	        GridLayout bclayoutb = new GridLayout();
+	        bclayoutb.numColumns = 2;
+	        bclayoutb.marginLeft = 0;
+	        bootstrapComposite.setLayout(bclayoutb);
+
+	        final Label createBootstrapFilesLabel = new Label(bootstrapComposite, SWT.NONE);
+	        createBootstrapFilesLabel.setText("Select the version of the bootstrapped distribution: ");
+	        createBootstrapFilesVersionsCombo = new Combo(bootstrapComposite, SWT.READ_ONLY);
+	        String[] choices = toJavaStringList_.toJavaStringList(versionsAvailableForBoostrap_.get_()).toArray(new String[0]);
+	        createBootstrapFilesVersionsCombo.setItems(choices);
+	        createBootstrapFilesVersionsCombo.select(0);
+	        bootstrapVersion = choices[0];
+	        createBootstrapFilesVersionsCombo.addListener(SWT.Selection, new Listener() {
+	            public void handleEvent(Event e) {
+	                bootstrapVersion = createBootstrapFilesVersionsCombo.getItem(
+	                                    createBootstrapFilesVersionsCombo.getSelectionIndex());
+	            }
+	        });
+
+	        return area;
+	    }
+	    
+	    public String getChosenVersion() {
+	    	return bootstrapVersion;
+	    }
+    }
+    
     public void run(IAction action) {
         CeylonProject<IProject, IResource, IFolder, IFile> ceylonProject = modelJ2C().ceylonModel().getProject(fProject);
         if (ceylonProject != null) {
             Shell shell = Display.getDefault().getActiveShell();
-            String[] choices = toJavaStringList_.toJavaStringList(versionsAvailableForBoostrap_.get_()).toArray(new String[0]);
-            String chosenVersion = (String) JOptionPane.showInputDialog(null, "Select a Ceylon version...",
-                "Ceylon bootstrap files creation", JOptionPane.QUESTION_MESSAGE, null, // Use
-                                                                                // default
-                                                                                // icon
-                choices, // Array of choices
-                choices[0]); // Initial choice
-
-            if (chosenVersion != null) {
-                boolean success = createBootstrapFiles(ceylonProject, chosenVersion, null);
+            ComboDialog comboDialog = new ComboDialog(shell);
+            if (comboDialog.open() == Window.OK) {
+            	String chosenVersion = comboDialog.getChosenVersion();
+                boolean success = createBootstrapFiles(ceylonProject, chosenVersion, shell);
                 if (success) {
                     if (shell == null) {
                         CeylonPlugin.log(Status.WARNING, "The Ceylon boostrap files already exist.");
