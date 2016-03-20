8c31ee91184ee6be0142ccb5ec3ee9849a579a04
gavinking <gavin@ceylon-lang.org>
2016-03-20 16:16:11 +0100

    fix #1744

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/outline/OutlinePopup.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/outline/OutlinePopup.java
@@ -21,7 +21,6 @@ import static com.redhat.ceylon.eclipse.ui.CeylonResources.CEYLON_OUTLINE;
 import static com.redhat.ceylon.eclipse.ui.CeylonResources.CONFIG_LABELS;
 import static com.redhat.ceylon.eclipse.ui.CeylonResources.HIDE_PRIVATE;
 import static com.redhat.ceylon.eclipse.ui.CeylonResources.SORT_ALPHA;
-import static com.redhat.ceylon.eclipse.ui.CeylonResources.imageRegistry;
 import static com.redhat.ceylon.eclipse.util.EditorUtil.triggersBinding;
 import static org.eclipse.ui.dialogs.PreferencesUtil.createPreferenceDialogOn;
 
@@ -210,7 +209,7 @@ public class OutlinePopup extends TreeViewPopup {
     }
     
     private class OutlineTreeViewer extends TreeViewer {
-        private boolean fIsFiltering= false;
+//        private boolean fIsFiltering = false;
 
         private OutlineTreeViewer(Tree tree) {
             super(tree);
@@ -219,23 +218,26 @@ public class OutlinePopup extends TreeViewPopup {
         @Override
         protected Object[] getFilteredChildren(Object parent) {
             Object[] result = getRawChildren(parent);
-            int unfilteredChildren = result.length;
+//            int unfilteredChildren = result.length;
             ViewerFilter[] filters = getFilters();
             if (filters != null) {
                 for(int i=0; i<filters.length; i++)
                     result = filters[i].filter(this, parent, result);
             }
-            fIsFiltering = unfilteredChildren != result.length;
+//            fIsFiltering = unfilteredChildren != result.length;
             return result;
         }
 
         @Override
         protected void internalExpandToLevel(Widget w, int level) {
-            if (!fIsFiltering && w instanceof Item) {
+            if (//!(fIsFiltering && !getFilterText().getText().isEmpty()) &&
+                getFilterText().getText().isEmpty() &&
+                    w instanceof Item) {
                 Item i = (Item) w;
                 if (i.getData() instanceof CeylonOutlineNode) {
                     CeylonOutlineNode node = 
-                            (CeylonOutlineNode) i.getData();
+                            (CeylonOutlineNode) 
+                                i.getData();
                     int cat = node.getCategory();
                     //TODO: leave unshared declarations collapsed?
                     if (cat==CeylonOutlineNode.IMPORT_LIST_CATEGORY) {
