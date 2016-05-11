a7128300571c28569ccce280b7f2cb4c3bd2102d
gavinking <gavin@ceylon-lang.org>
2016-05-12 00:21:57 +0200

    ws

--- plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/correct/AddConstructorHandler.java
+++ plugins/com.redhat.ceylon.eclipse.ui/src/com/redhat/ceylon/eclipse/code/correct/AddConstructorHandler.java
@@ -9,6 +9,7 @@ import static com.redhat.ceylon.eclipse.util.Nodes.findNode;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.antlr.runtime.CommonToken;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -32,22 +33,31 @@ public class AddConstructorHandler extends AbstractHandler {
         if (editor instanceof CeylonEditor) {
             CeylonEditor ce = (CeylonEditor) editor;
             Tree.CompilationUnit rootNode = 
-                    ce.getParseController().getTypecheckedRootNode();
+                    ce.getParseController()
+                      .getTypecheckedRootNode();
             if (rootNode!=null) {
                 IRegion selection = ce.getSelection();
                 int start = selection.getOffset();
                 int end = start + selection.getLength();
-                Node node = findDeclarationWithBody(rootNode, 
-                        findNode(rootNode, ce.getParseController().getTokens(), start, end));
+                List<CommonToken> tokens = 
+                        ce.getParseController()
+                          .getTokens();
+                Node node = 
+                        findDeclarationWithBody(rootNode, 
+                                findNode(rootNode, tokens, 
+                                        start, end));
                 List<ICompletionProposal> list = 
                         new ArrayList<ICompletionProposal>();
                 addConstructorProposal(getFile(editor), list, node, rootNode);
                 if (!list.isEmpty()) {
-                    IDocument doc = ce.getCeylonSourceViewer().getDocument();
+                    IDocument doc = 
+                            ce.getCeylonSourceViewer()
+                              .getDocument();
                     ICompletionProposal proposal = list.get(0);
                     proposal.apply(doc);
                     Point point = proposal.getSelection(doc);
-                    ce.getSelectionProvider().setSelection(new TextSelection(point.x, point.y));
+                    ce.getSelectionProvider()
+                      .setSelection(new TextSelection(point.x, point.y));
                 }
             }
         }
