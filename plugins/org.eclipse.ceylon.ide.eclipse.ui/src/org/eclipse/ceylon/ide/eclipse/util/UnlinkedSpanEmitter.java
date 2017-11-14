59191708d09eed090672b076dbb4aa979dfe2814
Stephane Epardaud <stef@epardaud.fr>
2017-11-14 16:20:17 +0100

    Added copyright headers

--- plugins/org.eclipse.ceylon.ide.eclipse.ui/src/org/eclipse/ceylon/ide/eclipse/util/UnlinkedSpanEmitter.java
+++ plugins/org.eclipse.ceylon.ide.eclipse.ui/src/org/eclipse/ceylon/ide/eclipse/util/UnlinkedSpanEmitter.java
@@ -1,3 +1,12 @@
+/********************************************************************************
+ * Copyright (c) {date} Red Hat Inc. and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-1.0
+ ********************************************************************************/
 package org.eclipse.ceylon.ide.eclipse.util;
 
 import com.github.rjeschke.txtmark.SpanEmitter;
